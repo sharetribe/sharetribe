9692f17397617390bf0bbd52bfadebeb479af193
Juho Makkonen <juho.makkonen@tkk.fi>
2008-08-29 07:29:54 +0000

    Ilmoituksen kuvalle loput testit. Lisäksi muutettu ilmoitusta siten, että validit kategoriat haetaan dynaamisesti, ja ilmoituksen testejä kompaktoitu. Tehty myös ilmoitukselle poistomahdollisuus.

--- app/models/filter.rb
+++ app/models/filter.rb
@@ -6,7 +6,7 @@ class Filter < ActiveRecord::Base
   #if category is nil, filter will concern all categories
   validates_presence_of :person_id, :keywords
   
-  validates_inclusion_of :category, :in => Listing::VALID_CATEGORIES, :allow_nil => true
+  validates_inclusion_of :category, :in => Listing.get_valid_categories, :allow_nil => true
   
   #this model allows lower and uppercase keywords but when they are parsed to an array,
   #all letters in those keywords should be converted all to downcase letters for making
