b042cdde4259bfcf7ef56e090fde8fcd76459967
Emmi Suhonen <emmi.suhonen@tkk.fi>
2008-08-29 07:19:25 +0000

    Lisätty tavaroiden perusselaus aakkosjärjestyksessä ja index testi

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,11 @@ class ItemsController < ApplicationController
   
   def index
     save_navi_state(['items','browse_items','',''])
+    @title =  :all_items  
+    @items_all = Item.find :all, :order => 'title ASC'
+    
+    @item_titles = Item.find(:all, :select => "title").collect(&:title).uniq
+    
   end
   
   def search
