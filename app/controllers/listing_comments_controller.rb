20a4fc59facbcb9a067296e50f589af0c241329a
Juho Makkonen <juho.makkonen@tkk.fi>
2008-09-08 16:24:45 +0000

    Ilmoitusten kommentointi korjattu. Kommentoinnin testi ei toistaiseksi toimi, joten se on vielä kommenteissa. Sivutuksessa ollut bugi korjattu (aiheutti virheilmoituksen, jos meni applikaation juureen).

--- app/controllers/listing_comments_controller.rb
+++ app/controllers/listing_comments_controller.rb
@@ -2,8 +2,13 @@ class ListingCommentsController < ApplicationController
 
   def create
     @listing = Listing.find(params[:listing_id])
-    @listing.comments.create(params[:listing_comment])
-    flash[:notice] = "comment_added"
+    @listing_comment = ListingComment.new(params[:listing_comment])
+    @listing_comment.author_id = @listing.id
+    if (@listing_comment.save)
+      flash[:notice] = "comment_added"
+    else
+      flash[:error] = "comment_not_added"
+    end  
     respond_to do |format|
       format.html { redirect_to @listing }
       format.js  
