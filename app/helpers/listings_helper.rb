35161d46be388b2fb90d0354b2fd53e91d8871b8
Juho Makkonen <juho.makkonen@tkk.fi>
2008-09-16 12:57:56 +0000

    Pieniä bugikorjauksia.

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -12,7 +12,7 @@ module ListingsHelper
       else
         case type
         when "category"
-          if params[:category]
+          if params[:id]
             path = listing_category_path(params.merge({:per_page => value}))
           else
             path = listing_category_path("all_categories", :per_page => value)
@@ -27,5 +27,48 @@ module ListingsHelper
     end
     links.join(" | ")  
   end
+  
+  # def translate_error_messages(error_message_groups)
+  #   translated_errors = []
+  #   error_message_groups.each do |error_messages|
+  #     error_messages.each do |message|
+  #       translated_errors << translate_error_message(message)
+  #     end
+  #   end
+  #   return translated_errors  
+  # end
+  # 
+  # def translate_error_message(message)
+  #   case message
+  #   # when "Title on pakollinen tieto."
+  #   #   t(:title_is_required)
+  #   when "Title on liian lyhyt (minimi on 2 merkkiä)."
+  #     t(:title_is_too_short)
+  #   when "Content on pakollinen tieto."
+  #     t(:content_is_required)
+  #   when "Good thru on pakollinen tieto."
+  #     "Voimassaoloaika on pakollinen tieto."
+  #   when "Language on pakollinen tieto."
+  #     "Ilmoituksella on oltava vähintään yksi kieli."       
+  #   else
+  #     message
+  #   end  
+  # end
+
+  # <%
+  # 
+  # <% errors = translate_error_messages([@listing.errors.full_messages]) %>
+  # <% unless errors.empty? %>
+  # <div id="form_error_messages">
+  # <h2><%= t(:listing_cannot_be_created) %></h2>
+  # <ul>
+  # <% errors.each do |error| %>
+  #   <li><%= error %></li>
+  # <% end %>
+  # </ul>
+  # </div>
+  # <% end %>
+  # 
+  # %>
 
 end
