642507109fd302b37f7e247e011237ef4ddcbf92
Antti Virolainen <antti.virolainen@iki.fi>
2008-09-19 13:31:47 +0000

    lisätty Person.test_person testausta varten

--- test/functional/people_controller_test.rb
+++ test/functional/people_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class PeopleControllerTest < ActionController::TestCase
 
-  #Tests are done in integral tests because they require loggin in to COS
+  def test_using_test_person
+    @test_person = Person.test_person
+    assert_not_nil( @test_person )
+    @test_person.coin_amount = 5
+    assert_equal(5, @test_person.coin_amount)
+  end
 
 end
