e3f13aef8e93d40b20bc526ec7e5c5aca444400a
Antti Virolainen <antti.virolainen@iki.fi>
2009-01-13 08:00:58 +0000

    tietokannat merkitty utf-8-muotoisiksi.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,22 @@
+ssh_options[:verbose] = :debug 
+
 set :application, "Kassi"
 set :repository,  "svn+ssh://alpha.sizl.org/svn/kassi/trunk"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
+
 set :deploy_to, "/var/datat/kassi"
+#set :deploy_to, "/usr/local/kassi"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+
+# role :app, "maps.cs.hut.fi"
+# role :web, "maps.cs.hut.fi"
+# role :db,  "maps.cs.hut.fi", :primary => true
 
 role :app, "alpha.sizl.org"
 role :web, "alpha.sizl.org"
