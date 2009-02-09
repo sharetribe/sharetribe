2a49682a67943db10227f163b3d3c5471c7a6efb
Antti Virolainen <antti.virolainen@iki.fi>
2009-02-09 16:57:07 +0000

    beta-finish scriptiin lisätty rev numero

--- beta-finish.sh
+++ beta-finish.sh
@@ -18,6 +18,19 @@ cd ..
 #change production environment to use production COS
 sed -i "s/cos\.alpha\.sizl/cos\.sizl/" config/environments/production.rb
 
+ REV=$((`svn info svn+ssh://alpha.sizl.org/svn/kassi | \
+ grep "^Last Changed Rev" | \
+ perl -pi -e "s/Last Changed Rev: //"`-`svn info svn+ssh://alpha.sizl.org/svn/kassi/tags | \
+ grep "^Last Changed Rev" | \
+ perl -pi -e "s/Last Changed Rev: //"`))
+echo "BETA_VERSION = \"0.6.$REV\"" >> config/environments/production.rb
+BUILD_DATE=`svn info svn+ssh://alpha.sizl.org/svn/kassi | \
+ grep "^Last Changed Date" | \
+ perl -pi -e "s/Last Changed Date: //" | perl -pi -e "s/\+.+$//"`
+echo "BUILT_AT = \"$BUILD_DATE\"" >> config/environments/production.rb
+
+
+
 
 rake db:migrate
 rake test
