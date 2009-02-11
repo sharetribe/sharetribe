e80e42ae41226a62231362a297c67451e72a0fa1
Juho Makkonen <juho.makkonen@tkk.fi>
2009-02-11 09:23:57 +0000

    Lisätty kavereille selitysteksti, muutettu kontaktit kassikumppaneiksi ja poistettu avatarin lisäys -formin toimimaton cancel-nappi näkyvistä.

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -7,15 +7,18 @@ class AvatarsController < ApplicationController
   end
 
   def create
-    @person = Person.find(params[:person_id])
-    begin
-      @person.update_avatar(params[:avatar][:image_file], session[:cookie])
-      flash[:notice] = :avatar_upload_successful
-      redirect_to @person
-    rescue ActiveResource::BadRequest => e
-      flash[:error] = e.response.body
-      render :action => :edit
+    if params[:avatar][:cancel]
+      redirect_to person_path(params[:person_id])
     end  
+    # @person = Person.find(params[:person_id])
+    # begin
+    #   @person.update_avatar(params[:avatar][:image_file], session[:cookie])
+    #   flash[:notice] = :avatar_upload_successful
+    #   redirect_to @person
+    # rescue ActiveResource::BadRequest => e
+    #   flash[:error] = e.response.body
+    #   render :action => :edit
+    # end  
   end
 
 end
