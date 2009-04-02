3d3d01e3bf88326d65db8151594f06ef1d323825
Juho Makkonen <juho.makkonen@tkk.fi>
2009-04-02 13:12:52 +0000

    Request-bugi korjattu (?) user mailerissa

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,18 +1,18 @@
 class UserMailer < ActionMailer::Base
 
-  def notification_of_new_message(recipient, message, cookie)
+  def notification_of_new_message(recipient, message, request=nil)
     subject_string = message.sender.name + ' on lähettänyt sinulle viestin Kassissa'
-    url = @request ? "#{@request.protocol}#{@request.host}#{person_inbox_path(@recipient, @message.conversation)}" : "test_url"
-    recipients recipient.email(cookie)
+    url = request ? "#{request.protocol}#{request.host}#{person_inbox_path(@recipient, @message.conversation)}" : "test_url"
+    recipients recipient.email
     from       KASSI_MAIL_FROM_ADRESS
     subject    subject_string
     body       :recipient => recipient, :message => message, :url => url
   end
   
-  def notification_of_new_comment(comment, cookie)
+  def notification_of_new_comment(comment, request=nil)
     subject_string = comment.author.name + ' on kommentoinut ilmoitustasi "' + comment.listing.title + '"'
-    url = @request ? "#{@request.protocol}#{@request.host}#{listing_path(@comment.listing.id)}##{@comment.id}" : "test_url"
-    recipients comment.listing.author.email(cookie)
+    url = request ? "#{request.protocol}#{request.host}#{listing_path(@comment.listing.id)}##{@comment.id}" : "test_url"
+    recipients comment.listing.author.email
     from       KASSI_MAIL_FROM_ADRESS
     subject    subject_string
     body       :comment => comment, :url => url
--- test/unit/user_mailer_test.rb
+++ test/unit/user_mailer_test.rb
@@ -2,30 +2,21 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase  
   
-  def setup
-    @test_person1, @session1 = get_test_person_and_session("kassi_testperson1")
-    @test_person2, @session2 = get_test_person_and_session("kassi_testperson2")
-    @cookie1 = @session1.cookie
-    @cookie2 = @session2.cookie
-  end
-  
   def test_notification_of_new_message
     recipient = people(:two)
     message = messages(:valid_message)
-    # Message is sent by person 1, so using cookie 1
-    mail = UserMailer.create_notification_of_new_message(recipient, message, @cookie1)
+    mail = UserMailer.create_notification_of_new_message(recipient, message)
     assert_equal(KASSI_MAIL_FROM_ADRESS, mail.from.first)
     assert_equal(message.sender.name + " on lähettänyt sinulle viestin Kassissa", mail.subject)
-    assert_equal(recipient.email(@cookie2), mail.to.first)
+    assert_equal(recipient.email, mail.to.first)
   end
   
   def test_notification_of_new_comment
     comment = listing_comments(:third_comment)
-    # Comment is left by person 2, so using cookie 2
-    mail = UserMailer.create_notification_of_new_comment(comment, @cookie2)
+    mail = UserMailer.create_notification_of_new_comment(comment)
     assert_equal(KASSI_MAIL_FROM_ADRESS, mail.from.first)
     assert_equal(comment.author.name + ' on kommentoinut ilmoitustasi "' + comment.listing.title + '"', mail.subject)
-    assert_equal(comment.listing.author.email(@cookie1), mail.to.first)
+    assert_equal(comment.listing.author.email, mail.to.first)
   end
   
 end
