0666a3cdc275d67af2e45cbfd6db48015524ae51
Juho Makkonen <juho.makkonen@tkk.fi>
2009-04-02 13:39:59 +0000

    Ja vielä kerran, puuh.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ActionMailer::Base
 
   def notification_of_new_message(recipient, message, request=nil)
     subject_string = message.sender.name + ' on lähettänyt sinulle viestin Kassissa'
-    url = request ? "#{request.protocol}#{request.host}#{person_inbox_path(@recipient, @message.conversation)}" : "test_url"
+    url = request ? "#{request.protocol}#{request.host}#{person_inbox_path(@recipient, message.conversation)}" : "test_url"
     recipients recipient.email
     from       KASSI_MAIL_FROM_ADRESS
     subject    subject_string
@@ -11,7 +11,7 @@ class UserMailer < ActionMailer::Base
   
   def notification_of_new_comment(comment, request=nil)
     subject_string = comment.author.name + ' on kommentoinut ilmoitustasi "' + comment.listing.title + '"'
-    url = request ? "#{request.protocol}#{request.host}#{listing_path(@comment.listing.id)}##{@comment.id}" : "test_url"
+    url = request ? "#{request.protocol}#{request.host}#{listing_path(comment.listing.id)}##{comment.id}" : "test_url"
     recipients comment.listing.author.email
     from       KASSI_MAIL_FROM_ADRESS
     subject    subject_string
