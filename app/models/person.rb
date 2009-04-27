d7cff797fbf42e86adc78a4d4ad729393d0d55c8
Juho Makkonen <juho.makkonen@tkk.fi>
2009-04-27 13:06:29 +0000

    Korjattu bugi käyttöehtojen hyväksymisessä.

--- app/models/person.rb
+++ app/models/person.rb
@@ -156,6 +156,7 @@ class Person < ActiveRecord::Base
     params[:family_name] = params[:family_name].slice(0, 28)
     Person.remove_root_level_fields(params, "name", ["given_name", "family_name"])  
     PersonConnection.put_attributes(params.except(:username, :email, :password, :password2), cos_id, cookie)
+    return cos_id
   end 
   
   def self.add_to_kassi_db(id)
