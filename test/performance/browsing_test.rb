2d71b799826400161fbe8794af6bc4bcb09e01ce
Antti Virolainen <antti.virolainen@iki.fi>
2009-06-30 13:10:44 +0000

    Pari performance testiä lisää ja korjattu ryhmäsivun bugi + tehty int testi ryhmäsivun lataamiselle

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -6,4 +6,30 @@ class BrowsingTest < ActionController::PerformanceTest
   def test_homepage
     get '/'
   end
+  
+  def test_people_page
+    get '/people'
+  end
+  
+  def test_random_listing_page
+    get '/listings/random'
+  end
+  
+  def test_items_page
+    get '/items'
+  end
+  
+  def test_favors_page
+    get '/favors'
+  end
+  
+  def test_groups_page
+    get '/groups'
+  end
+  
+  #TODO following tests
+  #view own profile
+  #registration
+  #change own address
+  
 end
