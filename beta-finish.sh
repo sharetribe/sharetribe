e0b3dd67352f77688986c64cac76b59226162b9d
Antti Virolainen <antti.virolainen@iki.fi>
2009-08-20 08:50:58 +0000

    positeltu cache commentteja vieweistä

--- beta-finish.sh
+++ beta-finish.sh
@@ -43,7 +43,8 @@ echo "BUILT_AT = \"$BUILD_DATE\"" >> config/environments/production.rb
 sudo rake gems:install
 
 rake db:migrate
-rake test
+# no tests on beta to make the downtime shorter
+#rake test
 rake db:migrate RAILS_ENV=production
 #script/server -d -e production -p 8000
 mongrel_rails cluster::start
