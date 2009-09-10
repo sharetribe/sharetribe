c6753bba42608b0f5439b088f23d2ba33c5f97df
Juho Makkonen <juho.makkonen@tkk.fi>
2009-09-10 08:17:25 +0000

    Korjattu bugi ilmoitusten seuraamisessa.

--- app/models/listing_comment.rb
+++ app/models/listing_comment.rb
@@ -7,7 +7,7 @@ class ListingComment < ActiveRecord::Base
   validates_presence_of :author_id, :listing_id, :content
   validates_numericality_of :listing_id, :only_integer => true
   
-  after_save :update_follow_status
+  after_create :update_follow_status
   
   attr_accessor :author_follow_status
   
