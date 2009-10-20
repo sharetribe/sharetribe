81c8a344515d86fc26a1d6519d6256fe039a7a11
Antti Virolainen <antti.virolainen@iki.fi>
2009-10-20 13:23:40 +0000

    lisätty Juho errormailien vastaanottajaksi

--- app/models/error_mailer.rb
+++ app/models/error_mailer.rb
@@ -4,7 +4,7 @@ class ErrorMailer < ActionMailer::Base
 
     content_type "text/html"
   
-    recipients  'gnomet@gmail.com'
+    recipients  'juho.makkonen@tkk.fi, gnomet@gmail.com'
     from        'Error Mailer <KassiErrors@sizl.org>'
     subject     "[Error] exception on #{PRODUCTION_SERVER} in #{request.request_uri}"
     sent_on    sent_on
