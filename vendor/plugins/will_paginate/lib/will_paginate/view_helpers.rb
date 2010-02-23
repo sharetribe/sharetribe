3ec4c3eb318fe889e58e52b597494db92a7b846f
Juho Makkonen <juho.makkonen@tkk.fi>
2010-02-23 12:47:23 +0000

    Sivutus rikkoo leiskan -ongelma korjattu, ehkä (pitää testata betalla).

--- vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
+++ vendor/plugins/will_paginate/lib/will_paginate/view_helpers.rb
@@ -25,7 +25,7 @@ module WillPaginate
       :class          => 'pagination',
       :previous_label => '&laquo; Previous',
       :next_label     => 'Next &raquo;',
-      :inner_window   => 4, # links around the current page
+      :inner_window   => 2, # links around the current page
       :outer_window   => 1, # links around beginning and end
       :separator      => ' ', # single space is friendly to spiders and non-graphic browsers
       :param_name     => :page,
