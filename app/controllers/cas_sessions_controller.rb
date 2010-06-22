0a675570696a79fedea1ed42c03bdf119032b49a
Antti Virolainen <antti.virolainen@iki.fi>
2010-06-22 17:30:18 +0300

    some structure for shibboleth handling, not working yet at all

--- app/controllers/cas_sessions_controller.rb
+++ app/controllers/cas_sessions_controller.rb
@@ -11,13 +11,40 @@ class CasSessionsController < ApplicationController
     #logger.info "SESSION INCLUDES: " + session.inspect
     proxy_granting_ticket = session[:cas_pgt]
     #logger.info "PGT: " + proxy_granting_ticket.inspect
-    pt_ticket = CASClient::Frameworks::Rails::Filter.client.request_proxy_ticket(proxy_granting_ticket, service_uri).ticket
-    #logger.info "PT_TICKET: " + pt_ticket.inspect
-
-#    @session = Session.create({ :username => params[:username], 
+   # begin
+      pt_ticket = CASClient::Frameworks::Rails::Filter.client.request_proxy_ticket(proxy_granting_ticket, service_uri).ticket
+      #logger.info "PT_TICKET: " + pt_ticket.inspect
+    #rescue RuntimeError => redirection #Note: it might be that 303 is not creating an exception (301,302 should)
+    #      # Read the redirection target
+    #      redirection_uri = JSON.parse(redirection.response.body)["redirect"]["uri"]
+    #      
+    #      #modify the redirection URI
+    #      
+    #      
+    #      #Send the user to the requested page with correct url
+    #      redirect_to redirection_uri
+    #end
+    
+    # begin
+#     @session = Session.create({ :username => params[:username], 
 #                               :password => params[:password] })
-    @session = CasSession.create({ :username => session[:cas_user],
-                                :password => pt_ticket })
+      @session = CasSession.create({ :username => session[:cas_user], 
+                                     :password => pt_ticket })
+    # rescue Exception => redirection 
+    #   # TODO: replace generic Error with more specific one ^
+    #   
+    #   # Read the redirection target
+    #   redirection_uri = JSON.parse(redirection.response.body)["redirect"]["uri"]
+    #   
+    #   #modify the redirection URI
+    #   succesfull_return_uri = "http://#{request.host}/cas_session"
+    #   # FIXME: change to more appropriate fallback address with message
+    #   fallback_uri = "http://#{request.host}/cas_session?login_failed=true"
+    #   redirection_uri += "&redirect=#{succesfull_return_uri}&fallback=#{fallback_uri}"
+    #   
+    #   #Send the user to the requested page with correct url
+    #   redirect_to redirection_uri
+    # end
     session[:cookie] = @session.headers["Cookie"]
     session[:person_id] = @session.person_id
 
@@ -53,5 +80,16 @@ class CasSessionsController < ApplicationController
     @session =  CasSession.new
     #debugger
     self.create
+  end
+  
+  def show 
+    # This is actually used only as a return target after account linking in Asi
+    # After the account linking is made we need to initate the session by trying to open CAS session again.
+    unless params["login_failed"]
+      redirect_to  :action => :new 
+    else
+      flash[:error] = :linking_accounts_failed
+      redirect_to :root_path
+    end
   end
 end
