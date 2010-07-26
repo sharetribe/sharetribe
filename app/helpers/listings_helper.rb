97b63c537713478af39d8b3fc779ed79d0c3bae1
Juho Makkonen <juho.makkonen@gmail.com>
2010-07-26 13:02:17 +0300

    Both requests and offers can now be created.

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,8 @@
 module ListingsHelper
+  
+  # Class is selected if listing type is currently selected
+  def get_new_listing_tab_class(listing_type)
+    "new_listing_form_tab_#{params[:type].eql?(listing_type) ? 'selected' : 'unselected'}"
+  end
+  
 end
