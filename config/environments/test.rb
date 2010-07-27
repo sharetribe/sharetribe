2cdbe6eaa20bdf19d1889bae565e098969cdcb20
Antti Virolainen <antti.virolainen@iki.fi>
2010-07-27 15:44:55 +0300

    Moved locale(language) setting to config.yml

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,10 +29,7 @@ Kassi::Application.configure do
   # This is necessary if your schema can't be completely dumped by the schema dumper,
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
-  
-  # All tests will be in english
-  config.i18n.default_locale = :en
-  
+    
   # Use chrome for testing javascript
   if ENV["USE_CHROME"]
     require "selenium-webdriver"
