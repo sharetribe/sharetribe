74b4d8646ddb9ee1a97838e7662ca7378b0ff5d9
Juho Makkonen <juho.makkonen@gmail.com>
2010-08-04 15:04:51 +0300

    Basic view for a listing.

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -1,7 +1,7 @@
 class ListingImage < ActiveRecord::Base
 
   belongs_to :listing
-  has_attached_file :image, :styles => { :medium => "250x250>", :thumb => "85x85#" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "85x85#" }
   validates_attachment_presence :image
   validates_attachment_size :image, :less_than => 5.megabytes
   validates_attachment_content_type :image,
