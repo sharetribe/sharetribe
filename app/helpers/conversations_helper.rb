29396e71da0603605c62427abe3416009092ee8f
Juho Makkonen <juho.makkonen@gmail.com>
2010-08-11 17:08:33 +0300

    Basic page for showing a single conversation, created a nested layout for conversation views.

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -6,7 +6,8 @@ module ConversationsHelper
   
   # Class is selected if listing type is currently selected
   def get_inbox_tab_class(tab_name)
-    "inbox_tab_#{action_name.eql?(tab_name) ? 'selected' : 'unselected'}"
+    current_tab_name = params[:conversation_type] || action_name
+    "inbox_tab_#{current_tab_name.eql?(tab_name) ? 'selected' : 'unselected'}"
   end
   
 end
