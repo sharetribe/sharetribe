93f24ff4e6a1f66bfff1a48d65f5634770dfd3d7
Juho Makkonen <juho.makkonen@gmail.com>
2010-08-19 14:19:20 +0300

    Removed irrelevant test stubs.

--- spec/controllers/testimonials_controller_spec.rb
+++ spec/controllers/testimonials_controller_spec.rb
@@ -2,18 +2,4 @@ require 'spec_helper'
 
 describe TestimonialsController do
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
 end
