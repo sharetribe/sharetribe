7f7938cdbad19df75ec30222a4ae8df692563faa
Juho Makkonen <juho.makkonen@gmail.com>
2010-08-20 14:26:30 +0300

    Setting invalid locale from url now falls back to default locale ("fi").

--- app/controllers/i18n_controller.rb
+++ app/controllers/i18n_controller.rb
@@ -2,7 +2,7 @@ class I18nController < ApplicationController
   
   # Changes locale and returns to the previous view
   def change_locale
-    redirect_to "/#{params[:locale]}/#{params[:redirect_uri]}"
+    redirect_to("/#{params[:locale]}/#{params[:redirect_uri]}")
   end
   
 end  
