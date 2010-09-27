86c163421f6e83df314bb6939b2ab7bf8976cf5c
Antti Virolainen <antti.virolainen@iki.fi>
2010-09-27 18:34:43 +0300

    migration to change listing_comments to comments

---
+++ db/migrate/20100927150547_migrate_listing_comments_for_kassi2.rb
@@ -0,0 +1,24 @@
+class ListingComment < ActiveRecord::Base
+end
+
+class MigrateListingCommentsForKassi2 < ActiveRecord::Migration
+  def self.up
+    
+    say  "This migration simply makes a comment from each listing_comment."
+    say "This does NOT DELETE the data from listing_comments", true
+    ListingComment.all.each do |listing_comment|
+      comment = Comment.new(:author_id => listing_comment.author_id,
+                            :listing_id => listing_comment.listing_id,
+                            :content => listing_comment.content,
+                            :created_at => listing_comment.created_at)
+      comment.save!
+      comment.update_attribute("updated_at", listing_comment.updated_at)
+      print "."; STDOUT.flush
+    end
+  end
+
+  def self.down
+    say "This migration just copied data from listing_comments to comments."
+    say "Rolling back doesn't do anything.", true
+  end
+end
