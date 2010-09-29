19b7ec69df09e320511b7390484ff736afe7be78
Antti Virolainen <antti.virolainen@iki.fi>
2010-09-29 17:03:24 +0300

    Added Ressi-reporting and Hoptoad

---
+++ config/initializers/hoptoad.rb
@@ -0,0 +1,8 @@
+#if APP_CONFIG.use_hoptoad
+  HoptoadNotifier.configure do |config|
+    config.api_key = 'ea9c77b4d7eb6ae2b42e793fd64b3b28'
+    config.js_notifier = true
+    # config.http_open_timeout = 60
+    # config.http_read_timeout = 60
+  end
+#end
---
+++ spec/models/cached_ressi_event_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe CachedRessiEvent do
+  @@OPTIONS = { :return_value=>"nil",
+     :parameters =>
+     { :format => "json",
+       :action => "show",
+       :id => "c_hs6WxWOr3RofaaWPEYjL",
+       :controller => "collections",
+       :app_id => "cWslSQyIyr3yiraaWPEYjL" }.to_json,
+     :application_id=>"cWslSQyIyr3yiraaWPEYjL",
+     :action=>"TestsController#testing_ressi_logging",
+     :session_id=>"63601",
+     :user_id=>"biPssKjrCr3PQyaaWPEYjL",
+     :ip_address=>"128.214.20.122",
+     :semantic_event_id => "running_cached_ressi_event_spec",
+     :headers => {"foo" => "bar"}.to_json }
+
+
+   it "should be created without errors" do
+     event = CachedRessiEvent.new(@@OPTIONS)
+     event.save!
+     CachedRessiEvent.count.should > 0
+   end
+
+   it "should be uploaded to ressi wtihout errors" do
+     if APP_CONFIG.log_to_ressi
+       begin
+         event = CachedRessiEvent.new(@@OPTIONS)
+         event.save!
+         event.upload
+       rescue Errno::ECONNREFUSED => e
+         puts "No connection to RESSI at #{APP_CONFIG.ressi_url}"
+       rescue Exception => e
+         assert false,  "Ressi timed out at #{APP_CONFIG.ressi_url}, #{e.message}"
+       end
+     end
+   end
+end
