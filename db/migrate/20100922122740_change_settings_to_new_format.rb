8f7c0a217f281cfdf7b28ad3c24878d089d5f1fb
Antti Virolainen <antti.virolainen@iki.fi>
2010-09-30 16:58:41 +0300

    fix migration of settings: strings instead of symbols

--- db/migrate/20100922122740_change_settings_to_new_format.rb
+++ db/migrate/20100922122740_change_settings_to_new_format.rb
@@ -6,13 +6,13 @@ class ChangeSettingsToNewFormat < ActiveRecord::Migration
     Person.all.each do |person|
       if settings = Setting.find_by_person_id(person.id)
         person.update_attributes({:preferences => {
-          :email_about_new_messages => (settings.email_when_new_message == 1),
-          :email_about_new_comments_to_own_listing => (settings.email_when_new_comment == 1),
-          :email_when_conversation_accepted => true,
-          :email_when_conversation_rejected => true,
-          :email_when_new_friend_request => (settings.email_when_new_friend_request == 1),
-          :email_when_new_feedback_on_transaction => (settings.email_when_new_comment_to_kassi_event == 1),
-          :email_when_new_listing_from_friend => (settings.email_when_new_listing_from_friend == 1)}})
+          "email_about_new_messages" => (settings.email_when_new_message == 1),
+          "email_about_new_comments_to_own_listing" => (settings.email_when_new_comment == 1),
+          "email_when_conversation_accepted" => true,
+          "email_when_conversation_rejected" => true,
+          "email_when_new_friend_request" => (settings.email_when_new_friend_request == 1),
+          "email_when_new_feedback_on_transaction" => (settings.email_when_new_comment_to_kassi_event == 1),
+          "email_when_new_listing_from_friend" => (settings.email_when_new_listing_from_friend == 1)}})
         
       end
       print "."; STDOUT.flush
