497f051696bc34b7fac11a7920851e53a57dc289
Antti Virolainen <antti.virolainen@iki.fi>
2010-09-30 16:15:22 +0300

    fix to migration for settings.
    Those settings that are not yet in use are not yet migrated, because then they would show up in UI. Those will stay in settings table and can be migrated later.

--- db/migrate/20100922122740_change_settings_to_new_format.rb
+++ db/migrate/20100922122740_change_settings_to_new_format.rb
@@ -9,10 +9,13 @@ class ChangeSettingsToNewFormat < ActiveRecord::Migration
           :email_about_new_messages => (settings.email_when_new_message == 1),
           :email_about_new_comments_to_own_listing => (settings.email_when_new_comment == 1),
           :email_when_conversation_accepted => true,
-          :email_when_conversation_rejected => true,
-          :email_when_new_friend_request => (settings.email_when_new_friend_request == 1),
-          :email_when_new_feedback_on_transaction => (settings.email_when_new_comment_to_kassi_event == 1),
-          :email_when_new_listing_from_friend => (settings.email_when_new_listing_from_friend == 1)}})
+          :email_when_conversation_rejected => true
+          
+          # These are not yet in use, so not migrated yet, because they would be shown in the UI
+          #:email_when_new_friend_request => (settings.email_when_new_friend_request == 1),
+          #:email_when_new_feedback_on_transaction => (settings.email_when_new_comment_to_kassi_event == 1),
+          #:email_when_new_listing_from_friend => (settings.email_when_new_listing_from_friend == 1)
+          }})
         
       end
       print "."; STDOUT.flush
