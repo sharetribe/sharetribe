330a8178725b62bcf892fa98421a50723bab96f1
Antti Virolainen <antti.virolainen@iki.fi>
2010-10-01 03:32:31 +0300

    Put listings in order on requests and offers pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -29,7 +29,7 @@ class ListingsController < ApplicationController
   def load
     @title = params[:listing_type]
     @to_render ||= {:partial => "listings/listed_listings"}
-    @listings = Listing.open.find_with(params, @current_user).paginate(:per_page => 15, :page => params[:page])
+    @listings = Listing.open.order("created_at DESC").find_with(params, @current_user).paginate(:per_page => 15, :page => params[:page])
     @request_path = request.fullpath
     if request.xhr? && params[:page] && params[:page].to_i > 1
       render :partial => "listings/additional_listings"
