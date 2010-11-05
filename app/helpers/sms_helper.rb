958ed15d1811aff747e99bb67ab398c5197e3b64
Antti Virolainen <antti.virolainen@iki.fi>
2010-11-05 20:00:29 +0200

    SMS payment mockup and also put the language choices come directly from config.yml

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -7,110 +7,135 @@ module SmsHelper
   class SmsParseError < StandardError
   end
   
-  def self.send(message, number)
+  if APP_CONFIG.use_sms
+    def self.send(message, number)
     
-    # clean up the number (for some reason putting ')' and '(' in regexp didn't work, so using 3 gsubs for cleaning)
-    # also change numbers starting with 0 to start with finnish 358
-    number = number.gsub(/[\s\+-]/, "").gsub("(", "").gsub(")","").gsub(/^0/, "358")
+      # clean up the number (for some reason putting ')' and '(' in regexp didn't work, so using 3 gsubs for cleaning)
+      # also change numbers starting with 0 to start with finnish 358
+      number = number.gsub(/[\s\+-]/, "").gsub("(", "").gsub(")","").gsub(/^0/, "358")
     
-    user_key = log_in_to_api
-    sms_uri = "http://api.medialab.sonera.fi/iw/rest/messaging/sms?serviceKey=#{APP_CONFIG.sms_service_key}&userKey=#{user_key}"
-    sms_text = '{"sms":{"@messageClass" : "1","@anonymous" : "true","address" : "' + number +'", "message" : "' + message + '" }}'
+      user_key = log_in_to_api
+      sms_uri = "http://api.medialab.sonera.fi/iw/rest/messaging/sms?serviceKey=#{APP_CONFIG.sms_service_key}&userKey=#{user_key}"
+      sms_text = '{"sms":{"@messageClass" : "1","@anonymous" : "true","address" : "' + number +'", "message" : "' + message + '" }}'
     
-    begin
-      Rails.logger.info  "Sending sms message: '#{message}' to #{number}"
-      response = RestClient.post(sms_uri, sms_text, :content_type => 'application/json')
-    rescue Exception => e
-      # HoptoadNotifier.notify(
-      #            :error_class => "Special Error", 
-      #            :error_message => "Special Error: #{e.message}", 
-      #          )
-      Rails.logger.error { "Sending message failed: #{e.inspect}, #{e.response}" }
+      begin
+        Rails.logger.info  "Sending sms message: '#{message}' to #{number}"
+        response = RestClient.post(sms_uri, sms_text, :content_type => 'application/json')
+      rescue Exception => e
+        # HoptoadNotifier.notify(
+        #            :error_class => "Special Error", 
+        #            :error_message => "Special Error: #{e.message}", 
+        #          )
+        Rails.logger.error { "Sending message failed: #{e.inspect}, #{e.response}" }
+      end
     end
-  end
   
-  def self.get_messages
+    def self.get_messages
   
-    user_key = log_in_to_api
+      user_key = log_in_to_api
 
-    # get all messages in the inbox
-    response = RestClient.get("http://api.medialab.sonera.fi/iw/rest/receive/#{APP_CONFIG.sms_username}/#{APP_CONFIG.sms_receiver_id}?userKey=#{user_key}&serviceKey=#{APP_CONFIG.sms_service_key}", {:Accept => 'application/json'})
+      # get all messages in the inbox
+      response = RestClient.get("http://api.medialab.sonera.fi/iw/rest/receive/#{APP_CONFIG.sms_username}/#{APP_CONFIG.sms_receiver_id}?userKey=#{user_key}&serviceKey=#{APP_CONFIG.sms_service_key}", {:Accept => 'application/json'})
     
-    message_strings = JSON.parse(response)["receiver"]["messages"]
-    return [] if message_strings.nil? # no messages in inbox
-    messages = []
+      message_strings = JSON.parse(response)["receiver"]["messages"]
+      return [] if message_strings.nil? # no messages in inbox
+      messages = []
     
-    if message_strings.class == Hash
-      # if only one message in the inbox (the usual case) wrap in array
-      # because then same iteration works also in cases with many messages
-      message_strings = [message_strings]
+      if message_strings.class == Hash
+        # if only one message in the inbox (the usual case) wrap in array
+        # because then same iteration works also in cases with many messages
+        message_strings = [message_strings]
+      end
+    
+      message_strings.each do |message|
+        messages.push(parse(message))
+      end
+    
+      return messages
     end
-    
-    message_strings.each do |message|
-      messages.push(parse(message))
-    end
-    
-    return messages
-  end
   
-  def self.delete_messages(id_array)
-    user_key = log_in_to_api
+    def self.delete_messages(id_array)
+      user_key = log_in_to_api
     
-    id_array.each do |sms_id|
-      response = RestClient.delete("http://api.medialab.sonera.fi/iw/rest/receive/#{APP_CONFIG.sms_username}/#{APP_CONFIG.sms_receiver_id}/#{sms_id}?userKey=#{user_key}&serviceKey=#{APP_CONFIG.sms_service_key}", {:Accept => 'application/json'}) unless sms_id.blank?
+      id_array.each do |sms_id|
+        response = RestClient.delete("http://api.medialab.sonera.fi/iw/rest/receive/#{APP_CONFIG.sms_username}/#{APP_CONFIG.sms_receiver_id}/#{sms_id}?userKey=#{user_key}&serviceKey=#{APP_CONFIG.sms_service_key}", {:Accept => 'application/json'}) unless sms_id.blank?
+      end
+    
+    
     end
-    
-    
-  end
   
-  def self.parse(message)
-    details = {:phone_number => message["msisdn"], :original_text => message["message"], :original_id => message["@id"]}    
-    parts = message["message"].split
+    def self.parse(message)
+      details = {:phone_number => message["msisdn"], :original_text => message["message"], :original_id => message["@id"]}    
+      parts = message["message"].split
+        
+      details[:category] = case parts[0]
+      when /#{all_translations("sms.rideshare")}/
+        "rideshare"
+      when /#{all_translations("sms.pay")}/
+        "pay"
+      else
+        raise_sms_parse_error(message)
+      end
     
-    details[:category] = case parts[0]
-    when /#{I18n.t("sms.rideshare")}/ 
-      "rideshare"
-    else
-      raise_sms_parse_error(message)
+      if details[:category] == "pay"
+        details[:receiver] = parts[1]
+        amount = parts[2]
+        raise_sms_parse_error(message) unless amount =~ /^\d+(\.|,)?\d*e?$/i
+        amount.gsub!(/e/i,"")
+        amount.gsub!(",",".")
+        details[:amount] = amount
+        return details
+      end
+    
+      details[:listing_type] = case parts[1]
+      when /#{all_translations("sms.offer")}/
+        "offer"
+      when /#{all_translations("sms.request")}/
+        "request"
+      else
+        raise_sms_parse_error(message)
+      end
+    
+      details[:origin] = parts[2]
+      details[:destination] = parts[3]
+    
+      time =  Time.zone.parse(parts[4]).to_datetime
+      if time < DateTime.now && time > 1.days.ago
+        # if only clock time is given and it's earlier than now,
+        # probably the time means tomorrow.
+        time += 1.days
+      end
+      details[:valid_until] = time
+      details[:description] = parts[5..(parts.length-1)].join(" ")
+    
+      return details
     end
-    
-    details[:listing_type] = case parts[1]
-    when /#{I18n.t("sms.offer")}/
-      "offer"
-    when /#{I18n.t("sms.request")}/
-      "request"
-    else
-      raise_sms_parse_error(message)
+  
+    # returns all translations for given key, joined with a |
+    # This is useful in sms parsing as we don't know the language of the sms
+    def self.all_translations(key)
+      combination = ""
+      I18n.available_locales.each do |loc|
+        combination += "|" unless combination.blank?
+        combination += I18n.t(key, :locale => loc)
+      end
+      return combination
     end
-    
-    details[:origin] = parts[2]
-    details[:destination] = parts[3]
-    
-    time =  Time.zone.parse(parts[4]).to_datetime
-    if time < DateTime.now && time > 1.days.ago
-      # if only clock time is given and it's earlier than now,
-      # probably the time means tomorrow.
-      time += 1.days
+  
+    private
+  
+    def self.raise_sms_parse_error(message, error_message=nil)
+      error = SmsController::SmsParseError.new(error_message.nil? ? I18n.t("sms.parsing_error") : error_message)
+      error.sms_id = message["@id"]
+      error.sms_text = message["message"]
+      error.sms_from = message["msisdn"]
+      raise error  
     end
-    details[:valid_until] = time
-    details[:description] = parts[5..(parts.length-1)].join(" ")
-    
-    return details
-  end
   
-  private
-  
-  def self.raise_sms_parse_error(message, error_message=nil)
-    error = SmsController::SmsParseError.new(error_message.nil? ? I18n.t("sms.parsing_error") : error_message)
-    error.sms_id = message["@id"]
-    error.sms_text = message["message"]
-    error.sms_from = message["msisdn"]
-    raise error  
-  end
-  
-  def self.log_in_to_api
-    # Log in to sms API
-    response = RestClient.get("http://api.medialab.sonera.fi/iw/rest/login?username=#{APP_CONFIG.sms_username}&password=#{APP_CONFIG.sms_password}&serviceKey=#{APP_CONFIG.sms_service_key}")
-    user_key = response[/<userKey>(.+)<\/userKey>/, 1]
+    def self.log_in_to_api
+      # Log in to sms API
+      response = RestClient.get("http://api.medialab.sonera.fi/iw/rest/login?username=#{APP_CONFIG.sms_username}&password=#{APP_CONFIG.sms_password}&serviceKey=#{APP_CONFIG.sms_service_key}")
+      user_key = response[/<userKey>(.+)<\/userKey>/, 1]
+    end
   end
 end
--- spec/helpers/sms_helper_spec.rb
+++ spec/helpers/sms_helper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe SmsHelper do
   describe "#parse" do
-    it "split a message to valid details" do
+    it "splits a ridesharing message to valid details" do
       message = {"@id"=>"3907911", "msisdn"=>358501234567, "message"=>"ride offer tkk taik 14:00", "calendar"=>"2010-10-10T08:03:04+00:00"}
       details = SmsHelper.parse(message)
       details.should_not be_nil
@@ -11,6 +11,17 @@ describe SmsHelper do
       details[:category].should == "rideshare"
       details[:origin].should == "tkk"
       details[:destination].should == "taik"
+    end
+    
+    it "splits a payment message to valid details" do
+      message = {"@id"=>"3907911", "msisdn"=>358501234567, "message"=>"pay Simo 8,50e ", "calendar"=>"2010-10-10T08:03:04+00:00"}
+      details = SmsHelper.parse(message)
+      details.should_not be_nil
+      details[:phone_number].should == message["msisdn"]
+      details[:category].should == "pay"
+      details[:receiver].should == "Simo"
+      details[:amount].should == "8.50"
+      
     end
   end
   
@@ -37,6 +48,16 @@ describe SmsHelper do
     it "should delete the messages specidied in params from the inbox" do
       RestClient.should_receive(:delete).with(/http:\/\/api.medialab.sonera.fi\/iw\/rest\/receive\/#{APP_CONFIG.sms_username}\/#{APP_CONFIG.sms_receiver_id}\/.*example_id.*/,  {:Accept => 'application/json'}).and_return(true)
       SmsHelper.delete_messages(["example_id"])
+    end
+  end
+  
+  describe "#all_translations" do
+    it "should retun all available translations for given key. Joined by |" do
+      translations = SmsHelper.all_translations("sms.offer")
+      translations.should include(I18n.t("sms.offer", :fi)) 
+      translations.should include(I18n.t("sms.offer", :en))
+      translations.should include("|")
+      
     end
   end
   
