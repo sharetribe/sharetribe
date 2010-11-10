bf19f685c7ac91d6ecba4d5d366c0efb92de0157
Antti Virolainen <antti.virolainen@iki.fi>
2010-11-10 13:49:19 +0200

    fixing a bug in sms_helper

--- app/helpers/sms_helper.rb
+++ app/helpers/sms_helper.rb
@@ -68,7 +68,8 @@ module SmsHelper
       details = {:phone_number => message["msisdn"], :original_text => message["message"], :original_id => message["@id"]}    
       parts = message["message"].split(" ")
         
-      #Rails.logger.info "Received a sms message and divided it to #{parts.inspect}"
+      Rails.logger.info "Received a sms message and divided it to #{parts}"
+
         
         
       case parts[0]
