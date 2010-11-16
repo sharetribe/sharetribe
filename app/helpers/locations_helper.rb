813235cbf8b947cb6934954d6a6171492489b3c8
Antti Virolainen <antti.virolainen@iki.fi>
2010-11-16 13:36:28 +0200

    prefer Helsinki area in geocoding requests

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -8,8 +8,8 @@ module LocationsHelper
   
   def get_coordinates(place_name)
     escaped_place_name = ApplicationHelper.escape_for_url(place_name)
-    #puts "Sending request to Google maps api: http://maps.googleapis.com/maps/api/geocode/json?address=#{escaped_place_name}&region=fi&sensor=false"
-    response = JSON.parse(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{escaped_place_name}&region=fi&sensor=false"))
+    # FIXME: Currently hardcoded to prioritize Finland and Helsinki in serches. Should depend on the community. 
+    response = JSON.parse(RestClient.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{escaped_place_name}&region=fi&sensor=false&bounds=59.5,24.5|60.5,25.5"))
     raise RuntimeError.new("Coordinates for #{place_name} not found. (Message: #{response["status"]})") unless response["status"] == "OK"
     return [response["results"][0]["geometry"]["location"]["lat"], response["results"][0]["geometry"]["location"]["lng"]] 
   end
