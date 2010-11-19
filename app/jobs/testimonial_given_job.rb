2ed5e9ffb6ef70435d6d8083521b68b455add25e
Juho Makkonen <juho.makkonen@gmail.com>
2010-11-19 11:47:10 +0200

    Added badge "helper"

--- app/jobs/testimonial_given_job.rb
+++ app/jobs/testimonial_given_job.rb
@@ -9,11 +9,13 @@ class TestimonialGivenJob < Struct.new(:testimonial_id, :host)
     if received.collect { |t| "#{t.participation.conversation.listing.listing_type}_#{t.participation.conversation.listing.category}" }.uniq.size == 5
       testimonial.receiver.give_badge("jack_of_all_trades", host) unless testimonial.receiver.has_badge?("jack_of_all_trades")
     end
-    offers = { :rides => 0 }
+    offers = { :favors => 0, :rides => 0 }
     received.each do |t|
       listing = t.participation.conversation.listing
+      offers[:favors] += 1 if listing.category.eql?("favor") && listing.offerer?(testimonial.receiver)
       offers[:rides] += 1 if listing.category.eql?("rideshare") && listing.offerer?(testimonial.receiver)
     end
+    Badge.assign_with_levels("helper", offers[:favors], testimonial.receiver, [2, 6, 15], host)
     Badge.assign_with_levels("chauffer", offers[:rides], testimonial.receiver, [2, 6, 15], host)
   end
   
