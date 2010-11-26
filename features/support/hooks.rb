7efe934fc71499e0a34ac37fb23476fcbb6927c2
Juho Makkonen <juho.makkonen@gmail.com>
2010-11-26 13:13:21 +0200

    A fix to make badge tests pass.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -5,6 +5,7 @@ Before('@badge') do
        | kassi_testperson1 |
        | kassi_testperson2 |
     And I am logged in as "kassi_testperson1"
+    And I belong to test group "4"
     When I go to the badges page of "kassi_testperson1"
     And I should see "Badges" within ".inbox_tab_selected"
     And I should see "Received feedback" within ".inbox_tab_unselected"
