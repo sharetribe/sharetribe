227a191425dd6ed2023ea153d45da8311e09ad20
Juho Makkonen <juho.makkonen@gmail.com>
2010-12-01 15:16:47 +0200

    Removed unnecessary print lines, added details to the email sent to users about badge migration.

--- app/jobs/badges_migrated_job.rb
+++ app/jobs/badges_migrated_job.rb
@@ -2,7 +2,6 @@ class BadgesMigratedJob < Struct.new(:person_id)
 
   def perform
     person = Person.find(person_id)
-    puts "Sending mail to #{person.name}"
     PersonMailer.badge_migration_notification(person).deliver
   end
 
