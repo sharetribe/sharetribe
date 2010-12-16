90e95ac39e57f2c675f585f0b521091f68bf05ac
Juho Makkonen <juho.makkonen@gmail.com>
2010-12-16 16:04:45 +0200

    Email from new contact requests.

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -2,7 +2,10 @@ class ContactRequestsController < ApplicationController
   
   def create
     @contact_request = ContactRequest.new(params[:contact_request])
-    session[:contact_request_sent] = true if @contact_request.save
+    if @contact_request.save
+      session[:contact_request_sent] = true 
+      PersonMailer.contact_request_notification(@contact_request.email).deliver
+    end
     redirect_to root
   end
   
