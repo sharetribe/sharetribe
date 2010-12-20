9fd14e704a3c922508ec17575b3ac80238fb65f8
Antti Virolainen <antti.virolainen@iki.fi>
2010-12-20 09:57:50 +0200

    Generating some load for performance testing the home page.

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 require 'rails/performance_test_help'
-
+#require File.expand_path('../../helper_modules', __FILE__)
 
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionController::PerformanceTest
+  
+  def setup
+    author = Factory(:person)
+    60.times do
+      Factory(:listing, :author => Person.first)
+    end
+  end
+  
   def test_homepage
     get 'test.lvh.me'
   end
