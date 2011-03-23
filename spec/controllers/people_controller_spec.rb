f2126290e5dc835c4293b288a4c44e900df7d80d
Antti Virolainen <antti.virolainen@iki.fi>
2011-03-23 09:57:52 +0200

    Fix to the typo in people controller spec tests

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -6,7 +6,7 @@ describe PeopleController do
   describe "#create" do
     it "should tell ASI to skip welcome mail if that's in the community's settings" do
       
-      PersonConnection.should_receive(:create_person).with(hash_including({:welcome_mail => false}), anything()).and_return({"entry" => {"id" => "dfskh3r29wefhsdifh"}})  
+      PersonConnection.should_receive(:create_person).with(hash_including({:welcome_email => false}), anything()).and_return({"entry" => {"id" => "dfskh3r29wefhsdifh"}})  
       
       @request.host = "test2.lvh.me"
       username = generate_random_username
@@ -15,7 +15,7 @@ describe PeopleController do
     
     it "should tell ASI to send welcome mail if that's in the community's settings" do
      
-      PersonConnection.should_receive(:create_person).with(hash_including({:welcome_mail => true}), anything()).and_return({"entry" => {"id" => "dfskh3r29wefhsdifh"}})  
+      PersonConnection.should_receive(:create_person).with(hash_including({:welcome_email => true}), anything()).and_return({"entry" => {"id" => "dfskh3r29wefhsdifh"}})  
       
       @request.host = "test.lvh.me"
       username = generate_random_username
