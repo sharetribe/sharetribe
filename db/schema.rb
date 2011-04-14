342d0f9014167e3abc3a4262c60de825b4cea86b
Eetu Jalonen <just@Eetu-Jalonens-MacBook-Pro.local>
2011-04-14 15:20:33 +0300

    _origin and _destination under listings

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20110414105702) do
-=======
-ActiveRecord::Schema.define(:version => 20110325120932) do
->>>>>>> 6b7a9ce351918c057b96e5b1f8ac0e074f4ebc8c
 
   create_table "badges", :force => true do |t|
     t.string   "person_id"
