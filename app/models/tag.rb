058e35ac6c031783373cd41944fb9cb473293b03
Kride <krister.landen@gmail.com>
2011-04-26 16:30:24 +0300

    Some modifications to tag cloud.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,8 +3,9 @@ class Tag < ActiveRecord::Base
 
   def self.tags(options = {})
     query = "select tags.id, name, count(*) as count"
-    query << " from taggings, tags"
-    query << " where tags.id = tag_id"
+    query << " from taggings, tags, listings"
+    query << " where tags.id = tag_id and listings.id = taggable_id"
+    query << " and listing_type = '#{options[:listing_type]}'" if options[:listing_type] != nil
     query << " group by tag_id"
     query << " order by #{options[:order]}" if options[:order] != nil
     query << " limit #{options[:limit]}" if options[:limit] != nil
