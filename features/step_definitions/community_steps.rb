0d3cdb157c6e015b4033fef29ab565c72f045cfc
Juho Makkonen <juho.makkonen@gmail.com>
2011-04-27 11:18:58 +0300

    Created a test for changing the terms of a community.

--- features/step_definitions/community_steps.rb
+++ features/step_definitions/community_steps.rb
@@ -7,3 +7,7 @@ Given /^the test community has following available locales:$/ do |locale_table|
   #here is expected that the first community is the test community where the subdomain is pointing by default
   Community.first.update_attributes({:settings => { "locales" => @locales }})
 end
+
+Given /^the terms of community "([^"]*)" are changed to "([^"]*)"$/ do |community, terms|
+  Community.find_by_domain(community).update_attribute(:consent, terms)
+end
