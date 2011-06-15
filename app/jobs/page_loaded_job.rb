3089f4db69d4fe79af40befb77bbcb2c120bba73
Antti Virolainen <antti.virolainen@iki.fi>
2011-06-15 18:28:36 +0300

    Changing the enthusiast badge disabling so that the last_page_load_date gets updated anyway.

--- app/jobs/page_loaded_job.rb
+++ app/jobs/page_loaded_job.rb
@@ -3,10 +3,11 @@ class PageLoadedJob < Struct.new(:person_id, :host)
   def perform
     current_user = Person.find(person_id)
     unless current_user.last_page_load_date && current_user.last_page_load_date.to_date.eql?(Date.today)
-      current_user.active_days_count += 1
+      # FIXME: Day counting and badge check disabled as it produced too big numbers for unknown reason
+      #current_user.active_days_count += 1
       current_user.last_page_load_date = DateTime.now
       current_user.save
-      Badge.assign_with_levels("enthusiast", current_user.active_days_count, current_user, [5, 30, 100], host)
+      #Badge.assign_with_levels("enthusiast", current_user.active_days_count, current_user, [5, 30, 100], host)
     end
   end
   
