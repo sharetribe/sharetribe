c942d0d41fd7a597b865abc267aa577f24877262
Antti Virolainen <gnomet@gmail.com>
2011-06-30 13:11:28 +0300

    change session validity to 1 year, to reduce the need for users to log in again.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, :key => '_kassi_session', :domain => APP_CONFIG.domain, :expire_after => 20.days
+Rails.application.config.session_store :cookie_store, :key => '_kassi_session', :domain => APP_CONFIG.domain, :expire_after => 1.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
