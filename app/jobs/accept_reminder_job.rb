befb48815af2caca8b4dd14ae255a3645631719c
Juho Makkonen <juho.makkonen@gmail.com>
2011-07-04 18:27:16 +0300

    Small fix to accept reminder so that it reminds on the right time.

--- app/jobs/accept_reminder_job.rb
+++ app/jobs/accept_reminder_job.rb
@@ -3,7 +3,7 @@ class AcceptReminderJob < Struct.new(:conversation_id, :last_message_id, :host)
   def perform
     conversation = Conversation.find(conversation_id)
     if conversation.status.eql?("pending")
-      if conversation.messages.last.created_at > 1.week.ago
+      if conversation.messages.last.created_at < 1.week.ago
         recipient = conversation.other_party(Message.find(last_message_id).sender)
         if recipient.preferences["email_about_accept_reminders"]
           PersonMailer.accept_reminder(conversation, recipient, host).deliver
