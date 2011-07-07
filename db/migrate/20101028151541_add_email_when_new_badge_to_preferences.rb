107ee0bafc211cea2b875527292e43f49f18b0e2
Juho Makkonen <juho.makkonen@gmail.com>
2011-07-07 19:54:36 +0300

    Added some warning comments about older migrations that contain erroneous code that is fixed in a later migration.

--- db/migrate/20101028151541_add_email_when_new_badge_to_preferences.rb
+++ db/migrate/20101028151541_add_email_when_new_badge_to_preferences.rb
@@ -1,5 +1,7 @@
 class AddEmailWhenNewBadgeToPreferences < ActiveRecord::Migration
   def self.up
+    # Note: the key should be a string instead of a symbol. This
+    # is fixed in a later migration, do not use this code as such!
     Person.all.each do |person|
       person.update_attribute(:preferences, person.preferences.merge({:email_about_new_received_testimonials => true}))
     end  
