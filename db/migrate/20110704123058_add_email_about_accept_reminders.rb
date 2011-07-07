181618847646d5b99e316699188407ddf3858a25
Juho Makkonen <juho.makkonen@gmail.com>
2011-07-07 19:54:36 +0300

    Added some warning comments about older migrations that contain erroneous code that is fixed in a later migration.

--- db/migrate/20110704123058_add_email_about_accept_reminders.rb
+++ db/migrate/20110704123058_add_email_about_accept_reminders.rb
@@ -1,5 +1,7 @@
 class AddEmailAboutAcceptReminders < ActiveRecord::Migration
   def self.up
+    # Note: the key should be a string instead of a symbol. This
+    # is fixed in a later migration, do not use this code as such!
     Person.all.each do |person|
       person.update_attribute(:preferences, person.preferences.merge({:email_about_accept_reminders => true}))
     end
