2559843fc498f323ce6c46a07093e056585894ee
Antti Virolainen <gnomet@gmail.com>
2011-07-20 14:11:34 +0300

    Remove unnecessary migration

--- db/migrate/20110505112659_remove_location_ok.rb
+++
@@ -1,9 +0,0 @@
-class RemoveLocationOk < ActiveRecord::Migration
-  def self.up
-    remove_column :locations, :location_ok
-  end
-
-  def self.down
-    add_column :locations, :location_ok, :boolean
-  end
-end
