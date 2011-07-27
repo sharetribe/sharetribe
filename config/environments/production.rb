ac351c6bb8d904635d066b2716ef965d96176406
Antti Virolainen <gnomet@gmail.com>
2011-07-27 17:39:10 +0300

    More flexible mail delivery method setting (can be missing in config without program crashing)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Kassi::Application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   
-  mail_delivery_method = APP_CONFIG.mail_delivery_method.to_sym || :sendmail
+  mail_delivery_method = (APP_CONFIG.mail_delivery_method.present? ? APP_CONFIG.mail_delivery_method.to_sym : :sendmail)
   
   config.action_mailer.delivery_method = mail_delivery_method
   if mail_delivery_method == :postmark
