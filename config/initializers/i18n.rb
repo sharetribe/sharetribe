369c62a1201433f52952a745f15f35868d9b91c7
Antti Virolainen <gnomet@gmail.com>
2011-08-01 17:28:19 +0300

    Add more fallback rules (becuse for some reason the fallbacks defaulted to russia, even though it was not the default_locale)

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,5 +1,9 @@
 I18n.backend.class.send(:include, I18n::Backend::Fallbacks)
 I18n.fallbacks.map('ru' => 'en')
+I18n.fallbacks.map('nl' => 'en')
+I18n.fallbacks.map('sw' => 'en')
+I18n.fallbacks.map('el' => 'en')
+I18n.fallbacks.map('ro' => 'en')
 
 module I18n
   def self.with_locale(locale, &block)
