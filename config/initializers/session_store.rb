3f09170d369213dda2128b99aea454815d311853
Antti Virolainen <gnomet@gmail.com>
2011-08-22 09:12:15 +0300

    Cookie settings work now also when domain is not set in config.yml

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Pick the session domain from APP_CONFIG. (Removing the possible port in the end)
-Rails.application.config.session_store :cookie_store, :key => '_kassi_session', :domain => APP_CONFIG.domain.split(":")[0], :expire_after => 1.years
+if APP_CONFIG.domain
+  domain = APP_CONFIG.domain.split(":")[0]
+else
+  domain = ""
+end
+
+Rails.application.config.session_store :cookie_store, :key => '_kassi_session', :domain => domain, :expire_after => 1.years
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
