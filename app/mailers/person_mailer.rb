3df603041e229857537d4d3da13a0ad95a768f43
Antti Virolainen <gnomet@gmail.com>
2011-09-09 10:24:48 +0300

    Change delivery method back to postmark for feedbacks to ensure they are not lost on the way,

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -75,7 +75,7 @@ class PersonMailer < ActionMailer::Base
     @current_community = current_community
     subject = "New #unanswered #feedback from #{@current_community.name} community from user #{feedback.author.try(:name)} "
     mail_to = @current_community.feedback_to_admin? ? @current_community.admin_emails : APP_CONFIG.feedback_mailer_recipients
-    mail(:to => mail_to, :subject => subject, :reply_to => @feedback.email, :delivery_method => :sendmail)
+    mail(:to => mail_to, :subject => subject, :reply_to => @feedback.email)
   end
   
   def badge_migration_notification(recipient)
