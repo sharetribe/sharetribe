533f10510758cf5e229076dfc8c1498aa3629f30
Juho Makkonen <juho.makkonen@gmail.com>
2011-12-05 22:57:32 +0100

    Modifications to dropdown.

--- public/javascripts/kassi_dashboard.js
+++ public/javascripts/kassi_dashboard.js
@@ -1,4 +1,4 @@
 function initialize_dashboard(email_default_text) { 
 	$('#contact_request_email').watermark(email_default_text, {className: 'default_text'});
-	$('select.language_select').selectmenu({style: 'dropdown', width: "150px"});
+	$('select.language_select').selectmenu({style: 'dropdown', width: "100px"});
 }
