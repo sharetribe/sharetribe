9fdcec17f70c33aa05919c52228dd19e43af8abc
Antti Virolainen <gnomet@gmail.com>
2012-05-14 19:16:27 -0400

    Make sure no notifications and comments stay when listing is removed. fix: https://www.pivotaltracker.com/story/show/29555053

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -20,8 +20,8 @@ class Listing < ActiveRecord::Base
   accepts_nested_attributes_for :listing_images, :reject_if => lambda { |t| t['image'].blank? }
   
   has_many :conversations
-  
-  has_many :comments
+  has_many :notifications, :as => :notifiable, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   
   has_one :location, :dependent => :destroy
   has_one :origin_loc, :class_name => "Location", :conditions => ['location_type = ?', 'origin_loc'], :dependent => :destroy
