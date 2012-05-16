71103d0f02f827c4ad90740e0f474523f1ae2a56
Juho Makkonen <juho.makkonen@gmail.com>
2012-05-16 12:44:15 +0300

    New schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303172713) do
+ActiveRecord::Schema.define(:version => 20120510175152) do
 
   create_table "badges", :force => true do |t|
     t.string   "person_id"
@@ -62,13 +62,16 @@ ActiveRecord::Schema.define(:version => 20120303172713) do
     t.text     "allowed_emails"
     t.boolean  "users_can_invite_new_users",                :default => false
     t.boolean  "select_whether_name_is_shown_to_everybody", :default => false
+    t.boolean  "news_enabled",                              :default => false
     t.boolean  "private",                                   :default => false
     t.string   "label"
-    t.boolean  "show_date_in_listings_list",                :default => false
-    t.boolean  "news_enabled",                              :default => false
     t.boolean  "all_users_can_add_news",                    :default => false
+    t.boolean  "show_date_in_listings_list",                :default => false
     t.boolean  "custom_frontpage_sidebar",                  :default => true
     t.boolean  "event_feed_enabled",                        :default => true
+    t.string   "slogan"
+    t.text     "description"
+    t.string   "category",                                  :default => "other"
   end
 
   create_table "communities_listings", :id => false, :force => true do |t|
@@ -354,14 +357,29 @@ ActiveRecord::Schema.define(:version => 20120303172713) do
     t.string   "id",                            :limit => 22,                   :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "is_admin",                      :default => 0
-    t.string   "locale",                        :default => "fi"
+    t.integer  "is_admin",                                    :default => 0
+    t.string   "locale",                                      :default => "fi"
     t.text     "preferences"
-    t.integer  "active_days_count",             :default => 0
+    t.integer  "active_days_count",                           :default => 0
     t.datetime "last_page_load_date"
-    t.integer  "test_group_number",             :default => 1
-    t.boolean  "active",                        :default => true
-    t.boolean  "show_real_name_to_other_users", :default => true
+    t.integer  "test_group_number",                           :default => 1
+    t.boolean  "active",                                      :default => true
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.boolean  "show_real_name_to_other_users",               :default => true
+    t.string   "username"
+    t.string   "email"
+    t.string   "encrypted_password",                          :default => "",   :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                               :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "password_salt"
     t.string   "given_name"
     t.string   "family_name"
     t.string   "phone_number"
@@ -370,21 +388,6 @@ ActiveRecord::Schema.define(:version => 20120303172713) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "username"
-    t.string   "email"
-    t.string   "encrypted_password",            :default => "",   :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                 :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "password_salt"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
   end
 
   add_index "people", ["confirmation_token"], :name => "index_people_on_confirmation_token", :unique => true
