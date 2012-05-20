b4de00a151635a8ef07ae2d7f0125cba105c20d0
Antti Virolainen <gnomet@gmail.com>
2012-05-20 19:00:06 -0400

    Customizations for URV

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -140,7 +140,15 @@ module ApplicationHelper
   end
   
   def facebook_like
-    content_tag :iframe, nil, :src => "https://www.facebook.com/plugins/like.php?locale=#{I18n.locale}_#{I18n.locale.to_s.upcase}&href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=150&action=recommend&font=arial&colorscheme=light&height=20", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => 120, :height => 20
+    
+    loc = case I18n.locale
+      when :en then "en_US"
+      when :ca then "ca_ES"
+      when :"es-ES" then "es_ES"    
+      else "#{I18n.locale}_#{I18n.locale.to_s.upcase}"
+    end
+    
+    content_tag :iframe, nil, :src => "https://www.facebook.com/plugins/like.php?locale=#{loc}&href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=150&action=recommend&font=arial&colorscheme=light&height=20", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => 120, :height => 20
   end
   
   def self.use_asi?
