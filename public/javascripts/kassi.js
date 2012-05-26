5bfa13a903e5fa5faf38f3366f31b142da7cd148
Antti Virolainen <gnomet@gmail.com>
2012-05-26 08:57:24 -0400

    Language updates and fix for private community login info

--- public/javascripts/kassi.js
+++ public/javascripts/kassi.js
@@ -636,8 +636,8 @@ function initialize_private_community_homepage(username_default_text, password_d
 		$('#password_forgotten').slideToggle('fast'); 
 		$('input.request_password').focus();
 	});
-	$('#username').watermark(username_default_text, {className: 'default_text'});
-	$('#password').watermark(password_default_text, {className: 'default_text'});
+	$('#person_username').watermark(username_default_text, {className: 'default_text'});
+	$('#person_password').watermark(password_default_text, {className: 'default_text'});
 	$('.wrapper').addClass('js_enabled');
 }	
 	
