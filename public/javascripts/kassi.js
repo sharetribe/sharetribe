32870dabb6329b32811025eaebc0d588256bfe95
Kusti <juho.makkonen@gmail.com>
2012-06-20 14:13:41 +0300

    Multiple invitations in a row works, other small fixes.

--- public/javascripts/kassi.js
+++ public/javascripts/kassi.js
@@ -576,37 +576,34 @@ function initialize_profile_feedback_view() {
 }
 
 function initialize_homepage() {
-  $('#invitation_email').focus(function() {
-    $('div.invitation_form_hidden_parts').slideDown('fast');
-    $(document).bind('focusin.invitation_form_hidden_parts click.invitation_form_hidden_parts',function(e) {
-      if ($(e.target).closest('.invitation_form_hidden_parts, #invitation_email').length) return;
-      $(document).unbind('.example');
-      $('div.invitation_form_hidden_parts').slideUp('fast');
-    });
-  });
-  $('div.invitation_form_hidden_parts').slideUp('fast');
-  $('#poll_answer_poll_option_id_value15').focus(function() {
-    alert("Focus here!");
-    // $('div.invitation_form_hidden_parts').slideDown('fast');
-    // $(document).bind('focusin.poll_form_hidden_parts click.poll_form_hidden_parts',function(e) {
-    //   if ($(e.target).closest('.poll_form_hidden_parts, .poll_answer[poll_option_id]').length) return;
-    //   $(document).unbind('.example');
-    //   $('div.poll_form_hidden_parts').slideUp('fast');
-    // });
-  });
-  $('div.poll_form_hidden_parts').slideUp('fast');
   $("input[type=radio]").uniform();
+}
+
+function initialize_invitation_form(locale, rails_env) {
+  // Focus triggering causes a recursion in Firefox in tests,
+  // so skipping it in test environment
+  if (rails_env == "test") {
+    $('div.invitation_form_hidden_parts').slideDown('fast');
+  } else {
+    $('#invitation_email').focus(function() {
+      $('div.invitation_form_hidden_parts').slideDown('fast');
+      $(document).bind('focusin.invitation_form_hidden_parts click.invitation_form_hidden_parts',function(e) {
+        if ($(e.target).closest('.invitation_form_hidden_parts, #invitation_email').length) return;
+        $(document).unbind('.example');
+        $('div.invitation_form_hidden_parts').slideUp('fast');
+      });
+    });
+    $('div.invitation_form_hidden_parts').slideUp('fast');
+  }
   auto_resize_text_areas();
-  var form_id = "#new_invitation"
-	$(form_id).validate({
-		rules: {
-		  "invitation[email]": {required: true, email: true},
-			"invitation[message]": {required: false, maxlength: 5000}
-		},
-		submitHandler: function(form) {
-		  disable_and_submit(form_id, form, "true", locale);
-		}
-	});
+  prepare_ajax_form(
+    "#new_invitation",
+    locale, 
+    {
+      "invitation[email]": {required: true, email: true},
+      "invitation[message]": {required: false, maxlength: 5000}
+    }
+  );
 }
 
 
