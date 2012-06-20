dc39e688b9da9241df1260859e5945927bb8b360
Kusti <juho.makkonen@gmail.com>
2012-06-20 14:13:41 +0300

    Multiple invitations in a row works, other small fixes.

--- public/javascripts/sharetribe_common.js
+++ public/javascripts/sharetribe_common.js
@@ -1,3 +1,7 @@
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+});
+
 function initialize_confirmation_pending_form(locale, email_in_use_message) {
 	$('#mistyped_email_link').click(function() { 
 		$('#password_forgotten').slideToggle('fast'); 
@@ -5,21 +9,26 @@ function initialize_confirmation_pending_form(locale, email_in_use_message) {
 	});
 	var form_id = "#change_mistyped_email_form"
   $(form_id).validate({
-   errorPlacement: function(error, element) {
+     errorPlacement: function(error, element) {
        error.insertAfter(element);
-   },
-   rules: {
-        "person[email]": {required: true, email: true, remote: "/people/check_email_availability_and_validity"}
-   },
-   messages: {
-     "person[email]": { remote: email_in_use_message }
-   },
-   onkeyup: false, //Only do validations when form focus changes to avoid exessive ASI calls
-   submitHandler: function(form) {
-        disable_and_submit(form_id, form, "false", locale);  
-   }
+     },
+     rules: {
+       "person[email]": {required: true, email: true, remote: "/people/check_email_availability_and_validity"}
+     },
+     messages: {
+       "person[email]": { remote: email_in_use_message }
+     },
+     onkeyup: false, //Only do validations when form focus changes to avoid exessive ASI calls
+     submitHandler: function(form) {
+       disable_and_submit(form_id, form, "false", locale);  
+     }
   });
 }
+
+// function disable_and_submit(form_id, form, ajax, locale) {
+//  disable_submit_button(form_id, locale);
+//  form.submit();
+// }
 
 function disable_and_submit(form_id, form, ajax, locale) {
 	$(form_id + ' input[type=submit]').attr('disabled', 'disabled');
@@ -31,6 +40,28 @@ function disable_and_submit(form_id, form, ajax, locale) {
 	} else {
   	form.submit();
 	}	
+}
+
+function prepare_ajax_form(form_id, locale, rules) {
+  $(form_id).ajaxForm({
+    dataType: 'script',
+    beforeSubmit: function() {
+      $(form_id).validate({
+        rules: rules
+      });
+      if ($(form_id).valid() == true) {
+        disable_submit_button(form_id, locale);
+  	  }
+      return $(form_id).valid();
+    }
+  });
+}
+
+function disable_submit_button(form_id, locale) {
+  $(form_id + ' input[type=submit]').attr('disabled', 'disabled');
+	jQuery.getJSON('/javascripts/locales/' + locale + '.json', function(json) {
+	  $(form_id + ' input[type=submit]').val(json.please_wait);
+	});
 }
 
 function auto_resize_text_areas() {
