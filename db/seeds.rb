68a6a000cd675025f30c12985f80f61ad78141d7
Konrad Markus <konker@gmail.com>
2012-06-27 14:05:22 +0300

    Added initial 'official' hobbies to db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,70 @@
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 #  listing_type = valid_st.keys[SecureRandom.random_number(valid_st.keys.length)]
 
-Person.all.each { |p| p.set_default_preferences }
+if (false)
+  Person.all.each { |p| p.set_default_preferences }
 
-user = Person.first
-for i in 1..500
-  listing_type = Listing::VALID_TYPES[SecureRandom.random_number(2)]
-  category = Listing::VALID_CATEGORIES[SecureRandom.random_number(4)]
-  valid_until = DateTime.now + 1.month
-  tag_list = [
-    "#{SecureRandom.random_number(33) + 1}tag",
-    "#{SecureRandom.random_number(33) + 1}longertag",
-    "#{SecureRandom.random_number(33) + 1}verylongtag"
-  ]
-  listing = user.create_listing(:listing_type => listing_type, :category => category, :valid_until => valid_until, :tag_list => tag_list, :description => "Test #{listing_type} #{category}")
-  
-  if(category.eql? "rideshare")
-    origin_lat = SecureRandom.random_number() * 0.15 + 60.15
-    origin_lng = SecureRandom.random_number() * 0.5 + 24.70
-    listing.build_origin_loc(:latitude => origin_lat, :longitude => origin_lng)
-    dest_lat = SecureRandom.random_number() * 0.15 + 60.15
-    dest_lng = SecureRandom.random_number() * 0.5 + 24.70
-    listing.build_destination_loc(:latitude => dest_lat, :longitude => dest_lng)
-    listing.origin = "Origin #{i}"
-    listing.destination = "Destination #{i}"
-  else
-    lat = SecureRandom.random_number() * 0.15 + 60.15
-    lng = SecureRandom.random_number() * 0.5 + 24.70
-    listing.build_location(:latitude => lat, :longitude => lng)
-    listing.title = "#{listing_type} - #{category} - test#{i}"
+  user = Person.first
+  for i in 1..500
+    listing_type = Listing::VALID_TYPES[SecureRandom.random_number(2)]
+    category = Listing::VALID_CATEGORIES[SecureRandom.random_number(4)]
+    valid_until = DateTime.now + 1.month
+    tag_list = [
+      "#{SecureRandom.random_number(33) + 1}tag",
+      "#{SecureRandom.random_number(33) + 1}longertag",
+      "#{SecureRandom.random_number(33) + 1}verylongtag"
+    ]
+    listing = user.create_listing(:listing_type => listing_type, :category => category, :valid_until => valid_until, :tag_list => tag_list, :description => "Test #{listing_type} #{category}")
+    
+    if(category.eql? "rideshare")
+      origin_lat = SecureRandom.random_number() * 0.15 + 60.15
+      origin_lng = SecureRandom.random_number() * 0.5 + 24.70
+      listing.build_origin_loc(:latitude => origin_lat, :longitude => origin_lng)
+      dest_lat = SecureRandom.random_number() * 0.15 + 60.15
+      dest_lng = SecureRandom.random_number() * 0.5 + 24.70
+      listing.build_destination_loc(:latitude => dest_lat, :longitude => dest_lng)
+      listing.origin = "Origin #{i}"
+      listing.destination = "Destination #{i}"
+    else
+      lat = SecureRandom.random_number() * 0.15 + 60.15
+      lng = SecureRandom.random_number() * 0.5 + 24.70
+      listing.build_location(:latitude => lat, :longitude => lng)
+      listing.title = "#{listing_type} - #{category} - test#{i}"
+    end
+    
+    if(Listing::VALID_SHARE_TYPES[listing_type][category])
+      share_type = Listing::VALID_SHARE_TYPES[listing_type][category][SecureRandom.random_number(Listing::VALID_SHARE_TYPES[listing_type][category].length)]
+      listing.share_types.build(:name => share_type)
+    end
+    listing.save
+    Community.find_by_domain("aalto").listings << listing
   end
-  
-  if(Listing::VALID_SHARE_TYPES[listing_type][category])
-    share_type = Listing::VALID_SHARE_TYPES[listing_type][category][SecureRandom.random_number(Listing::VALID_SHARE_TYPES[listing_type][category].length)]
-    listing.share_types.build(:name => share_type)
-  end
-  listing.save
-  Community.find_by_domain("aalto").listings << listing
 end
+
+hobbies = Hobby.create([
+  { :name => 'Travel',
+    :official => true },
+  { :name => 'Sports',
+    :official => true },
+  { :name => 'Arts/Crafts',
+    :official => true },
+  { :name => 'Fashion',
+    :official => true },
+  { :name => 'Gardening',
+    :official => true },
+  { :name => 'Pets/Animals',
+    :official => true },
+  { :name => 'Wildlife/Nature',
+    :official => true },
+  { :name => 'Music',
+    :official => true },
+  { :name => 'Cooking',
+    :official => true },
+  { :name => 'Computers/Technology',
+    :official => true },
+  { :name => 'Books/Magazines',
+    :official => true },
+  { :name => 'Gaming',
+    :official => true }
+])
+
