c6d644d49a8cb5b3f5e2ba1734a57589d2734e86
Kusti <juho.makkonen@gmail.com>
2012-07-10 12:43:14 +0300

    Started messaging functionality.

---
+++ db/migrate/20120710084323_remove_temporary_accommodation_share_type_from_listings.rb
@@ -0,0 +1,16 @@
+class RemoveTemporaryAccommodationShareTypeFromListings < ActiveRecord::Migration
+  def self.up
+    Listing.all.each do |listing|
+      if listing.share_type.eql?("temporary_accommodation")
+        if listing.listing_type.eql?("request")
+          listing.update_attribute(:share_type, "rent") 
+        else
+          listing.update_attribute(:share_type, "rent_out")
+        end
+      end
+    end
+  end
+
+  def self.down
+  end
+end
