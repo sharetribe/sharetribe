8ba75b10baed6e5aa6fdd91b7d22686a3dc0ff91
Antti Virolainen <gnomet@gmail.com>
2012-07-22 15:52:56 -0400

    Messages API additions. Now conversations can be started and viewed.

--- spec/controllers/api/conversations_controller_spec.rb
+++ spec/controllers/api/conversations_controller_spec.rb
@@ -17,8 +17,8 @@ describe Api::ConversationsController do
         @p1.ensure_authentication_token!
         
         @con1 = FactoryGirl.create(:conversation, :participants => [@p1, @p2])
-        FactoryGirl.create(:message, :conversation => @con1, :sender => @p1)
-        FactoryGirl.create(:message, :conversation => @con1, :sender => @p2)
+        FactoryGirl.create(:message, :conversation => @con1, :sender => @p1, :content => "Let's talk")
+        FactoryGirl.create(:message, :conversation => @con1, :sender => @p2, :content => "Ok! You start.")
 
       end
     
@@ -30,12 +30,52 @@ describe Api::ConversationsController do
         get :index, :person_id => @p1.id, :format => :json
         response.status.should == 200
         resp = JSON.parse(response.body)
-        puts response.body
+        #puts response.body
         #resp["conversations"].count.should == 3
         resp["page"].should == 1
         resp["per_page"].should == 50
       end
       
+    end
+    
+    describe "show" do
+      it "returns the messages of a single conversation" do
+        request.env['Sharetribe-API-Token'] = @p1.authentication_token
+        get :show, :person_id => @p1.id, :id => @con1.id, :format => :json
+        response.status.should == 200
+        resp = JSON.parse(response.body)
+        #puts response.body
+        # puts resp.to_yaml
+        resp["messages"].count.should == 2
+        resp["messages"][0]["content"].should == "Let's talk"
+        resp["messages"][1]["content"].should == "Ok! You start."
+      end
+      
+    end
+    
+    describe "create" do
+      it "creates a new conversation" do
+        request.env['Sharetribe-API-Token'] = @p1.authentication_token
+        post :create, :person_id => @p1.id, 
+                      :target_person_id => @p2.id,
+                      :listing_id => @l1.id,
+                      :status => "pending",
+                      :content => "This will be the first message of the conversation",
+                      :community_id => @c1.id,
+                      :format => :json
+        #response.status.should == 201
+        resp = JSON.parse(response.body)
+        #puts response.body
+        #puts resp.to_yaml
+        #check that title is done automatically
+        resp["title"].should == "Item offer: Sledgehammer"
+        resp["messages"].count.should == 1
+        resp["messages"][0]["content"].should == "This will be the first message of the conversation"
+        resp["messages"][0]["sender_id"].should == @p1.id
+        resp["status"].should == "pending"
+        resp["participations"][0]["person_id"].should == @p1.id
+        resp["participations"][1]["person_id"].should == @p2.id
+      end
     end
 
 
