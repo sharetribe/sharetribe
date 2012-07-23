33c51aa1fe171fcf9428b5ab1016457290789763
Antti Virolainen <gnomet@gmail.com>
2012-07-23 18:52:16 -0400

    People API includes email for the requester himself

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,7 +1,14 @@
 class Api::PeopleController < Api::ApiController
 
+  # TODO: limit visible attributes based on if the request is authenticated and who is the user
+
   def show
     @person = Person.find(params["id"])
+    
+    if current_person == @person
+      @show_email = true
+    end
+    
     respond_with @person
   end
 end
