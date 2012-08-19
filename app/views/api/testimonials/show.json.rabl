f7bfeeec3fc5fe19a6c3c3e5f44e977b0ff695c2
Antti Virolainen <gnomet@gmail.com>
2012-08-20 01:15:53 +0700

    Badges API

--- app/views/api/testimonials/show.json.rabl
+++ app/views/api/testimonials/show.json.rabl
@@ -1,6 +1,6 @@
 object @testimonial
 attributes :grade, :text, :author_id, :receiver_id, :created_at
 
-node  :converstation_id do |testimonial|
+node  :conversation_id do |testimonial|
   testimonial.participation.conversation.id
 end
--- spec/controllers/api/testimonials_controller_spec.rb
+++ spec/controllers/api/testimonials_controller_spec.rb
@@ -4,9 +4,12 @@ describe Api::TestimonialsController do
   before(:each) do
     @p1 = FactoryGirl.create(:person)
     @p2 = FactoryGirl.create(:person)
-    @t1 = FactoryGirl.create(:testimonial, :author => @p2, :receiver => @p1, :grade => 0.5, :text => "well done")
-    @t2 = FactoryGirl.create(:testimonial, :author => @p2, :receiver => @p1, :grade => 0.75, :text => "Nice job!")
-    @t3 = FactoryGirl.create(:testimonial, :author => @p1, :receiver => @p2, :grade => 1.0, :text => "awesome!")
+    @c1 = FactoryGirl.create(:conversation)
+    @pa1 = FactoryGirl.create(:participation, :person => @p1, :conversation => @c1)
+    @pa2 = FactoryGirl.create(:participation, :person => @p2, :conversation => @c1)
+    @t1 = FactoryGirl.create(:testimonial, :author => @p2, :receiver => @p1, :grade => 0.5, :text => "well done", :participation => @pa1)
+    @t2 = FactoryGirl.create(:testimonial, :author => @p2, :receiver => @p1, :grade => 0.75, :text => "Nice job!", :participation => @pa1)
+    @t3 = FactoryGirl.create(:testimonial, :author => @p1, :receiver => @p2, :grade => 1.0, :text => "awesome!", :participation => @pa2)
   end
 
   describe "index" do
@@ -14,7 +17,7 @@ describe Api::TestimonialsController do
       get :index, :person_id => @p1.id, :format => :json
       response.status.should == 200
       resp = JSON.parse(response.body)
-
+      #puts resp.to_yaml
       resp["feedbacks"].count.should == 2
       resp["page"].should == 1
       resp["per_page"].should == 50
@@ -23,6 +26,8 @@ describe Api::TestimonialsController do
       resp["feedbacks"][0]["grade"].should == 0.75
       resp["feedbacks"][1]["text"].should == "well done"
       resp["feedbacks"][0]["text"].should == "Nice job!"
+      resp["feedbacks"][1]["conversation_id"].should == @c1.id
+      resp["feedbacks"][0]["conversation_id"].should == @c1.id      
     end
   end
 end
