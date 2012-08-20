1aa370172a10a129259523f8aa45d54a73c18e35
Antti Virolainen <gnomet@gmail.com>
2012-08-20 07:03:44 +0700

    add location to people GET API

--- spec/controllers/api/people_controller_spec.rb
+++ spec/controllers/api/people_controller_spec.rb
@@ -5,12 +5,15 @@ describe Api::PeopleController do
     describe "show" do
     
       before(:each) do
-        @p1 = FactoryGirl.create(:person, :given_name => "Danny", :family_name => "van Testburg")
+        @p1 = FactoryGirl.create(:person, :given_name => "Danny", :family_name => "van Testburg", :phone_number => "123456789")
         @c1 = FactoryGirl.create(:community)
         @c2 = FactoryGirl.create(:community)
+        @l1 = FactoryGirl.create(:location, :person => @p1, :location_type => "person")
         @p1.communities << @c1
         @p1.communities << @c2
         @p1.ensure_authentication_token!
+        @p2 = FactoryGirl.create(:person)
+        @p2.ensure_authentication_token!
       end
     
       it "returns basic json of a person" do
@@ -32,6 +35,32 @@ describe Api::PeopleController do
         resp["id"].should == @p1.id
         resp["email"].should == @p1.email
       end
+      
+      it "includes phone and location if asker is logged in" do
+        request.env['Sharetribe-API-Token'] = @p2.authentication_token
+        get :show, :id => @p1.id, :format => :json
+        response.status.should == 200
+        resp = JSON.parse(response.body)
+        #puts resp.to_yaml
+        resp["id"].should == @p1.id
+        resp["email"].should be_nil
+        resp["phone_number"].should == "123456789"
+        resp["location"].should_not be_nil
+        
+      end
+      
+      it "should not include phone and location if asker is not logged in" do
+        get :show, :id => @p1.id, :format => :json
+        response.status.should == 200
+        resp = JSON.parse(response.body)
+        #puts resp.to_yaml
+        resp["id"].should == @p1.id
+        resp["email"].should be_nil
+        resp["phone_number"].should be_nil
+        resp["location"].should be_nil
+      end
+      
+      
     end
   end
 end
