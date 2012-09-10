00ffd281b56b8c0d59f0475d4050b46c4077c5e0
Kusti <juho.makkonen@gmail.com>
2012-09-10 16:22:45 +0300

    Migration run

--- db/schema.rb
+++ db/schema.rb
@@ -207,13 +207,6 @@ ActiveRecord::Schema.define(:version => 20120907023525) do
     t.integer "listing_id"
   end
 
-  create_table "hobbies", :force => true do |t|
-    t.string   "name"
-    t.boolean  "official",   :default => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "invitations", :force => true do |t|
     t.string   "code"
     t.integer  "community_id"
@@ -392,7 +385,7 @@ ActiveRecord::Schema.define(:version => 20120907023525) do
     t.boolean  "show_real_name_to_other_users",               :default => true
     t.string   "username"
     t.string   "email"
-    t.string   "encrypted_password",            :default => "",         :null => false
+    t.string   "encrypted_password",                          :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -412,7 +405,6 @@ ActiveRecord::Schema.define(:version => 20120907023525) do
     t.datetime "image_updated_at"
     t.string   "facebook_id"
     t.string   "authentication_token"
-    t.string   "hobby_status",                  :default => "Existing"
   end
 
   add_index "people", ["confirmation_token"], :name => "index_people_on_confirmation_token", :unique => true
@@ -445,13 +437,6 @@ ActiveRecord::Schema.define(:version => 20120907023525) do
     t.integer  "is_read",          :default => 0
     t.datetime "last_sent_at"
     t.datetime "last_received_at"
-  end
-
-  create_table "person_hobbies", :id => false, :force => true do |t|
-    t.string   "person_id"
-    t.integer  "hobby_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "person_interesting_listings", :force => true do |t|
