348ef6ecf6d1184f3cdb46357c0a6074da882a48
Antti Virolainen <antti@sharetribe.com>
2012-09-24 10:49:24 +0800

    Organize config loader and make ST work without confing.yml

---
+++ config/config_loader.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+require 'ostruct'
+def load_app_config
+  if File.exists?("#{Rails.root}/config/config.yml")
+    conf_hash = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+  else
+    #If there's no config file, this is probably Heroku installation, so use empty hash, where ENV is merged
+    conf_hash = {}
+  end
+  conf = OpenStruct.new(conf_hash.merge(ENV).symbolize_keys)
+  # FIXME Temporary cludge to make Heroku work
+  conf.available_locales = [["English", "en"], ["Suomi", "fi"]] if conf.available_locales.nil?
+  return conf
+end
