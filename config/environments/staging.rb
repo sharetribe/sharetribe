c3dd279993d4ae2bff1c3517db8ca6f2202ee996
Antti Virolainen <antti@sharetribe.com>
2012-09-27 10:01:38 +0800

    Create a staging environment

---
+++ config/environments/staging.rb
@@ -0,0 +1,78 @@
+Kassi::Application.configure do
+  # Settings specified here will take precedence over those in config/environment.rb
+
+  # The staging environment is meant to be very similar to production, but to allow easier testing and debuging
+  
+  # Code is not reloaded between requests
+  # If live updates for translations are in use, caching is set to false.
+  config.cache_classes = (APP_CONFIG.update_translations_on_every_page_load.present? ? false : true)
+
+  # Full error reports are disabled and caching is turned on
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = true
+
+  # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = "X-Sendfile"
+
+  # For nginx:
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
+
+  # If you have no front-end server that supports something like X-Sendfile,
+  # just comment this out and Rails will serve the files
+
+  # See everything in the log (default is :info)
+  config.log_level = :debug
+
+  # Use a different logger for distributed setups
+  # config.logger = SyslogLogger.new
+
+  # Use a different cache store in production
+  config.cache_store = :mem_cache_store
+
+  # Disable Rails's static asset server
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = APP_CONFIG.serve_static_assets_in_production || false
+
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host = "http://assets.example.com"
+
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+
+  # Enable threaded mode
+  # config.threadsafe!
+
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  # config.i18n.fallbacks = true #fallbacks defined in intitializers/i18n.rb
+  
+  config.action_mailer.raise_delivery_errors = true
+  
+  mail_delivery_method = (APP_CONFIG.mail_delivery_method.present? ? APP_CONFIG.mail_delivery_method.to_sym : :sendmail)
+  
+  config.action_mailer.delivery_method = mail_delivery_method
+  if mail_delivery_method == :postmark
+    config.action_mailer.postmark_settings = { :api_key => APP_CONFIG.postmark_api_key }
+  elsif mail_delivery_method == :smtp
+    ActionMailer::Base.smtp_settings = {
+      :address              => APP_CONFIG.smtp_email_address,
+      :port                 => APP_CONFIG.smtp_email_port,
+      :domain               => APP_CONFIG.smtp_email_domain,
+      :user_name            => APP_CONFIG.smtp_email_user_name,
+      :password             => APP_CONFIG.smtp_email_password,
+      :authentication       => 'plain',
+      :enable_starttls_auto => true  
+    }
+  end
+  
+  # Sendmail is used for some mails (e.g. Newsletter) so configure it even when postmark is the main method
+  ActionMailer::Base.sendmail_settings = {
+    :location       => '/usr/sbin/sendmail',
+    :arguments      => '-i -t'
+  }  
+  
+    
+    
+  ActionMailer::Base.perform_deliveries = true # the "deliver_*" methods are available
+  ActionMailer::Base.default_charset = "utf-8"
+end
