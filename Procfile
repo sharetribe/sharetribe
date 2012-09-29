141643a75c849a4ccd9ed6cd1d1c05597e049ba0
Antti Virolainen <antti@sharetribe.com>
2012-09-29 20:43:42 +0800

    Test running on unicorn in heroku

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: bundle exec rails server thin -p $PORT
+web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
 worker:  bundle exec rake jobs:work
 
---
+++ config/unicorn.rb
@@ -0,0 +1,33 @@
+worker_processes 3
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  # Replace with MongoDB or whatever
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+
+  # If you are using Redis but not Resque, change this
+  if defined?(Resque)
+    Resque.redis = ENV['REDIS_URI']
+    Rails.logger.info('Connected to Redis')
+  end
+end
