6ddbbf742640dc7c9927986d58d921a27c006df6
Antti Virolainen <antti@sharetribe.com>
2012-10-06 17:42:36 +0800

    Changes to config loader, to make spec work with Travis CI

--- config/config_loader.rb
+++ config/config_loader.rb
@@ -2,11 +2,20 @@ require 'yaml'
 require 'ostruct'
 def load_app_config
   if File.exists?("#{Rails.root}/config/config.yml")
+    
     conf_hash = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+  
   else
-    #If there's no config file, this is probably Heroku installation, so use empty hash, where ENV is merged
-    conf_hash = {}
+    
+    #If there's no config file, this is probably Heroku installation or running tests in CI
+    if Rails.env.test?
+      # use example file for Continuous integrations tests
+      conf_hash = YAML.load_file("#{Rails.root}/config/config.example.yml")["test"]
+    else
+      conf_hash = {} # empty hash where Heroku env variables can be merged
+    end
   end
+  
   conf = OpenStruct.new(conf_hash.merge(ENV).symbolize_keys)
   # FIXME Temporary cludge to make Heroku work
   #conf.available_locales = [["English", "en"], ["Suomi", "fi"]] if conf.available_locales.nil?
