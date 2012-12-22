a1720570eba7233b3b12b85b7d03a7b46dc165a8
Antti Virolainen <antti@sharetribe.com>
2012-12-22 11:29:40 +0200

    Search works on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -13,8 +13,27 @@ class HomepageController < ApplicationController
     # If requesting a specific page on non-ajax request, we'll ignore that
     # and show the normal front page starting from newest listing
     params[:page] = 1 unless request.xhr? 
+    @query = params[:q]
+    
+    if @query # Search used
+      with = {:open => true}
+      unless @current_user && @current_user.communities.include?(@current_community)
+        with[:visible_to_everybody] = true
+      end
+      with[:community_ids] = @current_community.id
 
-    @listings = Listing.visible_to(@current_user, @current_community).currently_open.order("created_at DESC").paginate(:per_page => listings_per_page, :page => params[:page])
+      @listings = Listing.search(@query, 
+                                :include => :listing_images, 
+                                :page => params[:page],
+                                :per_page => listings_per_page, 
+                                :star => true,
+                                :with => with
+                                )
+      
+    else # no search used
+
+      @listings = Listing.visible_to(@current_user, @current_community).currently_open.order("created_at DESC").paginate(:per_page => listings_per_page, :page => params[:page])
+    end
     
     if request.xhr? # checks if AJAX request
       render :partial => "recent_listing", :collection => @listings, :as => :listing   
