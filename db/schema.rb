23121ade888fb45615ca7a629e1b925f517d1c64
Antti Virolainen <antti@sharetribe.com>
2013-01-03 15:05:28 +0200

    Migration that handles private community listings so that they are not too visible in new UI

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130103081705) do
+ActiveRecord::Schema.define(:version => 20130103125240) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
