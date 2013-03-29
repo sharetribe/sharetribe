8181721fb6e08611b0679af492e4cfdf771a4646
Antti Virolainen <antti@sharetribe.com>
2013-03-29 10:21:13 +0200

    Migrations for update and clear the DB for new statuses and reminders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328155825) do
+ActiveRecord::Schema.define(:version => 20130329081612) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
