9b379c0826aa46b47a99bf54ed8bc0ddf266f06e
Antti Virolainen <antti@sharetribe.com>
2013-04-02 01:40:10 +0300

    another fix to make the migrations go through

--- db/migrate/20130326160252_update_categories_with_price_and_translations.rb
+++ db/migrate/20130326160252_update_categories_with_price_and_translations.rb
@@ -1,7 +1,8 @@
 class UpdateCategoriesWithPriceAndTranslations < ActiveRecord::Migration
   def up
     CategoriesHelper.update_translations
-    CategoriesHelper.add_custom_price_quantity_placeholders
+    # Skip this at this point as later migration will call it again when there's also payments added, so it won't crash
+    #CategoriesHelper.add_custom_price_quantity_placeholders
   end
 
   def down
