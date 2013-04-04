bc7f3fa9852fef55580becd1009696ecbe6935ff
Antti Virolainen <antti@sharetribe.com>
2013-04-04 22:53:45 +0300

    Better script for customized categories

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -43,8 +43,20 @@ module CategoriesHelper
       "accept_for_free" => {:parent => "request", :categories => ["housing"]}
   }
   
-  def self.load_categories_and_share_types_to_db(community, categories, share_types, translations={})
-    community_id = community ? community.id : nil
+  def self.load_default_categories_to_db(params={})
+    load_categories_and_share_types_to_db(:community_id => nil, :categories => DEFAULT_CATEGORIES, :share_types => DEFAULT_SHARE_TYPES)
+
+    add_custom_price_quantity_placeholders unless params[:without_price_updates]
+    update_default_rent_out_quantiy_placeholder # Just a small change to default categorization
+  end
+  
+  
+  
+  def self.load_categories_and_share_types_to_db(params={})
+    community_id = params[:community_id] || (params[:community] ? params[:community].id : nil)
+    categories = params[:categories]
+    share_types = params[:share_types]
+    translations = params[:translations]
     
     categories.each do |category| 
       if category.class == String
@@ -71,95 +83,22 @@ module CategoriesHelper
         CommunityCategory.create(:category => c, :share_type => s, :community_id => community_id, :price => details[:price], :payment => details[:payment], :price_quantity_placeholder => details[:price_quantity_placeholder]) if c && ! CommunityCategory.find_by_category_id_and_share_type_id_and_community_id(c.id, s.id, community_id)
       end
     end
-
-    # Store translations for all that can be found from translation files
-    Kassi::Application.config.AVAILABLE_LOCALES.each do |loc|
-      locale = loc[1]
-      Category.find_each do |category|
-        begin 
-          translated_string =  (translations[locale] && translations[locale][category.name]) || I18n.t!(category.name, :locale => locale, :scope => ["common", "categories"], :raise => true)
-          CategoryTranslation.create(:category => category, :locale => locale, :name => translated_string) unless CategoryTranslation.find_by_category_id_and_locale(category.id, locale)
-        rescue I18n::MissingTranslationData
-          # just skip storing translation for this one
-        end
-      end
-
-      ShareType.find_each do |share_type|
-        begin
-          translated_string = (translations[locale] && translations[locale][share_type.name]) || I18n.t!(share_type.name, :locale => locale, :scope => ["common", "share_types"], :raise => true)
-          ShareTypeTranslation.create(:share_type => share_type, :locale => locale, :name => translated_string) unless ShareTypeTranslation.find_by_share_type_id_and_locale(share_type.id, locale)
-        rescue I18n::MissingTranslationData
-          # just skip storing translation for this one
-        end
-      end
-
-    end
-
-    sell = ShareType.find_by_name("sell")
-    buy = ShareType.find_by_name("buy")
-
-    Category.all.each do |category|
-      sell_category = CommunityCategory.where("category_id = ? AND share_type_id = ? AND community_id = ?", category.id.to_s, sell.id.to_s, community_id.to_s).first
-      sell_category.update_attributes(:price => true, :price_quantity_placeholder => "mass", :payment => true) if sell_category  
-
-      buy_category = CommunityCategory.where("category_id = ? AND share_type_id = ? AND community_id = ?", category.id.to_s, buy.id.to_s, community_id.to_s).first
-      buy_category.update_attributes(:payment => true) if buy_category
-
-    end
     
-    
-    #TODO: Add here a custom line to put , :price_quantity_placeholder => "long_time" for renting out spaces WHEN BUILDING DEFAULT CATEGORIES
+    update_translations(:translations => translations) unless params[:skip_translations]
     
   end
   
-  
-  
-  
-  def self.load_default_categories_to_db(params={})
-    
-    DEFAULT_CATEGORIES.each do |category| 
-      if category.class == String
-        Category.create([{:name => category, :icon => category}]) unless Category.find_by_name(category)
-      elsif category.class == Hash
-        parent = Category.find_by_name(category.keys.first) || Category.create(:name => category.keys.first, :icon => category.keys.first) 
-        category.values.first.each do |subcategory|
-          c = Category.find_by_name(subcategory) || Category.create({:name => subcategory, :icon => subcategory, :parent_id => parent.id}) 
-          # As subcategories won't get their own link to share_types (as they inherit that from parent category)
-          # We create a CommunityCategory entry here to mark that these subcategories exist in the default tribe
-          CommunityCategory.create(:category => c) unless CommunityCategory.find_by_category_id(c.id)
-        end
-      else
-        puts "Invalid data for default_categories. It must be array of Strings and Hashes."
-        return
-      end
-    end
-
-    DEFAULT_SHARE_TYPES.each do |share_type, details|
-      parent = ShareType.find_by_name(details[:parent]) if details[:parent]
-      s =  ShareType.find_by_name(share_type) || ShareType.create(:name => share_type, :icon => share_type, :parent => parent)
-      details[:categories].each do |category_name|
-        c = Category.find_by_name(category_name)
-        CommunityCategory.create(:category => c, :share_type => s) if c && ! CommunityCategory.find_by_category_id_and_share_type_id(c.id, s.id)
-      end
-    end
-    
-    
-    
-    update_translations(params)
-    add_custom_price_quantity_placeholders unless params[:without_price_updates]
-    
-  end
-
   def self.update_translations(params={})
+    translations = params[:translations] || {}
     # Store translations for all that can be found from translation files
     Kassi::Application.config.AVAILABLE_LOCALES.each do |loc|
       locale = loc[1]
       Category.find_each do |category|
         begin 
-          translated_name = I18n.t!(category.name, :locale => locale, :scope => ["common", "categories"], :raise => true)
+          translated_name = (translations[locale] && translations[locale][category.name]) || I18n.t!(category.name, :locale => locale, :scope => ["common", "categories"], :raise => true)
           
           begin 
-            translated_description = I18n.t!(category.name, :locale => locale, :scope => ["listings", "new"], :raise => true)
+            translated_description = (translations[locale] && translations[locale][:descriptions] && translations[locale][:descriptions][category.name]) || I18n.t!(category.name, :locale => locale, :scope => ["listings", "new"], :raise => true)
           rescue
             translated_description = nil #if description is nil, still continue to translate the name
           end  
@@ -180,12 +119,13 @@ module CategoriesHelper
         end
       end
       
+      
       ShareType.find_each do |share_type|
         begin
-          translated_name = I18n.t!(share_type.name, :locale => locale, :scope => ["common", "share_types"], :raise => true)
+          translated_name = (translations[locale] && translations[locale][share_type.name]) || I18n.t!(share_type.name, :locale => locale, :scope => ["common", "share_types"], :raise => true)
           
           begin 
-            translated_description = I18n.t!(share_type.name, :locale => locale, :scope => ["listings", "new"], :raise => true)
+            translated_description = (translations[locale] && translations[locale][:descriptions] && translations[locale][:descriptions][share_type.name]) || I18n.t!(share_type.name, :locale => locale, :scope => ["listings", "new"], :raise => true)
           rescue
             translated_description = nil #if description is nil, still continue to translate the name
           end
@@ -207,6 +147,7 @@ module CategoriesHelper
       
     end
   end
+
   
   def self.add_custom_price_quantity_placeholders
     sell = ShareType.find_by_name("sell")
@@ -240,6 +181,12 @@ module CategoriesHelper
     rent_housing = CommunityCategory.where("category_id = ? AND share_type_id = ? AND community_id IS NULL", housing.id.to_s, rent.id.to_s).first
     rent_housing.update_attributes(:payment => true)
     
+  end
+  
+  def self.update_default_rent_out_quantiy_placeholder
+    # This custom line sets the default housing rent_out offer to 
+    # have price_quantity_placeholder "long_time" instead of the normal time :)
+    CommunityCategory.find_by_category_id_and_share_type_id_and_community_id(Category.find_by_name("housing").id, ShareType.find_by_name("rent_out").id, nil).update_attribute(:price_quantity_placeholder, "long_time")
   end
   
   def self.remove_all_categories_from_db
---
+++ spec/helpers/categories_helper_spec.rb
@@ -0,0 +1,122 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+include CategoriesHelper
+
+describe CategoriesHelper do
+  describe "#load_categories_and_share_types_to_db" do
+
+    it "loads categorization to db based on given params" do
+      
+      categories = [
+        {
+        "wood_based_materials" => [
+                "wooden_disk",
+                "wooden_board",
+                "wood_shavings",
+                "misc_wood"
+          ]
+        },
+        {
+        "plastic_and_rubber" => [
+                "plastic_disk",
+                "plastic_membranes",
+                "misc_plastic"
+          ]
+        },
+        {
+        "metal" => [
+                "iron_and_steel",
+                "aluminium",
+                "copper",
+                "other_metal"
+          ]
+        },
+        "concrete_and_brick",
+        "glass_and_porcelain",
+        "textile_and_leather",
+        "soil_materials",
+        "liquid_materials",
+        "manufacturing_error_materials",
+        "misc_material"
+      ]
+
+      top_categories = ["wood_based_materials",
+      "plastic_and_rubber" ,
+      "metal",
+      "concrete_and_brick",
+      "glass_and_porcelain",
+      "textile_and_leather",
+      "soil_materials",
+      "liquid_materials",
+      "manufacturing_error_materials",
+      "misc_material"
+      ]
+
+      custom_translations = {
+        "fi" => {
+          "wood_based_materials" => "Puu, paperi ja kartonki",
+          "wooden_disk" => "Levyt",
+          "wooden_board" => "Lauta",
+          "wood_shavings" => "Purut, lastut",
+          "misc_wood" => "Sekalaiset",
+          "plastic_and_rubber" => "Muovi ja kumi",
+          "plastic_disk" => "Levyt",
+          "plastic_membranes" => "Kalvot",
+          "misc_plastic" => "Sekalaiset",
+          "metal" => "Metallit",
+          "iron_and_steel" => "Rauta, teräs",
+          "aluminium" => "Alumiini",
+          "copper" => "Kupari",
+          "other_metal" => "Muut",
+          "concrete_and_brick" => "Betoni ja tiili",
+          "glass_and_porcelain" => "Lasi ja posliini",
+          "textile_and_leather" => "Tekstiilit ja nahka",
+          "soil_materials" => "Maa-ainekset, kivi ja tuhka",
+          "liquid_materials" => "Nestemäiset ja lietteet",
+          "manufacturing_error_materials" => "Valmistusvirhe-erät",
+          "misc_material" => "Sekalaista"
+        }
+      }
+
+      share_types = {
+        "offer" => {:categories => top_categories},
+          "sell" => {:parent => "offer", :categories => top_categories},
+          "give_away" => {:parent => "offer", :categories => top_categories},
+        "request" => {:categories => top_categories},
+          "buy" => {:parent => "request", :categories => top_categories},
+          "receive" => {:parent => "request", :categories => top_categories}, 
+      }
+      
+      community = FactoryGirl.create(:community)
+      CategoriesHelper.load_categories_and_share_types_to_db(:community_id => community.id, :categories => categories, :share_types => share_types)
+      #puts community.categories.collect(&:name)
+      #puts community.main_categories.collect(&:name)
+      #puts community.share_types.collect(&:name)
+      community.main_categories.count.should == 10
+      community.categories.count.should == 21
+      #puts community.categories_tree.to_yaml
+      
+    end
+
+    
+  end
+  
+  describe "#load_default_categories_to_db" do
+    it "should load default categories" do
+      CategoriesHelper.load_default_categories_to_db
+      Category.count.should == 19
+      ShareType.count.should == 14
+      community = FactoryGirl.create(:community)
+      community.main_categories.count.should == 4
+      community.listing_types.count.should == 2
+      CommunityCategory.find_by_category_id_and_share_type_id_and_community_id(Category.find_by_name("housing").id, ShareType.find_by_name("rent_out").id, nil).price_quantity_placeholder.should == "long_time"
+      CommunityCategory.find_by_category_id_and_share_type_id_and_community_id(Category.find_by_name("item").id, ShareType.find_by_name("sell").id, nil).price.should be_true
+      
+      #puts community.categories_tree.to_yaml
+    end
+    
+  end
+  
+end
