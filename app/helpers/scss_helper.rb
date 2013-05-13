e1ed1b40cf9d11f78b620b42e37ecd8fc9431667
Antti Virolainen <antti@sharetribe.com>
2013-05-13 13:22:35 +0200

    Delete scss_helper as it is no longer used

--- app/helpers/scss_helper.rb
+++
@@ -1,9 +0,0 @@
-module ScssHelper
-  
-  def additional_font_pack_imports
-    if APP_CONFIG.icon_pack == "ss-pika"
-      return "@import 'ss-social'; \n @import 'ss-pika';".html_safe  
-    end
-  end
-  
-end
