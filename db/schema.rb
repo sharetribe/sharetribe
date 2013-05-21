894cdf620b5c859d71fe976d1500a57279f0009c
Kusti <juho.makkonen@gmail.com>
2013-05-21 19:48:15 +0300

    Minor text fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20130521124342) do
 
   create_table "auth_tokens", :force => true do |t|
