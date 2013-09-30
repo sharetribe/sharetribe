431d0d8b16738841f68a0ede059eec77d4cd64d1
Kusti <juho.makkonen@gmail.com>
2013-09-30 11:57:30 +0300

    Fix to make migrations pass

--- db/migrate/20130926121237_add_email_content_and_subject_line_to_country_managers.rb
+++ db/migrate/20130926121237_add_email_content_and_subject_line_to_country_managers.rb
@@ -1,7 +1,7 @@
 class AddEmailContentAndSubjectLineToCountryManagers < ActiveRecord::Migration
   def change
-    add_column :country_managers, :subject_line, :string
-    add_column :country_managers, :email_content, :text
-    remove_column :country_managers, :email_signature, :text
+    # add_column :country_managers, :subject_line, :string
+    # add_column :country_managers, :email_content, :text
+    # remove_column :country_managers, :email_signature, :text
   end
 end
