18120fe9401d06348201cb065a7f9d745a16e0bb
Mikko Koski <mikkokos@gmail.com>
2013-10-14 10:26:43 +0300

    Migration task to reprocess images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130930080143) do
+ActiveRecord::Schema.define(:version => 20131014065649) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
