c982720db8bedf7ee5d28e363994b833f3db908a
Antti Virolainen <antti@sharetribe.com>
2013-10-30 22:50:45 +0200

    Add possibility for domain_alias
    - also support FB login detecting if real domain is used, otherwise fallback to default FB app. (useful for preproduction env with other domain)

--- app/models/community.rb
+++ app/models/community.rb
@@ -225,15 +225,13 @@ class Community < ActiveRecord::Base
     if domain_string =~ /\:/ #string includes port which should be removed
       domain_string = domain_string.split(":").first
     end
-    if domain_string =~ /\./ # not just a subdomain
-      if domain_string.match(APP_CONFIG.domain.split(":").first) # subdomain with default domain attached
-        Community.where(["domain = ?", domain_string.split(".").first]).first
-      else # custom domain
-        Community.where(["domain = ?", domain_string]).first
-      end
-    else # just a subdomain
-      Community.where(["domain = ?", domain_string]).first
-    end
+    
+    # search for exact match or then match by first part of domain string.
+    # first priority is the domain, then domain_alias
+    return Community.where(["domain = ?", domain_string]).first || 
+           Community.where(["domain = ?", domain_string.split(".").first]).first ||
+           Community.where(["domain_alias = ?", domain_string]).first ||
+           Community.where(["domain_alias = ?", domain_string.split(".").first]).first
   end
   
   # Check if communities with this category are email restricted
@@ -539,11 +537,11 @@ class Community < ActiveRecord::Base
     end
   end
   
-  def facebook_login_method
-    unless facebook_connect_id
-      return :facebook
-    else
+  def facebook_login_method(host=nil)
+    if facebook_connect_id && (!host || full_domain.match(host))
       return "facebook_app_#{facebook_connect_id}".to_sym
+    else
+      return :facebook
     end
   end
 
