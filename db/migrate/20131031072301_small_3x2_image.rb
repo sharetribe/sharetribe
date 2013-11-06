293a66653c06617e35b3c6989e235b0d8197d6bd
Antti Virolainen <antti@sharetribe.com>
2013-11-06 11:46:09 +0200

    Modify a migration to not mark images as processing, as that breaks the following migration

--- db/migrate/20131031072301_small_3x2_image.rb
+++ db/migrate/20131031072301_small_3x2_image.rb
@@ -5,6 +5,12 @@ class Small3x2Image < ActiveRecord::Migration
     Listing.all.each do |listing|
       listing.listing_images.each do |listing_image|
         listing_image.image.reprocess! :small_3x2
+        # This is added as the delayed_paperclip would otherwise
+        # mark all images as processing, although there's no need
+        # as most imagesizes are already ok.
+        # Setting image_processing to false doesn't stop small_3x2 from 
+        # being created in the background job
+        listing_image.update_column(:image_processing, false)
         print "."
         STDOUT.flush
       end
