22c614f1201d860f0a9b0d4323c641728d5f09b8
Antti Virolainen <antti@sharetribe.com>
2013-11-11 17:18:49 +0200

    fix tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,6 +8,6 @@ describe SessionsController, "POST create" do
   it "redirects back to original community's domain" do
     @request.host = "test.lvh.me"
     post :create, {:person  => {:login => "kassi_testperson1", :password => "testi"}, :community => "test"}
-    response.should redirect_to "http://test.lvh.me:9887/?locale=en"
+    response.should redirect_to "http://test.lvh.me/?locale=en"
   end  
 end
