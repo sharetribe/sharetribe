b5228d2429f142ffe276953c0ed5c8b9b2ecc6bd
Antti Virolainen <antti@sharetribe.com>
2013-11-21 16:01:50 +0200

    Fix tests and update travis notification emails

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,7 +4,7 @@ class SettingsController < ApplicationController
     controller.ensure_logged_in t("layouts.notifications.you_must_log_in_to_view_your_settings")
   end
   
-  before_filter do |controller|
+  before_filter :except => :unsubscribe do |controller|
     controller.ensure_authorized t("layouts.notifications.you_are_not_authorized_to_view_this_content")
   end
   
