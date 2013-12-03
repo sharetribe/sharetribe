fd60886123d494a3b2bd78c0445517c280468ed1
Mikko Koski <mikkokos@gmail.com>
2013-12-03 16:34:56 +0200

    Mock Braintree API

--- spec/controllers/braintree_account_controller_spec.rb
+++ spec/controllers/braintree_account_controller_spec.rb
@@ -14,6 +14,10 @@ describe BraintreeAccountsController do
     end
 
     it "should create braintree details with detailed information" do
+      # Mock BraintreeService
+      BraintreeService.should_receive(:create_merchant_account)
+        .and_return(Braintree::SuccessfulResult.new(:merchant_account => HashClass.new(:status => "pending")))
+
       post :create, :braintree_account => {
         :person_id => @person.id,
         :first_name => "Joe",
--- spec/services/email_service_spec.rb
+++ spec/services/email_service_spec.rb
@@ -1,15 +1,3 @@
-# This should go to a helper
-# http://pullmonkey.com/2008/01/06/convert-a-ruby-hash-into-a-class-object/
-class HashClass
-  def initialize(hash)
-    hash.each do |k,v|
-      self.instance_variable_set("@#{k}", v)  ## create and initialize an instance variable for this key/value pair
-      self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})  ## create the getter that returns the instance variable
-      self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})  ## create the setter that sets the instance variable
-    end
-  end
-end
-
 def email(h)
   defaults = {confirmed_at: Time.now, send_notifications: true}
   HashClass.new(defaults.merge(h))
