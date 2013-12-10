d9c12fdfb690f05268a4d8cd09a4258df81678f3
Juho Makkonen <juho.makkonen@gmail.com>
2013-12-10 18:41:25 +0200

    Fixed failing tests

--- db/schema.rb
+++ db/schema.rb
@@ -189,18 +189,6 @@ ActiveRecord::Schema.define(:version => 20131209133946) do
   add_index "communities_listings", ["community_id"], :name => "index_communities_listings_on_community_id"
   add_index "communities_listings", ["listing_id", "community_id"], :name => "communities_listings"
 
-  create_table "communities_payment_gateways", :force => true do |t|
-    t.integer  "community_id"
-    t.integer  "payment_gateway_id"
-    t.string   "braintree_merchant_id"
-    t.string   "braintree_master_merchant_id"
-    t.string   "braintree_public_key"
-    t.string   "braintree_private_key"
-    t.text     "braintree_client_side_encryption_key"
-    t.datetime "created_at",                           :null => false
-    t.datetime "updated_at",                           :null => false
-  end
-
   create_table "community_categories", :force => true do |t|
     t.integer  "community_id"
     t.integer  "category_id"
