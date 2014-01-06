55d486dc672c6eb07175166c4cb8f5ee22078a25
Mikko Koski <mikkokos@gmail.com>
2014-01-06 14:06:18 +0200

    Sort also answers

--- app/models/custom_field_value.rb
+++ app/models/custom_field_value.rb
@@ -1,4 +1,6 @@
 class CustomFieldValue < ActiveRecord::Base
+  include Comparable
+  
   belongs_to :listing
   belongs_to :question, :class_name => "CustomField", :foreign_key => "custom_field_id"
   attr_accessible :text_value
@@ -6,4 +8,9 @@ class CustomFieldValue < ActiveRecord::Base
   has_many :selected_options
 
   delegate :with_type, :to => :question
+
+  def <=> other
+    # Answer follows question's sort priority
+    question.sort_priority <=> other.question.sort_priority
+  end
 end
