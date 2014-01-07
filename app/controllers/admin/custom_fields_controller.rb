494d36c23b775f43413597fa3003ae23a87a075d
Mikko Koski <mikkokos@gmail.com>
2014-01-07 14:51:05 +0200

    Remove custom field

--- app/controllers/admin/custom_fields_controller.rb
+++ app/controllers/admin/custom_fields_controller.rb
@@ -15,5 +15,11 @@ class Admin::CustomFieldsController < ApplicationController
     @custom_field = CustomField.create(params[:custom_field])
     redirect_to admin_custom_fields_path
   end
+
+  def destroy
+    @custom_field = CustomField.find(params[:id])
+    success = @custom_field.destroy
+    redirect_to admin_custom_fields_path
+  end
   
 end
