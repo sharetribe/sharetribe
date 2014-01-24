e16f40aa4b6da63530a851e08981cfeb6f39c2b7
Antti Virolainen <antti@sharetribe.com>
2014-01-24 12:24:21 +0200

    More fixes to make sphinx tests pass

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -1,4 +1,4 @@
 Given /^the (\w+) indexes are processed$/ do |model|
-  ThinkingSphinx::Test.index model.downcase
+  ThinkingSphinx::Test.index "#{model.downcase}_core", "#{model.downcase}_delta"
   sleep(0.25) # Wait for Sphinx to catch up
 end
