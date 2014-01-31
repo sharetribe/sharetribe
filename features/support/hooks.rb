9d7d943c70c73093ab821fd096a513c4f477eaad
Juho Makkonen <juho.makkonen@gmail.com>
2014-01-31 15:10:34 +0200

    Remove mentions of fixtures from cucumber hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,11 +1,6 @@
 start_time = Time.now
 
 Before do
-  # ActiveRecord::Fixtures.reset_cache
-  # fixtures_folder = File.join(Rails.root  , 'spec', 'fixtures')
-  # fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
-  # ActiveRecord::Fixtures.create_fixtures(fixtures_folder, fixtures)
-  
   load_default_test_data_to_db
 
   Capybara.default_host = 'test.lvh.me'
