bbf0bde80ab9c1dc0c7389244a9b0cfec1556cb8
Mikko Koski <mikkokos@gmail.com>
2014-02-03 17:13:02 +0200

    Remove child category if parent is removed
    - Tests are probably failing

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,7 +5,7 @@ class Category < ActiveRecord::Base
   # Classification module contains methods that are common to Category and ShareType
   include Classification
 
-  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id"
+  has_many :subcategories, :class_name => "Category", :foreign_key => "parent_id", :dependent => :destroy
   # children is a more generic alias for sub categories, used in classification.rb
   has_many :children, :class_name => "Category", :foreign_key => "parent_id"
   belongs_to :parent, :class_name => "Category"
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe Category do
 
   before(:each) do
-    @category = FactoryGirl.create(:category)
+    @community = FactoryGirl.create(:community)
+    @category = FactoryGirl.create(:category, :community => @community)
+    @subcategory = FactoryGirl.create(:category)
+    @subcategory.update_attribute(:parent_id, @category.id)
+
+    @community.reload
+    @category.reload
+    @subcategory.reload
   end
 
   it "has listings?" do
@@ -13,6 +20,25 @@ describe Category do
     @category.reload
 
     @category.has_listings?.should be_true
+  end
+
+  it "can not be deleted if it's the only top level category" do
+    Category.find(@category.id).should_not be_nil
+
+    @category.destroy
+
+    Category.find(@category.id).should_not be_nil
+  end
+
+  it "removes subcategories if parent is removed" do
+    @category2 = FactoryGirl.create(:category, :community => @community)
+    Category.find(@category.id).should_not be_nil
+    Category.find(@subcategory.id).should_not be_nil
+
+    @category.destroy
+
+    Category.find(@category.id).should be_nil
+    Category.find(@subcategory.id).should be_nil
   end
 
 end
