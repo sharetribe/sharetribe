d66d48ffcf461004d298b6a890f5ef27c28cc42f
Antti Virolainen <antti@sharetribe.com>
2014-02-04 12:43:29 +0200

    Migration structure ready for custom categories (not tried yet)

--- app/models/transaction_types/free_service.rb
+++
@@ -1,3 +0,0 @@
-class FreeService < TransactionType
-
-end
--- db/migrate/20140204101010_create_community_specific_categories.rb
+++ db/migrate/20140204101010_create_community_specific_categories.rb
@@ -17,25 +17,15 @@ class CreateCommunitySpecificCategories < ActiveRecord::Migration
       
 
         main_categories.each do |category|
-          create_new_cat_and_trans_if_needed(category, community.id)
-
-          # A luo cat jos tarvii
-          # B Luo trans type jos tarvii
-          # C linkitä noi jos tarvii
-
-          # D transalations mukanan
-          # E Community id mukana
+          create_new_cat_and_trans_if_needed(category, community)
         end
 
         # Second loop sub categories
         subcategories.each do |category|
-          create_new_cat_and_trans_if_needed(category, community.id)
+          create_new_cat_and_trans_if_needed(category, community)
         end
 
         print_stat "c"
-
-
-
 
       else # Using default categories
 
@@ -68,53 +58,145 @@ class CreateCommunitySpecificCategories < ActiveRecord::Migration
     # TODO
   end
 
-  def create_new_cat_and_trans_if_needed(old_cat, community_id)
+  def create_new_cat_and_trans_if_needed(old_cat, community)
+
+    # FIRST CREATE THE CATEGORY
+
     if old_cat.parent_id
       # Find new parent, which should be created at this point (Error if not)
-      new_parent_id = Category.where(:community_id => community_id, :name => old_cat.parent.name).first.id
+      new_parent_id = Category.where(:community_id => community.id, :name => old_cat.parent.name).first.id
     else
       new_parent_id = nil
     end
 
-    if new_cat = Category.find_by_community_id_and_name(community_id, old_cat.name) 
+    if new_cat = Category.find_by_community_id_and_name(community.id, old_cat.name) 
       #Avoid duplicates
-      puts "WARNING: category #{old_cat.name} already exists for community_id: #{community_id}"
-
+      puts "WARNING: category #{old_cat.name} already exists for community_id: #{community.id}"
     else
-      new_cat = Category.create(:name => old_cat.name, :parent_id => new_parent_id, :icon => old_cat.icon, :community_id => community_id)
-      # TODO Translations
+      new_cat = Category.create(:name => old_cat.name, :parent_id => new_parent_id, :icon => old_cat.icon, :community_id => community.id)
+      old_cat.translations.each do |old_trans|
+        CategoryTranslation.create!(:category_id => new_cat.id, :locale => old_trans.locale, :name => old_cat.name, :description => old_cat.description)
+      end
     end
 
-    com_cats_for_this_cat = CommunityCategory.find_all_by_community_id_and_category_id(community_id, old_cat.id)
+
+    # THEN FETCH THE COMMUNITYCATEOGRIES AND CREATE NEEDED TRANSACTIONTYPES
+
+    com_cats_for_this_cat = CommunityCategory.find_all_by_community_id_and_category_id(community.id, old_cat.id)
     if com_cats_for_this_cat.empty?
       # defaults in use
       com_cats_for_this_cat = CommunityCategory.find_all_by_community_id_and_category_id(nil, old_cat.id)
     end
 
+    # Update sort priority based on first com_cat for this category
+    new_cat.update_attribute(:sort_priority, com_cats_for_this_cat.first.sort_priority)
+
     com_cats_for_this_cat.each do |community_category|
+      new_type_class = SHARE_TYPE_MAP[community_category.share_type.name]
 
-      new_type_class = get_new_type_based_on_old(community_category.share_type)
-
-      if  new_trt = new_type_class.find_by_community_id(community_id)
-
-
+      if  new_trt = new_type_class.find_by_community_id(community.id)
+        puts "WARNING: transaction type #{new_trt.class.name} for community_id: #{community.id} already exists"
       else 
-        new_trt = new_type_class.create(:community_id => community_id, :sort_priority => com_cat.sort_priority, :price_field => com_cat.price)
+        new_trt = new_type_class.create!(:community_id => community.id, :sort_priority => com_cat.sort_priority, :price_field => com_cat.price)
+      end
+
+      create_translations_for(new_trt, community)
+
+      # Link category and transaction type
+      CategoryTransactionType.create!(:category_id => new_cat.id, :transaction_type_id => new_trt.id)
+
+    end
+    
+  end
+
+  # Creates translations for new transaciton type
+  def create_translations_for(trans_type, community)
+
+    community.locales.each do |locale|
+      if TransactionTypeTranslation.find_by_transaction_type_id_and_locale(trans_type.id, locale)
+        puts "WARNING: #{locale} translation for trans_type.id #{trans_type.id} already exists"
+
+      else
+        TransactionTypeTranslation.create(:locale => locale, :transaction_type_id => trans_type.id, 
+          :name => I18n.t(trans_type.class.downcase, :locale => locale, :scope => ["admin", "transaction_types"]))
       end
     end
 
-              # A luo cat jos tarvii
-          # B Luo trans type jos tarvii
-          # C linkitä noi jos tarvii
-
-          # D transalations mukanan
-          # E Community id mukana
-          # F parent_id uuteen id:hen
-    
   end
 
-  def get_new_type_based_on_old(old_share_type)
-    # TODO
-    
-  end
+
+  SHARE_TYPE_MAP = {
+    "offer" => Service,
+    "sell" => Sell,
+    "rent_out" => Rent,
+    "lend" => Loan,
+    "offer_to_swap" => Swap,
+    "give_away" => Give,
+    "share_for_free" => Service,
+    "request" => Request,
+    "buy" => Request,
+    "rent" => Request,
+    "borrow" => Request,
+    "request_to_swap" => Swap,
+    "receive" => Request,
+    "accept_for_free" => Request,
+    "sell_material" => Sell,
+    "give_away_material" => Give,
+    "search_material" => Request,
+    "sell_alt" => Sell,
+    "offer_to_swap_alt" => Swap,
+    "show_collection" => Service,
+    "offer_to_barter" => Swap,
+    "request_to_barter" => Swap,
+    "rent_out_a_bike" => Rent,
+    "sell_a_bike" => Sell,
+    "get_a_bike" => Request,
+    "sell_mass" => Sell,
+    "give_away_mass" => Give,
+    "search_mass" => Request,
+    "offer_job" => Service,
+    "request_job" => Request,
+    "offer_bf" => Service,
+    "request_bf" => Request,
+    "give_vg" => Give,
+    "ask_vg" => Request,
+    "offer_furnished_place" => Rent,
+    "free_up_flat_furnished" => Rent,
+    "free_up_room_furnished" => Rent,
+    "sublet_flat_furnished" => Rent,
+    "sublet_room_furnished" => Rent,
+    "offer_non_furnished_place" => Rent,
+    "free_up_flat_non_furnished" => Rent,
+    "free_up_room_non_furnished" => Rent,
+    "sublet_flat_non_furnished" => Rent,
+    "sublet_room_non_furnished" => Rent,
+    "quiero_aprender_conocimientos" => Request,
+    "online_apprender" => Request,
+    "presencial_apprender" => Request,
+    "quiero_ensenar_conocimientos" => Service,
+    "online_ensenar" => Service,
+    "presencial_ensenar" => Service,
+    "request_quiver" => Request,
+    "offer_quiver" => Rent,
+    "sell_tickets" => Sell,
+    "buy_tickets" => Request,
+    "request_cf" => Request,
+    "offer_cf" => Service,
+    "sell_leasing" => Sell,
+    "buy_leasing" => Request,
+    "ar_give_away" => Give,
+    "bb_offer" => Sell,
+    "ss_offer_entreegratuite" => Service,
+    "ss_offer_entreepayante" => Sell,
+    "ss_offer_location" => Rent,
+    "ss_offer_miseadispositiongratuite" => Service,
+    "ss_request_entreegratuite" => Request,
+    "ss_request_entreepayante" => Request,
+    "ss_request_location" => Request,
+    "ss_request_miseadispositiongratuite" => Request,
+    "offer_parking_spots" => Rent,
+    "request_parking_spots" => Request,
+    "dvotd_sell" => Sell,
+  }
+
 end
