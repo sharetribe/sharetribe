800338fd8d0de73c567dafa9278e347582e930b3
Mikko Koski <mikkokos@gmail.com>
2014-02-13 17:47:02 +0200

    Remove all categories selected

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -11,10 +11,6 @@ module HomepageHelper
     end
   end
 
-  def all_categories_selected(category_param)
-    category_param.nil? || category_param == "all"
-  end
-
   def with_first_listing_image(listing, &block)
     if listing.listing_images.size > 0 && !listing.listing_images.first.image_processing
       block.call(listing.listing_images.first)
