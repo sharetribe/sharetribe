6829985c7c8197f09b970a8b675b6d765a12c3b8
Mikko Koski <mikkokos@gmail.com>
2014-02-14 15:46:54 +0200

    Fix Settings controller
    - Community membership missing

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -2,51 +2,50 @@ require 'spec_helper'
 
 describe SettingsController do
 
+  before(:each) do
+    @community = FactoryGirl.create(:community)
+    @request.host = "#{@community.domain}.lvh.me"
+    @person = FactoryGirl.create(:person)
+
+    FactoryGirl.create(:community_membership, :person => @person, :community => @community)
+  end
+
   describe "#unsubscribe" do
     
     it "should unsubscribe the user from the email specified in parameters" do
-      pending("fix this")
-      @request.host = "test.lvh.me"
-      p1 = FactoryGirl.create(:person)
-      sign_in_for_spec(p1)
-      
-      p1.set_default_preferences     
-      
-      p1.min_days_between_community_updates.should == 1
-      get :unsubscribe, {:email_type => "community_updates", :person_id => p1.id}
+      sign_in_for_spec(@person)
+      @person.set_default_preferences
+      @person.min_days_between_community_updates.should == 1
+
+      get :unsubscribe, {:email_type => "community_updates", :person_id => @person.id}
       puts response.body
       response.status.should == 200
-      p1.min_days_between_community_updates.should == 100000
-      #response.body.should include("Unsubscribe succesful")
-      
+      @person.min_days_between_community_updates.should == 100000
     end
     
     it "should unsubscribe even if auth token is expired" do
-      @request.host = "test.lvh.me"
-      p1 = FactoryGirl.create(:person)
-      t = p1.new_email_auth_token
+      t = @person.new_email_auth_token
       AuthToken.find_by_token(t).update_attribute(:expires_at, 2.days.ago)
-      p1.set_default_preferences
-      p1.min_days_between_community_updates.should == 1
-      get :unsubscribe, {:email_type => "community_updates", :person_id => p1.id, :auth => t}
+      @person.set_default_preferences
+      @person.min_days_between_community_updates.should == 1
+      
+      get :unsubscribe, {:email_type => "community_updates", :person_id => @person.id, :auth => t}
       response.status.should == 302 #redirection to url withouth token in query string
       session[:expired_auth_token].should == t
-      get :unsubscribe, {:email_type => "community_updates", :person_id => p1.id}, 
+      get :unsubscribe, {:email_type => "community_updates", :person_id => @person.id},
                         {:expired_auth_token => t}
       response.status.should == 200
-      p1 = Person.find(p1.id) # fetch again to refresh
-      p1.min_days_between_community_updates.should == 100000
+      @person = Person.find(@person.id) # fetch again to refresh
+      @person.min_days_between_community_updates.should == 100000
     end
     
     it "should not unsubscribe if no token provided" do
-      @request.host = "test.lvh.me"
-      p1 = FactoryGirl.create(:person)
-      p1.set_default_preferences
-      p1.min_days_between_community_updates.should == 1
-      get :unsubscribe, {:email_type => "community_updates", :person_id => p1.id}
-      response.status.should == 401
-      p1.min_days_between_community_updates.should == 1
+      @person.set_default_preferences
+      @person.min_days_between_community_updates.should == 1
       
+      get :unsubscribe, {:email_type => "community_updates", :person_id => @person.id}
+      response.status.should == 401
+      @person.min_days_between_community_updates.should == 1
     end
   end
 end
