87aafd1b47db20a9e481b422baa38b72970a081b
Mikko Koski <mikkokos@gmail.com>
2014-02-17 14:45:22 +0200

    Maybe fix classifications
    - Probably won't work... :/

--- app/controllers/api/communities_controller.rb
+++ app/controllers/api/communities_controller.rb
@@ -9,36 +9,19 @@ class Api::CommunitiesController < Api::ApiController
   def classifications    
     @classifications = {}
     
-    @community.community_categories.each do |cc|
-      if cc.price_quantity_placeholder 
-        price_quantity_placeholder  = t("listings.form.price.#{cc.price_quantity_placeholder}")
-      else
-        price_quantity_placeholder = nil
+    @community.categories.each do |category|
+      unless @classifications[category.id]
+        @classifications[category.id] = 
+            {:translated_name => category.display_name(I18n.locale)}
       end
-      
-      if cc.share_type
-        unless @classifications[cc.share_type.name]
-          @classifications[cc.share_type.name] = 
-              {:translated_name => cc.share_type.display_name, 
-                :description => cc.share_type.description,
-                :price => cc.price,
-                :price_quantity_placeholder => price_quantity_placeholder,
-                :payment => cc.payment
-              }
-        end
-      end
-      if cc.category
-        unless @classifications[cc.category.name]
-          
+    end
 
-            
-          @classifications[cc.category.name] = 
-              {:translated_name => cc.category.display_name, 
-                :description => cc.category.description,
-                :price => cc.price,
-                :price_quantity_placeholder => price_quantity_placeholder,
-                :payment => cc.payment}
-        end
+    @community.transaction_types.each do |transaction_type|
+      unless @classifications[transaction_type.id]
+        @classifications[transaction_type.id] = 
+            {:translated_name => transaction_type.display_name(I18n.locale),
+              :price => transaction_type.price_field
+            }
       end
     end
      
