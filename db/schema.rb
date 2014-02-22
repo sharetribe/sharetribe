d36db331ccc5b263d65af340c3c17b3a4df88870
Antti Virolainen <antti@sharetribe.com>
2014-02-22 12:24:42 +0200

    Migration to remove Service trans type from places it doesn't belong

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140219162023) do
+ActiveRecord::Schema.define(:version => 20140222080916) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
