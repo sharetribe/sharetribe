55a2623004b6fed594040690b7778eba40b9c47c
Mikko Koski <mikkokos@gmail.com>
2014-02-26 13:53:11 +0200

    Fix broken spec

--- spec/models/dropdown_value_spec.rb
+++ spec/models/dropdown_value_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
 
-describe CustomFieldValue do
+describe DropdownValue do
   describe "validations" do
     it "should have 1 selected options" do
       # Hard-coded 1 for dropdown
-      @value = CustomFieldValue.new
+      @value = DropdownValue.new
       @value.should_not be_valid
 
-      @value1 = CustomFieldValue.new
+      @value1 = DropdownValue.new
       @value1.custom_field_option_selections << CustomFieldOptionSelection.new
       @value1.should be_valid
 
-      @value2 = CustomFieldValue.new
+      @value2 = DropdownValue.new
       @value2.custom_field_option_selections << CustomFieldOptionSelection.new
       @value2.custom_field_option_selections << CustomFieldOptionSelection.new
       @value2.should_not be_valid
-
-      @value3 = CustomFieldValue.new
-      @value3.text_value = "Test" 
-      @value3.should be_valid
     end
   end
 end
