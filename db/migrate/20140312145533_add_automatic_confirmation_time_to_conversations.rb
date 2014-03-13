bd92e413e413b755d6e191e342eefcd2b230014d
Mikko Koski <mikkokos@gmail.com>
2014-03-13 11:01:45 +0200

    Add automatic confirmation days after

--- db/migrate/20140312145533_add_automatic_confirmation_time_to_conversations.rb
+++
@@ -1,5 +0,0 @@
-class AddAutomaticConfirmationTimeToConversations < ActiveRecord::Migration
-  def change
-    add_column :conversations, :automatic_confirmation_time, :datetime, :after => :last_message_at
-  end
-end
