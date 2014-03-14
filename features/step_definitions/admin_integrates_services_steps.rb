277c291f3304387d5d32d13fe4a840f658e64332
Sampo Toiva <sampo.toiva@gmail.com>
2014-03-14 17:18:14 +0200

    First pass on twitter-handle.

--- features/step_definitions/admin_integrates_services_steps.rb
+++ features/step_definitions/admin_integrates_services_steps.rb
@@ -1,7 +1,11 @@
 When(/^I add "(.*?)" to Twitter handle field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  steps %Q{
+    When I fill in "Twitter handle" with "#{arg1}"
+  }
 end
 
 Then(/^I should see "(.*?)" in the Twitter handle field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+   steps %Q{
+    Then I should see "#{arg1}" within "#community_twitter_handle"
+  }
 end
