ebc783a4241d7bb09819f9550a21116f873f9dd6
Mikko Koski <mikkokos@gmail.com>
2014-03-18 15:28:03 +0200

    Word wrap long line

--- app/models/community_customization.rb
+++ app/models/community_customization.rb
@@ -1,6 +1,20 @@
 class CommunityCustomization < ActiveRecord::Base
   
-  attr_accessible :community_id, :description, :locale, :name, :slogan, :blank_slate, :welcome_email_content, :about_page_content, :how_to_use_page_content, :terms_page_content, :privacy_page_content, :storefront_label, :signup_info_content, :private_community_homepage_content, :verification_to_post_listings_info_content
+  attr_accessible :community_id,
+    :description,
+    :locale,
+    :name,
+    :slogan,
+    :blank_slate,
+    :welcome_email_content,
+    :about_page_content,
+    :how_to_use_page_content,
+    :terms_page_content,
+    :privacy_page_content,
+    :storefront_label,
+    :signup_info_content,
+    :private_community_homepage_content,
+    :verification_to_post_listings_info_content
   
   has_one :community
   
