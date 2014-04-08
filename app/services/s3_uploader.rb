2fe8759f0c3bc8bee0fa0a07d5d971ab7fc254fb
Sampo Toiva <sampo.toiva@gmail.com>
2014-04-08 22:11:00 +0300

    Remove deprecated Digest::Digest call.

--- app/services/s3_uploader.rb
+++ app/services/s3_uploader.rb
@@ -62,7 +62,7 @@ class S3Uploader
   def signature
     Base64.encode64(
       OpenSSL::HMAC.digest(
-        OpenSSL::Digest::Digest.new('sha1'),
+        OpenSSL::Digest.new('sha1'),
         @aws_secret_access_key, policy
       )
     ).gsub("\n", "")
