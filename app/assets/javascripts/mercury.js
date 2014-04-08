c2ff0ea3d4bef8dcb297385897a8ba3e6a3911e7
Sampo Toiva <sampo.toiva@gmail.com>
2014-04-08 22:12:22 +0300

    Update mercury config saveURL from save_url to save-url.
    New version of haml changes underscores in attributes to hyphens by
    default.

--- app/assets/javascripts/mercury.js
+++ app/assets/javascripts/mercury.js
@@ -442,7 +442,7 @@ window.Mercury = {
 
 $(window).on('mercury:ready', function() {
   var link = $('#mercury_iframe').contents().find('#edit_link');
-  Mercury.saveUrl = link.data('save_url');
+  Mercury.saveUrl = link.data('save-url');
   link.hide();
 });
 
