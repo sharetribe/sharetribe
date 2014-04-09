e7df77300e2c790f5b5e0d083fbb5762ef223f92
Mikko Koski <mikkokos@gmail.com>
2014-04-09 11:14:07 +0300

    Use new cropped big style

--- app/models/listing_image.rb
+++ app/models/listing_image.rb
@@ -8,10 +8,10 @@ class ListingImage < ActiveRecord::Base
         :medium => "360x270#",
         :thumb => "120x120#",
         :original => "#{APP_CONFIG.original_image_width}x#{APP_CONFIG.original_image_height}>",
-        :big => "800x800>",
+        :big => Proc.new { |instance| instance.crop_big },
         :email => "150x100#"}
 
-  before_save :extract_dimensions
+  before_save :set_dimensions!
 
   process_in_background :image, :processing_image_url => "/assets/listing_image/processing.png"
   validates_attachment_size :image, :less_than => APP_CONFIG.max_image_filesize.to_i, :unless => Proc.new {|model| model.image.nil? }
@@ -19,16 +19,32 @@ class ListingImage < ActiveRecord::Base
                                     :content_type => ["image/jpeg", "image/png", "image/gif", "image/pjpeg", "image/x-png"], # the two last types are sent by IE.
                                     :unless => Proc.new {|model| model.image.nil? }
 
+
+  def set_dimensions!
+    # Silently return, if there's no `width` and `height`
+    # Prevents old migrations from crashing
+    return unless self.respond_to?(:width) && self.respond_to?(:height)
+
+    geometry = extract_dimensions
+
+    if geometry
+      self.width = geometry.width.to_i
+      self.height = geometry.height.to_i
+    end
+  end
+
+  def crop_big
+    geometry = Paperclip::Geometry.from_file(Paperclip.io_adapters.for(image.queued_for_write[:original]))
+    max_landscape_crop_percentage = 0.2
+    ListingImage.construct_big_style({:width => geometry.width.round, :height => geometry.height.round}, max_landscape_crop_percentage)
+  end
+
   # Retrieves dimensions for image assets
   # @note Do this after resize operations to account for auto-orientation.
   # https://github.com/thoughtbot/paperclip/wiki/Extracting-image-dimensions
   def extract_dimensions
-    return unless image_downloaded?
+    return unless image_ready?
     tempfile = image.queued_for_write[:original]
-
-    # Silently return, if there's no `width` and `height`
-    # Prevents old migrations from crashing
-    return unless self.respond_to?(:width) && self.respond_to?(:height)
 
     # Works with uploaded files and existing files
     path_or_url = if !tempfile.nil? then
@@ -43,8 +59,6 @@ class ListingImage < ActiveRecord::Base
     end
 
     geometry = Paperclip::Geometry.from_file(path_or_url)
-    self.width = geometry.width.to_i
-    self.height = geometry.height.to_i
   end
 
   def authorized?(user)
@@ -82,5 +96,70 @@ class ListingImage < ActiveRecord::Base
 
   def image_ready?
     image_downloaded && !image_processing
+  end
+
+  def self.crop_need(x, desired_x)
+    x - desired_x
+  end
+
+  def self.crop_need_percentage(x, desired_x)
+    self.crop_need(x, desired_x).to_f / x.to_f
+  end
+
+  def self.should_crop?(dimensions, desired_dimensions, max_crop_difference)
+    scaled = self.scale_to_cover(dimensions, desired_dimensions)
+
+    width_crop_need = crop_need_percentage(scaled[:width], desired_dimensions[:width])
+    height_crop_need = crop_need_percentage(scaled[:height], desired_dimensions[:height])
+
+    width_crop_need <= max_crop_difference && height_crop_need <= max_crop_difference
+  end
+
+  def self.scale_by(source, target, by)
+    scale_factor = source[by] / target[by].to_f
+      {
+        :width => (source[:width] / scale_factor),
+        :height => (source[:height] / scale_factor)
+      }
+  end
+
+  def self.scale_to_direction_by(source, target, direction, by)
+    scale = direction == :up ? source[by] < target[by] : source[by] > target[by]
+
+    if scale
+      scale_by(source, target, by)
+    else
+      source
+    end
+  end
+
+  def self.scale_to_direction_cover(dimensions, area_to_cover, direction)
+    scaled_width = scale_to_direction_by(dimensions, area_to_cover, direction, :width)
+    scaled_width_height = scale_to_direction_by(scaled_width, area_to_cover, direction, :height)
+    return scaled_width_height
+  end
+
+  def self.scale_direction(dimensions, area_to_cover)
+    if dimensions[:width] > area_to_cover[:width] && dimensions[:height] > area_to_cover[:height]
+      :down
+    else
+      :up
+    end
+  end
+
+  def self.scale_to_cover(dimensions, area_to_cover)
+    direction = self.scale_direction(dimensions, area_to_cover)
+    self.scale_to_direction_cover(dimensions, area_to_cover, direction)
+  end
+
+  def self.construct_big_style(dimensions, desired_dimensions, max_crop_difference)
+    default_style = "#{desired_dimensions[:width]}x#{desired_dimensions[:height]}>"
+    crop_style = "#{desired_dimensions[:width]}x#{desired_dimensions[:height]}#"
+
+    if self.should_crop?(dimensions, desired_dimensions, max_crop_difference)
+      crop_style
+    else
+      default_style
+    end
   end
 end
