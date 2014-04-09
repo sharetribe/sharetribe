ca572880199728abc5560e871d8ae08ec5248b11
Mikko Koski <mikkokos@gmail.com>
2014-04-09 17:41:12 +0300

    Remove migration hacks

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,6 +2,3 @@ require 'delayed_job'
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 3.minutes # In order to recover from hanging DelayedDelta. Currently no jobs should be longer than 3min.
-
-# Require custom delayed jobs that are accessed from migrations
-require "#{Rails.root}/app/jobs/reprocess_listing_image_job"
--- db/migrate/20140402070714_reprocess_listing_images.rb
+++ db/migrate/20140402070714_reprocess_listing_images.rb
@@ -2,8 +2,6 @@ class ReprocessListingImages < ActiveRecord::Migration
   say "This migration will reprocess all the images from #{Listing.count} listings"
 
   def up
-    require "#{Rails.root}/app/jobs/reprocess_listing_image_job"
-
     ListingImage.order("id DESC").each do |listing_image|
       if listing_image.image_ready?
         Delayed::Job.enqueue(ReprocessListingImageJob.new(listing_image.id, :big), priority: 10)
