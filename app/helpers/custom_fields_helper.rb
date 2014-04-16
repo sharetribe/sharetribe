b9ccb172da8322b2f132aba13a625de5d1b8740e
Mikko Koski <mikkokos@gmail.com>
2014-04-16 10:47:09 +0300

    Translations

--- app/helpers/custom_fields_helper.rb
+++ app/helpers/custom_fields_helper.rb
@@ -1,7 +1,18 @@
 module CustomFieldsHelper
 
+  def field_type_translation(type)
+    tranlation_map = {
+      "DropdownField" => "admin.custom_fields.field_types.dropdown",
+      "TextField" => "admin.custom_fields.field_types.text",
+      "NumericField" => "admin.custom_fields.field_types.number",
+      "CheckboxField" => "admin.custom_fields.field_types.checkbox_group"
+    }
+
+    t(tranlation_map[type])
+  end
+
   def custom_field_dropdown_options(options)
-    options.collect { |option| [t("admin.custom_fields.field_types.#{option.downcase}"), option] }.insert(0, [t("admin.custom_fields.index.select_one"), nil])
+    options.collect { |option| [field_type_translation(option), option] }.insert(0, [t("admin.custom_fields.index.select_one"), nil])
   end
 
 end
