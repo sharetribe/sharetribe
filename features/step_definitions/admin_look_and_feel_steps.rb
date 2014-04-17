1e45b459ab8224fa35197aa8251a17d830faf562
Sampo Toiva <sampo.toiva@gmail.com>
2014-04-17 15:04:32 +0300

    Merge admin look-and-feel -steps to one file.

--- features/step_definitions/admin_look_and_feel_steps.rb
+++ features/step_definitions/admin_look_and_feel_steps.rb
@@ -18,3 +18,33 @@ Then(/^I should see that the background color of Post a new listing button is "(
     Then "#new-listing-link" should have CSS property "background-color" with value "#{expected_color}"
   }
 end
+
+Given(/^community "(.*?)" has default browse view "(.*?)"$/) do |community, browse_view|
+  Community.find_by_domain(community).update_attributes(default_browse_view: browse_view)
+end
+
+When(/^I change the default browse view to "(.*?)"$/) do |browse_view|
+  steps %Q{
+    When I select "#{browse_view}" from "community_default_browse_view"
+    And I press submit
+  }
+end
+
+Then(/^I should see the browse view selected as "(.*?)"$/) do |browse_view|
+  find(".home-toolbar-button-group .selected").should have_content(browse_view)
+end
+
+Given(/^community "(.*?)" has name display type "(.*?)"$/) do |community, name_display_type|
+  Community.find_by_domain(community).update_attributes(name_display_type: name_display_type)
+end
+
+When(/^I change the name display type to "(.*?)"$/) do |name_display_type|
+  steps %Q{
+    When I select "#{name_display_type}" from "community_name_display_type"
+    And I press submit
+  }
+end
+
+Then(/^I should see my name displayed as "(.*?)"$/) do |name|
+  find(".user-name").should have_content(name)
+end
