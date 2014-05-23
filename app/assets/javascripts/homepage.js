8459b9cf556e8e0572249d059b59e2bd05e3795f
Mikko Koski <mikkokos@gmail.com>
2014-05-23 16:33:31 +0300

    Show all listing field

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -13,7 +13,9 @@ $(function() {
   });
 
   // Relocate filters
-  relocate(768, $("#filters"), $("#desktop-filters").get(0));
+  if ($("#filters").length && $("#desktop-filters").length) {
+    relocate(768, $("#filters"), $("#desktop-filters").get(0));
+  }
 
   relocate(768, $("#header-menu-mobile-anchor"), $("#header-menu-desktop-anchor").get(0));
   relocate(768, $("#header-user-mobile-anchor"), $("#header-user-desktop-anchor").get(0));
