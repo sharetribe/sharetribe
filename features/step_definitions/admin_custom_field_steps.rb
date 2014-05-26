e0ca51b00f213bf3f035d981359a49631f415872
Mikko Koski <mikkokos@gmail.com>
2014-05-26 10:56:07 +0300

    No custom fields step fixes to match current impl

--- features/step_definitions/admin_custom_field_steps.rb
+++ features/step_definitions/admin_custom_field_steps.rb
@@ -45,9 +45,8 @@ Then /^I should see that there is no custom field "(.*?)"$/ do |field_name|
 end
 
 Then /^I should see that I do not have any custom fields$/ do
-  steps %Q{
-    Then I should see "No marketplace specific listing fields"
-  }
+  predefined_fields = ["Price"]
+  (all(".custom-field-title").collect(&:text) - predefined_fields).should be_empty
 end
 
 When /^I remove custom field "(.*?)"$/ do |title|
