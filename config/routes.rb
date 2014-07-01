4053fd9965ea52cf364152cf287555cc3b9744a3
Ville Sundberg <sundberg.ville@gmail.com>
2014-07-01 16:38:23 +0300

    Follow button, FollowersController

--- config/routes.rb
+++ config/routes.rb
@@ -96,7 +96,6 @@ Kassi::Application.routes.draw do
             end
           end
           resources :braintree_payments
-
         end
         resource :settings do
           member do
@@ -115,6 +114,7 @@ Kassi::Application.routes.draw do
             post :send_confirmation
           end
         end
+        resources :followers
       end
 
       # List few specific routes here for Devise to understand those
