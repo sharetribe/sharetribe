da6012a2e8368c800f12591363b3125e6c87b6b9
Ville Sundberg <sundberg.ville@gmail.com>
2014-07-01 17:15:58 +0300

    NotifyFollowersJob

---
+++ app/jobs/notify_followers_job.rb
@@ -0,0 +1,34 @@
+class NotifyFollowersJob < Struct.new(:listing_id, :community_id)
+  
+  include DelayedAirbrakeNotification
+
+  # This before hook should be included in all Jobs to make sure that the service_name is
+  # correct as it's stored in the thread and the same thread handles many different communities
+  # if the job doesn't have community_id parameter, should call the method with nil, to set the default service_name
+  def before(job)
+    # Set the correct service name to thread for I18n to pick it
+    ApplicationHelper.store_community_service_name_to_thread_from_community_id(community_id)
+  end
+  
+  def perform
+    return if !listing || listing.closed? || !author
+    author.followers.members_of(community).each do |follower|
+      # notify
+    end
+  end
+  
+  private
+  
+  def listing
+    @listing ||= Listing.find_by_id(listing_id)
+  end
+  
+  def author
+    listing.author
+  end
+  
+  def community
+    listing.community
+  end
+
+end
