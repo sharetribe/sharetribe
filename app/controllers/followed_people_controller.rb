913e5b90f45a3165f893c1a2f316c5d6369c66b9
Ville Sundberg <sundberg.ville@gmail.com>
2014-07-08 14:46:15 +0300

    No longer require users to be logged in to view followed people

--- app/controllers/followed_people_controller.rb
+++ app/controllers/followed_people_controller.rb
@@ -2,10 +2,6 @@ class FollowedPeopleController < ApplicationController
   
   skip_before_filter :dashboard_only
   
-  before_filter do |controller|
-    controller.ensure_logged_in t("layouts.notifications.you_must_log_in_to_view_this_content")
-  end
-  
   def index
     @person = Person.find_by_id(params[:person_id])
     @followed_people = @person.followed_people
