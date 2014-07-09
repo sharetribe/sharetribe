cd556fcb69e72133b87f8d978a5e2bc3717295c3
Ville Sundberg <sundberg.ville@gmail.com>
2014-07-09 11:52:04 +0300

    Delete legacy actions wdc, okl, faq, api, pricing from DashbordController

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,35 +12,6 @@ class DashboardController < ApplicationController
     @contact_request = session[:contact_request_sent] ? ContactRequest.find(session[:contact_request_sent]) : ContactRequest.new
   end
 
-  # A custom action for World Design Capital
-  # Helsinki 2012 special page
-  def wdc
-    redirect_to root and return
-    I18n.locale = "fi"
-    @communities = Community.where(:label => "wdc").order("name")
-  end
-
-  # A custom action for World Design Capital
-  # Helsinki 2012 special page
-  def okl
-    redirect_to root and return
-    I18n.locale = "fi"
-    @communities = Community.where(:label => "okl").order("name")
-  end
-
-  def faq
-    redirect_to root and return
-  end
-
-  def api
-    redirect_to root and return
-  end
-
-  def pricing
-    redirect_to root and return
-    redirect_to :faq
-  end
-
   # This is for all the custom "campaign" sites
   def campaign
     redirect_to root and return
