491ffc81800c4df8d1805c9617c6e12e34feac95
Ville Sundberg <sundberg.ville@gmail.com>
2014-07-10 15:28:56 +0300

    Rely on person_belongs_to_current_community in controllers related to following

--- app/controllers/followed_people_controller.rb
+++ app/controllers/followed_people_controller.rb
@@ -2,8 +2,9 @@ class FollowedPeopleController < ApplicationController
   
   skip_before_filter :dashboard_only
   
+  before_filter :person_belongs_to_current_community
+  
   def index
-    @person = Person.find_by_id(params[:person_id])
     @followed_people = @person.followed_people
     respond_to do |format|
       format.js { render :partial => "people/followed_person", :collection => @followed_people, :as => :person }
