48abef599ef83a8c5e0da267185c96db80637831
Sampo Toiva <sampo.toiva@gmail.com>
2014-08-06 16:33:51 +0300

    Sorting for created at and last activity.

--- app/controllers/admin/community_transactions_controller.rb
+++ app/controllers/admin/community_transactions_controller.rb
@@ -3,8 +3,37 @@ class Admin::CommunityTransactionsController < ApplicationController
   skip_filter :dashboard_only
 
   def index
-      @community = @current_community
-      @conversations = Conversation.where(:community_id => @current_community.id)
-                                   .paginate(:page => params[:page], :per_page => 50)
+    @community = @current_community
+    @conversations = ListingConversation.where(:community_id => @current_community.id)
+      .includes(:listing)
+      .paginate(:page => params[:page], :per_page => 50)
+      .order("#{sort_column} #{sort_direction}")
+  end
+
+  private
+
+  def sort_column
+    case params[:sort]
+    # when "listing"
+    #   "listings.title"
+    # when "status"
+    #   "listings.current_state"
+    # when "sum"
+    #   "listing.payment.total_sum"
+    when "started"
+      "created_at"
+    when "last_activity"
+      "last_message_at"
+    # when "initiated_by"
+    #   "participations.starter."
+    # when "other_party"
+    #   "participations.starter."
+    else
+      "last_message_at"
+    end
+  end
+
+  def sort_direction
+    params[:direction] || "desc"
   end
 end
