c0beb10e9da05b9e3f9b5d4accbd7b700e44ec6c
Mikko Koski <mikkokos@gmail.com>
2014-08-07 14:19:21 +0300

    Better tests for preauthorize expiration

--- spec/models/listing_conversation_spec.rb
+++ spec/models/listing_conversation_spec.rb
@@ -13,21 +13,25 @@ describe ListingConversation do
 
   describe "#preauthorization_expire_at" do
 
+    def days(num)
+      num.days.from_now.to_date
+    end
+
     it "expires after 5 days" do
       puts @listing_conversation.preauthorization_expire_at
       puts 5.days.from_now
-      @listing_conversation.preauthorization_expire_at.should be_eql 5.days.from_now
+      @listing_conversation.preauthorization_expire_at.should be_eql days(5)
     end
 
     it "expires after 5 days, if booking ends after 5 days" do
-      @listing_conversation.booking = FactoryGirl.build(:booking, start_on: 10.days.from_now, end_on: 12.days.from_now)
-      @listing_conversation.preauthorization_expire_at.should be_eql 5.days.from_now
+      @listing_conversation.booking = FactoryGirl.build(:booking, start_on: days(10), end_on: days(12))
+      @listing_conversation.preauthorization_expire_at.should be_eql days(5)
     end
 
     it "expires when the booking ends, if booking ends before 5" do
-      ends = 3.days.from_now
+      ends = days(3)
 
-      @listing_conversation.booking = FactoryGirl.build(:booking, start_on: 1.days.from_now, end_on: ends)
+      @listing_conversation.booking = FactoryGirl.build(:booking, start_on: days(1), end_on: ends.to_date)
       @listing_conversation.preauthorization_expire_at.should be_eql ends
     end
   end
