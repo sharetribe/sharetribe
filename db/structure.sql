b4fbcb75712efa813d19860b97672bd51d763fc9
Mikko Koski <mikkokos@gmail.com>
2014-08-13 16:57:00 +0300

    Remove duplicated migration

--- db/structure.sql
+++ db/structure.sql
@@ -1828,8 +1828,6 @@ INSERT INTO schema_migrations (version) VALUES ('20140724084559');
 
 INSERT INTO schema_migrations (version) VALUES ('20140724093459');
 
-INSERT INTO schema_migrations (version) VALUES ('20140724123125');
-
 INSERT INTO schema_migrations (version) VALUES ('20140805102757');
 
 INSERT INTO schema_migrations (version) VALUES ('20140811133602');
