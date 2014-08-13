3bcbd5c472e75a6592a7ff1cb1c9290d459db722
Mikko Koski <mikkokos@gmail.com>
2014-08-13 15:50:30 +0300

    Use forked version of bootstrap datepicker

--- vendor/assets/javascripts/datepicker/bootstrap-datepicker.js
+++ vendor/assets/javascripts/datepicker/bootstrap-datepicker.js
@@ -1,3 +1,15 @@
+/**
+  NOTICE!
+
+  This is a forked version of Bootstrap datepicker. The standard version of the datepicker
+  has a bug which allows use to select end date before start date.
+
+  There is an outstanding Pull Request which fixes the issue but it's not merged. This forked
+  version contains the fix.
+
+  Source: https://github.com/sharetribe/bootstrap-datepicker
+*/
+
 /* =========================================================
  * bootstrap-datepicker.js
  * Repo: https://github.com/eternicode/bootstrap-datepicker/
@@ -1284,6 +1296,8 @@
 			var dp = $(e.target).data('datepicker'),
 				new_date = dp.getUTCDate(),
 				i = $.inArray(e.target, this.inputs),
+				j = i - 1,
+				k = i + 1,
 				l = this.inputs.length;
 			if (i === -1)
 				return;
@@ -1293,16 +1307,16 @@
 					p.setUTCDate(new_date);
 			});
 
-			if (new_date < this.dates[i]){
+			if (new_date < this.dates[j]){
 				// Date being moved earlier/left
-				while (i >= 0 && new_date < this.dates[i]){
-					this.pickers[i--].setUTCDate(new_date);
+				while (j >= 0 && new_date < this.dates[j]){
+					this.pickers[j--].setUTCDate(new_date);
 				}
 			}
-			else if (new_date > this.dates[i]){
+			else if (new_date > this.dates[k]){
 				// Date being moved later/right
-				while (i < l && new_date > this.dates[i]){
-					this.pickers[i++].setUTCDate(new_date);
+				while (k < l && new_date > this.dates[k]){
+					this.pickers[k++].setUTCDate(new_date);
 				}
 			}
 			this.updateDates();
