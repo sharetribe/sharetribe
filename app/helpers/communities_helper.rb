1ae26727d8f1354aebf1fc547750dee81b137e45
Vesa Luusua <vesa.luusua@gmail.com>
2014-08-19 20:01:38 +0300

    work-in-progress: second draft of admin ui for transaction agreement

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -17,20 +17,68 @@ module CommunitiesHelper
   end
 
 
-  def transaction_agreement_label_locals
+
+  #FIXME
+  def community_name_locals
+    translations = find_community_customizations(:name)
     {
-      available_locales: available_locales,
-      input_name: 'input_name',
-      input_value: I18n.t("admin.communities.edit_details.transaction_agreement_label_placeholder")
+      header: t("admin.communities.edit_details.community_name"),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_name_description"),
+      input_name: "community-name",
+      translations: translations
+    }
+  end
+
+  def community_slogan_locals
+    translations = find_community_customizations(:slogan)
+    {
+      header: t("admin.communities.edit_details.community_slogan"),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_slogan_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"))),
+      input_name: "community-slogan",
+      translations: translations
+    }
+  end
+
+  def community_description_locals
+    translations = find_community_customizations(:description)
+    {
+      header: t("admin.communities.edit_details.community_description"),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_description_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"))),
+      input_name: "community-description",
+      translations: translations
+    }
+  end
+
+  def transaction_agreement_label_locals
+    //FIXME
+    translations = {en: {language: "English", translation: ""}, fi: {language: "Suomi", translation: ""}}#find_community_customizations(:agreement_label)
+    {
+      header: "",
+      info_text: I18n.t("admin.communities.edit_details.transaction_agreement_checkbox_label_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/")),
+      input_name: "ransaction-agreement-label",
+      translations: translations
     }
   end
 
   def transaction_agreement_text_locals
+    //FIXME
+    translations = {en: {language: "English", translation: ""}, fi: {language: "Suomi", translation: ""}}#find_community_customizations(:agreement_content)
     {
-      available_locales: available_locales,
-      input_name: 'input_name',
-      input_value: I18n.t("admin.communities.edit_details.transaction_agreement_text_placeholder")
+      info_text: t("admin.communities.edit_details.transaction_agreement_tos_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/")),
+      input_name: "ransaction_agreement_text",
+      translations: translations
     }
   end
+
+  def find_community_customizations(customization_key)
+    translations = {}
+
+    available_locales.each do |locale_name, locale_value|
+      translation = @current_community.community_customizations.find_by_locale(locale_value)[customization_key] || ""
+      translations[locale_value] = {language: locale_name, translation: translation};
+    end
+    translations
+  end
+  #/FIXME
 
 end
