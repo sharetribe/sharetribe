15d432c9f69bb127f21410f3fa91e7595e2cd646
Mikko Koski <mikkokos@gmail.com>
2014-08-20 11:26:11 +0300

    Remove searcbox hardcoding

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -16,9 +16,6 @@ module CommunitiesHelper
     @current_user.emails.select{|e| e.confirmed_at.present?}.include?(session[:email])
   end
 
-
-
-  #FIXME
   def community_name_locals
     translations = find_community_customizations(:name)
     {
@@ -33,7 +30,7 @@ module CommunitiesHelper
     translations = find_community_customizations(:slogan)
     {
       header: t("admin.communities.edit_details.community_slogan"),
-      info_text: I18n.t("admin.communities.edit_details.edit_community_slogan_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"))),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_slogan_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/?big_cover_photo=true", id: "view_slogan_link")),
       input_name: "slogan",
       translations: translations
     }
@@ -43,42 +40,47 @@ module CommunitiesHelper
     translations = find_community_customizations(:description)
     {
       header: t("admin.communities.edit_details.community_description"),
-      info_text: I18n.t("admin.communities.edit_details.edit_community_description_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"))),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_description_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/?big_cover_photo=true")),
       input_name: "description",
       translations: translations
     }
   end
 
+  def community_search_placeholder_locals
+    translations = find_community_customizations(:search_placeholder)
+    {
+      header: t("admin.communities.edit_details.community_search_placeholder"),
+      info_text: I18n.t("admin.communities.edit_details.edit_community_search_placeholder_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/")),
+      input_name: "search_placeholder",
+      translations: translations
+    }
+  end
+
   def transaction_agreement_label_locals
-    #FIXME
-    translations = {en: {language: "English", translation: ""}, fi: {language: "Suomi", translation: ""}}#find_community_customizations(:agreement_label)
+    translations = find_community_customizations(:transaction_agreement_label)
     {
       header: "",
-      info_text: I18n.t("admin.communities.edit_details.transaction_agreement_checkbox_label_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/")),
+      info_text: I18n.t("admin.communities.edit_details.transaction_agreement_checkbox_label_description"),
       input_name: "transaction_agreement_label",
       translations: translations
     }
   end
 
   def transaction_agreement_text_locals
-    #FIXME
-    translations = {en: {language: "English", translation: ""}, fi: {language: "Suomi", translation: ""}}#find_community_customizations(:agreement_content)
+    translations = find_community_customizations(:transaction_agreement_content)
     {
-      info_text: t("admin.communities.edit_details.transaction_agreement_tos_description", :see_how_it_looks_like => link_to(t("admin.communities.edit_details.see_how_it_looks_like"), "/")),
+      info_text: t("admin.communities.edit_details.transaction_agreement_tos_description"),
       input_name: "transaction_agreement_content",
       translations: translations
     }
   end
 
   def find_community_customizations(customization_key)
-    translations = {}
-
-    available_locales.each do |locale_name, locale_value|
-      translation = @current_community.community_customizations.find_by_locale(locale_value)[customization_key] || ""
+    available_locales.inject({}) do |translations, (locale_name, locale_value)|
+      translation = @community_customizations[locale_value][customization_key] || ""
       translations[locale_value] = {language: locale_name, translation: translation};
+      translations
     end
-    translations
   end
-  #/FIXME
 
 end
