b682452a1526521cc1bb817099188d71550ab565
Mikko Koski <mikkokos@gmail.com>
2014-08-22 11:10:49 +0300

    Don't inherit ApplicationController

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,11 +1,24 @@
-class ErrorsController < ApplicationController
-  skip_filter :dashboard_only
+class ErrorsController < ActionController::Base
 
   def server_error
-    exception = env["action_dispatch.exception"]
-    env["airbrake.error_id"] = Airbrake.notify(exception) if APP_CONFIG.use_airbrake
-
-    @airbrake_url = "https://airbrake.io/locate/#{env["airbrake.error_id"]}" if env["airbrake.error_id"]
+    @airbrake_url = nofity_airbrake
     render "500", layout: false, status: 500, :formats => [:html]
+  end
+
+  def exception
+    env["action_dispatch.exception"]
+  end
+
+  def nofity_airbrake
+    env["airbrake.error_id"] = Airbrake.notify(exception) if can_notify_airbrake && use_airbrake
+    "https://airbrake.io/locate/#{env["airbrake.error_id"]}" if env["airbrake.error_id"]
+  end
+
+  def can_notify_airbrake
+    Airbrake && Airbrake.respond_to?(:notify)
+  end
+
+  def use_airbrake
+    APP_CONFIG && APP_CONFIG.use_airbrake
   end
 end
