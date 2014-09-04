1c4633fc72d3892071ee98a38fd8be363457ce0d
Mikko Koski <mikkokos@gmail.com>
2014-09-04 13:46:14 +0300

    Move data from conversation to transaction

--- app/models/transaction_transition.rb
+++ app/models/transaction_transition.rb
@@ -2,18 +2,18 @@
 #
 # Table name: transaction_transitions
 #
-#  id              :integer          not null, primary key
-#  to_state        :string(255)
-#  metadata        :text
-#  sort_key        :integer          default(0)
-#  conversation_id :integer
-#  created_at      :datetime
-#  updated_at      :datetime
+#  id             :integer          not null, primary key
+#  to_state       :string(255)
+#  metadata       :text
+#  sort_key       :integer          default(0)
+#  transaction_id :integer
+#  created_at     :datetime
+#  updated_at     :datetime
 #
 # Indexes
 #
-#  index_transaction_transitions_on_conversation_id               (conversation_id)
-#  index_transaction_transitions_on_sort_key_and_conversation_id  (sort_key,conversation_id) UNIQUE
+#  index_transaction_transitions_on_conversation_id               (transaction_id)
+#  index_transaction_transitions_on_sort_key_and_conversation_id  (sort_key,transaction_id) UNIQUE
 #
 
 class TransactionTransition < ActiveRecord::Base
---
+++ db/migrate/20140904091719_move_listing_conversation_to_transactions.rb
@@ -0,0 +1,30 @@
+class MoveListingConversationToTransactions < ActiveRecord::Migration
+  def up
+    execute("
+      INSERT INTO transactions (starter_id, listing_id, conversation_id, automatic_confirmation_after_days, created_at, updated_at)
+      (SELECT
+        participations.person_id,
+        conversations.listing_id,
+        conversations.id,
+        conversations.automatic_confirmation_after_days,
+        conversations.created_at,
+        conversations.updated_at
+        FROM conversations
+        INNER JOIN participations ON (conversations.id = participations.conversation_id AND participations.is_starter = 1)
+        WHERE (conversations.type = 'ListingConversation')
+      )
+    ")
+  end
+
+  def down
+    execute("
+      UPDATE conversations
+      INNER JOIN transactions ON (conversations.id = transactions.conversation_id)
+      SET conversations.listing_id = transactions.listing_id,
+          conversations.automatic_confirmation_after_days = transactions.automatic_confirmation_after_days,
+          conversations.type = 'ListingConversation'
+    ")
+
+    execute("DELETE FROM transactions")
+  end
+end
