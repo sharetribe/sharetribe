e62fd724591a27a5c2b155a681762387717c1a07
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-09-04 09:58:58 +0300

    form helper to dynamically create form objects

--- app/services/util.rb
+++ app/services/util.rb
@@ -1,4 +1,61 @@
 module Util
+  module FormUtils
+    module_function
+
+    # Define a form class that can be used with ActiveSupport form bindings
+    #
+    # Usage:
+    #
+    # -- in some_controller.rb --
+    #
+    # MyForm = Util::FormUtils.define_form("MyForm", :name, :password)
+    #   .with_validations { validates_presence_of :name }
+    #
+    # def new
+    #   render locals: { form_obj: MyForm.new }
+    # end
+    #
+    # def create
+    #   myForm = MyForm.new(params[:my_form])
+    #   if myForm.valid?
+    #     ...
+    #
+    def define_form(form_name, *ks)
+      Class.new(Object) { |klass|
+        include ActiveModel::Validations
+        include ActiveModel::Conversion
+
+        @__keys = ks
+        @__form_name = form_name
+
+        def self.keys
+          @__keys
+        end
+
+        attr_reader(*ks)
+
+        def initialize(opts = {})
+          self.class.keys.each { |k|
+            instance_variable_set("@#{k.to_s}", opts[k]) unless opts[k].nil?
+          }
+        end
+
+        def persisted?
+          false
+        end
+
+        def self.model_name
+          ActiveModel::Name.new(self, nil, @__form_name)
+        end
+
+        def self.with_validations(&block)
+          class_exec(&block)
+          self
+        end
+      }
+    end
+  end
+
   module HashUtils
     module_function
 
