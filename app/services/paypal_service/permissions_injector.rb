6b0321fca8832b198ef72d196cb98feebeb3c2fc
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-09-09 09:44:20 +0300

    Can use service injector directly, not just mixin

--- app/services/paypal_service/permissions_injector.rb
+++ app/services/paypal_service/permissions_injector.rb
@@ -1,20 +1,25 @@
 module PaypalService
   module PermissionsInjector
     def paypal_permissions
-      @paypal_permissions ||= PaypalService::Permissions.new(
+      @paypal_permissions ||= build_paypal_permissions
+    end
+
+    module_function
+
+    def build_paypal_permissions
+      PaypalService::Permissions.new(
         build_endpoint(APP_CONFIG),
-        build_api_credentials(APP_CONFIG)
-      )
+        build_api_credentials(APP_CONFIG))
     end
 
 
     private
 
-    def build_endpoint(config)
+    def self.build_endpoint(config)
       PaypalService::DataTypes.create_endpoint(config.paypal_endpoint)
     end
 
-    def build_api_credentials(config)
+    def self.build_api_credentials(config)
       PaypalService::DataTypes.create_api_credentials(
         config.paypal_username,
         config.paypal_password,
