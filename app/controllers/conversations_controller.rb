bc4b519f8a116151eea96ae445073d943229acd9
Mikko Koski <mikkokos@gmail.com>
2014-09-11 13:33:49 +0300

    Transaction controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -9,20 +9,11 @@ class ConversationsController < ApplicationController
     validates_presence_of :content, :conversation_id, :sender_id
   }
 
-
   before_filter do |controller|
     controller.ensure_logged_in t("layouts.notifications.you_must_log_in_to_view_your_inbox")
   end
 
-  before_filter :only => [ :index, :received ] do |controller|
-    controller.ensure_authorized t("layouts.notifications.you_are_not_authorized_to_view_this_content")
-  end
-
   skip_filter :dashboard_only
-
-  def index
-    redirect_to person_inbox_path(:person_id => @current_user.id)
-  end
 
   def show
     conversation_id = params[:id]
@@ -43,93 +34,21 @@ class ConversationsController < ApplicationController
     message_form = MessageForm.new({sender_id: @current_user.id, conversation_id: conversation_id})
 
     h = conversation_data.to_h
+
     other = conversation_data[:participants].reject { |participant| participant.id == @current_user.id }.first
+
     h[:other_party] = other.to_h.merge({url: person_path(id: other[:username])})
-
-    transaction = conversation_data[:transaction]
-
-    h[:listing_url] = if transaction
-      listing_path(id: transaction[:listing][:id])
-    end
 
     messages = h[:messages].map(&:to_h).map { |message|
       sender = conversation_data[:participants].find { |participant| participant.id == message[:sender_id] }
-      message.merge({mood: :neutral}).merge(sender: sender)
+      message.merge({mood: :neutral, type: :message}).merge(sender: sender)
     }
 
-    transaction = if h[:transaction].present?
-      transaction = h[:transaction].to_h
-      author_id = transaction[:listing][:author_id]
-      starter_id = transaction[:starter_id]
-
-      author = conversation_data[:participants].find { |participant| participant.id == author_id }
-      starter = conversation_data[:participants].find { |participant| participant.id == starter_id }
-
-      author_url = {url: person_path(id: author[:username])}
-      starter_url = {url: person_path(id: starter[:username])}
-
-      transaction.merge({author: author, starter: starter})
-    else
-      {}
-    end
-
-    messages_and_actions = merge_messages_and_transitions(messages, create_messages_from_actions(transaction))
-
     render locals: {
-      messages: messages_and_actions.reverse,
+      messages: messages.reverse,
       conversation_data: h,
       message_form: message_form,
       message_form_action: person_message_messages_path(@current_user, :message_id => h[:id])
     }
-  end
-
-  def merge_messages_and_transitions(messages, transitions)
-    messages_with_type = messages.map(&:to_h).map do |message|
-      message[:type] = "message"
-      message
-    end
-
-    transitions_with_type = transitions.map(&:to_h).map do |transition|
-      transition[:type] = "transition"
-      transition
-    end
-
-    (messages_with_type + transitions_with_type).sort_by { |hash| hash[:created_at] }
-  end
-
-  def create_messages_from_actions(transaction)
-    transitions = transaction[:transitions]
-    return [] if transitions.blank?
-
-    previous_states = [nil] + transitions.map { |transition| transition[:to_state] }
-
-    transitions.reject { |transition|
-      ["free", "pending"].include? transition.to_state
-    }. zip(previous_states).map { |(transition, previous_state)|
-      create_message_from_action(transaction, transition, previous_state)
-    }
-  end
-
-  def create_message_from_action(transaction, transition, old_state)
-    direction = transaction[:direction]
-
-    case transition[:to_state]
-    when "preauthorized"
-      {sender: transaction[:starter], content: t("conversations.message.paid", sum: humanized_money_with_symbol(transaction[:payment_sum])), created_at: transition[:created_at], mood: :positive }
-    when "accepted"
-      {sender: transaction[:author], content: t("conversations.message.accepted_#{direction}"), created_at: transition[:created_at], mood: :positive }
-    when "rejected"
-      {sender: transaction[:author], content: t("conversations.message.rejected_#{direction}"), created_at: transition[:created_at], mood: :negative }
-    when "paid" && old_state == "preauthorized"
-      {sender: transaction[:author], content: t("conversations.message.accepted_#{direction}"), created_at: transition[:created_at], mood: :positive }
-    when "paid" && old_state == "accepted"
-      {sender: transaction[:starter], content: t("conversations.message.paid", sum: humanized_money_with_symbol(transaction[:payment_sum])), created_at: transition[:created_at], mood: :positive }
-    when "canceled"
-      {sender: transaction[:author], content: t("conversations.message.canceled_#{direction}"), created_at: transition[:created_at], mood: :negative }
-    when "confirmed"
-      {sender: transaction[:author], content: t("conversations.message.confirmed_#{direction}"), created_at: transition[:created_at], mood: :positive }
-    else
-      raise("Unknown transition to state: #{transaction[:to_state]}")
-    end
   end
 end
---
+++ app/services/marketplace_service/transaction.rb
@@ -0,0 +1,112 @@
+module MarketplaceService
+  module Transaction
+    TransactionModel = ::Transaction
+
+    module Entity
+      Listing = Struct.new(
+        :id,
+        :title,
+        :author_id
+      )
+
+      Transaction = Struct.new(
+        :id,
+        :last_transition,
+        :last_transition_at,
+        :listing,
+        :direction, # :offer or :request
+        :status,
+        :author_skipped_feedback,
+        :starter_skipped_feedback,
+        :starter_id,
+        :testimonials,
+        :transitions,
+        :payment_sum,
+        :conversation
+      )
+
+      Transition = Struct.new(
+        :to_state,
+        :created_at
+      )
+
+      Testimonial = Struct.new(
+        :author_id,
+        :receiver_id,
+        :grade
+      )
+
+      ConversationEntity = MarketplaceService::Conversation::Entity
+      Conversation = ConversationEntity::Conversation
+      ConversationParticipant = ConversationEntity::ConversationParticipant
+
+      module_function
+
+      def waiting_testimonial_from?(transaction, person_id)
+        if transaction[:starter_id] == person_id
+          if transaction[:starter_skipped_feedback]
+            false
+          else
+            testimonial_from(transaction, person_id).nil?
+          end
+        else
+          if transaction[:author_skipped_feedback]
+            false
+          else
+            testimonial_from(transaction, person_id).nil?
+          end
+        end
+      end
+
+      def testimonial_from(transaction, person_id)
+        transaction[:testimonials].select { |testimonial| testimonial[:author_id] == person_id }
+      end
+
+      def transaction(transaction_model)
+        listing_model = transaction_model.listing
+        listing = EntityUtils.from_hash(Listing,
+          EntityUtils.model_to_hash(transaction_model.listing).merge(author_id: listing_model.author.id))
+
+        EntityUtils.from_hash(Transaction, EntityUtils.model_to_hash(transaction_model).merge({
+          status: transaction_model.transaction_transitions.last.to_state,
+          last_transition_at: transaction_model.transaction_transitions.last.created_at,
+          listing: listing,
+          testimonials: transaction_model.testimonials.map { |testimonial|
+            EntityUtils.from_hash(Testimonial, EntityUtils.model_to_hash(testimonial))
+          },
+          starter_id: transaction_model.starter.id,
+          transitions: transaction_model.transaction_transitions.map { |transition|
+            EntityUtils.from_hash(Transition, EntityUtils.model_to_hash(transition))
+          },
+          direction: listing_model.transaction_type.direction.to_sym,
+          payment_sum: Maybe(transaction_model).payment.total_sum.or_else { nil }
+        }))
+      end
+
+      def transaction_with_conversation(transaction_model)
+        transaction = Entity.transaction(transaction_model)
+        transaction.conversation = ConversationEntity.conversation(transaction_model.conversation)
+        transaction
+      end
+    end
+
+    module Command
+      module_function
+    end
+
+    module Query
+
+      module_function
+
+      def transaction_with_conversation(transaction_id, person_id, community_id)
+        transaction_model = TransactionModel.joins(:listing)
+          .where(id: transaction_id)
+          .where(community_id: community_id)
+          .where("starter_id = ? OR listings.author_id = ?", person_id, person_id)
+          .first
+
+        Entity.transaction_with_conversation(transaction_model)
+      end
+    end
+  end
+end
