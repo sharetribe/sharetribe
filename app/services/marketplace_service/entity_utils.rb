a7bb80640d5fb4051e734d8571d685e8d49a6ed6
Mikko Koski <mikkokos@gmail.com>
2014-09-12 09:33:34 +0300

    Create a new Entity class, which is a subclass of Hash

--- app/services/marketplace_service/entity_utils.rb
+++ app/services/marketplace_service/entity_utils.rb
@@ -2,6 +2,41 @@ module MarketplaceService
   module EntityUtils
     module_function
 
+    # Define an entity class, which is a Hash
+    #
+    # Usage:
+    #
+    # -- in some service / Entity --
+    #
+    # Person = MarketplaceService::EntityUtils.define_entity(
+    #   :username,
+    #   :password)
+    #
+    # -- in some service / Query --
+    #
+    # def person(person_id)
+    #   Maybe(Person.where(person_id: person_id.first)
+    #     .map { |model| Person.new(model) }
+    #     .or_else(nil)
+    # end
+    #
+    def define_entity(*ks)
+      Class.new(Hash) { |klass|
+
+        @__keys = ks
+
+        def self.keys
+          @__keys
+        end
+
+        def initialize(opts = {})
+          self.class.keys.each { |k|
+            self[k.to_sym] = opts[k] unless opts[k].nil?
+          }
+        end
+      }
+    end
+
     # Ensure first level keys are all symbols, not strings
     def hash_keys_to_symbols(hash)
       Hash[hash.map { |(k, v)| [k.to_sym, v] }]
