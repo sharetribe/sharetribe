b1861d45ab9122ee557974df1c0dc37db334d9e9
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-09-12 14:27:51 +0300

    Remodel billing agr, model + migration

--- app/models/billing_agreement.rb
+++ app/models/billing_agreement.rb
@@ -3,23 +3,18 @@
 # Table name: billing_agreements
 #
 #  id                   :integer          not null, primary key
-#  from_account_id      :integer          not null
-#  to_account_id        :integer          not null
-#  status               :string(255)      default("pending"), not null
+#  paypal_account_id    :integer          not null
 #  billing_agreement_id :string(255)
 #  created_at           :datetime         not null
 #  updated_at           :datetime         not null
+#  paypal_username_to   :string(255)      not null
+#  request_token        :string(255)      not null
 #
 
 class BillingAgreement < ActiveRecord::Base
-  attr_accessible :status, :billing_agreement_id, :from_account, :to_account
+  attr_accessible :paypal_account, :request_token, :billing_agreement_id, :paypal_username_to
 
-  belongs_to :from_account, class_name: "PaypalAccount"
-  belongs_to :to_account, class_name: "PaypalAccount"
+  belongs_to :paypal_account, class_name: "PaypalAccount"
 
-  validates_presence_of :from_account, :to_account
-
-  def status
-    read_attribute(:status).to_sym
-  end
+  validates_presence_of :paypal_account, :request_token, :paypal_username_to
 end
