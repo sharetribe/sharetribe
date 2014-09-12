f84a56b0552afe10a0579db6615f331a68837aa5
Mikko Koski <mikkokos@gmail.com>
2014-09-12 09:39:24 +0300

    Create Entity Proc, which returns a Hash when run

--- app/services/marketplace_service/entity_utils.rb
+++ app/services/marketplace_service/entity_utils.rb
@@ -2,6 +2,34 @@ module MarketplaceService
   module EntityUtils
     module_function
 
+    # Define an entity constructor Proc, which returns a Hash
+    #
+    # Usage:
+    #
+    # -- in some service / Entity --
+    #
+    # Person = MarketplaceService::EntityUtils.define_entity(
+    #   :username,
+    #   :password)
+    #
+    # -- in some service / Query --
+    #
+    # def person(person_id)
+    #   Maybe(Person.where(person_id: person_id.first)
+    #     .map { |model| Person.call(model) }
+    #     .or_else(nil)
+    # end
+    #
+    def define_entity(*ks)
+      -> (opts = {}) {
+
+        ks.inject({}) do |memo, k|
+          memo[k.to_sym] = opts[k] unless opts[k].nil?
+          memo
+        end
+      }
+    end
+
     # Ensure first level keys are all symbols, not strings
     def hash_keys_to_symbols(hash)
       Hash[hash.map { |(k, v)| [k.to_sym, v] }]
