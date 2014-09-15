2785ff57cf4bf90dd05577e74bed8692a934de40
Mikko Koski <mikkokos@gmail.com>
2014-09-15 14:09:15 +0300

    Inbox ensure logged in

--- app/controllers/inboxes_controller.rb
+++ app/controllers/inboxes_controller.rb
@@ -2,6 +2,9 @@ class InboxesController < ApplicationController
   include MoneyRails::ActionViewExtension
 
   skip_filter :dashboard_only
+  before_filter do |controller|
+    controller.ensure_logged_in t("layouts.notifications.you_must_log_in_to_view_your_inbox")
+  end
 
   def show
     params[:page] = 1 unless request.xhr?
