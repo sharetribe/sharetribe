fafa658c5be6ea2b97eb8fe9fda998f57aede783
Mikko Koski <mikkokos@gmail.com>
2014-09-16 15:32:33 +0300

    Fix one admin test

--- features/step_definitions/admin_transaction_steps.rb
+++ features/step_definitions/admin_transaction_steps.rb
@@ -13,25 +13,41 @@ module AdminTransactionSteps
     message = FactoryGirl.build(:message, created_at: last_message)
     conversation = FactoryGirl.build(:conversation, created_at: last_message, messages: [message])
 
+    author = Person.find_by_username(transaction_data[:other_party])
+    starter = Person.find_by_username(transaction_data[:starter])
+
+    # TODO This is almost copy-paste from conversation_steps
+    conversation.participations.build({
+      person_id: starter.id,
+      is_starter: true,
+      is_read: true
+    })
+
+    conversation.participations.build({
+      person_id: author.id,
+      is_starter: false,
+      is_read: false
+    })
+
     transaction_opts = {
       created_at: eval(transaction_data[:started_at].gsub(' ', '.')),
       community: Community.first
     }
     sum = transaction_data[:sum].to_i * 100 unless transaction_data[:sum].empty?
 
-    author = Person.find_by_username(transaction_data[:other_party])
-    starter = Person.find_by_username(transaction_data[:starter])
-
     transaction = FactoryGirl.build(
       :transaction,
       transaction_opts.merge({
-          transaction_transitions: [ FactoryGirl.build(:transaction_transition, { to_state: transaction_data[:status].to_sym }) ],
           listing: FactoryGirl.build(:listing, title: transaction_data[:listing], author: author),
           payment: sum ? FactoryGirl.build(:braintree_payment, { sum_cents: sum, currency: transaction_data[:currency] }) : nil,
           conversation: conversation,
           starter: starter
         })
       )
+
+    transaction.transaction_transitions.build({
+      to_state: transaction_data[:status].to_sym
+    })
 
     transaction
   end
