7b8299c4b2ca107269422499122d245a3f20ae31
Mikko Koski <mikkokos@gmail.com>
2014-09-16 16:22:34 +0300

    Fail tests if background job fails

--- features/support/background_job_helpers.rb
+++ features/support/background_job_helpers.rb
@@ -1,7 +1,11 @@
 module BackgroundJobHelpers
 
   def process_jobs
-    Delayed::Worker.new(:quiet => false).work_off
+    success, failure = Delayed::Worker.new(:quiet => false).work_off
+
+    if failure > 0
+      raise "Delayed job failed"
+    end
   end
 end
 
