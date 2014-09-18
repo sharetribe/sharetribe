7a7f3aa8821d88f8be76b1232b545a25caa2126f
Sampo Toiva <sampo.toiva@gmail.com>
2014-09-18 16:35:57 +0300

    Reversible with up and down methods

--- db/migrate/20140909074331_remodel_order_permission.rb
+++ db/migrate/20140909074331_remodel_order_permission.rb
@@ -1,10 +1,17 @@
 class RemodelOrderPermission < ActiveRecord::Migration
-  def change
+  def up
     rename_column(:order_permissions, :from_account_id, :paypal_account_id)
     remove_columns(:order_permissions, :to_account_id, :status)
     add_column(:order_permissions, :request_token, :string, null: false)
     add_column(:order_permissions, :paypal_username_to, :string, null: false)
     add_column(:order_permissions, :scope, :string, null: false)
     add_column(:order_permissions, :verification_code, :string, null: true)
+  end
+
+  def down
+    rename_column(:order_permissions, :paypal_account_id, :from_account_id)
+    add_column(:order_permissions, :to_account_id, :integer, null: false)
+    add_column(:order_permissions, :status, :string, null: false, default: "pending")
+    remove_columns(:order_permissions, :request_token, :paypal_username_to, :scope, :verification_code)
   end
 end
