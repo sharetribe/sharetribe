c99f61c181068a047b6d8a458f7c5b51fac5baac
Mikko Koski <mikkokos@gmail.com>
2014-09-19 10:12:48 +0300

    Cleaner monkeypatching

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -39,37 +39,25 @@ module I18n
   end
 end
 
-# Monkey patch the translate method to include always some variables for interpolation
 I18n.module_eval do
+  # Save the original translate method
+  @__original_translate = I18n.method(:translate)
 
   class << self
 
-    # The method is quite strictly the original, only modifcation of "options" is added few lines below.
+    # Monkey patch the translate method to include service name options
     def translate(*args)
+      service_name = ApplicationHelper.fetch_community_service_name_from_thread
 
       options  = args.last.is_a?(Hash) ? args.pop : {}
-      key      = args.shift
-      backend  = config.backend
-      locale   = options.delete(:locale) || config.locale
-      handling = options.delete(:throw) && :throw || options.delete(:raise) && :raise # TODO deprecate :raise
 
-      # insert here the variables
-      service_name = ApplicationHelper.fetch_community_service_name_from_thread
-      #puts "in I18n using service name: #{service_name}"
-      options.merge!(:service_name => service_name) unless options.key?(:service_name)
-      # end added code here
-
-       raise I18n::ArgumentError if key.is_a?(String) && key.empty?
-
-        result = catch(:exception) do
-          if key.is_a?(Array)
-            key.map { |k| backend.translate(locale, k, options) }
-          else
-            backend.translate(locale, key, options)
-          end
-        end
-        result.is_a?(I18n::MissingTranslation) ? handle_exception(handling, result, locale, key, options) : result
+      with_service_name = if !options.key?(:service_name)
+        options.merge(:service_name => service_name)
+      else
+        options
       end
-      alias :t :translate
+
+      @__original_translate.call(*(args << with_service_name))
+    end
   end
 end
