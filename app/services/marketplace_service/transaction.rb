814b53277f0bda7b1bdd5fc2d2e81fc4feb0c638
Mikko Koski <mikkokos@gmail.com>
2014-09-23 14:35:38 +0300

    Still fix minor error in should_notify?

--- app/services/marketplace_service/transaction.rb
+++ app/services/marketplace_service/transaction.rb
@@ -78,7 +78,7 @@ module MarketplaceService
           person_id == transaction[:starter_id]
         elsif status == "paid"
           person_id == transaction[:starter_id]
-        elsif status == "confirmed" || "canceled"
+        elsif status == "confirmed" || status == "canceled"
           waiting_testimonial_from?(transaction, person_id)
         else
           false
