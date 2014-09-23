84b64a1360f52a5ee00532e781f74313f1d33231
Mikko Koski <mikkokos@gmail.com>
2014-09-23 11:36:51 +0300

    Fix testimonial tests

--- spec/mailers/person_mailer_spec.rb
+++ spec/mailers/person_mailer_spec.rb
@@ -101,8 +101,9 @@ describe PersonMailer do
   it "should send email about a new testimonial" do
     @test_person.update_attributes({ "given_name" => "Teppo", "family_name" => "Testaaja" })
 
+    transition = FactoryGirl.build(:transaction_transition, to_state: "confirmed")
     listing = FactoryGirl.build(:listing, author: @test_person)
-    transaction = FactoryGirl.create(:transaction, starter: @test_person2, listing: listing)
+    transaction = FactoryGirl.create(:transaction, starter: @test_person2, listing: listing, transaction_transitions: [transition])
     testimonial = FactoryGirl.create(:testimonial, :grade => 0.75, :text => "Yeah", :author => @test_person, :receiver => @test_person2, :transaction => transaction)
 
     email = PersonMailer.new_testimonial(testimonial, @community).deliver
