e86b6fef394e58d16f0b917946403073dcf592b1
Vesa Luusua <vesa.luusua@gmail.com>
2014-09-25 13:07:52 +0300

    remove api_password and api_signature columns from paypal_accounts table

--- app/models/paypal_account.rb
+++ app/models/paypal_account.rb
@@ -2,14 +2,12 @@
 #
 # Table name: paypal_accounts
 #
-#  id            :integer          not null, primary key
-#  person_id     :string(255)
-#  community_id  :integer
-#  email         :string(255)      not null
-#  api_password  :string(255)
-#  api_signature :string(255)
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
+#  id           :integer          not null, primary key
+#  person_id    :string(255)
+#  community_id :integer
+#  email        :string(255)      not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
 #
 
 class PaypalAccount < ActiveRecord::Base
