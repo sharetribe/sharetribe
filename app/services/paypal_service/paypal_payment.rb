eca4a7312baae3c6a065e5af49f1436ba5477247
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-10-07 15:51:43 +0300

    don't allow updating order details after payment created

--- app/services/paypal_service/paypal_payment.rb
+++ app/services/paypal_service/paypal_payment.rb
@@ -13,9 +13,6 @@ module PaypalService
 
       OPT_UPDATE_FIELDS = [
         :order_id,
-        :order_date,
-        :currency,
-        :order_total_cents,
         :authorization_id,
         :authorization_date,
         :authorization_total_cents,
@@ -31,19 +28,15 @@ module PaypalService
       end
 
       def from_order(order)
-        currency, cent_totals = [:order_total, :authorization_total, :fee_total, :payment_total]
-          .reduce([nil, {}]) do |(curr, cent_totals), m_key|
-             m = order[m_key]
-             if m.nil?
-               [curr, cent_totals] if m.nil?
-             else
-               [m.currency.iso_code, cent_totals.merge({"#{m_key}_cents".to_sym => m.cents})]
-             end
+        cent_totals = [:authorization_total, :fee_total, :payment_total]
+          .reduce({}) do |cent_totals, m_key|
+            m = order[m_key]
+            cent_totals["#{m_key}_cents".to_sym] = m.cents unless m.nil?
+            cent_totals
           end
 
         payment_update = PaymentUpdate.call(order.merge({payment_status: order[:payment_status].downcase.to_sym}))
-        payment_update = payment_update.merge(cent_totals).merge({currency: currency}) unless currency.nil?
-        payment_update = payment_update.merge(HashUtils.sub(order, *OPT_UPDATE_FIELDS))
+        payment_update = payment_update.merge(HashUtils.sub(order, *OPT_UPDATE_FIELDS)).merge(cent_totals)
         raise ArgumentError.new("Must have either order_id or authorization_id") unless valid?(payment_update)
 
         return payment_update
@@ -63,19 +56,25 @@ module PaypalService
         end
 
         payment.update_attributes!(payment_entity)
-        Result::Success.new
+
+        Result::Success.new # TODO Should return a hash of the updated account instead
       end
 
 
       ## Privates
 
-      # TODO validate that payer_id and receiver_id match?
       def find_payment(payment_entity)
         if (payment_entity[:order_id])
-          PaypalPaymentModel.where(order_id: payment_entity[:order_id]).first
+          payment = PaypalPaymentModel.where(order_id: payment_entity[:order_id]).first
         else
-          PaypalPaymentModel.where(authorization_id: payment_entity[:authorization_id]).first
+          payment = PaypalPaymentModel.where(authorization_id: payment_entity[:authorization_id]).first
         end
+
+        if (payment && payment_entity[:receiver_id] == payment.receiver_id && payment_entity[:payer_id] == payment.payer_id)
+          return payment
+        end
+
+        return nil
       end
     end
   end
