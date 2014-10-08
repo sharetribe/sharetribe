e7708fa8eec13a34cac36e0f78701feb7b7eeb23
Sampo Toiva <sampo.toiva@gmail.com>
2014-10-08 10:55:20 +0300

    Raise and show 404 on missing how to use content

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -10,6 +10,12 @@ class InfosController < ApplicationController
   def how_to_use
     @selected_tribe_navi_tab = "about"
     @selected_left_navi_link = "how_to_use"
+    case(how_to_use_content?)
+    when None, Some(false)
+      raise ActiveRecord::RecordNotFound
+    else
+      render locals: { how_to_use_content: @community_customization.how_to_use_page_content }
+    end
   end
 
   def terms
@@ -22,4 +28,9 @@ class InfosController < ApplicationController
     @selected_left_navi_link = "privacy"
   end
 
+  private
+
+  def how_to_use_content?
+    Maybe(@community_customization).map { |customization| !customization.how_to_use_page_content.nil? }
+  end
 end
