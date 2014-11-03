259fda07adf2a5012936b4def1590e155309b3d9
Mikko Koski <mikkokos@gmail.com>
2014-11-03 19:30:54 +0200

    Ensure listing author has paypal account

--- app/controllers/preauthorize_transactions_controller.rb
+++ app/controllers/preauthorize_transactions_controller.rb
@@ -8,7 +8,7 @@ class PreauthorizeTransactionsController < ApplicationController
   before_filter :ensure_listing_is_open
   before_filter :ensure_listing_author_is_not_current_user
   before_filter :ensure_authorized_to_reply
-  before_filter :ensure_can_receive_payment, only: [:preauthorize, :preauthorized]
+  before_filter :ensure_can_receive_payment
 
   skip_filter :dashboard_only
 
@@ -337,11 +337,22 @@ class PreauthorizeTransactionsController < ApplicationController
   end
 
   def ensure_can_receive_payment
-    Maybe(@current_community).payment_gateway.each do |gateway|
-      unless gateway.can_receive_payments?(@listing.author)
-        flash[:error] = t("layouts.notifications.listing_author_payment_details_missing")
-        redirect_to (session[:return_to_content] || root)
-      end
+    payment_type = MarketplaceService::Community::Query.payment_type(@current_community.id)
+
+    ready =
+      case payment_type
+        when :paypal
+          paypal_account = PaypalService::PaypalAccount::Query.personal_account(@listing.author.id, @current_community.id)
+          PaypalService::PaypalAccount::Entity.paypal_account_prepared?(paypal_account)
+        when :braintree, :checkout
+          @current_community.payment_gateway.can_receive_payments?(@listing.author)
+        else
+          false
+        end
+
+    unless ready
+      flash[:error] = t("layouts.notifications.listing_author_payment_details_missing")
+      return redirect_to listing_path(@listing)
     end
   end
 
