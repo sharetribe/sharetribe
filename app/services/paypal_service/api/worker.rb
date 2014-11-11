348c84ba6163b7e75142fef64a0b907ca008b56e
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-11-11 17:25:59 +0200

    Use high priority for all paypal payment jobs

--- app/services/paypal_service/api/worker.rb
+++ app/services/paypal_service/api/worker.rb
@@ -2,6 +2,8 @@ module PaypalService::API::Worker
 
   ProcessTokenStore = PaypalService::Store::ProcessToken
 
+  JOB_OPTIONS = { priority: 0 } # Use high priority, user is waiting
+
   module_function
 
   def enqueue_payments_op(community_id:, transaction_id:, op_name:, op_input:)
@@ -45,12 +47,14 @@ module PaypalService::API::Worker
 
   def schedule_payments_job(proc_token)
     Delayed::Job.enqueue(
-      PaypalService::Jobs::ProcessPaymentsCommand.new(proc_token[:process_token]))
+      PaypalService::Jobs::ProcessPaymentsCommand.new(proc_token[:process_token]),
+      JOB_OPTIONS)
   end
 
   def schedule_billing_agreements_job(proc_token)
     Delayed::Job.enqueue(
-      PaypalService::Jobs::ProcessBillingAgreementsCommand.new(proc_token[:process_token]))
+      PaypalService::Jobs::ProcessBillingAgreementsCommand.new(proc_token[:process_token]),
+      JOB_OPTIONS)
   end
 
 end
