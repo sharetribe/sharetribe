7aea6d141831bbd4a4d45a1c1769f7320c5f53c6
Vesa Luusua <vesa.luusua@gmail.com>
2014-12-08 13:27:07 +0200

    refactoring: is_valid_country_code -> country_code_valid?

--- app/utils/localization_utils.rb
+++ app/utils/localization_utils.rb
@@ -2,7 +2,7 @@ module LocalizationUtils
   module_function
 
   # Check if the country code (ISO 3166-1 alpha-2) is valid (i.e. sv_SE -> SE is the country code)
-  def is_valid_country_code?(country_code)
+  def country_code_valid?(country_code)
     unless country_code.blank?
       CountrySelect.countries.select{|key, hash|
         return true if key.downcase == country_code.downcase
