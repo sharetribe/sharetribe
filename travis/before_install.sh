0e3ced08b70b0f37a996cc349e1e48470dc6b960
Josh Kalderimis <josh.kalderimis@gmail.com>
2014-12-09 17:31:36 +0100

    the custom bundle cache isn't needed anymore
    use the built in travis caching instead

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,43 +3,8 @@
 echo "Running before_install"
 echo "SUITE: ${SUITE}"
 
-"echo 'gem: --no-ri --no-rdoc' > ~/.gemrc"
-
-if [ "$SUITE" = "rspec" ]
+if [ "$SUITE" = "mocha" ]
 then
-	gem install nokogiri -- --use-system-libraries
-	gem install bundler specific_install
-	# Install a modified version of bundle_cache gem
-	# If the modifications get merged to bundle_cache use the upstream repo, not this one
-	gem specific_install https://github.com/sharetribe/bundle_cache
-	bundle_cache_install
-	exit
-elif [ "$SUITE" = "rubocop" ]
-then
-	gem install nokogiri -- --use-system-libraries
-	gem install bundler specific_install
-	# Install a modified version of bundle_cache gem
-	# If the modifications get merged to bundle_cache use the upstream repo, not this one
-	gem specific_install https://github.com/sharetribe/bundle_cache
-	bundle_cache_install
-	exit
-elif [ "$SUITE" = "cucumber" ]
-then
-	gem install nokogiri -- --use-system-libraries
-	gem install bundler specific_install
-	# Install a modified version of bundle_cache gem
-	# If the modifications get merged to bundle_cache use the upstream repo, not this one
-	gem specific_install https://github.com/sharetribe/bundle_cache
-	bundle_cache_install
-	exit
-elif [ "$SUITE" = "mocha" ]
-then
-	gem install nokogiri -- --use-system-libraries
-	gem install bundler specific_install
-	# Install a modified version of bundle_cache gem
-	# If the modifications get merged to bundle_cache use the upstream repo, not this one
-	gem specific_install https://github.com/sharetribe/bundle_cache
-	bundle_cache_install
 	npm install
 	npm install -g grunt-cli
 	exit
@@ -48,7 +13,4 @@ then
 	npm install
 	npm install -g grunt-cli
 	exit
-else
-	echo -e "Error: SUITE is illegal or not set\n"
-	exit 1
 fi
