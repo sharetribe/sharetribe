d0ff0634187d54207b4b267ad8822c96dfa2fa85
Mikko Koski <mikkokos@gmail.com>
2014-12-09 16:17:40 +0200

    Remove contact request

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,6 @@ class DashboardController < ApplicationController
   skip_filter :fetch_community, :only => :api
 
   def index
-    @contact_request = session[:contact_request_sent] ? ContactRequest.find(session[:contact_request_sent]) : ContactRequest.new
   end
 
 end
