41178ca516063f13680111db3ba4ae927acc56c8
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2014-12-17 16:24:00 +0200

    Add reference_transaction to list of pp permissions

--- app/services/paypal_service/data_types/permissions.rb
+++ app/services/paypal_service/data_types/permissions.rb
@@ -9,6 +9,7 @@ module PaypalService
             "EXPRESS_CHECKOUT",
             "AUTH_CAPTURE",
             "REFUND",
+            "REFERENCE_TRANSACTION",
             "TRANSACTION_DETAILS",
             "EXPRESS_CHECKOUT",
             "ACCESS_BASIC_PERSONAL_DATA"
