c68bddf7f5cea460760c15d46760843081cafd74
Mikko Koski <mikkokos@gmail.com>
2014-12-25 15:05:25 +0200

    Display name view util

---
+++ app/view_utils/person_view_utils.rb
@@ -0,0 +1,77 @@
+module PersonViewUtils
+  module_function
+
+  # This method is an adapter to `display_name` method.
+  # Makes the use easier by allowing Person and Community models as parameters
+  def person_display_name(person, community)
+    display_name(
+      first_name: person.given_name,
+      last_name: person.family_name,
+      username: person.username,
+
+      name_display_type: community.name_display_type,
+
+      is_organization: person.is_organization?,
+      organization_name: person.organization_name,
+
+      is_deleted: person.deleted?,
+      deleted_user_text: I18n.translate("common.removed_user"),
+    )
+  end
+
+  # This is another adapter to `display_name`.
+  # It accepts person entity
+  def person_entity_display_name(person_entity, name_display_type)
+    PersonViewUtils.display_name(
+      first_name: person_entity[:first_name],
+      last_name: person_entity[:last_name],
+      organization_name: nil,
+      username: person_entity[:username],
+      name_display_type: name_display_type,
+      is_organization: false,
+      is_deleted: person_entity[:is_deleted],
+      deleted_user_text: I18n.translate("common.removed_user")
+    )
+  end
+
+  # rubocop:disable ParameterLists
+  def display_name(
+        first_name:,
+        last_name:,
+        organization_name:,
+        username:,
+        name_display_type:,
+        is_organization:,
+        is_deleted:,
+        deleted_user_text:)
+    name_present = first_name.present?
+
+    case [is_deleted, is_organization, name_present, name_display_type]
+    when matches([true])
+      deleted_user_text
+    when matches([__, true])
+      organization_name
+    when matches([__, __, true, "first_name_with_initial"])
+      first_name_with_initial(first_name, last_name)
+    when matches([__, __, true, "first_name_only"])
+      first_name
+    when matches([__, __, true, __])
+      full_name(first_name, last_name)
+    else
+      username
+    end
+  end
+  # rubocop:enable ParameterLists
+
+  def full_name(first_name, last_name)
+    "#{first_name} #{last_name}"
+  end
+
+  def first_name_with_initial(first_name, last_name)
+    if last_name.present?
+      "#{first_name} #{last_name[0, 1]}"
+    else
+      first_name
+    end
+  end
+end
