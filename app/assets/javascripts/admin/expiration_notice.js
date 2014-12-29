3a3794dc1ad801896adf889e9ad3146dd27740b8
Vesa Luusua <vesa.luusua@gmail.com>
2014-12-29 18:16:13 +0200

    translations. These are not included to webtranslate

--- app/assets/javascripts/admin/expiration_notice.js
+++ app/assets/javascripts/admin/expiration_notice.js
@@ -3,5 +3,5 @@ window.ST = window.ST || {};
 (function(module) {
   module.initializeExpirationNotice = function(expirationNoticeId) {
     $('#'+expirationNoticeId).lightbox_me({modalCSS: {top: '5px'}, zIndex: 1000000, closeEsc: false, closeClick: false});
-  }
+  };
 })(window.ST);
