e80a10e985469bdfb55878923fa9bd137f15566e
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-01-12 13:24:12 +0200

    Fix and cleanup paypal accounts controller

--- app/view_utils/paypal_helper.rb
+++ app/view_utils/paypal_helper.rb
@@ -11,16 +11,19 @@ module PaypalHelper
     PaypalAccountEntity.paypal_account_prepared?(paypal_account)
   end
 
-  def community_ready_for_payments?(community)
-    admin_account = PaypalAccountQuery.admin_account(community.id)
-    community.commission_from_seller.present? &&
-      community.minimum_price.present? &&
-      PaypalAccountEntity.order_permission_verified?(admin_account)
+  def community_ready_for_payments?(community_id)
+    admin_account = PaypalAccountQuery.admin_account(community_id)
+    PaypalAccountEntity.order_permission_verified?(admin_account) &&
+      Maybe(TransactionService::API::Api.settings.get_active(community_id: community_id))
+      .map {|res| res[:success] ? res[:data] : nil}
+      .select {|set| set[:payment_gateway] == :paypal && set[:commission_from_seller] && set[:minimum_price_cents]}
+      .map {|_| true}
+      .or_else(false)
   end
 
   def user_and_community_ready_for_payments?(user, community)
     PaypalHelper.personal_account_prepared?(user, community) &&
-      PaypalHelper.community_ready_for_payments?(community)
+      PaypalHelper.community_ready_for_payments?(community.id)
   end
 
   # Check if PayPal has been provisioned for a community.
