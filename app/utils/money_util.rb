5279f12c2e13a52d5a11d003a18108eb2877c757
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-01-15 09:28:14 +0200

    Use subunits in money str parsing

--- app/utils/money_util.rb
+++ app/utils/money_util.rb
@@ -1,18 +1,21 @@
 module MoneyUtil
   module_function
 
-  # Give string that represents money and get back the amount in cents
+  def parse_str_to_cents(money_str)
+    raise "Dude, cents ain't the only subunit out there"
+  end
+
+  # Give string that represents money and get back the amount in currency subunits
   #
   # Notice! The parsing strategy should follow the frontend validation strategy
-  def parse_str_to_cents(money_str)
+  def parse_str_to_subunits(money_str, currency)
     # Current front-end validation: /^\d+((\.|\,)\d{0,2})?$/
-    normalized = money_str.sub(",", ".")
-    cents = normalized.to_f * 100
-    cents.to_i
+    (money_str.sub(",", ".").to_f * Money::Currency.new(currency).subunit_to_unit)
+      .to_i
   end
 
   def parse_str_to_money(money_str, currency)
-    Money.new(parse_str_to_cents(money_str), currency)
+    Money.new(parse_str_to_subunits(money_str, currency), currency)
   end
 
   def to_money(cents, currency)
--- spec/utils/money_util_spec.rb
+++ spec/utils/money_util_spec.rb
@@ -2,6 +2,28 @@ require 'spec_helper'
 
 describe MoneyUtil do
 
+  it "#parse_str_to_money" do
+    expect(MoneyUtil.parse_str_to_money("5000", "JPY"))
+      .to eql(Money.new(5000, "JPY"))
+    expect(MoneyUtil.parse_str_to_money("50", "EUR"))
+      .to eql(Money.new(5000, "EUR"))
+    expect(MoneyUtil.parse_str_to_money("0.12", "USD"))
+      .to eql(Money.new(12, "USD"))
+    expect(MoneyUtil.parse_str_to_money("0,12", "EUR"))
+      .to eql(Money.new(12, "EUR"))
+  end
+
+  it "#parse_str_to_subunits" do
+    expect(MoneyUtil.parse_str_to_subunits("100", "EUR")).to eql(10000)
+    expect(MoneyUtil.parse_str_to_subunits("100.00", "EUR")).to eql(10000)
+    expect(MoneyUtil.parse_str_to_subunits("100,00", "EUR")).to eql(10000)
+    expect(MoneyUtil.parse_str_to_subunits("99,99", "EUR")).to eql(9999)
+    expect(MoneyUtil.parse_str_to_subunits("99.99", "EUR")).to eql(9999)
+    expect(MoneyUtil.parse_str_to_subunits("0.12", "EUR")).to eql(12)
+    expect(MoneyUtil.parse_str_to_subunits("0,12", "EUR")).to eql(12)
+    expect(MoneyUtil.parse_str_to_subunits("10", "JPY")).to eql(10)
+  end
+
   it "#to_dot_separated_str" do
     expect(MoneyUtil.to_dot_separated_str(Money.new(10001, "USD")))
       .to eq("100.01")
