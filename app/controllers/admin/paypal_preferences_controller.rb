a09816219aadd810161ba9872fdf98b8eff9f3bf
Mikko Koski <mikkokos@gmail.com>
2015-01-26 10:52:25 +0200

    PayPal admin controller uses new API

--- app/controllers/admin/paypal_preferences_controller.rb
+++ app/controllers/admin/paypal_preferences_controller.rb
@@ -91,9 +91,15 @@ class Admin::PaypalPreferencesController < ApplicationController
   end
 
   def account_create
-    PaypalAccountCommand.create_admin_account(@current_community.id)
-
-    permissions_url = request_paypal_permissions_url
+    community_country_code = LocalizationUtils.valid_country_code(@current_community.country)
+    response = accounts_api.request(
+      PaypalService::API::DataTypes.create_create_account_request(
+      {
+        community_id: @current_community.id,
+        callback_url: permissions_verified_admin_community_paypal_preferences_url,
+        country: community_country_code
+      }))
+    permissions_url = response.data[:redirect_url]
 
     if permissions_url.blank?
       flash[:error] = t("paypal_accounts.new.could_not_fetch_redirect_url")
@@ -109,30 +115,21 @@ class Admin::PaypalPreferencesController < ApplicationController
       return redirect_to action: :index
     end
 
-    access_token_res = fetch_access_token(params[:request_token], params[:verification_code])
-    return flash_error_and_redirect_to_settings(error_response: access_token_res) unless access_token_res[:success]
+    response = accounts_api.create(
+      @current_community.id, nil, params[:request_token],
+      PaypalService::API::DataTypes
+        .create_account_permission_verification_request(
+          {
+            verification_code: params[:verification_code]
+          }))
 
-    personal_data_res = fetch_personal_data(access_token_res[:token], access_token_res[:token_secret])
-    return flash_error_and_redirect_to_settings(error_response: personal_data_res) unless personal_data_res[:success]
-
-    PaypalAccountCommand.update_admin_account(
-      @current_community.id,
-      {
-        email: personal_data_res[:email],
-        payer_id: personal_data_res[:payer_id]
-      }
-    )
-    PaypalAccountCommand.confirm_pending_permissions_request(
-      nil,
-      @current_community.id,
-      params[:request_token],
-      access_token_res[:scope].join(","),
-      params[:verification_code]
-    )
-
-    PaypalAccountCommand.activate_account(nil, personal_data_res[:payer_id], @current_community.id)
-
-    redirect_to action: :index
+    if response[:success]
+      paypal_account = PaypalAccountQuery.admin_account(@current_community.id)
+      PaypalAccountCommand.activate_account(nil, paypal_account[:payer_id], @current_community.id)
+      redirect_to action: :index
+    else
+      flash_error_and_redirect_to_settings(error_response: response)
+    end
   end
 
   private
@@ -153,47 +150,6 @@ class Admin::PaypalPreferencesController < ApplicationController
     end
   end
 
-  def request_paypal_permissions_url
-    permission_request = PaypalService::DataTypes::Permissions
-      .create_req_perm({callback: permissions_verified_admin_community_paypal_preferences_url })
-
-    response = paypal_permissions.do_request(permission_request)
-    if response[:success]
-      PaypalAccountCommand.create_pending_permissions_request(
-          nil,
-          @current_community.id,
-          response[:username_to],
-          permission_request[:scope],
-          response[:request_token]
-        )
-      response[:redirect_url]
-    else
-      nil
-    end
-  end
-
-  # TODO This is copy-paste
-  def fetch_access_token(request_token, verification_code)
-    access_token_req = PaypalService::DataTypes::Permissions.create_get_access_token(
-      {
-        request_token: params[:request_token],
-        verification_code: params[:verification_code]
-      }
-    )
-    access_token_res = paypal_permissions.do_request(access_token_req)
-  end
-
-  # TODO This is copy-paste
-  def fetch_personal_data(token, token_secret)
-    personal_data_req = PaypalService::DataTypes::Permissions.create_get_basic_personal_data(
-      {
-        token: token,
-        token_secret: token_secret
-      }
-    )
-    paypal_permissions.do_request(personal_data_req)
-  end
-
   def flash_error_and_redirect_to_settings(error_response: nil)
     error =
       if (error_response && error_response[:error_code] == "570058")
@@ -212,6 +168,10 @@ class Admin::PaypalPreferencesController < ApplicationController
 
   def tx_settings_api
     TransactionService::API::Api.settings
+  end
+
+  def accounts_api
+    PaypalService::API::Api.accounts_api
   end
 
 end
