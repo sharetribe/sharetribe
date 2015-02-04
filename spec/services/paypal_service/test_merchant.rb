1b03a71cf99e3925c987716bee02f6185c4fb79f
Mikko Koski <mikkokos@gmail.com>
2015-02-04 11:22:49 +0200

    Move FakePal to test_merchant

--- spec/services/paypal_service/test_merchant.rb
+++ spec/services/paypal_service/test_merchant.rb
@@ -8,12 +8,119 @@ module PaypalService
     end
   end
 
+  class FakePalMerchant
+    def initialize
+      @tokens = {}
+      @payments_by_order_id = {}
+      @payments_by_auth_id = {}
+      @billing_agreements_by_token = {}
+    end
+
+    def save_token(req)
+      token = {
+        token: SecureRandom.uuid,
+        item_name: req[:item_name],
+        item_quantity: req[:item_quantity],
+        item_price: req[:item_price],
+        order_total: req[:order_total],
+        receiver_id: req[:receiver_username]
+      }
+
+      @tokens[token[:token]] = token
+      token
+    end
+
+    def get_token(token)
+      @tokens[token]
+    end
+
+    def create_and_save_payment(token)
+      payment = {
+        order_date: Time.now,
+        payment_status: "pending",
+        pending_reason: "order",
+        order_id: SecureRandom.uuid,
+        order_total: token[:order_total],
+        receiver_id: token[:receiver_id]
+      }
+
+      @payments_by_order_id[payment[:order_id]] = payment
+      payment
+    end
+
+    def create_and_save_billing_agreement(token)
+      billing_agreement = {
+        billing_agreement_id: SecureRandom.uuid
+      }
+
+      @billing_agreements_by_token[token[:token]] = billing_agreement
+      billing_agreement
+    end
+
+    def authorize_payment(order_id, authorization_total)
+      payment = @payments_by_order_id[order_id]
+      raise "No order with order id: #{order_id}" if payment.nil?
+      raise "Cannot authorize more than order_total" if authorization_total.cents > payment[:order_total].cents
+
+      auth_id = SecureRandom.uuid
+      auth_payment = payment.merge({
+        authorization_date: Time.now,
+        authorization_total: authorization_total,
+        authorization_id: auth_id,
+        payment_status: "pending",
+        pending_reason: "authorization",
+        })
+
+      @payments_by_order_id[order_id] = auth_payment
+      @payments_by_auth_id[auth_id] = auth_payment
+      auth_payment
+    end
+
+    def capture_payment(auth_id, payment_total)
+      payment = @payments_by_auth_id[auth_id]
+      raise "No payment for auth id: #{auth_id}" if payment.nil?
+      raise "Cannot capture more than authorization_total" if payment_total.cents > payment[:authorization_total].cents
+
+      payment_id = SecureRandom.uuid
+      captured_payment = payment.merge({
+          payment_id: payment_id,
+          payment_total: payment_total,
+          fee_total: Money.new((payment_total.cents*0.1).to_i, payment_total.currency.iso_code),
+          payment_date: Time.now,
+          payment_status: "completed",
+          pending_reason: "none"
+        })
+    end
+
+    def get_payment(auth_or_order_id)
+      @payments_by_auth_id[auth_or_order_id] || @payments_by_order_id[auth_or_order_id]
+    end
+
+    def get_billing_agreement(token)
+      @billing_agreements_by_token[token[:token]]
+    end
+
+    def void(auth_or_order_id)
+      payment = get_payment(auth_or_order_id)
+      raise "No payment with order or auth id: #{auth_or_order_id}" if payment.nil?
+
+      voided_payment = payment.merge({
+          payment_status: "voided",
+          pending_reason: "none"
+        })
+
+      @payments_by_order_id[voided_payment[:order_id]] = voided_payment
+      @payments_by_auth_id[voided_payment[:authorization_id]] = voided_payment unless voided_payment[:authorization_id].nil?
+      voided_payment
+    end
+  end
+
   # rubocop:disable all
   class TestMerchantActions
     attr_reader :default_test_actions
 
     def initialize
-      @fake_pal = FakePal.new
+      @fake_pal = FakePalMerchant.new
       @default_test_actions = build_default_test_actions
     end
 
