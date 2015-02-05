9e930a7108fd579716ed4b312162ca48e059810a
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-02-05 09:17:24 +0200

    add commission_type calculated property to payment_settings

--- app/services/transaction_service/store/payment_settings.rb
+++ app/services/transaction_service/store/payment_settings.rb
@@ -2,7 +2,7 @@ module TransactionService::Store::PaymentSettings
 
   PaymentSettingsModel = ::PaymentSettings
 
-  PaymentSettings = EntityUtils.define_builder(
+  PaymentSettingsUpdate = EntityUtils.define_builder(
     [:active, :to_bool, default: false],
     [:community_id, :mandatory, :fixnum],
     [:payment_gateway, :to_symbol, one_of: [:paypal, :braintree, :checkout, :none], default: :none],
@@ -13,16 +13,28 @@ module TransactionService::Store::PaymentSettings
     [:confirmation_after_days, :fixnum, default: 14]
   )
 
+  PaymentSettings = EntityUtils.define_builder(
+    [:active, :to_bool, default: false],
+    [:community_id, :mandatory, :fixnum],
+    [:payment_gateway, :to_symbol, one_of: [:paypal, :braintree, :checkout, :none], default: :none],
+    [:payment_process, :to_symbol, one_of: [:preauthorize, :postpay, :free], default: :free],
+    [:commission_from_seller, :fixnum],
+    [:minimum_price_cents, :fixnum],
+    [:minimum_transaction_fee_cents, :fixnum],
+    [:confirmation_after_days, :fixnum, default: 14],
+    [:commission_type, :mandatory, one_of: [:relative, :fixed, :both, :none]]
+  )
+
   module_function
 
   def create(opts)
-    settings = HashUtils.compact(PaymentSettings.call(opts))
+    settings = HashUtils.compact(PaymentSettingsUpdate.call(opts))
     model = PaymentSettingsModel.create!(settings)
     from_model(model)
   end
 
   def update(opts)
-    settings = HashUtils.compact(PaymentSettings.call(opts)).except(:active, :payment_process, :payment_gateway)
+    settings = HashUtils.compact(PaymentSettingsUpdate.call(opts)).except(:active, :payment_process, :payment_gateway)
     model = find(opts[:community_id], opts[:payment_gateway], opts[:payment_process])
     raise ArgumentError.new("Cannot find settings to update: cid: #{opts[:community_id]}, gateway: #{opts[:payment_gateway]}, process: #{opts[:payment_process]}") if model.nil?
 
@@ -71,8 +83,23 @@ module TransactionService::Store::PaymentSettings
 
   def from_model(model)
     Maybe(model)
-      .map { |m| PaymentSettings.call(EntityUtils.model_to_hash(m)) }
+      .map { |m| EntityUtils.model_to_hash(m) }
+      .map { |hash| PaymentSettings.call(hash.merge({commission_type: commission_type(hash)})) }
       .or_else(nil)
+  end
+
+  def commission_type(settings)
+    s = Maybe(settings)
+    case [s[:commission_from_seller].>(0).or_else(false), s[:minimum_transaction_fee_cents].>(0).or_else(false)]
+    when [true, true]
+      :both
+    when [false, false]
+      :none
+    when [true, false]
+      :relative
+    when [false, true]
+      :fixed
+    end
   end
 
   def find(community_id, payment_gateway, payment_process)
