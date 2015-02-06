842fdd236e9cb5e9867639c297c1e53fc917f671
Mikko Koski <mikkokos@gmail.com>
2015-02-06 14:08:04 +0200

    Remove not null from merchant_id

--- db/migrate/20150205155400_add_merchant_id_to_paypal_payments.rb
+++ db/migrate/20150205155400_add_merchant_id_to_paypal_payments.rb
@@ -1,5 +1,5 @@
 class AddMerchantIdToPaypalPayments < ActiveRecord::Migration
   def change
-    add_column :paypal_payments, :merchant_id, :string, null: false, after: :receiver_id
+    add_column :paypal_payments, :merchant_id, :string, after: :receiver_id
   end
 end
