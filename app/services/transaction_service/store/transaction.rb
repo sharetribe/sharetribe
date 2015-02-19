bc82f0e2a8eecd3955ac19f0f23552fb69e6893a
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-02-19 02:16:42 +0200

    Move creating & loading tx to a store module

---
+++ app/services/transaction_service/store/transaction.rb
@@ -0,0 +1,115 @@
+module TransactionService::Store::Transaction
+
+  TransactionModel = ::Transaction
+
+  NewTransaction = EntityUtils.define_builder(
+    [:community_id, :fixnum, :mandatory],
+    [:listing_id, :fixnum, :mandatory],
+    [:starter_id, :string, :mandatory],
+    [:listing_quantity, :fixnum, default: 1],
+    [:listing_title, :string, :mandatory],
+    [:listing_author_id, :string, :mandatory],
+    [:unit_price, :money, default: Money.new(0)],
+    [:payment_process, one_of: [:none, :postpay, :preauthorize]],
+    [:payment_gateway, one_of: [:paypal, :checkout, :braintree, :none]],
+    [:commission_from_seller, :fixnum, default: 0],
+    [:automatic_confirmation_after_days, :fixnum, :mandatory],
+    [:minimum_commission, :money, default: Money.new(0)],
+    [:content, :string],
+    [:booking_fields, :hash])
+
+  Transaction = EntityUtils.define_builder(
+    [:id, :fixnum, :mandatory],
+    [:community_id, :fixnum, :mandatory],
+    [:listing_id, :fixnum, :mandatory],
+    [:starter_id, :string, :mandatory],
+    [:listing_quantity, :fixnum, :mandatory],
+    [:listing_title, :string, :mandatory],
+    [:listing_author_id, :string, :mandatory],
+    [:unit_price, :money, :mandatory],
+    [:payment_process, :to_symbol, one_of: [:none, :postpay, :preauthorize]],
+    [:payment_gateway, :to_symbol, one_of: [:paypal, :checkout, :braintree, :none]],
+    [:commission_from_seller, :fixnum, :mandatory],
+    [:automatic_confirmation_after_days, :fixnum, :mandatory],
+    [:minimum_commission, :money, :mandatory],
+    [:last_transition_at, :time],
+    [:current_state, :to_symbol])
+
+  module_function
+
+  def create(opts)
+    tx_data = HashUtils.compact(NewTransaction.call(opts))
+    tx_model = TransactionModel.new(tx_data.except(:content, :booking_fields))
+    build_conversation(tx_model, tx_data)
+    build_booking(tx_model, tx_data)
+
+    tx_model.save!
+    from_model(tx_model)
+  end
+
+  def get(transaction_id)
+    Maybe(TransactionModel.where(id: transaction_id).first)
+      .map { |m| from_model(m) }
+      .or_else(nil)
+  end
+
+
+  ## Privates
+
+  def from_model(model)
+    Maybe(model)
+      .map { |m|
+        EntityUtils.model_to_hash(m)
+        .merge({unit_price: m.unit_price , minimum_commission: m.minimum_commission })
+      }
+      .map { |hash| Transaction.call(hash) }
+      .or_else(nil)
+  end
+
+  def build_conversation(tx_model, tx_data)
+    conversation = tx_model.build_conversation(
+      tx_data.slice(:community_id, :listing_id))
+
+    conversation.participations.build(
+      person_id: tx_data[:listing_author_id],
+      is_starter: false,
+      is_read: false)
+
+    conversation.participations.build(
+      person_id: tx_data[:starter_id],
+      is_starter: true,
+      is_read: true)
+
+    if tx_data[:content].present?
+      conversation.messages.build({
+          content: tx_data[:content],
+          sender_id: tx_data[:starter_id]})
+    end
+  end
+
+  def build_booking(tx_model, tx_data)
+    if is_booking?(tx_data)
+
+      # TODO What's the correct place for the booking calculation logic?
+      # Make sure listing_quantity equals duration
+      if booking_duration(tx_data) != tx_model.listing_quantity
+        raise ArgumentException.new("Listing quantity (#{tx_listing_quantity}) must be equal to booking duration in days (#{booking_duration(tx_data)})")
+      end
+
+      start_on = opts[:booking_fields][:start_on]
+      end_on = opts[:booking_fields][:end_on]
+      tx_model.build_booking({start_on: start_on, end_on: end_on})
+    end
+  end
+
+  def is_booking?(tx_data)
+    tx_data[:booking_fields] && tx_data[:booking_fields][:start_on] && tx_data[:booking_fields][:end_on]
+  end
+
+  def booking_duration(tx_data)
+    start_on = tx_data[:booking_fields][:start_on]
+    end_on = tx_data[:booking_fields][:end_on]
+    DateUtils.duration_days(start_on, end_on)
+  end
+
+end
--- app/services/transaction_service/transaction.rb
+++ app/services/transaction_service/transaction.rb
@@ -5,12 +5,13 @@ module TransactionService::Transaction
 
   PaymentSettingsStore = TransactionService::Store::PaymentSettings
 
-  TxUtil = TransactionService::Util
+  TxStore = TransactionService::Store::Transaction
 
   module_function
 
   def query(transaction_id)
-    model_to_entity(TransactionModel.find(transaction_id))
+    tx = TxStore.get(transaction_id)
+    to_tx_response(tx)
   end
 
   def has_unfinished_transactions(person_id)
@@ -69,7 +70,7 @@ module TransactionService::Transaction
         tx_data_paypal(opts_tx)
       end
 
-    transaction = TxUtil.create_tx_model_with_conversation(
+    tx = TxStore.create(
       opts_tx.merge({minimum_commission: minimum_commission,
                      commission_from_seller: commission_from_seller,
                      automatic_confirmation_after_days: auto_confirm_days}))
@@ -77,29 +78,20 @@ module TransactionService::Transaction
     gateway_fields_response =
       case [opts_tx[:payment_gateway], opts_tx[:payment_process]]
       when [:braintree, :preauthorize]
-        create_tx_braintree(transaction: transaction, opts: opts, async: false)
+        create_tx_braintree(tx: tx, gateway_fields: opts[:gateway_fields], async: false)
       when [:paypal, :preauthorize]
-        create_tx_paypal(transaction: transaction, opts: opts, async: paypal_async)
+        create_tx_paypal(tx: tx, gateway_fields: opts[:gateway_fields], async: paypal_async)
       else
         # Braintree Postpay (/ Other payment type?)
         Result::Success.new({})
       end
 
     if gateway_fields_response[:success]
-      transaction.save!
-      #TODO: Fix to more sustainable solution (use model_to_entity, and add paypal and braintree relevant fields)
-      #transition info is now added in controllers
       Result::Success.new(
-        DataTypes.create_transaction_response(opts_tx.merge({
-          id: transaction.id,
-          created_at: transaction.created_at,
-          updated_at: transaction.updated_at
-        }),
-        gateway_fields_response[:data]))
+        DataTypes.create_transaction_response(to_tx_response(tx), gateway_fields_response[:data]))
     else
-      gateway_fields_response
+      gateway_fields_respon
     end
-
   end
 
   # Private
@@ -111,18 +103,20 @@ module TransactionService::Transaction
   end
 
   # Private
-  def create_tx_braintree(transaction:, opts:, async:)
-    payment_gateway_id = BraintreePaymentGateway.where(community_id: opts[:transaction][:community_id]).pluck(:id).first
-    transaction.payment = BraintreePayment.new({
-      community_id: opts[:transaction][:community_id],
-      payment_gateway_id: payment_gateway_id,
-      status: "pending",
-      payer_id: opts[:transaction][:starter_id],
-      recipient_id: opts[:transaction][:listing_author_id],
-      currency: "USD",
-      sum: transaction.unit_price * transaction.listing_quantity})
+  def create_tx_braintree(tx:, gateway_fields:, async:)
+    payment_gateway_id = BraintreePaymentGateway.where(community_id: tx[:community_id]).pluck(:id).first
+    payment = BraintreePayment.create(
+      {
+        transaction_id: tx[:id],
+        community_id: tx[:community_id],
+        payment_gateway_id: payment_gateway_id,
+        status: :pending,
+        payer_id: tx[:starter_id],
+        recipient_id: tx[:listing_author_id],
+        currency: "USD",
+        sum: tx[:unit_price] * tx[:listing_quantity]})
 
-    result = BraintreeSaleService.new(transaction.payment, opts[:gateway_fields]).pay(false)
+    result = BraintreeSaleService.new(payment, gateway_fields).pay(false)
 
     unless result.success?
       return Result::Error.new(result.message)
@@ -142,24 +136,24 @@ module TransactionService::Transaction
   end
 
   # Private
-  def create_tx_paypal(transaction:, opts:, async:)
+  def create_tx_paypal(tx:, gateway_fields:, async:)
     # Note: Quantity may be confusing in Paypal Checkout page, thus, we don't use separated unit price and quantity,
     # only the total price.
-    total = transaction.unit_price * transaction.listing_quantity
+    total = tx[:unit_price] * tx[:listing_quantity]
 
     create_payment_info = PaypalService::API::DataTypes.create_create_payment_request({
-      transaction_id: transaction.id,
-      item_name: transaction.listing_title,
+      transaction_id: tx[:id],
+      item_name: tx[:listing_title],
       item_quantity: 1,
       item_price: total,
-      merchant_id: opts[:transaction][:listing_author_id],
+      merchant_id: tx[:listing_author_id],
       order_total: total,
-      success: opts[:gateway_fields][:success_url],
-      cancel: opts[:gateway_fields][:cancel_url],
-      merchant_brand_logo_url: opts[:gateway_fields][:merchant_brand_logo_url]})
+      success: gateway_fields[:success_url],
+      cancel: gateway_fields[:cancel_url],
+      merchant_brand_logo_url: gateway_fields[:merchant_brand_logo_url]})
 
     result = PaypalService::API::Api.payments.request(
-      opts[:transaction][:community_id],
+      tx[:community_id],
       create_payment_info,
       async: async)
 
@@ -272,46 +266,6 @@ module TransactionService::Transaction
 
   # private
 
-  # Warning!
-  # This is only an intermediate solution. Ideally, we would store all the required
-  # transaction data in transaction service, but now we have to fetch the data from here and there.
-  # However, this method is only used to get the API interface right, even though the data model
-  # doesn't match the interface.
-  #
-  def model_to_entity(model)
-    payment_total =
-      case model.payment_gateway.to_sym
-      when :checkout, :braintree
-        Maybe(model).payment.total_sum.or_else(nil)
-      when :paypal
-        payment = paypal_payment_api().get_payment(model.community_id, model.id)
-        Maybe(payment).select { |p| p[:success] }[:data][:payment_total].or_else(nil)
-      end
-
-    checkout_details = checkout_details(model)
-    DataTypes.create_transaction({
-        id: model.id,
-        payment_process: model.payment_process.to_sym,
-        payment_gateway: model.payment_gateway.to_sym,
-        community_id: model.community_id,
-        starter_id: model.starter_id,
-        listing_id: model.listing_id,
-        listing_title: model.listing_title,
-        listing_price: model.unit_price,
-        listing_author_id: model.listing_author_id,
-        listing_quantity: model.listing_quantity,
-        automatic_confirmation_after_days: model.automatic_confirmation_after_days,
-        last_transition_at: model.last_transition_at,
-        current_state: model.current_state.to_sym,
-        payment_total: payment_total,
-        minimum_commission: model.minimum_commission,
-        commission_from_seller: Maybe(model.commission_from_seller).or_else(0),
-        checkout_total:   checkout_details[:total_price],
-        commission_total: checkout_details[:commission_total],
-        charged_commission: checkout_details[:charged_commission],
-        payment_gateway_fee: checkout_details[:payment_gateway_fee]})
-  end
-
   def charge_commission(transaction_id)
     transaction = query(transaction_id)
     payment = paypal_payment_api.get_payment(transaction[:community_id], transaction[:id])[:data]
@@ -333,20 +287,50 @@ module TransactionService::Transaction
     end
   end
 
-  def checkout_details(model)
+  def to_tx_response(tx)
+    checkout_details = checkout_details(tx)
+    commission_total = calculate_commission(checkout_details[:total_price], tx[:commission_from_seller], tx[:minimum_commission])
 
-    case model.payment_gateway.to_sym
+    DataTypes.create_transaction(
+      {
+        id: tx[:id],
+        payment_process: tx[:payment_process],
+        payment_gateway: tx[:payment_gateway],
+        community_id: tx[:community_id],
+        starter_id: tx[:starter_id],
+        listing_id: tx[:listing_id],
+        listing_title: tx[:listing_title],
+        listing_price: tx[:unit_price],
+        listing_author_id: tx[:listing_author_id],
+        listing_quantity: tx[:listing_quantity],
+        automatic_confirmation_after_days: tx[:automatic_confirmation_after_days],
+        last_transition_at: tx[:last_transition_at],
+        current_state: tx[:current_state],
+        payment_total: checkout_details[:payment_total],
+        minimum_commission: tx[:minimum_commission],
+        commission_from_seller: tx[:commission_from_seller],
+        checkout_total: checkout_details[:total_price],
+        commission_total: commission_total,
+        charged_commission: checkout_details[:charged_commission],
+        payment_gateway_fee: checkout_details[:payment_gateway_fee]})
+  end
+
+  def checkout_details(tx)
+    case tx[:payment_gateway]
+    when :checkout, :braintree
+      payment_total = Maybe(Payment.where(transaction_id: tx[:id]).first).total_sum.or_else(nil)
+      total_price = tx[:unit_price] * 1 # TODO fixme for booking (model.listing_quantity)
+      { payment_total: payment_total,
+        total_price: total_price }
     when :paypal
-      payment = paypal_payment_api().get_payment(model.community_id, model.id).maybe
-      total = Maybe(payment[:payment_total].or_else(payment[:authorization_total].or_else(nil)))
-              .or_else(model.unit_price)
-      { total_price: total,
-        commission_total: calculate_commission(total, model.commission_from_seller, model.minimum_commission),
+      payment = paypal_payment_api().get_payment(tx[:community_id], tx[:id]).maybe
+      payment_total = payment[:payment_total].or_else(nil)
+      total_price = Maybe(payment[:payment_total].or_else(payment[:authorization_total].or_else(nil)))
+              .or_else(tx[:unit_price])
+      { payment_total: payment_total,
+        total_price: total_price,
         charged_commission: payment[:commission_total].or_else(nil),
         payment_gateway_fee: payment[:fee_total].or_else(nil) }
-    else
-      total = model.unit_price * 1 #TODO fixme for booking (model.listing_quantity)
-      { total_price: total, commission_total: calculate_commission(total, model.commission_from_seller, model.minimum_commission) }
     end
   end
 
