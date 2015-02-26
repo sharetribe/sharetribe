723260db7f30e5aba1ea248eaa476ecd8b3840c5
Mikko Koski <mikkokos@gmail.com>
2015-02-26 17:49:29 +0200

    Allow dot in username

--- app/models/community.rb
+++ app/models/community.rb
@@ -137,7 +137,7 @@ class Community < ActiveRecord::Base
   monetize :minimum_price_cents, :allow_nil => true, :with_model_currency => :default_currency
 
   validates_length_of :username, :in => 2..50
-  validates_format_of :username, :with => /\A[A-Z0-9_\-]*\z/i
+  validates_format_of :username, :with => /\A[A-Z0-9_\-\.]*\z/i
   validates_uniqueness_of :username
   validates_length_of :domain, :in => 2..50
   validates_format_of :domain, :with => /\A[A-Z0-9_\-\.]*\z/i
