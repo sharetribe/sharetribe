d7f62833a501d2869517e610d1a4af9e3a09f6ca
Mikko Koski <mikkokos@gmail.com>
2015-03-06 15:47:09 +0200

    Create only one TransactionProcess per process/community

--- db/migrate/20150304074313_populate_transaction_process.rb
+++ db/migrate/20150304074313_populate_transaction_process.rb
@@ -1,18 +1,17 @@
 class PopulateTransactionProcess < ActiveRecord::Migration
   def up
-    add_column :transaction_processes, :transaction_type_id, :int
-
     execute("
-      INSERT INTO transaction_processes (process, transaction_type_id, created_at, updated_at)
+      INSERT INTO transaction_processes (process, community_id, created_at, updated_at)
       (
         SELECT
           CASE WHEN transaction_types.price_field = 0                 THEN 'none'
+               WHEN transaction_types.type = 'Request'                THEN 'none'
                WHEN payment_settings.id IS NOT NULL                   THEN payment_settings.payment_process
                WHEN payment_gateways.type = 'Checkout'                THEN 'postpay'
                WHEN payment_gateways.type = 'BraintreePaymentGateway' THEN IF(transaction_types.preauthorize_payment, 'preauthorize', 'postpay')
                ELSE 'none'
           END as process,
-          transaction_types.id,
+          transaction_types.community_id,
           transaction_types.created_at,
           transaction_types.updated_at
         FROM transaction_types
@@ -21,15 +20,34 @@ class PopulateTransactionProcess < ActiveRecord::Migration
         LEFT JOIN payment_gateways ON (payment_gateways.community_id = transaction_types.community_id)
 
         WHERE transaction_types.transaction_process_id IS NULL
+        GROUP BY community_id, process
       )
     ")
 
     execute("
-     UPDATE transaction_types, transaction_processes
-     SET transaction_types.transaction_process_id = transaction_processes.id
-     WHERE transaction_processes.transaction_type_id = transaction_types.id")
+      UPDATE transaction_types
 
-    remove_column :transaction_processes, :transaction_type_id
+      LEFT JOIN payment_settings ON (payment_settings.community_id = transaction_types.community_id AND payment_settings.active = 1)
+      LEFT JOIN payment_gateways ON (payment_gateways.community_id = transaction_types.community_id)
+
+      LEFT JOIN transaction_processes ON (
+        transaction_types.community_id = transaction_processes.community_id AND
+
+        process =
+          CASE WHEN transaction_types.price_field = 0                 THEN 'none'
+               WHEN transaction_types.type = 'Request'                THEN 'none'
+               WHEN payment_settings.id IS NOT NULL                   THEN payment_settings.payment_process
+               WHEN payment_gateways.type = 'Checkout'                THEN 'postpay'
+               WHEN payment_gateways.type = 'BraintreePaymentGateway' THEN IF(transaction_types.preauthorize_payment, 'preauthorize', 'postpay')
+               ELSE 'none'
+          END
+      )
+
+      SET transaction_types.transaction_process_id = transaction_processes.id
+
+      WHERE transaction_process_id IS NULL
+  ")
+
   end
 
   def down
