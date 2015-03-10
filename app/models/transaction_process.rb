2e90b3ad107a4b74ef7199dc8869b69d6a9ddeb1
Mikko Koski <mikkokos@gmail.com>
2015-03-10 10:44:29 +0200

    Missing migration

--- app/models/transaction_process.rb
+++ app/models/transaction_process.rb
@@ -2,10 +2,12 @@
 #
 # Table name: transaction_processes
 #
-#  id         :integer          not null, primary key
-#  process    :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :integer          not null, primary key
+#  community_id     :integer
+#  process          :string(32)       not null
+#  author_is_seller :boolean
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 
 class TransactionProcess < ActiveRecord::Base
