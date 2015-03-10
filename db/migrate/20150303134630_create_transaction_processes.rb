41b593fc2e8479440b4c40fbf7181982ee3faa3a
Mikko Koski <mikkokos@gmail.com>
2015-03-10 08:38:26 +0200

    Add author_is_seller

--- db/migrate/20150303134630_create_transaction_processes.rb
+++ db/migrate/20150303134630_create_transaction_processes.rb
@@ -3,6 +3,7 @@ class CreateTransactionProcesses < ActiveRecord::Migration
     create_table :transaction_processes do |t|
       t.integer :community_id
       t.string :process, null: false, limit: 32
+      t.boolean :author_is_seller
 
       t.timestamps
     end
