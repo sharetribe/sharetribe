7b6283d46b6e23eb44f366ef87a08b055f268d6c
Sampo Toiva <sampo.toiva@gmail.com>
2015-03-16 16:11:54 +0200

    Add shipping enabled to transaction type

--- app/models/marketplace_settings.rb
+++ app/models/marketplace_settings.rb
@@ -5,6 +5,8 @@
 #  id               :integer          not null, primary key
 #  shipping_enabled :boolean          default(FALSE)
 #  community_id     :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 
 class MarketplaceSettings < ActiveRecord::Base
