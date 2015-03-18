8394205eb02bf5fa0743d30e8d114e8505305d6d
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-03-18 15:33:59 +0200

    Add down strategy to null constrain autoconfirm migration

--- db/migrate/20150311113118_null_constrain_automatic_confirmation_after_days.rb
+++ db/migrate/20150311113118_null_constrain_automatic_confirmation_after_days.rb
@@ -1,5 +1,9 @@
 class NullConstrainAutomaticConfirmationAfterDays < ActiveRecord::Migration
-  def change
+  def up
     change_column :transactions, :automatic_confirmation_after_days, :int, null: false
+  end
+
+  def down
+    change_column :transactions, :automatic_confirmation_after_days, :int, null: true
   end
 end
