ffc719bcd1f52cd223c8a72f47f95213a4159d2f
Mikko Koski <mikkokos@gmail.com>
2015-03-23 17:31:41 +0200

    Migration: Remove price from listings where transaction_type price is not enabled

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150323085034) do
+ActiveRecord::Schema.define(:version => 20150323152147) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
