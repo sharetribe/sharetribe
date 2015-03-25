b5413198312d45c562fa724d60956612dd57293b
Mikko Koski <mikkokos@gmail.com>
2015-03-25 15:22:44 +0200

    Changes to Payment Reg Guard

--- app/services/payment_registration_guard.rb
+++ app/services/payment_registration_guard.rb
@@ -7,25 +7,23 @@ class PaymentRegistrationGuard
   end
 
   def requires_registration_before_accepting?
-    requires_registration?
+    @listing.price && @listing.price > 0 && @community.payments_in_use? && not_registered_already?
   end
 
   def requires_registration_before_posting?
-    requires_registration? && preauthorize_flow_in_use?
-  end
-
-  private
-
-  def requires_registration?
     find_opts = { community_id: @community.id, transaction_type_id: @listing.transaction_type_id }
     res = ListingService::API::Api.shapes.get(find_opts)
 
-    res.maybe.map { |shape|
-      shape[:price_enabled] && not_registered_already?
+    price_enabled = res.maybe.map { |shape|
+      shape[:price_enabled]
     }.or_else(nil).tap { |result|
       raise ArgumentError.new("Can not find shape: #{find_opts}") if result.nil?
     }
+
+    price_enabled && preauthorize_flow_in_use?
   end
+
+  private
 
   def preauthorize_flow_in_use?
     opts = {
