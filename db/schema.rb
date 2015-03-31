e607b18c454e53a7213ec276223551ffafdb1d8a
Mikko Koski <mikkokos@gmail.com>
2015-03-31 14:01:42 +0300

    Populate listing shape sort priority

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150331103317) do
+ActiveRecord::Schema.define(:version => 20150331105616) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
