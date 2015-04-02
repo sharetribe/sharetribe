59abec1a831fc976ced0f99b6d84c592f68917c2
Mikko Koski <mikkokos@gmail.com>
2015-04-02 15:11:23 +0300

    Populate CategoryListingShape

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150402090934) do
+ActiveRecord::Schema.define(:version => 20150402111115) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
