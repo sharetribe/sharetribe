59c62f579fd55aa769c293425c0fe0f0f93fa33c
Mikko Koski <mikkokos@gmail.com>
2015-04-04 18:15:22 +0300

    Remove some transation type references

--- app/view_utils/listing_shape_helper.rb
+++ app/view_utils/listing_shape_helper.rb
@@ -6,7 +6,6 @@ module ListingShapeHelper
   # deprecated
   #
   # This method is deprecated, but it's still in use in Atom API
-  # def transaction_types_to_direction_map(community)
   def shape_direction_map(shapes, processes)
     ArrayUtils.zip_by(shapes, processes) { |shape, process|
       shape[:transaction_process_id] == process[:id]
