2f43b0d657beb4d1a6664e50e71f688d5fa1d136
Mikko Koski <mikkokos@gmail.com>
2015-04-13 18:31:04 +0300

    TransactionMailer: Use with_locale

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -31,113 +31,127 @@ class TransactionMailer < ActionMailer::Base
 
     # TODO Now that we have splitted "new message", we could be more specific here, and say that this message
     # is about a new transaction!
-    premailer_mail(
-      mail_params(recipient, community, t("emails.new_message.you_have_a_new_message", :sender_name => sender_name))) do |format|
+    with_locale(recipient.locale, community.id) do
+      premailer_mail(
+        mail_params(recipient, community, t("emails.new_message.you_have_a_new_message", :sender_name => sender_name))) do |format|
         format.html {
           render locals: {
-            recipient: recipient,
-            reply_url: reply_url,
-            sender_name: sender_name,
-          }
+                   recipient: recipient,
+                   reply_url: reply_url,
+                   sender_name: sender_name,
+                 }
         }
+      end
     end
   end
 
   def transaction_preauthorized(transaction)
     @transaction = transaction
-    @community = @transaction.community
+    @community = transaction.community
 
-    set_up_urls(@transaction.author, @transaction.community)
+    recipient = transaction.author
+    set_up_urls(recipient, transaction.community)
+    with_locale(recipient.locale, transaction.community.id) do
 
-    payment_type = MarketplaceService::Community::Query.payment_type(@community.id)
-    gateway_expires = MarketplaceService::Transaction::Entity.authorization_expiration_period(payment_type)
+      payment_type = MarketplaceService::Community::Query.payment_type(@community.id)
+      gateway_expires = MarketplaceService::Transaction::Entity.authorization_expiration_period(payment_type)
 
-    premailer_mail(
-      mail_params(
-        @recipient,
-        @community,
-        t("emails.transaction_preauthorized.subject", requester: @transaction.starter.name, listing_title: @transaction.listing.title))) do |format|
-      format.html {
-        render locals: {
-          payment_expires_in_days: gateway_expires
+      premailer_mail(
+        mail_params(
+          @recipient,
+          @community,
+          t("emails.transaction_preauthorized.subject", requester: transaction.starter.name, listing_title: transaction.listing.title))) do |format|
+        format.html {
+          render locals: {
+                   payment_expires_in_days: gateway_expires
+                 }
         }
-      }
+      end
     end
   end
 
   def transaction_preauthorized_reminder(transaction)
     @transaction = transaction
-    @community = @transaction.community
+    @community = transaction.community
 
-    set_up_urls(@transaction.author, @transaction.community)
+    recipient = transaction.author
+    set_up_urls(recipient, transaction.community)
+    with_locale(recipient.locale, transaction.community.id) do
 
-    premailer_mail(
-      mail_params(
-        @recipient,
-        @community,
-        t("emails.transaction_preauthorized_reminder.subject", requester: @transaction.starter.name, listing_title: @transaction.listing.title)))
+      premailer_mail(
+        mail_params(
+          @recipient,
+          @community,
+          t("emails.transaction_preauthorized_reminder.subject", requester: transaction.starter.name, listing_title: transaction.listing.title)))
+    end
   end
 
   def braintree_new_payment(payment, community)
+    recipient = payment.recipient
     prepare_template(community, payment.recipient, "email_about_new_payments")
+    with_locale(recipient.locale, community.id) do
 
-    service_fee = payment.total_commission
-    you_get = payment.seller_gets
+      service_fee = payment.total_commission
+      you_get = payment.seller_gets
 
-    transaction = payment.transaction
-    unit_type = translate_quantity_unit(transaction.unit_type)
-    duration = payment.transaction.booking.present? ? payment.transaction.booking.duration : nil
+      transaction = payment.transaction
+      unit_type = translate_quantity_unit(transaction.unit_type)
+      duration = payment.transaction.booking.present? ? payment.transaction.booking.duration : nil
 
-    premailer_mail(:to => payment.recipient.confirmed_notification_emails_to,
-         :from => community_specific_sender(community),
-         :subject => t("emails.new_payment.new_payment")) { |format|
-      format.html {
-        render "braintree_payment_receipt_to_seller", locals: {
-          conversation_url: person_transaction_url(payment.recipient, @url_params.merge({:id => payment.transaction.id.to_s})),
-          listing_title: payment.transaction.listing_title,
-          price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
-          listing_price: humanized_money_with_symbol(payment.transaction.unit_price),
-          listing_quantity: payment.transaction.listing_quantity,
-          duration: duration,
-          payment_total: humanized_money_with_symbol(payment.total_sum),
-          payment_service_fee: humanized_money_with_symbol(-service_fee),
-          payment_seller_gets: humanized_money_with_symbol(you_get),
-          payer_full_name: payment.payer.name(community),
-          payer_given_name: payment.payer.given_name_or_username,
-          automatic_confirmation_days: payment.transaction.automatic_confirmation_after_days,
-          show_money_will_be_transferred_note: true
+      premailer_mail(:to => payment.recipient.confirmed_notification_emails_to,
+                     :from => community_specific_sender(community),
+                     :subject => t("emails.new_payment.new_payment")) { |format|
+        format.html {
+          render "braintree_payment_receipt_to_seller", locals: {
+                   conversation_url: person_transaction_url(payment.recipient, @url_params.merge({:id => payment.transaction.id.to_s})),
+                   listing_title: payment.transaction.listing_title,
+                   price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
+                   listing_price: humanized_money_with_symbol(payment.transaction.unit_price),
+                   listing_quantity: payment.transaction.listing_quantity,
+                   duration: duration,
+                   payment_total: humanized_money_with_symbol(payment.total_sum),
+                   payment_service_fee: humanized_money_with_symbol(-service_fee),
+                   payment_seller_gets: humanized_money_with_symbol(you_get),
+                   payer_full_name: payment.payer.name(community),
+                   payer_given_name: payment.payer.given_name_or_username,
+                   automatic_confirmation_days: payment.transaction.automatic_confirmation_after_days,
+                   show_money_will_be_transferred_note: true
+                 }
         }
       }
-    }
+    end
   end
 
   def braintree_receipt_to_payer(payment, community)
-    prepare_template(community, payment.payer, "email_about_new_payments")
+    recipient = payment.payer
+    prepare_template(community, recipient, "email_about_new_payments")
+    with_locale(recipient.locale, community.id) do
 
-    unit_type = translate_quantity_unit(payment.transaction.unit_type)
-    duration = payment.transaction.booking.present? ? payment.transaction.booking.duration : nil
+      unit_type = translate_quantity_unit(payment.transaction.unit_type)
+      duration = payment.transaction.booking.present? ? payment.transaction.booking.duration : nil
 
-    premailer_mail(:to => payment.payer.confirmed_notification_emails_to,
-         :from => community_specific_sender(community),
-         :subject => t("emails.receipt_to_payer.receipt_of_payment")) { |format|
-      format.html {
-        render "payment_receipt_to_buyer", locals: {
-          conversation_url: person_transaction_url(payment.payer, @url_params.merge({:id => payment.transaction.id.to_s})),
-          listing_title: payment.transaction.listing_title,
-          price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
-          listing_price: humanized_money_with_symbol(payment.transaction.unit_price),
-          listing_quantity: payment.transaction.listing_quantity,
-          duration: duration,
-          payment_total: humanized_money_with_symbol(payment.total_sum),
-          subtotal: humanized_money_with_symbol(payment.total_sum),
-          shipping_total: nil,
-          recipient_full_name: payment.recipient.name(community),
-          recipient_given_name: payment.recipient.given_name_or_username,
-          automatic_confirmation_days: payment.transaction.automatic_confirmation_after_days,
-          show_money_will_be_transferred_note: true
+      premailer_mail(:to => payment.payer.confirmed_notification_emails_to,
+                     :from => community_specific_sender(community),
+                     :subject => t("emails.receipt_to_payer.receipt_of_payment")) { |format|
+        format.html {
+          render "payment_receipt_to_buyer", locals: {
+                   conversation_url: person_transaction_url(payment.payer, @url_params.merge({:id => payment.transaction.id.to_s})),
+                   listing_title: payment.transaction.listing_title,
+                   price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
+                   listing_price: humanized_money_with_symbol(payment.transaction.unit_price),
+                   listing_quantity: payment.transaction.listing_quantity,
+                   duration: duration,
+                   payment_total: humanized_money_with_symbol(payment.total_sum),
+                   subtotal: humanized_money_with_symbol(payment.total_sum),
+                   shipping_total: nil,
+                   recipient_full_name: payment.recipient.name(community),
+                   recipient_given_name: payment.recipient.given_name_or_username,
+                   automatic_confirmation_days: payment.transaction.automatic_confirmation_after_days,
+                   show_money_will_be_transferred_note: true
+                 }
         }
       }
-    }
+    end
   end
 
   # seller_model, buyer_model and community can be passed as params for testing purposes
@@ -151,32 +165,34 @@ class TransactionMailer < ActionMailer::Base
     gateway_fee = transaction[:payment_gateway_fee]
 
     prepare_template(community, seller_model, "email_about_new_payments")
+    with_locale(seller_model.locale, community.id) do
 
-    you_get = payment_total - service_fee - gateway_fee
+      you_get = payment_total - service_fee - gateway_fee
 
-    unit_type = translate_quantity_unit(transaction[:unit_type])
+      unit_type = translate_quantity_unit(transaction[:unit_type])
 
-    premailer_mail(:to => seller_model.confirmed_notification_emails_to,
-         :from => community_specific_sender(community),
-         :subject => t("emails.new_payment.new_payment")) do |format|
-      format.html {
-        render "paypal_payment_receipt_to_seller", locals: {
-          conversation_url: person_transaction_url(seller_model, @url_params.merge(id: transaction[:id])),
-          listing_title: transaction[:listing_title],
-          price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
-          listing_price: humanized_money_with_symbol(transaction[:listing_price]),
-          listing_quantity: transaction[:listing_quantity],
-          duration: transaction[:booking].present? ? transaction[:booking][:duration] : nil,
-          subtotal: humanized_money_with_symbol(transaction[:item_total]),
-          payment_total: humanized_money_with_symbol(payment_total),
-          shipping_total: humanized_money_with_symbol(transaction[:shipping_price]),
-          payment_service_fee: humanized_money_with_symbol(-service_fee),
-          paypal_gateway_fee: humanized_money_with_symbol(-gateway_fee),
-          payment_seller_gets: humanized_money_with_symbol(you_get),
-          payer_full_name: buyer_model.name(community),
-          payer_given_name: buyer_model.given_name_or_username,
+      premailer_mail(:to => seller_model.confirmed_notification_emails_to,
+                     :from => community_specific_sender(community),
+                     :subject => t("emails.new_payment.new_payment")) do |format|
+        format.html {
+          render "paypal_payment_receipt_to_seller", locals: {
+                   conversation_url: person_transaction_url(seller_model, @url_params.merge(id: transaction[:id])),
+                   listing_title: transaction[:listing_title],
+                   price_per_unit_title: t("emails.new_payment.price_per_unit_type", unit_type: unit_type),
+                   listing_price: humanized_money_with_symbol(transaction[:listing_price]),
+                   listing_quantity: transaction[:listing_quantity],
+                   duration: transaction[:booking].present? ? transaction[:booking][:duration] : nil,
+                   subtotal: humanized_money_with_symbol(transaction[:item_total]),
+                   payment_total: humanized_money_with_symbol(payment_total),
+                   shipping_total: humanized_money_with_symbol(transaction[:shipping_price]),
+                   payment_service_fee: humanized_money_with_symbol(-service_fee),
+                   paypal_gateway_fee: humanized_money_with_symbol(-gateway_fee),
+                   payment_seller_gets: humanized_money_with_symbol(you_get),
+                   payer_full_name: buyer_model.name(community),
+                   payer_given_name: buyer_model.given_name_or_username,
+                 }
         }
-      }
+      end
     end
   end
 
@@ -187,30 +203,32 @@ class TransactionMailer < ActionMailer::Base
     community ||= Community.find(transaction[:community_id])
 
     prepare_template(community, buyer_model, "email_about_new_payments")
+    with_locale(buyer_model.locale, community.id) do
 
-    unit_type = translate_quantity_unit(transaction[:unit_type])
+      unit_type = translate_quantity_unit(transaction[:unit_type])
 
-    premailer_mail(:to => buyer_model.confirmed_notification_emails_to,
-         :from => community_specific_sender(community),
-         :subject => t("emails.receipt_to_payer.receipt_of_payment")) { |format|
-      format.html {
-        render "payment_receipt_to_buyer", locals: {
-          conversation_url: person_transaction_url(buyer_model, @url_params.merge({:id => transaction[:id]})),
-          listing_title: transaction[:listing_title],
-          price_per_unit_title: t("emails.receipt_to_payer.price_per_unit_type", unit_type: unit_type),
-          listing_price: humanized_money_with_symbol(transaction[:listing_price]),
-          listing_quantity: transaction[:listing_quantity],
-          duration: transaction[:booking].present? ? transaction[:booking][:duration] : nil,
-          subtotal: humanized_money_with_symbol(transaction[:item_total]),
-          shipping_total: humanized_money_with_symbol(transaction[:shipping_price]),
-          payment_total: humanized_money_with_symbol(transaction[:payment_total]),
-          recipient_full_name: seller_model.name(community),
-          recipient_given_name: seller_model.given_name_or_username,
-          automatic_confirmation_days: nil,
-          show_money_will_be_transferred_note: false
+      premailer_mail(:to => buyer_model.confirmed_notification_emails_to,
+                     :from => community_specific_sender(community),
+                     :subject => t("emails.receipt_to_payer.receipt_of_payment")) { |format|
+        format.html {
+          render "payment_receipt_to_buyer", locals: {
+                   conversation_url: person_transaction_url(buyer_model, @url_params.merge({:id => transaction[:id]})),
+                   listing_title: transaction[:listing_title],
+                   price_per_unit_title: t("emails.receipt_to_payer.price_per_unit_type", unit_type: unit_type),
+                   listing_price: humanized_money_with_symbol(transaction[:listing_price]),
+                   listing_quantity: transaction[:listing_quantity],
+                   duration: transaction[:booking].present? ? transaction[:booking][:duration] : nil,
+                   subtotal: humanized_money_with_symbol(transaction[:item_total]),
+                   shipping_total: humanized_money_with_symbol(transaction[:shipping_price]),
+                   payment_total: humanized_money_with_symbol(transaction[:payment_total]),
+                   recipient_full_name: seller_model.name(community),
+                   recipient_given_name: seller_model.given_name_or_username,
+                   automatic_confirmation_days: nil,
+                   show_money_will_be_transferred_note: false
+                 }
         }
       }
-    }
+    end
   end
 
   private
@@ -226,7 +244,6 @@ class TransactionMailer < ActionMailer::Base
     @current_community = community
     @recipient = recipient
     @url_params = build_url_params(community, recipient)
-    set_locale(recipient.locale)
   end
 
   def mail_params(recipient, community, subject)
