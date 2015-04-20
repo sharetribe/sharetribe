b5b29648b9a992358617e82eb0eca4fa99721330
Mikko Koski <mikkokos@gmail.com>
2015-04-20 11:03:35 +0300

    Do not delete all checkbox group answers, if one option is deleted

--- app/models/custom_field_option_selection.rb
+++ app/models/custom_field_option_selection.rb
@@ -14,9 +14,6 @@
 #
 
 class CustomFieldOptionSelection < ActiveRecord::Base
-  # WARNING! This expects that there's only one selection (Dropdown).
-  # If there are multiple selections, the custom_field_value should not be deleted
-  # if one of the selected options are deleted
-  belongs_to :custom_field_value, dependent: :destroy
+  belongs_to :custom_field_value
   belongs_to :custom_field_option
 end
