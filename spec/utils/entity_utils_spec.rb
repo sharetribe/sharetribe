f0a89ebe9c17f8e148f1fc7a23f11cc9e34d62d4
Mikko Koski <mikkokos@gmail.com>
2015-04-21 08:47:31 +0300

    Add benchmark test

--- spec/utils/entity_utils_spec.rb
+++ spec/utils/entity_utils_spec.rb
@@ -240,4 +240,31 @@ describe EntityUtils do
     expect{entity.call({tags: nil})}
       .to_not raise_error
   end
+
+  # You can enable this test to measure the performance
+  xit "#define_builder is fast" do
+    name_details_entity = EntityUtils.define_builder(
+      [:first, :string, :mandatory],
+      [:middle, :string, default: "Middle"],
+      [:last, :string, :mandatory]
+    )
+
+    entity = EntityUtils.define_builder(
+      [:name, :mandatory, entity: name_details_entity]
+    )
+    bm = 1000 * Benchmark.realtime {
+      1000.times {
+        entity.call(
+          {
+            name: {
+              first: "John",
+              last: "Doe"
+            }
+          }
+        )
+      }
+    }
+
+    expect(bm).to be < 50
+  end
 end
