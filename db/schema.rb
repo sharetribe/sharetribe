fbf5bc5698b619e1579efa83da5145351bdcc0e1
Mikko Koski <mikkokos@gmail.com>
2015-04-29 19:25:51 +0300

    Migration: Add missing processes (none + author_is_seller true & false)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150426113955) do
+ActiveRecord::Schema.define(:version => 20150429155804) do
 
   create_table "auth_tokens", :force => true do |t|
     t.string   "token"
