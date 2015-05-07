293c1003fabaa6fa17d98e6da12043efbb661afd
Mikko Koski <mikkokos@gmail.com>
2015-05-07 22:46:50 +0300

    Close listings

--- app/services/listing_service/store/listing.rb
+++ app/services/listing_service/store/listing.rb
@@ -1,21 +1,40 @@
 module ListingService::Store::Listing
   ListingModel = ::Listing
 
+  UpdateListing = EntityUtils.define_builder(
+    [:open, :bool]
+  )
+
   module_function
 
   def count(community_id:, query:)
-    sql_query = ListingModel
-                .includes(:communities)
-                .where(communities: {id: community_id})
-                .where(query.except(:open))
+    sql_query = where_community(community_id).where(query.except(:open))
 
     sql_query =
-      if query[:open]
+      if query[:open] == true
         sql_query.currently_open
+      elsif query[:open] == false
+        raise NotImplementedError.new("Count of closed listings is not implemented")
       else
         sql_query
       end
 
     sql_query.count
+  end
+
+  def update_all(community_id: nil, query:, opts:)
+    if community_id != nil
+      raise NotImplementedError.new("Community id is ignored")
+    end
+
+    ListingModel.where(query).update_all(opts)
+  end
+
+  # private
+
+  def where_community(community_id)
+    ListingModel
+      .includes(:communities)
+      .where(communities: {id: community_id})
   end
 end
--- spec/services/listing_service/api/listings_spec.rb
+++ spec/services/listing_service/api/listings_spec.rb
@@ -13,12 +13,26 @@ describe ListingService::API::Listings do
     FactoryGirl.create(:listing, opts.merge(communities: [community]))
   end
 
+  before(:each) do
+    2.times { create_listing(listing_shape_id: sell_id) }
+    1.times { create_listing(listing_shape_id: rent_id) }
+    1.times { create_listing(listing_shape_id: rent_id, open: false) }
+  end
+
   describe "#count" do
     context "success" do
       it "returns count of listing" do
-        2.times { create_listing(listing_shape_id: sell_id) }
-        1.times { create_listing(listing_shape_id: rent_id) }
-        1.times { create_listing(listing_shape_id: rent_id, open: false) }
+        all_count = listings_api.listings.count(
+          community_id: community.id)
+
+        expect(all_count.success).to eq true
+        expect(all_count.data).to eq 4
+
+        all_open = listings_api.listings.count(
+          community_id: community.id, query: {open: true})
+
+        expect(all_open.success).to eq true
+        expect(all_open.data).to eq 3
 
         sell_count = listings_api.listings.count(
           community_id: community.id, query: { listing_shape_id: sell_id })
@@ -52,6 +66,27 @@ describe ListingService::API::Listings do
         expect(sell_count.data).to eq 0
         expect(rent_count.success).to eq true
         expect(rent_count.data).to eq 0
+      end
+    end
+  end
+
+  describe "#update" do
+    context "success" do
+      it "closes listings with specific listing shape" do
+        all_open_before = listings_api.listings.count(
+          community_id: community.id, query: {open: true}).data
+
+        expect(all_open_before).to eq 3
+
+        listings_api.listings.update_all(
+          community_id: nil,
+          query: {listing_shape_id: sell_id},
+          opts: {open: false})
+
+        all_open_after = listings_api.listings.count(
+          community_id: community.id, query: {open: true}).data
+
+        expect(all_open_after).to eq 1
       end
     end
   end
