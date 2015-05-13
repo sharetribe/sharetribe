9bda6906366ce9e7db07a8c23221045c68b58a92
Sampo Toiva <sampo.toiva@gmail.com>
2015-05-13 10:34:51 +0300

    Update memcachier production confs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,15 @@ Kassi::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :dalli_store, { :namespace => "sharetribe", :compress => true }
+  config.cache_store = :dalli_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","), {
+    username: ENV["MEMCACHIER_USERNAME"],
+    password: ENV["MEMCACHIER_PASSWORD"],
+    failover:  true,
+    socket_timeout: 1.5,
+    socket_failure_delay:  0.2,
+    namespace: "sharetribe-production",
+    compress: true
+  }
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
