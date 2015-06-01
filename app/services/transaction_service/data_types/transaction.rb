2c8b4b619a772cf0b1f9c47bc97c8ee2c653d772
Mikko Koski <mikkokos@gmail.com>
2015-06-01 08:57:29 +0300

    Remove :piece unit type

--- app/services/transaction_service/data_types/transaction.rb
+++ app/services/transaction_service/data_types/transaction.rb
@@ -15,7 +15,7 @@ module TransactionService::DataTypes::Transaction
     [:shipping_price, :money],
     [:listing_author_id, :string, :mandatory],
     [:listing_quantity, :fixnum, default: 1],
-    [:unit_type, :to_symbol, one_of: [:piece, :hour, :day, :night, :week, :month, :custom, nil]],
+    [:unit_type, :to_symbol, one_of: [:hour, :day, :night, :week, :month, :custom, nil]],
     [:unit_tr_key, :string, :optional],
     [:unit_selector_tr_key, :string, :optional],
     [:automatic_confirmation_after_days, :fixnum],
