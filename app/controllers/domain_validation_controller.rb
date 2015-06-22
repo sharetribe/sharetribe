54ffe4035fd6b6bc73447e064dd8fab3aa967220
Sampo Toiva <sampo.toiva@gmail.com>
2015-06-22 12:15:43 +0300

    Skip filters

--- app/controllers/domain_validation_controller.rb
+++ app/controllers/domain_validation_controller.rb
@@ -1,4 +1,8 @@
 class DomainValidationController < ApplicationController
+
+  skip_before_filter :verify_authenticity_token, :fetch_logged_in_user, :redirect_to_marketplace_domain, :fetch_community_membership
+  skip_filter :check_email_confirmation
+
   def index
     if params[:dv_file] == @current_community.dv_test_file_name
       render text: @current_community.dv_test_file, content_type: "text/plain"
