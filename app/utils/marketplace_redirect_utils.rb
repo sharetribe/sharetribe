c511d70c25d7647139ab9a39a9f2705fbd3b74a3
Mikko Koski <mikkokos@gmail.com>
2015-07-29 17:00:09 +0300

    Protocol redirect 1/3
    - [X] Redirect if always_use_ssl
    - [ ] No need to redirect if the request comes from proxy
    - [ ] Don't redirect robots.txt and DV

--- app/utils/marketplace_redirect_utils.rb
+++ app/utils/marketplace_redirect_utils.rb
@@ -20,17 +20,21 @@ module MarketplaceRedirectUtils
 
     new_protocol_opt = always_use_ssl ? "https" : (protocol == "http://" ? "http" : "https")
     new_protocol_url = always_use_ssl ? "https://" : (protocol == "http://" ? "http://" : "https://")
+    protocol_needs_redirect = protocol != new_protocol_url
+    new_status = protocol_needs_redirect ? :moved_permanently : nil
 
     if !found_community
       if no_communities
-        block.call(new_community_path.merge(status: :found, protocol: new_protocol_opt))
+        block.call(new_community_path.merge(status: (new_status || :found), protocol: new_protocol_opt))
       else
-        block.call(community_not_found_url.merge(status: :found, protocol: new_protocol_opt))
+        block.call(community_not_found_url.merge(status: (new_status || :found), protocol: new_protocol_opt))
       end
     elsif community_deleted
-      block.call(community_not_found_url.merge(status: :moved_permanently, protocol: new_protocol_opt))
+      block.call(community_not_found_url.merge(status: (new_status || :moved_permanently), protocol: new_protocol_opt))
     elsif community_domain.present? && redirect_to_domain && host != community_domain
-      block.call({url: "#{new_protocol_url}#{community_domain}#{port_string}#{fullpath}", status: :moved_permanently})
+      block.call({url: "#{new_protocol_url}#{community_domain}#{port_string}#{fullpath}", status: (new_status || :moved_permanently)})
+    elsif protocol_needs_redirect
+      block.call({url: "#{new_protocol_url}#{host}#{port_string}#{fullpath}", status: new_status})
     end
 
   end
--- spec/utils/marketplace_redirect_utils_spec.rb
+++ spec/utils/marketplace_redirect_utils_spec.rb
@@ -12,7 +12,7 @@ describe MarketplaceRedirectUtils do
     it "does not redirect to full domain if the host is already the full domain" do
       expect_redirect(
         host: "www.marketplace.com",
-        protocol: "https",
+        protocol: "https://",
         always_use_ssl: true,
         fullpath: "/listings",
         port_string: "",
@@ -188,6 +188,24 @@ describe MarketplaceRedirectUtils do
         community_domain: "www.marketplace.com").to eq(url: "https://www.marketplace.com/listings", status: :moved_permanently)
     end
 
+    it "redirects to https even if there's no other reason to do redirect" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        headers: {},
+        is_ssl: false,
+        always_use_ssl: true,
+        protocol: "http://",
+        fullpath: "/listings",
+        port_string: "",
+        redirect_to_domain: false,
+        community_not_found_url: {route_name: :not_found},
+        community_deleted: false,
+        no_communities: false,
+        found_community: true,
+        new_community_path: {route_name: :new_community},
+        community_domain: nil).to eq(url: "https://marketplace.sharetribe.com/listings", status: :moved_permanently)
+    end
+
     it "redirects to protocol in use if always_use_ssl configuration is set false" do
       expect_redirect(
         host: "marketplace.sharetribe.com",
@@ -204,6 +222,24 @@ describe MarketplaceRedirectUtils do
         found_community: true,
         new_community_path: {route_name: :new_community},
         community_domain: "www.marketplace.com").to eq(url: "http://www.marketplace.com/listings", status: :moved_permanently)
+    end
+
+    it "redirects with moved permanently if the protocol needs redirect even if it would otherwise used found status" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        protocol: "http://",
+        always_use_ssl: true,
+        fullpath: "/listings",
+        port_string: "",
+        headers: {},
+        is_ssl: true,
+        redirect_to_domain: nil,
+        community_not_found_url: {route_name: :not_found},
+        community_deleted: nil,
+        no_communities: false,
+        found_community: false,
+        new_community_path: {route_name: :new_community},
+        community_domain: nil).to eq(route_name: :not_found, status: :moved_permanently, protocol: "https")
     end
   end
 end
