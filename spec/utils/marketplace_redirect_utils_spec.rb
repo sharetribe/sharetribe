15f0d310f6a15c6bbf28f27fd6ea9249e4247bf4
Mikko Koski <mikkokos@gmail.com>
2015-07-29 11:27:17 +0300

    Add test descriptions

--- spec/utils/marketplace_redirect_utils_spec.rb
+++ spec/utils/marketplace_redirect_utils_spec.rb
@@ -7,45 +7,55 @@ describe MarketplaceRedirectUtils do
     expect(url_and_status)
   end
 
-  it "redirects to domain" do
-    expect_redirect(
-      host: "www.marketplace.com",
-      protocol: "https",
-      fullpath: "/listings",
-      port_string: "",
-      community_domain: "www.marketplace.com",
-      redirect_to_domain: true).to eq(nil)
+  describe "#needs_redirect" do
 
-    expect_redirect(
-      host: "marketplace.sharetribe.com",
-      protocol: "https://",
-      port_string: "",
-      fullpath: "/listings",
-      redirect_to_domain: false).to eq(nil)
+    it "does not redirect to full domain if the host is already the full domain" do
+      expect_redirect(
+        host: "www.marketplace.com",
+        protocol: "https",
+        fullpath: "/listings",
+        port_string: "",
+        community_domain: "www.marketplace.com",
+        redirect_to_domain: true).to eq(nil)
+    end
 
-    expect_redirect(
-      host: "marketplace.sharetribe.com",
-      protocol: "https://",
-      fullpath: "/listings",
-      port_string: "",
-      redirect_to_domain: true,
-      community_domain: "www.marketplace.com").to eq(["https://www.marketplace.com/listings", :moved_permanently])
+    it "does not redirect to full domain if full domain is not provided" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        protocol: "https://",
+        port_string: "",
+        fullpath: "/listings",
+        redirect_to_domain: false).to eq(nil)
+    end
 
-    expect_redirect(
-      host: "marketplace.sharetribe.com",
-      protocol: "https://",
-      fullpath: "/listings",
-      port_string: "",
-      redirect_to_domain: false,
-      community_domain: "www.marketplace.com").to eq(nil)
+    it "redirects to full domain, if marketplace is accessed with the subdomain (ident) and full domain is provided and redirect_to_domain is true" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        protocol: "https://",
+        fullpath: "/listings",
+        port_string: "",
+        redirect_to_domain: true,
+        community_domain: "www.marketplace.com").to eq(["https://www.marketplace.com/listings", :moved_permanently])
+    end
 
-    expect_redirect(
-      host: "marketplace.sharetribe.com",
-      protocol: "https://",
-      fullpath: "/listings",
-      port_string: ":3333",
-      redirect_to_domain: true,
-      community_domain: "www.marketplace.com").to eq(["https://www.marketplace.com:3333/listings", :moved_permanently])
+    it "does not redirect if redirect_to_domain is false" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        protocol: "https://",
+        fullpath: "/listings",
+        port_string: "",
+        redirect_to_domain: false,
+        community_domain: "www.marketplace.com").to eq(nil)
+    end
+
+    it "includes port" do
+      expect_redirect(
+        host: "marketplace.sharetribe.com",
+        protocol: "https://",
+        fullpath: "/listings",
+        port_string: ":3333",
+        redirect_to_domain: true,
+        community_domain: "www.marketplace.com").to eq(["https://www.marketplace.com:3333/listings", :moved_permanently])
+    end
   end
-
 end
