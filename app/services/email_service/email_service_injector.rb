4992a770b0f8419aeb993e4b393922fccf9e4456
Mikko Koski <mikkokos@gmail.com>
2015-08-06 09:36:31 +0300

    Return existing address_api instance

--- app/services/email_service/email_service_injector.rb
+++ app/services/email_service/email_service_injector.rb
@@ -1,6 +1,6 @@
 module EmailService::EmailServiceInjector
   def addresses_api
-    @addresses = EmailService::API::Addresses.new(
+    @addresses ||= EmailService::API::Addresses.new(
       default_sender: APP_CONFIG.sharetribe_mail_from_address
     )
   end
