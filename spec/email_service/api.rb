bfff74ca07f49b5792b8e302ddf7f175ac216dd6
Olli Vanhapiha <olli.vanhapiha@gmail.com>
2015-08-11 10:02:23 +0300

    Move and refactor addresses spec

--- spec/email_service/api.rb
+++
@@ -1,9 +0,0 @@
-module EmailService::API
-  class Api
-
-    def self.addresses
-      @addresses ||= EmailService::API::Addresses.new(
-        default_sender: "Default Sender Name <default_sender@example.com.invalid>")
-    end
-  end
-end
--- spec/services/email_service/api/addresses_spec.rb
+++ spec/services/email_service/api/addresses_spec.rb
@@ -1,22 +1,26 @@
-require_relative '../api'
-
 describe EmailService::API::Addresses do
 
-  let(:emails_api) { EmailService::API::Api }
+  AddressStore = EmailService::Store::Address
+
+  let(:addresses_wo_ses) do
+    EmailService::API::Addresses.new(
+      default_sender: "Default Sender Name <default_sender@example.com.invalid>",
+      ses_client: nil)
+  end
+
   let(:now) { Time.zone.local(2015, 8, 7) }
 
   describe "#get_sender" do
     context "user defined sender address" do
 
       it "gets sender address by community id" do
-        emails_api.addresses.create(
+        addresses_wo_ses.create(
           community_id: 123, address: {
             name: "Email Sender Name",
             email: "hello@mymarketplace.invalid",
-            verification_status: :verified
           })
 
-        res = emails_api.addresses.get_sender(community_id: 123)
+        res = addresses_wo_ses.get_sender(community_id: 123)
 
         expect(res.success).to eq(true)
         expect(res.data).to eq(
@@ -26,23 +30,21 @@ describe EmailService::API::Addresses do
       end
 
       it "gets the last verified sender address" do
-        emails_api.addresses.create(
+        addresses_wo_ses.create(
           community_id: 123, address: {
             name: "Email Sender Name",
             email: "hello@mymarketplace.invalid",
-            verification_status: :verified
           })
 
         Timecop.travel(1.second.from_now) do
 
-          emails_api.addresses.create(
+          addresses_wo_ses.create(
             community_id: 123, address: {
               name: "Email 2 Sender Name",
               email: "hello2@mymarketplace.invalid",
-              verification_status: :verified
             })
 
-          res = emails_api.addresses.get_sender(community_id: 123)
+          res = addresses_wo_ses.get_sender(community_id: 123)
 
           expect(res.success).to eq(true)
           expect(res.data).to eq(
@@ -54,13 +56,13 @@ describe EmailService::API::Addresses do
       end
 
       it "allows nil name" do
-        emails_api.addresses.create(
+        addresses_wo_ses.create(
           community_id: 123, address: {
             email: "hello@mymarketplace.invalid",
             verification_status: :verified
           })
 
-        res = emails_api.addresses.get_sender(community_id: 123)
+        res = addresses_wo_ses.get_sender(community_id: 123)
 
         expect(res.success).to eq(true)
         expect(res.data).to eq(
@@ -75,40 +77,41 @@ describe EmailService::API::Addresses do
     context "default sender address" do
 
       it "returns default address if verified user defined address is not found" do
-        emails_api.addresses.create(
-          community_id: 123, address: {
+        AddressStore.create(
+          community_id: 123,
+          address: {
             name: "Email Sender Name",
             email: "hello@mymarketplace.invalid",
             verification_status: :requested,
           })
 
-        res = emails_api.addresses.get_sender(community_id: 123)
+        res = addresses_wo_ses.get_sender(community_id: 123)
 
         expect(res.success).to eq(true)
-        expect(res.data).to eq(
-                              type: :default,
-                              display_format: "Default Sender Name <default_sender@example.com.invalid>",
-                              smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
+        expect(res.data)
+          .to eq(type: :default,
+                 display_format: "Default Sender Name <default_sender@example.com.invalid>",
+                 smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
       end
 
       it "returns default address user defined address is not set" do
-        res = emails_api.addresses.get_sender(community_id: 999)
+        res = addresses_wo_ses.get_sender(community_id: 999)
 
         expect(res.success).to eq(true)
-        expect(res.data).to eq(
-                              type: :default,
-                              display_format: "Default Sender Name <default_sender@example.com.invalid>",
-                              smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
+        expect(res.data)
+          .to eq(type: :default,
+                 display_format: "Default Sender Name <default_sender@example.com.invalid>",
+                 smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
       end
 
       it "returns default address if community id is nil" do
-        res = emails_api.addresses.get_sender(community_id: nil)
+        res = addresses_wo_ses.get_sender(community_id: nil)
 
         expect(res.success).to eq(true)
-        expect(res.data).to eq(
-                              type: :default,
-                              display_format: "Default Sender Name <default_sender@example.com.invalid>",
-                              smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
+        expect(res.data)
+          .to eq(type: :default,
+                 display_format: "Default Sender Name <default_sender@example.com.invalid>",
+                 smtp_format: "Default Sender Name <default_sender@example.com.invalid>")
       end
     end
   end
@@ -119,49 +122,46 @@ describe EmailService::API::Addresses do
       it "returns unique user defined address by community id and email" do
         now = Time.zone.local(2015, 8, 10)
         Timecop.freeze(now) do
-          created = emails_api.addresses.create(
+          created = addresses_wo_ses.create(
             community_id: 123, address: {
               name: "Email Sender Name",
               email: "hello@mymarketplace.invalid",
-              verification_status: :verified
             }).data
 
-          res = emails_api.addresses.get_user_defined(community_id: 123)
+          res = addresses_wo_ses.get_user_defined(community_id: 123)
           expect(res.success).to eq(true)
-          expect(res.data).to eq({
-                                   id: created[:id],
-                                   community_id: 123,
-                                   name: "Email Sender Name",
-                                   email: "hello@mymarketplace.invalid",
-                                   updated_at: created[:updated_at],
-                                   verification_status: :verified,
-                                   verification_requested_at: nil,
-                                   display_format: "Email Sender Name <hello@mymarketplace.invalid>",
-                                   smtp_format: "\"Email Sender Name\" <hello@mymarketplace.invalid>"})
+          expect(res.data)
+            .to eq({id: created[:id],
+                    community_id: 123,
+                    name: "Email Sender Name",
+                    email: "hello@mymarketplace.invalid",
+                    updated_at: created[:updated_at],
+                    verification_status: :verified,
+                    verification_requested_at: nil,
+                    display_format: "Email Sender Name <hello@mymarketplace.invalid>",
+                    smtp_format: "\"Email Sender Name\" <hello@mymarketplace.invalid>"})
         end
       end
 
       it "returns always the latest email" do
         now = Time.zone.local(2015, 8, 10)
         Timecop.freeze(now) do
-          emails_api.addresses.create(
+          addresses_wo_ses.create(
             community_id: 123, address: {
               name: "Email Sender Name",
               email: "hello@mymarketplace.invalid",
-              verification_status: :verified,
             })
         end
 
         Timecop.freeze(now + 1.second) do
 
-          created = emails_api.addresses.create(
+          created = addresses_wo_ses.create(
             community_id: 123, address: {
               name: "Email 2 Sender Name",
               email: "hello2@mymarketplace.invalid",
-              verification_status: :verified,
             }).data
 
-          res = emails_api.addresses.get_user_defined(community_id: 123)
+          res = addresses_wo_ses.get_user_defined(community_id: 123)
           expect(res.success).to eq(true)
           expect(res.data).to eq({
                                    id: created[:id],
@@ -181,7 +181,7 @@ describe EmailService::API::Addresses do
 
     context "error" do
       it "returns error if none found" do
-        res = emails_api.addresses.get_user_defined(community_id: 999)
+        res = addresses_wo_ses.get_user_defined(community_id: 999)
         expect(res.success).to eq(false)
       end
     end
