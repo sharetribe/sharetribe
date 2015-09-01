52569cfc02845575ef00d7dbefd8eb3cd613b76c
Mikko Koski <mikkokos@gmail.com>
2015-09-01 10:27:36 +0300

    Explicitly say to jQuery that we expect the response to be HTML
    - Otherwise jQuery tries to guess the response type and in some corner cases it will guess it wrong

--- app/assets/javascripts/googlemaps.js
+++ app/assets/javascripts/googlemaps.js
@@ -569,8 +569,12 @@ function addListingMarkers(listings, community_location_lat, community_location_
             infowindow.setMaxHeight(150);
             infowindow.setMinHeight(150);
             infowindow.open(map,marker);
-            $.get('/' + locale + '/listing_bubble/' + entry["id"], function(data) {
-              $('#map_bubble').html(data);
+            $.ajax({
+              url: '/' + locale + '/listing_bubble/' + entry.id,
+              dataType: "html",
+              success: function(data) {
+                $('#map_bubble').html(data);
+              }
             });
           }
         });
