4e1fd333ee0ee883d78a9fd21f1870d381a74248
Sampo Toiva <sampo.toiva@gmail.com>
2015-10-08 16:34:20 +0300

    Make migration reversible

--- db/migrate/20151008130725_add_run_at_and_priority_to_attempts_in_delayed_job.rb
+++ db/migrate/20151008130725_add_run_at_and_priority_to_attempts_in_delayed_job.rb
@@ -1,6 +1,11 @@
 class AddRunAtAndPriorityToAttemptsInDelayedJob < ActiveRecord::Migration
-  def change
+  def up
     remove_index :delayed_jobs, column: :attempts
     add_index :delayed_jobs, [:attempts, :run_at, :priority]
+  end
+
+  def down
+    remove_index :delayed_jobs, name: "index_delayed_jobs_on_attempts_and_run_at_and_priority"
+    add_index :delayed_jobs, :attempts
   end
 end
