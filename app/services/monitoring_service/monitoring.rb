702a799057a82e9ec4323c436eaad2dada083426
Sampo Toiva <sampo.toiva@gmail.com>
2015-10-09 08:49:24 +0300

    Add monitoring service and a poor mans cache

---
+++ app/services/monitoring_service/monitoring.rb
@@ -0,0 +1,26 @@
+module MonitoringService::Monitoring
+
+  SAMPLING_PERIOD_IN_SECONDS = ENV['MONITORING_SAMPLING_PERIOD'] || 15
+  @delayed_job_last_reported = 0
+
+  module_function
+
+  def report_queue_size
+    if Librato.tracker.should_start? && should_report?
+      # Priorities are from 0..10, where 0..6 are high priority and 7..10 low
+      priority_counts = Delayed::Job.where('attempts < ? AND run_at < ?', 3, Time.now).group(:priority).count
+
+      Librato.group 'delayed_job_queue' do |g|
+        # We are measuring an average in the sampling period, by default 60 seconds
+        g.measure 'high', priority_counts.select { |p, _| p < 7 }.values.sum
+        g.measure 'low', priority_counts.select { |p, _| p >= 7 }.values.sum
+      end
+      @delayed_job_last_reported = Time.now
+    end
+  end
+
+  def should_report?
+    (Time.now - @delayed_job_last_reported).to_i > SAMPLING_PERIOD_IN_SECONDS
+  end
+  private_class_method :should_report?
+end
