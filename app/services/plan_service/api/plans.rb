7ec2074d74f466e9e8b03a8f7cbb9a12f0ea06ae
Mikko Koski <mikkokos@gmail.com>
2015-10-23 10:58:47 +0300

    Tests for OS plan

--- app/services/plan_service/api/plans.rb
+++ app/services/plan_service/api/plans.rb
@@ -6,25 +6,27 @@ module PlanService::API
   class Plans
 
     def initialize(ext_plan_service)
-      @active = Maybe(ext_plan_service)[:active].map { true }.or_else(false)
+      @active = Maybe(ext_plan_service)[:active].map {
+        |v| !!v # to bool
+      }.or_else(false)
     end
 
     def create(community_id:, plan:)
+      if !@active
+        Result::Error.new("External plan service is not in use.")
+      else
 
-      unless @active
-        Result::Failure.new("External plan service is not in use.")
+        Result::Success.new(
+          with_expiration_status(
+            PlanStore.create_plan(community_id: community_id, plan: plan)))
+
+        # deprecated
+        # TODO remove this
+        # Use create_plan and create_initial_trial methods instead
+        Result::Success.new(
+          with_expiration_status(
+            PlanStore.create(community_id: community_id, plan: plan)))
       end
-
-      Result::Success.new(
-        with_expiration_status(
-          PlanStore.create_plan(community_id: community_id, plan: plan)))
-
-      # deprecated
-      # TODO remove this
-      # Use create_plan and create_initial_trial methods instead
-      Result::Success.new(
-        with_expiration_status(
-          PlanStore.create(community_id: community_id, plan: plan)))
     end
 
     # Create an initial trial plan
@@ -36,26 +38,27 @@ module PlanService::API
     #
     def create_initial_trial(community_id:, plan:)
 
-      unless @active
-        Result::Failure.new("External plan service is not in use.")
+      if !@active
+        Result::Error.new("External plan service is not in use.")
+      else
+
+        Result::Success.new(
+          with_expiration_status(
+            PlanStore.create_trial(community_id: community_id, plan: plan)))
+
+        # deprecated
+        # TODO remove this
+        # Use create_plan and create_initial_trial methods instead
+        Result::Success.new(
+          with_expiration_status(
+            PlanStore.create(community_id: community_id, plan: plan)))
       end
-
-      Result::Success.new(
-        with_expiration_status(
-          PlanStore.create_trial(community_id: community_id, plan: plan)))
-
-      # deprecated
-      # TODO remove this
-      # Use create_plan and create_initial_trial methods instead
-      Result::Success.new(
-        with_expiration_status(
-          PlanStore.create(community_id: community_id, plan: plan)))
     end
 
     def get_current(community_id:)
       if !@active
         Result::Success.new(
-          Plan.new(
+          Plan.call(
           community_id: community_id,
           plan_level: PlanService::Levels::OS,
           expires_at: nil,
@@ -72,19 +75,23 @@ module PlanService::API
     end
 
     def expired?(community_id:)
-      Maybe(PlanStore.get_current(community_id: community_id)).map { |plan|
-        Result::Success.new(
-          Maybe(plan[:expires_at]).map { |expires_at|
-            expires_at < Time.now }
-          .or_else(false))
-      }.or_else {
-        Result::Error.new("Can not find plan for community id: #{community_id}")
-      }
+      if !@active
+        Result::Success.new(false)
+      else
+        Maybe(PlanStore.get_current(community_id: community_id)).map { |plan|
+          Result::Success.new(
+            Maybe(plan[:expires_at]).map { |expires_at|
+              expires_at < Time.now }
+            .or_else(false))
+        }.or_else {
+          Result::Error.new("Can not find plan for community id: #{community_id}")
+        }
+      end
     end
 
     def get_trials(after:)
       unless @active
-        Result::Failure.new("External plan service is not in use.")
+        Result::Error.new("External plan service is not in use.")
       end
 
       Result::Success.new(PlanStore.get_trials(after: after))
--- spec/services/plan_service/api/plans_spec.rb
+++ spec/services/plan_service/api/plans_spec.rb
@@ -1,140 +1,183 @@
 describe PlanService::API::Plans do
 
-  let(:plans_api) { PlanService::API::Api }
+  context "external service in use" do
+    let(:plans) { PlanService::API::Plans.new(active: true) }
 
-  describe "#create" do
-    context "#get_current" do
-      it "creates a new initial trial" do
-        Timecop.freeze(Time.now.change(usec: 0)) {
-          expires_at = 1.month.from_now.change(usec: 0)
+    describe "#create" do
+      context "#get_current" do
+        it "creates a new initial trial" do
+          Timecop.freeze(Time.now.change(usec: 0)) {
+            expires_at = 1.month.from_now.change(usec: 0)
 
-          plans_api.plans.create_initial_trial(
-            community_id: 123, plan: {
-              plan_level: PlanService::Levels::FREE,
-              expires_at: expires_at,
-            })
+            plans.create_initial_trial(
+              community_id: 123, plan: {
+                plan_level: PlanService::Levels::FREE,
+                expires_at: expires_at,
+              })
 
-          res = plans_api.plans.get_current(community_id: 123)
+            res = plans.get_current(community_id: 123)
 
-          expect(res.success).to eq(true)
-          expect(res.data[:id]).to be_a(Fixnum)
-          expect(res.data.except(:id)).to eq(
-                                community_id: 123,
-                                plan_level: 0,
-                                expires_at: expires_at,
-                                created_at: Time.now,
-                                updated_at: Time.now,
-                                expired: false,
-                                          )
+            expect(res.success).to eq(true)
+            expect(res.data[:id]).to be_a(Fixnum)
+            expect(res.data.except(:id)).to eq(
+                                              community_id: 123,
+                                              plan_level: 0,
+                                              expires_at: expires_at,
+                                              created_at: Time.now,
+                                              updated_at: Time.now,
+                                              expired: false,
+                                            )
 
-        }
+          }
+        end
+
+        it "creates a new plan" do
+          Timecop.freeze(Time.now.change(usec: 0)) {
+            expires_at = 1.month.from_now.change(usec: 0)
+
+            plans.create(
+              community_id: 123, plan: {
+                plan_level: PlanService::Levels::SCALE,
+                expires_at: expires_at,
+              })
+
+            res = plans.get_current(community_id: 123)
+
+            expect(res.success).to eq(true)
+            expect(res.data[:id]).to be_a(Fixnum)
+            expect(res.data.except(:id)).to eq(
+                                              community_id: 123,
+                                              plan_level: 4,
+                                              expires_at: expires_at,
+                                              created_at: Time.now,
+                                              updated_at: Time.now,
+                                              expired: false,
+                                            )
+
+          }
+        end
+
+        it "creates a new plan that never expires" do
+          Timecop.freeze(Time.now.change(usec: 0)) {
+            plans.create(
+              community_id: 123, plan: {
+                plan_level: PlanService::Levels::PRO
+              })
+
+            res = plans.get_current(community_id: 123)
+
+            expect(res.success).to eq(true)
+            expect(res.data[:id]).to be_a(Fixnum)
+            expect(res.data.except(:id)).to eq(
+                                              community_id: 123,
+                                              plan_level: 2,
+                                              expires_at: nil,
+                                              created_at: Time.now,
+                                              updated_at: Time.now,
+                                              expired: false,
+                                            )
+          }
+        end
       end
 
-      it "creates a new plan" do
-        Timecop.freeze(Time.now.change(usec: 0)) {
-          expires_at = 1.month.from_now.change(usec: 0)
-
-          plans_api.plans.create(
+      context "error" do
+        it "raises error if both plan level and plan name are missing" do
+          expect { plans.create(
             community_id: 123, plan: {
-              plan_level: PlanService::Levels::SCALE,
-              expires_at: expires_at,
-            })
+              expires_at: 1.month.from_now
+            }) }.to raise_error(ArgumentError)
+        end
 
-          res = plans_api.plans.get_current(community_id: 123)
-
-          expect(res.success).to eq(true)
-          expect(res.data[:id]).to be_a(Fixnum)
-          expect(res.data.except(:id)).to eq(
-                                community_id: 123,
-                                plan_level: 4,
-                                expires_at: expires_at,
-                                created_at: Time.now,
-                                updated_at: Time.now,
-                                expired: false,
-                                          )
-
-        }
-      end
-
-      it "creates a new plan that never expires" do
-        Timecop.freeze(Time.now.change(usec: 0)) {
-          plans_api.plans.create(
-            community_id: 123, plan: {
-              plan_level: PlanService::Levels::PRO
-            })
-
-          res = plans_api.plans.get_current(community_id: 123)
-
-          expect(res.success).to eq(true)
-          expect(res.data[:id]).to be_a(Fixnum)
-          expect(res.data.except(:id)).to eq(
-                                            community_id: 123,
-                                            plan_level: 2,
-                                            expires_at: nil,
-                                            created_at: Time.now,
-                                            updated_at: Time.now,
-                                            expired: false,
-                                          )
-        }
+        it "returns error if plan can not be found" do
+          res = plans.get_current(community_id: 123)
+          expect(res.success).to eq(false)
+        end
       end
     end
 
-    context "error" do
-      it "raises error if both plan level and plan name are missing" do
-        expect { plans_api.plans.create(
-          community_id: 123, plan: {
-            expires_at: 1.month.from_now
-          }) }.to raise_error(ArgumentError)
+    describe "#expired?" do
+      context "success" do
+        it "returns false if plan never expires" do
+          plans.create(
+            community_id: 111, plan: {
+              plan_level: 5,
+              expires_at: nil, # plan never expires
+            })
+
+          res = plans.expired?(community_id: 111).data
+          expect(res).to eq(false)
+        end
+
+        it "returns false if plan has not yet expired" do
+          plans.create(
+            community_id: 111, plan: {
+              plan_level: 5,
+              expires_at: 1.month.from_now,
+            })
+
+          res = plans.expired?(community_id: 111).data
+          expect(res).to eq(false)
+        end
+
+        it "returns true if plan has expired" do
+          plans.create(
+            community_id: 111, plan: {
+              plan_level: 5,
+              expires_at: 1.month.ago,
+            })
+
+          res = plans.expired?(community_id: 111).data
+          expect(res).to eq(true)
+        end
       end
 
-      it "returns error if plan can not be found" do
-        res = plans_api.plans.get_current(community_id: 123)
-        expect(res.success).to eq(false)
+      context "error" do
+        it "returns error if plan can not be found" do
+          res = plans.get_current(community_id: 123)
+          expect(res.success).to eq(false)
+        end
       end
+    end
+
+  end
+
+
+  context "external service not in use" do
+    let(:plans) { PlanService::API::Plans.new(active: false) }
+
+    it "does not creates a new initial trial" do
+      res = plans.create_initial_trial(
+        community_id: 123, plan: {
+          plan_level: PlanService::Levels::FREE,
+        })
+
+      expect(res.success).to eq(false)
+    end
+
+    it "does not creates a new plan" do
+      res = plans.create(
+        community_id: 123, plan: {
+          plan_level: PlanService::Levels::SCALE,
+        })
+
+      expect(res.success).to eq(false)
+    end
+
+    it "returns OS plan" do
+      res = plans.get_current(community_id: 123)
+      expect(res.success).to eq(true)
+      expect(res.data).to include(
+                       plan_level: 99999,
+                       community_id: 123,
+                       expires_at: nil
+                     )
+    end
+
+    it "never expires" do
+      res = plans.expired?(community_id: 123)
+      expect(res.success).to eq(true)
+      expect(res.data).to eq(false)
     end
   end
 
-  describe "#expired?" do
-    context "success" do
-      it "returns false if plan never expires" do
-        plans_api.plans.create(
-          community_id: 111, plan: {
-            plan_level: 5,
-            expires_at: nil, # plan never expires
-          })
-
-        res = plans_api.plans.expired?(community_id: 111).data
-        expect(res).to eq(false)
-      end
-
-      it "returns false if plan has not yet expired" do
-        plans_api.plans.create(
-          community_id: 111, plan: {
-            plan_level: 5,
-            expires_at: 1.month.from_now,
-          })
-
-        res = plans_api.plans.expired?(community_id: 111).data
-        expect(res).to eq(false)
-      end
-
-      it "returns true if plan has expired" do
-        plans_api.plans.create(
-          community_id: 111, plan: {
-            plan_level: 5,
-            expires_at: 1.month.ago,
-          })
-
-        res = plans_api.plans.expired?(community_id: 111).data
-        expect(res).to eq(true)
-      end
-    end
-
-    context "error" do
-      it "returns error if plan can not be found" do
-        res = plans_api.plans.get_current(community_id: 123)
-        expect(res.success).to eq(false)
-      end
-    end
-  end
 end
