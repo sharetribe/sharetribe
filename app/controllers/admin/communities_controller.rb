3a3c3839a4c78978a1b4c34c6147eb6e223c202c
Mikko Koski <mikkokos@gmail.com>
2015-11-04 10:44:02 +0200

    Cleaner URL

--- app/controllers/admin/communities_controller.rb
+++ app/controllers/admin/communities_controller.rb
@@ -11,21 +11,6 @@ class Admin::CommunitiesController < ApplicationController
     render locals: {paypal_enabled: PaypalHelper.paypal_active?(@current_community.id)}
   end
 
-  def plan
-    marketplace_default_name = @current_community.name(@current_community.default_locale)
-
-    PlanService::API::Api.plans.get_external_service_link(
-      id: @current_community.id,
-      ident: @current_community.ident,
-      domain: @current_community.domain,
-      marketplace_default_name: marketplace_default_name
-    ).on_success { |link|
-      redirect_to link
-    }.on_error { |error_msg|
-      render_not_found!(error_msg)
-    }
-  end
-
   def edit_look_and_feel
     @selected_left_navi_link = "tribe_look_and_feel"
     @community = @current_community
---
+++ app/controllers/admin/plans_controller.rb
@@ -0,0 +1,21 @@
+class Admin::PlansController < ApplicationController
+
+  before_filter :ensure_is_admin
+
+  # Redirect to external plan service. Nothing else.
+  def show
+    marketplace_default_name = @current_community.name(@current_community.default_locale)
+
+    PlanService::API::Api.plans.get_external_service_link(
+      id: @current_community.id,
+      ident: @current_community.ident,
+      domain: @current_community.domain,
+      marketplace_default_name: marketplace_default_name
+    ).on_success { |link|
+      redirect_to link
+    }.on_error { |error_msg|
+      render_not_found!(error_msg)
+    }
+  end
+
+end
