a1e63770e4cec9b2bf81acf7cf800a3224ae2e8c
Mikko Koski <mikkokos@gmail.com>
2015-12-16 16:15:46 +0200

    Remove assets group from Gemfile
    Rails 4.0 removed the =assets= group from Gemfile. You'd need to remove
    that line from your Gemfile when upgrading. You should also update your
    application file (in =config/application.rb=):
    
        # Require the gems listed in Gemfile, including any gems
        # you've limited to :test, :development, or :production.
        Bundler.require(*Rails.groups)
    
    *Action:* Changed the Gemfile and application.rb

--- Gemfile
+++ Gemfile
@@ -7,13 +7,9 @@ gem 'rails', '3.2.21'
 # To use debugger
 #gem 'ruby-debug'
 
-# Gems used only for assets and not required
-# in production environments by default.
-group :assets do
-  gem 'coffee-rails', "~> 3.2.2"
-  gem 'uglifier'
-  gem 'asset_sync', '~> 1.0.0'
-end
+gem 'coffee-rails', "~> 3.2.2"
+gem 'uglifier'
+gem 'asset_sync', '~> 1.0.0'
 
 gem 'sass-rails'
 gem 'compass-rails', '~> 1.1'
--- config/application.rb
+++ config/application.rb
@@ -9,12 +9,9 @@ require File.expand_path('../config_loader', __FILE__)
 
 require File.expand_path('../available_locales', __FILE__)
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 module Kassi
   class Application < Rails::Application
