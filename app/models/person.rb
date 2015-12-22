a6e7e8e25e2fd5d1983b56a045e924d005b2120d
Mikko Koski <mikkokos@gmail.com>
2015-12-22 17:46:31 +0200

    Remove deprecated conditions:, replace with lambdas

--- app/models/person.rb
+++ app/models/person.rb
@@ -87,10 +87,10 @@ class Person < ActiveRecord::Base
 
   attr_protected :is_admin
 
-  has_many :listings, :dependent => :destroy, :foreign_key => "author_id", :conditions => { :deleted => 0 }
+  has_many :listings, -> { where(deleted: 0) }, :dependent => :destroy, :foreign_key => "author_id"
   has_many :emails, :dependent => :destroy, :inverse_of => :person
 
-  has_one :location, :conditions => ['location_type = ?', 'person'], :dependent => :destroy
+  has_one :location, -> { where(location_type: :person) }, :dependent => :destroy
   has_one :braintree_account, :dependent => :destroy
   has_one :checkout_account, dependent: :destroy
 
@@ -98,12 +98,12 @@ class Person < ActiveRecord::Base
   has_many :conversations, :through => :participations, :dependent => :destroy
   has_many :authored_testimonials, :class_name => "Testimonial", :foreign_key => "author_id", :dependent => :destroy
   has_many :received_testimonials, :class_name => "Testimonial", :foreign_key => "receiver_id", :order => "id DESC", :dependent => :destroy
-  has_many :received_positive_testimonials, :class_name => "Testimonial", :foreign_key => "receiver_id", :conditions => "grade IN (0.5,0.75,1)", :order => "id DESC"
-  has_many :received_negative_testimonials, :class_name => "Testimonial", :foreign_key => "receiver_id", :conditions => "grade IN (0.0,0.25)", :order => "id DESC"
+  has_many :received_positive_testimonials, -> { where("grade IN (0.5,0.75,1)").order("id DESC") }, :class_name => "Testimonial", :foreign_key => "receiver_id"
+  has_many :received_negative_testimonials, -> { where("grade IN (0.0,0.25)").order("id DESC") }, :class_name => "Testimonial", :foreign_key => "receiver_id"
   has_many :messages, :foreign_key => "sender_id"
   has_many :authored_comments, :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy
   has_many :community_memberships, :dependent => :destroy
-  has_many :communities, :through => :community_memberships, :conditions => ['status = ?', 'accepted']
+  has_many :communities, -> { where('status = ?', 'accepted') }, :through => :community_memberships
   has_many :invitations, :foreign_key => "inviter_id", :dependent => :destroy
   has_many :auth_tokens, :dependent => :destroy
   has_many :follower_relationships
