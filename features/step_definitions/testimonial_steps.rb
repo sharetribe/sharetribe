a2d4899f328fd8cf6acd6013a70d178e31554a25
Antti Virolainen <antti@sharetribe.com>
2016-01-12 15:12:20 +0200

    fix tests

--- features/step_definitions/testimonial_steps.rb
+++ features/step_definitions/testimonial_steps.rb
@@ -1,5 +1,13 @@
 Given /^I have "([^"]*)" testimonials? with grade "([^"]*)"$/ do |amount, grade|
   amount.to_i.times do
-    FactoryGirl.create(:testimonial, author: @people["kassi_testperson2"], :receiver => @people["kassi_testperson1"], grade: grade)
+
+    tr = FactoryGirl.create(:transaction, community: @current_community)
+
+    FactoryGirl.create(:testimonial,
+                        author: @people["kassi_testperson2"],
+                        receiver: @people["kassi_testperson1"],
+                        grade: grade,
+                        transaction: tr
+                      )
   end
 end
