a46e745dd6d9dde73e037b8c4b8ab60ef36ecca2
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-01-26 14:20:11 +0200

    Zappy returns ID:s instead of complete results

--- app/services/listing_index_service/search/zappy_adapter.rb
+++ app/services/listing_index_service/search/zappy_adapter.rb
@@ -2,54 +2,47 @@ module ListingIndexService::Search
 
   class ZappyAdapter < SearchEngineAdapter
 
-   def initialize
-     @conn = Faraday.new(url: "http://127.0.0.1:8080") do |c|
-        c.request  :url_encoded             # form-encode POST params
-        c.response :logger                  # log requests to STDOUT
-        c.response :json, :content_type => /\bjson$/
-        c.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-     end
-   end
+    def initialize
+      @conn = Faraday.new(url: "http://127.0.0.1:8080") do |c|
+         c.request  :url_encoded             # form-encode POST params
+         c.response :logger                  # log requests to STDOUT
+         c.response :json, :content_type => /\bjson$/
+         c.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+      end
+    end
+ 
+    def search(community_id:, search:, includes: nil)
+ 
+      begin
+        res = @conn.get do |req|
+          req.url '/api/v1/marketplace/1/listings', {keywords: search[:keywords]}
+          req.headers['Authorization'] = 'apikey key=asdf1234'
+        end.body
+        Result::Success.new(parse_response(res["result"], includes))
+      rescue StandardError => e
+        Result::Error.new(e)
+      end
+    end
 
-   def search(community_id:, search:, includes: nil)
+    private
 
-     begin
-       res = @conn.get('/search', {community_id: community_id, keywords: search[:keywords]}).body
-       Result::Success.new(parse_response(res))
-     rescue StandardError => e
-       Result::Error.new(e)
-     end
-   end
+    def listings_from_ids(ids, includes)
+      # use pluck for much faster query after updating to Rails >4.1.6
+      # http://collectiveidea.com/blog/archives/2015/03/05/optimizing-rails-for-memory-usage-part-3-pluck-and-database-laziness/
+      # https://github.com/rails/rails/issues/17049
 
+      Listing
+        .where(id: res) # use find_each for more efficient batch processing after updating to Rails 4.1
+        .order("field(listings.id, #{res.join ','})")
+        .map {
+          |l| ListingIndexService::Search::Commons.listing_hash(l, includes)
+        }
+    end
 
-   private
-
-   def parse_response(res)
-     {count: res.count,
-      listings: res.map { |l| parse_listing(l) }}
-   end
-
-   def parse_author(author_res)
-     author_res = HashUtils.symbolize_keys(author_res)
-     {id: author_res[:id],
-      username: author_res[:username],
-      first_name: author_res[:given_name],
-      last_name: author_res[:family_name],
-      organization_name: "N/A", # Missing from index
-      is_organization: false, # Missing from index
-      avatar: { thumb: nil } # Incorrect format for images in index
-     }
-   end
-
-   def parse_listing(listing_res)
-     listing_res = HashUtils.symbolize_keys(listing_res)
-     listing_res
-       .slice(:id, :title, :description, :updated_at, :created_at)
-       .merge({category_id: 1, # Missing from index
-               author: parse_author(listing_res[:author]),
-               listing_images: []}) # Incorrect format for images in index
-   end
-
-
+    def parse_response(res, includes)
+      listings = res.count > 0 ? listings_from_ids(res) : []
+      {count: res.count,
+       listings: listings}
+    end
   end
 end
