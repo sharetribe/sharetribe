eb685fefe9afa6de34d573bf03c83c3a30c8877e
Mikko Koski <mikkokos@gmail.com>
2016-02-02 16:05:01 +0200

    Remove Mercury Image delete

--- app/controllers/mercury/images_controller.rb
+++ app/controllers/mercury/images_controller.rb
@@ -1,19 +1,12 @@
 class Mercury::ImagesController < MercuryController
 
-  respond_to :json
-
   # POST /images.json
   def create
-    @image = Mercury::Image.new(params[:image])
-    @image.save
-    respond_with @image
-  end
+    image = Mercury::Image.new(params[:image])
+    image.save
 
-  # DELETE /images/1.json
-  def destroy
-    @image = Mercury::Image.find(params[:id])
-    @image.destroy
-    respond_with @image
+    respond_to do |format|
+      format.json { render json: image }
+    end
   end
-
 end
