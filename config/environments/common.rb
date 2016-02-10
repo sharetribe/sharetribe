fd39819cd8c8cfda0c4311d3361da2edde20f40e
Mikko Koski <mikkokos@gmail.com>
2016-02-10 09:32:02 +0200

    Add LOG_LEVEL configuration
    Get rid of the DEPRECATION warning:
    
    DEPRECATION WARNING: You did not specify a `log_level` in `production.rb`. Currently, the default value for `log_level` is `:info` for the production environment and `:debug` in all other environments. In Rails 5 the default value will be unified to `:debug` across all environments.

--- config/environments/common.rb
+++ config/environments/common.rb
@@ -1,9 +1,18 @@
 Kassi::Application.configure do
 
+  str_to_lowercase_sym = ->(v) {
+    if v.nil? || v.is_a?(Symbol)
+      v
+    else
+      v.downcase.to_sym
+    end
+  }
+
   Config = EntityUtils.define_builder(
     [:asset_host, :string, :optional],
     [:eager_load, :bool, :mandatory, :str_to_bool],
-    [:serve_static_files, :bool, :optional, :str_to_bool]
+    [:serve_static_files, :bool, :optional, :str_to_bool],
+    [:log_level, transform_with: str_to_lowercase_sym, one_of: [:debug, :info, :warn, :error]]
   )
 
   m_config = Maybe(Config.call(APP_CONFIG.to_h))
@@ -18,5 +27,9 @@ Kassi::Application.configure do
 
   m_config[:serve_static_files].each { |serve_static_files|
     config.serve_static_files = serve_static_files
+  }
+
+  m_config[:log_level].each { |log_level|
+    config.log_level = log_level
   }
 end
