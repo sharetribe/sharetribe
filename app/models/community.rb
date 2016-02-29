62a8f1dfb298c0d71dfb3fcb2c0a3a1947820b26
Sampo Toiva <sampo.toiva@gmail.com>
2016-02-29 11:48:22 +0200

    Remove obsolete marketplace settings model

--- app/models/community.rb
+++ app/models/community.rb
@@ -128,7 +128,6 @@ class Community < ActiveRecord::Base
 
   has_many :listings
 
-  has_one :marketplace_settings, dependent: :destroy
   has_one :payment_gateway, :dependent => :destroy
   has_one :paypal_account # Admin paypal account
 
