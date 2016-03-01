21e68c00204fafa7c41dc14ccdba7d392f253194
Hannu Lyytikainen <hannu@sharetribe.com>
2016-03-01 20:00:31 +0200

    Update commynity_membership person IDs
    Update person IDs according to the duplicated rows in people table.
    Order of migrations also changed so that person ID update is applied
    before adding community ID values to the people table.

--- db/migrate/20160301170912_add_community_id_values_to_people.rb
+++ db/migrate/20160301170912_add_community_id_values_to_people.rb
@@ -1,15 +1,10 @@
 class AddCommunityIdValuesToPeople < ActiveRecord::Migration
   def up
     execute("
-      UPDATE people AS p, community_memberships AS cm 
+      UPDATE people AS p, community_memberships AS cm
       SET p.community_id = cm.community_id
       WHERE cm.person_id = p.id
-    ") 
-    execute("
-      UPDATE people AS p, community_memberships AS cm 
-      SET p.community_id = cm.community_id
-      WHERE cm.person_id = p.cloned_from
-    ") 
+    ")
   end
   def down
     execute("
