6f958d9415008321869b1da3da756138ddc524ee
Hannu Lyytikainen <hannu@sharetribe.com>
2016-03-15 16:49:24 +0200

    Update checkout_accounts based on cloned_from

---
+++ db/migrate/20160315141717_update_checkout_accounts_based_on_cloned_from.rb
@@ -0,0 +1,33 @@
+class UpdateCheckoutAccountsBasedOnClonedFrom < ActiveRecord::Migration
+  def up
+    people = select_all("
+      SELECT * FROM people
+      WHERE cloned_from IN
+      (SELECT person_id
+       FROM checkout_accounts)      
+      ")
+
+    people.each { |p|
+      execute("
+        INSERT INTO checkout_accounts (
+          company_id_or_personal_id, merchant_id,
+          merchant_key, person_id, created_at, updated_at)
+	      (SELECT
+          company_id_or_personal_id, merchant_id,
+          merchant_key, #{quote(p['id'])}, created_at, updated_at
+        FROM checkout_accounts
+        WHERE person_id = #{quote(p['cloned_from'])})
+        ")
+    }
+  end
+
+  def down
+    execute("
+      DELETE ca
+      FROM checkout_accounts AS ca, people AS p
+      WHERE 
+        ca.person_id = p.id AND
+        p.cloned_from IS NOT NULL
+      ")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315105748) do
+ActiveRecord::Schema.define(version: 20160315141717) do
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string   "token",            limit: 255
