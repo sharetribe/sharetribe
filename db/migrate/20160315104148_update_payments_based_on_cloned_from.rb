ea1b2eaf7b42c8609829d3662f9016ccd1d57a60
Hannu Lyytikainen <hannu@sharetribe.com>
2016-03-15 16:10:21 +0200

    Update payments based on cloned_from

---
+++ db/migrate/20160315104148_update_payments_based_on_cloned_from.rb
@@ -0,0 +1,41 @@
+class UpdatePaymentsBasedOnClonedFrom < ActiveRecord::Migration
+  def up
+    # Update payer id
+    execute("
+      UPDATE payments AS pa, people AS pe
+      SET pa.payer_id = pe.id
+      WHERE
+        pa.payer_id = pe.cloned_from AND
+        pa.community_id = pe.community_id
+    ")
+
+    # Update recipient id
+    execute("
+      UPDATE payments AS pa, people AS pe
+      SET pa.recipient_id = pe.id
+      WHERE
+        pa.recipient_id = pe.cloned_from AND
+        pa.community_id = pe.community_id
+    ")
+  end
+
+  def down
+    # Roll back payer id
+    execute("
+      UPDATE payments AS pa, people AS pe
+      SET pa.payer_id = pe.cloned_from
+      WHERE
+        pa.payer_id = pe.id AND
+        pe.cloned_from IS NOT NULL
+    ")
+
+    # Roll back recipient id
+    execute("
+      UPDATE payments AS pa, people AS pe
+      SET pa.recipient_id = pe.cloned_from
+      WHERE
+        pa.recipient_id = pe.id AND
+        pe.cloned_from IS NOT NULL
+    ")
+  end
+end
