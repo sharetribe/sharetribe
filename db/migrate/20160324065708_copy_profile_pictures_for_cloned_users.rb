054a2c8a35090470a49b2775513ce64ea9397d19
Mikko Koski <mikkokos@gmail.com>
2016-03-24 09:12:19 +0200

    Copy profile pictures

---
+++ db/migrate/20160324065708_copy_profile_pictures_for_cloned_users.rb
@@ -0,0 +1,25 @@
+require File.expand_path('../../migrate_helpers/logging_helpers', __FILE__)
+
+class CopyProfilePicturesForClonedUsers < ActiveRecord::Migration
+  include LoggingHelper
+
+  def up
+    cloned_users = Person.where("cloned_from IS NOT NULL")
+
+    progress = ProgressReporter.new(cloned_users.count, 100)
+
+    cloned_users.each { |cloned_user|
+      cloned_from_user = Person.find_by(id: cloned_user.cloned_from)
+
+      cloned_user.image = cloned_from_user.image
+      cloned_user.save!
+
+      # Print progress
+      progress.next
+      print_dot
+    }
+  end
+
+  def down
+  end
+end
