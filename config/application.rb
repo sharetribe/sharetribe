c96d17cec223517bab9c026ce912dec0bbc36db0
Mikko Koski <mikkokos@gmail.com>
2016-03-31 14:47:47 +0300

    Add font-awesome.min.css to precompile array

--- config/application.rb
+++ config/application.rb
@@ -51,7 +51,15 @@ module Kassi
     config.assets.paths << VENDOR_CSS_PATH
 
     # Define here additional Assset Pipeline Manifests to include to precompilation
-    config.assets.precompile += ['markerclusterer.js', 'communities/custom-style-*', 'ss-*', 'modernizr.min.js', 'mercury.js','jquery-1.7.js']
+    config.assets.precompile += [
+      'markerclusterer.js',
+      'communities/custom-style-*',
+      'ss-*',
+      'modernizr.min.js',
+      'mercury.js',
+      'jquery-1.7.js',
+      'font-awesome.min.css'
+    ]
 
     # Read the config from the config.yml
     APP_CONFIG = ConfigLoader.load_app_config
