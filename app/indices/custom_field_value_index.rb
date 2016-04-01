785d16527214a4d123c698b8ab0eded319980380
Boyan Tabakov <boyan@sharetribe.com>
2016-04-01 14:48:00 +0300

    Allow disabling Flying Shinx incremental indexing delayed jobs

--- app/indices/custom_field_value_index.rb
+++ app/indices/custom_field_value_index.rb
@@ -1,14 +1,16 @@
-ThinkingSphinx::Index.define :custom_field_value, :with => :active_record, :delta => ThinkingSphinx::Deltas::DelayedDelta do
+if APP_CONFIG.use_thinking_sphinx_indexing.to_s.casecmp("true") == 0
+  ThinkingSphinx::Index.define :custom_field_value, :with => :active_record, :delta => ThinkingSphinx::Deltas::DelayedDelta do
 
-  #Thinking Sphinx will automatically add the SQL command SET NAMES utf8 as
-  # part of the indexing process if the database connection settings have
-  # encoding set to utf8. This is default in Rails but with Heroku, we need to
-  # be explicit.
-  set_property :utf8? => true
+    #Thinking Sphinx will automatically add the SQL command SET NAMES utf8 as
+    # part of the indexing process if the database connection settings have
+    # encoding set to utf8. This is default in Rails but with Heroku, we need to
+    # be explicit.
+    set_property :utf8? => true
 
-  # attributes
-  has listing_id
-  has custom_field_id
-  has numeric_value
+    # attributes
+    has listing_id
+    has custom_field_id
+    has numeric_value
 
+  end
 end
