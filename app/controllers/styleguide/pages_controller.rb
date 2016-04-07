1b52ce46b15823860d46bf54867430ef26de3592
Kimmo Puputti <kimmo@sharetribe.com>
2016-04-07 15:42:04 +0300

    Move example views under styleguide path

--- app/controllers/styleguide/pages_controller.rb
+++ app/controllers/styleguide/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class Styleguide::PagesController < ApplicationController
   include ReactOnRails::Controller
   layout "helloworld"
 
@@ -16,6 +16,10 @@ class PagesController < ApplicationController
     Rails.logger.error(err.backtrace.join("\n"))
     redirect_to client_side_hello_world_path,
                 flash: { error: "Error prerendering in react_on_rails. See server logs." }
+  end
+
+  def index
+    render
   end
 
   # See files in spec/dummy/app/views/pages
