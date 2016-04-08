040ae54f749792a3d216b108088a55343fbc22a8
Vesa Luusua <vesa.luusua@gmail.com>
2016-04-08 14:48:12 +0300

    hello world examples from spec/dummy

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,49 @@
+class PagesController < ApplicationController
+  include ReactOnRails::Controller
+  layout "helloworld"
+
+  before_action do
+    session[:something_useful] = "REALLY USEFUL"
+  end
+
+  before_action :data
+
+  before_action :initialize_shared_store, only: [:client_side_hello_world_shared_store_controller,
+                                                 :server_side_hello_world_shared_store_controller]
+
+  rescue_from ReactOnRails::PrerenderError do |err|
+    Rails.logger.error(err.message)
+    Rails.logger.error(err.backtrace.join("\n"))
+    redirect_to client_side_hello_world_path,
+                flash: { error: "Error prerendering in react_on_rails. See server logs." }
+  end
+
+  # See files in spec/dummy/app/views/pages
+
+  private
+
+  def initialize_shared_store
+    redux_store("SharedReduxStore", props: @app_props_server_render)
+  end
+
+  def data
+    # This is the props used by the React component.
+    @app_props_server_render = {
+      helloWorldData: {
+        name: "Mr. Server Side Rendering"
+      }
+    }
+
+    @app_props_hello = {
+      helloWorldData: {
+        name: "Mrs. Client Side Rendering"
+      }
+    }
+
+    @app_props_hello_again = {
+      helloWorldData: {
+        name: "Mrs. Client Side Hello Again"
+      }
+    }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,25 @@ Kassi::Application.routes.draw do
 
   mount Mercury::Engine => '/'
 
+  get 'helloworld' => "pages#index"
+  get "client_side_hello_world" => "pages#client_side_hello_world"
+  get "client_side_hello_world_shared_store" => "pages#client_side_hello_world_shared_store"
+  get "client_side_hello_world_shared_store_controller" => "pages#client_side_hello_world_shared_store_controller"
+  get "client_side_hello_world_shared_store_defer" => "pages#client_side_hello_world_shared_store_defer"
+  get "server_side_hello_world_shared_store" => "pages#server_side_hello_world_shared_store"
+  get "server_side_hello_world_shared_store_controller" => "pages#server_side_hello_world_shared_store_controller"
+  get "server_side_hello_world_shared_store_defer" => "pages#server_side_hello_world_shared_store_defer"
+  get "server_side_hello_world" => "pages#server_side_hello_world"
+  get "client_side_log_throw" => "pages#client_side_log_throw"
+  get "server_side_log_throw" => "pages#server_side_log_throw"
+  get "server_side_log_throw_raise" => "pages#server_side_log_throw_raise"
+  get "server_side_hello_world_es5" => "pages#server_side_hello_world_es5"
+  get "server_side_redux_app" => "pages#server_side_redux_app"
+  get "server_side_hello_world_with_options" => "pages#server_side_hello_world_with_options"
+  get "server_side_redux_app_cached" => "pages#server_side_redux_app_cached"
+  get "render_js" => "pages#render_js"
+  get "react_router(/*all)" => "react_router#index", as: :react_router
+  get "pure_component" => "pages#pure_component"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
