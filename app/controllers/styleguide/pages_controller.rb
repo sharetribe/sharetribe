e38c95222168bba4124d1f139a4b822f238ae66e
Vesa Luusua <vesa.luusua@gmail.com>
2016-04-11 14:58:48 +0300

    refactoring and cleaning

--- app/controllers/styleguide/pages_controller.rb
+++ app/controllers/styleguide/pages_controller.rb
@@ -11,12 +11,6 @@ class Styleguide::PagesController < ApplicationController
                 flash: { error: "Error prerendering in react_on_rails. See server logs." }
   end
 
-  def index
-    render
-  end
-
-  # See files in spec/dummy/app/views/pages
-
   private
 
   def initialize_shared_store
@@ -25,22 +19,7 @@ class Styleguide::PagesController < ApplicationController
 
   def data
     # This is the props used by the React component.
-    @app_props_server_render = {
-      helloWorldData: {
-        name: "Mr. Server Side Rendering"
-      }
-    }
+    @app_props_server_render = {}
 
-    @app_props_hello = {
-      helloWorldData: {
-        name: "Mrs. Client Side Rendering"
-      }
-    }
-
-    @app_props_hello_again = {
-      helloWorldData: {
-        name: "Mrs. Client Side Hello Again"
-      }
-    }
   end
 end
