e7e2d902ca17bc13fce1d3d0cfa060f4c4ecb08b
Mikko Koski <mikkokos@gmail.com>
2016-04-11 16:40:07 +0300

    Add generic migration helpers for common migration patterns (#1895)
    * Add generic migration helpers for common migration patterns
    * Generic migration helper taken into use
    * Use `exec_update` to get the info about rows affected
    * Schema and annotations

--- app/models/email.rb
+++ app/models/email.rb
@@ -15,9 +15,10 @@
 #
 # Indexes
 #
-#  index_emails_on_address       (address)
 #  index_emails_on_community_id  (community_id)
-#  index_emails_on_person_id     (person_id)
+#  index_emails_on_address                   (address)
+#  index_emails_on_address_and_community_id  (address,community_id) UNIQUE
+#  index_emails_on_person_id                 (person_id)
 #
 
 class Email < ActiveRecord::Base
