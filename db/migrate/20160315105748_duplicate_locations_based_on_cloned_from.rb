1f3f36f506c8c496888cdd979162b69194a18c9a
Mikko Koski <mikkokos@gmail.com>
2016-04-12 10:57:34 +0300

    Fix column count didn't match

--- db/migrate/20160315105748_duplicate_locations_based_on_cloned_from.rb
+++ db/migrate/20160315105748_duplicate_locations_based_on_cloned_from.rb
@@ -2,7 +2,7 @@ class DuplicateLocationsBasedOnClonedFrom < ActiveRecord::Migration
   def up
     execute("
       INSERT INTO locations
-        (latitude, longitude, address, google_address, created_at, updated_at, person_id)
+        (latitude, longitude, address, google_address, created_at, updated_at, person_id, location_type)
         (SELECT l.latitude, l.longitude, l.address, l.google_address, l.created_at, l.updated_at, cloned_people.id, 'person'
          FROM locations as l
          LEFT JOIN people AS cloned_people ON cloned_people.cloned_from = l.person_id          WHERE l.location_type = 'person'
