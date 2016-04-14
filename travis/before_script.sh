695d2e7aba94c2d2801cfe546d3bc1f96b112d63
Kimmo Puputti <kimmo@sharetribe.com>
2016-04-14 13:38:33 +0300

    Clean up Travis scripts

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
+set -e
+
 echo "Running before_script"
 echo "SUITE: ${SUITE}"
 
-if [ "$SUITE" = "rspec" ]
-then
-	cp config/database.example.yml config/database.yml
-	mysql -e 'create database sharetribe_test;'
-	bundle exec rake db:test:load
-	exit
-elif [ "$SUITE" = "rubocop" ]
-then
-	exit
-elif [ "$SUITE" = "cucumber" ]
-then
-	cp config/database.example.yml config/database.yml
-	mysql -e 'create database sharetribe_test;'
-	bundle exec rake db:test:load
-	exit
-else
-	echo -e "Error: SUITE is illegal or not set\n"
-	exit 1
-fi
+case "$SUITE" in
+    rspec|cucumber)
+        echo "Setting up database for $SUITE"
+        cp config/database.example.yml config/database.yml
+        mysql -e 'create database sharetribe_test;'
+        bundle exec rake db:test:load
+        ;;
+esac
