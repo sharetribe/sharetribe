49d10432972699207150313f367d4566883a5b6a
Kimmo Puputti <kimmo@sharetribe.com>
2016-04-14 11:12:34 +0300

    Run client/server builds on CI

--- travis/script.sh
+++ travis/script.sh
@@ -17,11 +17,18 @@ elif [ "$SUITE" = "cucumber" ]
 then
   echo "PhantomJS version:"
   phantomjs --version
+  echo "Node.js version:"
+  node --version
+  echo "NPM version:"
+  npm --version
+  foreman start -f Procfile.spec &
   phantomjs --webdriver=8910 &
   PHANTOMJS=true bundle exec cucumber -ptravis 2>&1
   exit
 elif [ "$SUITE" = "eslint" ]
 then
+  node --version
+  npm --version
   cd client && npm run lint 2>&1
   exit
 fi
