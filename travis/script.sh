61fe64b62839bca60d7c82d1c5851407c47acd20
Kimmo Puputti <kimmo@sharetribe.com>
2016-04-15 10:37:48 +0300

    Run webpack builds without foreman in Travis

--- travis/script.sh
+++ travis/script.sh
@@ -7,22 +7,23 @@ echo "SUITE: ${SUITE}"
 
 if [ "$SUITE" = "rspec" ]
 then
-  bundle exec rspec spec 2>&1
-  exit
+    bundle exec rspec spec 2>&1
+    exit
 elif [ "$SUITE" = "rubocop" ]
 then
-  bundle exec rubocop -R 2>&1
-  exit
+    bundle exec rubocop -R 2>&1
+    exit
 elif [ "$SUITE" = "cucumber" ]
 then
-  echo "PhantomJS version:"
-  phantomjs --version
-  bundle exec foreman start -f Procfile.spec &
-  phantomjs --webdriver=8910 &
-  PHANTOMJS=true bundle exec cucumber -ptravis 2>&1
-  exit
+    echo "PhantomJS version:"
+    phantomjs --version
+    echo "Running client and server builds"
+    (cd client && npm run build:dev:client && npm run build:dev:server)
+    phantomjs --webdriver=8910 &
+    PHANTOMJS=true bundle exec cucumber -ptravis 2>&1
+    exit
 elif [ "$SUITE" = "eslint" ]
 then
-  cd client && npm run lint 2>&1
-  exit
+    cd client && npm run lint 2>&1
+    exit
 fi
