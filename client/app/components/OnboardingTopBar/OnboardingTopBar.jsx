bde1327f8f27ed7402699a124bb3b95ce9ebc442
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-04-15 10:04:26 +0300

    Hot reload doesn't support stateless functional components

--- client/app/components/OnboardingTopBar/OnboardingTopBar.jsx
+++ client/app/components/OnboardingTopBar/OnboardingTopBar.jsx
@@ -2,38 +2,39 @@ import React, { PropTypes } from 'react';
 
 import css from './OnboardingTopBar.scss';
 
-const OnboardingTopBar = (props) => {
-  function t(translationKey) {
-    return props.translations[translationKey];
+class OnboardingTopBar extends React.Component {
+
+  t(translationKey) {
+    return this.props.translations[translationKey];
   }
 
-  function next() {
-    if (!props.status.slogan_and_description) {
+  next() {
+    if (!this.props.status.slogan_and_description) {
       return {
         title: 'Add Slogan & Description',
         link: '/admin/effe',
       };
-    } else if (!props.status.cover_photo) {
+    } else if (!this.props.status.cover_photo) {
       return {
         title: 'Upload cover photo',
         link: '/admin/effe',
       };
-    } else if (!props.status.filter) {
+    } else if (!this.props.status.filter) {
       return {
         title: 'Add Fields & Filters',
         link: '/admin/effe',
       };
-    } else if (!props.status.paypal) {
+    } else if (!this.props.status.paypal) {
       return {
         title: 'Accept payments',
         link: '/admin/effe',
       };
-    } else if (!props.status.listing) {
+    } else if (!this.props.status.listing) {
       return {
         title: 'Add a listing',
         link: '/admin/effe',
       };
-    } else if (!props.status.invitation) {
+    } else if (!this.props.status.invitation) {
       return {
         title: 'Invite users',
         link: '/admin/effe',
@@ -42,27 +43,27 @@ const OnboardingTopBar = (props) => {
     return null;
   }
 
-  function progress() {
-    const steps = Object.keys(props.status).filter((entry) =>
+  progress() {
+    const steps = Object.keys(this.props.status).filter((entry) =>
       ['slogan_and_description',
        'cover_photo',
        'filter',
        'paypal',
        'listing',
-       'invitation'].includes(entry)).map((key) => [key, props.status[key]]);
+       'invitation'].includes(entry)).map((key) => [key, this.props.status[key]]);
     const completed = steps.filter((entry) => entry[1]);
     return 100 * completed.length / steps.length;
   }
 
-  function nextElement() {
-    if (next()) {
+  nextElement() {
+    if (this.next()) {
       return (
         <div className={css.nextContainer}>
           <div className={css.nextLabel}>
-            {t('next_step')}:
+            {this.t('next_step')}:
           </div>
-          <a href={next().link} className={css.nextButton}>
-            <span>{next().title}</span>
+          <a href={this.next().link} className={css.nextButton}>
+            <span>{this.next().title}</span>
           </a>
         </div>
       );
@@ -70,20 +71,23 @@ const OnboardingTopBar = (props) => {
     return null;
   }
 
-  return (
-    <div className={css.topbarContainer}>
-      <div className={css.topbar}>
-        <div className={css.progressLabel}>
-          {t('progress_label')}: <span className={css.progressLabelPercentage}>{progress().toPrecision(2)} %</span>
+  render() {
+    return (
+      <div className={css.topbarContainer}>
+        <div className={css.topbar}>
+          <div className={css.progressLabel}>
+            {this.t('progress_label')}:
+            <span className={css.progressLabelPercentage}>{this.progress().toPrecision(2)} %</span>
+          </div>
+          <div className={css.progressBarBackground}>
+            <div className={css.progressBar} style={{ width: `${this.progress()}%` }} />
+          </div>
+          {this.nextElement()}
         </div>
-        <div className={css.progressBarBackground}>
-          <div className={css.progressBar} style={{ width: `${progress()}%` }} />
-        </div>
-        {nextElement()}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 OnboardingTopBar.propTypes = {
   translations: PropTypes.object,
