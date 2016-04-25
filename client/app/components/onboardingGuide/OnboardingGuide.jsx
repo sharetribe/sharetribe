01da1a3eb1db1964d30995693ae90c2d55ec05d6
Vesa Luusua <vesa.luusua@gmail.com>
2016-04-25 18:30:48 +0300

    Define PropTypes

--- client/app/components/onboardingGuide/OnboardingGuide.jsx
+++ client/app/components/onboardingGuide/OnboardingGuide.jsx
@@ -3,6 +3,24 @@ import React, { PropTypes } from 'react';
 
 export default class OnboardingGuide extends React.Component {
 
+  static propTypes = {
+    actions: PropTypes.shape({
+      updateGuidePage: PropTypes.func.isRequired,
+    }).isRequired,
+    railsContext: PropTypes.object.isRequired,
+    data: PropTypes.shape({
+      path: PropTypes.string.isRequired,
+      pathHistoryForward: PropTypes.bool,
+      name: PropTypes.string.isRequired,
+      translations: PropTypes.object.isRequired,
+      onboarding_data: PropTypes.objectOf(PropTypes.shape({
+        infoImage: PropTypes.string.isRequired,
+        link: PropTypes.string.isRequired,
+        status: PropTypes.bool.isRequired,
+      }).isRequired).isRequired,
+    }).isRequired,
+  };
+
 
   constructor(props, context) {
     super(props, context);
