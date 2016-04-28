7abcc6a3330065732ac1a6d37b02bf68adc4ae60
Sampo Toiva <sampo.toiva@gmail.com>
2016-04-28 22:52:43 +0300

    Refer to accepted_community instead of communities

--- app/services/listing_visibility_guard.rb
+++ app/services/listing_visibility_guard.rb
@@ -39,7 +39,7 @@ class ListingVisibilityGuard
   end
 
   def user_member_of_community?
-    @user.communities.include?(@community)
+    @user.accepted_community == @community
   end
 
   def public_community?
