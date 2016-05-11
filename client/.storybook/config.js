5bd4cca26ba5c40a57bbdd9b495d13d1fe927513
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-05-11 15:27:01 +0300

    Colors and typography in styleguide

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,9 +1,9 @@
 import { configure } from '@kadira/storybook';
-//import '../app/components/OnboardingTopbar/OnboardingTopbar.scss';
+import '../app/assets/styles/base.scss';
 
 function loadStories() {
   require('../app/components/OnboardingTopbar/OnboardingTopbar.story.js');
-  // require as many stories as you need.
+  require('../app/components/Styleguide/ColorsAndTypography.js');
 }
 
 configure(loadStories, module);
