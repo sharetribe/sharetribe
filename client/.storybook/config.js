126a3a50890a1a1b5a9e7c21e6fe74442a9611ec
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-05-13 16:06:09 +0300

    Fix styleguide to understand new I18n

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from '@kadira/storybook';
 import '../app/assets/styles/base.scss';
+import '../app/i18n/all';
 
 function loadStories() {
   require('../app/components/OnboardingTopbar/OnboardingTopbar.story.js');
