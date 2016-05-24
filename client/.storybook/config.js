2216e8347f0bcf09202795caceb8010191c0e63c
Vesa Luusua <vesa.luusua@gmail.com>
2016-05-24 11:45:51 +0300

    initialize I18n environment using 'en'

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,6 +1,10 @@
 import { configure } from '@kadira/storybook';
 import '../app/assets/styles/base.css';
 import '../app/i18n/all';
+import { initialize as initializeI18n } from '../app/utils/i18n';
+
+// initializeI18n needs to know railsContext and node_env
+initializeI18n({i18nLocale: 'en', i18nDefaultLocale: 'en'}, process.env.NODE_ENV);
 
 function loadStories() {
   require('../app/components/Styleguide/ColorsAndTypography.js');
