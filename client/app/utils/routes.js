bb58e297a686bad40333e9da3f5c607b64fd96ff
Mikko Koski <mikkokos@gmail.com>
2016-05-26 11:50:30 +0300

    Load routes with require

--- client/app/utils/routes.js
+++ client/app/utils/routes.js
@@ -1,4 +1,10 @@
-import Routes from '../routes/routes';
+let Routes = {};
+
+try {
+  Routes = require('../routes/routes.js');
+} catch (e) {
+  console.warn('Can not load route bundle routes.js');
+}
 
 function initialize(railsContext) {
   Routes.options.default_url_options.locale = railsContext.i18nLocale;
