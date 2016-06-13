0aac5b5cb0adccaca947c6b7cba0f95fb4e7de0e
Kimmo Puputti <kimmo@sharetribe.com>
2016-06-13 10:29:59 +0300

    SearchBar location autocompletion

--- client/app/components/composites/SearchBar/SearchBar.js
+++ client/app/components/composites/SearchBar/SearchBar.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-set-state */
+
 import { Component, PropTypes } from 'react';
 import { form, input, button, span } from 'r-dom';
 
@@ -13,16 +15,139 @@ const SEARCH_MODES = [
   SEARCH_MODE_KEYWORD_AND_LOCATION,
 ];
 
+const coordinates = (place) => {
+  if (place && place.geometry) {
+    return place.geometry.location.toUrlValue();
+  }
+  return null;
+};
+
+const getDetails = (placeId) => new Promise((resolve, reject) => {
+  const serviceStatus = window.google.maps.places.PlacesServiceStatus;
+  const div = document.createElement('div');
+  const service = new window.google.maps.places.PlacesService(div);
+
+  service.getDetails({ placeId }, (place, status) => {
+    if (status !== serviceStatus.OK) {
+      reject(new Error(`Could not get details for place id "${placeId}"`));
+    } else {
+      resolve(place);
+    }
+  });
+});
+
+const getPrediction = (location) => new Promise((resolve, reject) => {
+  const serviceStatus = window.google.maps.places.PlacesServiceStatus;
+  const service = new window.google.maps.places.AutocompleteService();
+
+  service.getPlacePredictions({ input: location }, (predictions, status) => {
+    if (status !== serviceStatus.OK) {
+      reject(new Error(`Prediction service status not OK: ${status}`));
+    } else if (predictions.length === 0) {
+      reject(new Error(`No predictions found for location "${location}"`));
+    } else {
+      resolve(getDetails(predictions[0].place_id));
+    }
+  });
+});
+
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    // The values of the inputs are synced to the component state
-    // until the form is submitted.
-    this.state = {
-      keyword: '',
-      location: '',
-    };
+    // Bind `this` within methods
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = { selectedPlace: null };
+  }
+  componentDidMount() {
+    document.body.classList.add(css.topLevelBody);
+    if (!this.locationInput) {
+      return;
+    }
+    const bounds = { north: -90, east: -180, south: 90, west: 180 };
+    const autocomplete = new window.google.maps.places.Autocomplete(this.locationInput, { bounds });
+    autocomplete.setTypes(['geocode']);
+    this.placeChangedListener = window.google.maps.event.addListener(
+      autocomplete,
+      'place_changed',
+      () => this.setState({ selectedPlace: autocomplete.getPlace() })
+    );
+  }
+  componentWillUnmount() {
+    document.body.classList.remove(css.topLevelBody);
+
+    if (this.placeChangedListener) {
+      this.placeChangedListener.remove();
+    }
+
+    // Clean up the generated autocompletion UI. Assumes there is only one.
+    const container = document.body.querySelector('.pac-container');
+
+    if (container) {
+      document.body.removeChild(container);
+    }
+  }
+  handleSubmit() {
+    if (!this.keywordInput && !this.locationInput) {
+      throw new Error('No input refs saved to submit SearchBar form');
+    }
+
+    const keywordValueStr = this.keywordInput ? this.keywordInput.value.trim() : '';
+    const locationValueStr = this.locationInput ? this.locationInput.value.trim() : '';
+
+    if ((keywordValueStr + locationValueStr).length === 0) {
+      // Skip submit when all inputs are empty
+      return;
+    }
+
+    const onSubmit = this.props.onSubmit;
+
+    if (!this.locationInput) {
+      // Only keyword input, submitting current input value
+      onSubmit({
+        keyword: keywordValueStr,
+        location: null,
+        coordinates: null,
+      });
+      return;
+    }
+
+    const keywordValue = this.keywordInput ? this.keywordInput.value : null;
+    const coords = coordinates(this.state.selectedPlace);
+
+    if (coords) {
+      // Place already selected, submitting selected value
+      onSubmit({
+        keyword: keywordValue,
+        location: locationValueStr,
+        coordinates: coords,
+      });
+    } else if (locationValueStr) {
+      // Predict location from the typed value
+      getPrediction(locationValueStr)
+        .then((place) => {
+          const predictedCoords = coordinates(place);
+          if (!predictedCoords) {
+            throw new Error(`Could not get coordinates from place predicted from location "${locationValueStr}"`);
+          }
+          onSubmit({
+            keyword: keywordValue,
+            location: locationValueStr,
+            coordinates: predictedCoords,
+          });
+        })
+        .catch((e) => {
+          console.error('failed to predict location:', e); // eslint-disable-line no-console
+        });
+    } else if (this.keywordInput) {
+      // Only keyword value present, submit that
+      onSubmit({
+        keyword: keywordValue,
+        location: '',
+        coords: null,
+      });
+    }
   }
   render() {
     const { mode, keywordPlaceholder, locationPlaceholder } = this.props;
@@ -31,14 +156,24 @@ class SearchBar extends Component {
       type: 'search',
       className: css.keywordInput,
       placeholder: keywordPlaceholder,
-      onChange: (e) => this.setState({ keyword: e.target.value }), // eslint-disable-line react/no-set-state
+      ref: (c) => {
+        this.keywordInput = c;
+      },
     });
     const locationInput = input({
       type: 'search',
       className: css.locationInput,
       placeholder: locationPlaceholder,
       autoComplete: 'off',
-      onChange: (e) => this.setState({ location: e.target.value }), // eslint-disable-line react/no-set-state
+
+      // When the user edits the selected location value, the fetched
+      // place object is not up to date anymore and has to be cleared.
+      onChange: () => this.setState({ selectedPlace: null }),
+
+      ref: (c) => {
+        this.locationInput = c;
+      },
+
     });
 
     const hasKeywordInput = mode === SEARCH_MODE_KEYWORD || mode === SEARCH_MODE_KEYWORD_AND_LOCATION;
@@ -48,7 +183,7 @@ class SearchBar extends Component {
       classSet: { [css.root]: true },
       onSubmit: (e) => {
         e.preventDefault();
-        this.props.onSubmit(this.state);
+        this.handleSubmit();
       },
     }, [
       hasKeywordInput ? keywordInput : null,
