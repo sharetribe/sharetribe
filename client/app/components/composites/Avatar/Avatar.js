7454c7d1f6b06198cf0336f7f7742fad1c7f49e8
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-06-16 13:11:00 +0300

    Open by hover

--- client/app/components/composites/Avatar/Avatar.js
+++ client/app/components/composites/Avatar/Avatar.js
@@ -1,5 +1,6 @@
 import { Component, PropTypes } from 'react';
 import r, { div, img } from 'r-dom';
+import _ from 'lodash';
 import classNames from 'classnames';
 import { className } from '../../../utils/PropTypes';
 import ArrowDropdown from './ArrowDropdown';
@@ -7,8 +8,39 @@ import ArrowDropdown from './ArrowDropdown';
 import css from './Avatar.css';
 
 class Avatar extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleHover = this.handleHover.bind(this);
+
+    this.state = {
+      menuOpen: false,
+    };
+
+    this.menuCloseDelay = 300;
+  }
+
+  handleHover(event) {
+    if (event && event.type === 'mouseover') {
+      if (this.debouncedClose) {
+        this.debouncedClose.cancel();
+        this.debouncedClose = null;
+      }
+      this.setState({ menuOpen: true }); // eslint-disable-line react/no-set-state
+    } else if (event && event.type === 'mouseout') {
+      if (!this.debouncedClose) {
+        this.debouncedClose = _.debounce(() => {
+          this.setState({ menuOpen: false }); // eslint-disable-line react/no-set-state
+        }, this.menuCloseDelay);
+        this.debouncedClose();
+      }
+    }
+  }
+
   render() {
     return div({
+      onMouseOver: this.handleHover,
+      onMouseOut: this.handleHover,
       className: classNames(this.props.className, css.avatar),
     }, [
       img({
@@ -16,7 +48,13 @@ class Avatar extends Component {
         src: this.props.image,
         className: css.avatarImage,
       }),
-      r(ArrowDropdown, { customColor: this.props.customColor, actions: this.props.actions }),
+      this.state.menuOpen ?
+        r(ArrowDropdown, {
+          customColor: this.props.customColor,
+          actions: this.props.actions,
+          onMouseOver: this.handleHover,
+          onMouseOut: this.handleHover,
+        }) : null,
     ]);
   }
 }
