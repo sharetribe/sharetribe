405d2d0113895f0ab90b18bbfba2f174ec4d3652
Antti Virolainen <antti@sharetribe.com>
2016-06-16 13:12:20 +0300

    Modify the allowed_emails type migration to allow downward rollback

--- db/migrate/20160615145518_increase_max_size_of_allowed_emails.rb
+++ db/migrate/20160615145518_increase_max_size_of_allowed_emails.rb
@@ -1,5 +1,8 @@
 class IncreaseMaxSizeOfAllowedEmails < ActiveRecord::Migration
-  def change
+  def up
     change_column :communities, :allowed_emails, :text, limit: 16.megabytes - 1
+  end
+  def down
+    change_column :communities, :allowed_emails, :text
   end
 end
