34ecc8cdea0c8acb5930ae10b271dfbd81228f5b
Sampo Toiva <sampo.toiva@gmail.com>
2016-06-21 16:14:52 +0300

    Redirect on signout to landing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -78,7 +78,7 @@ class SessionsController < ApplicationController
     sign_out
     session[:person_id] = nil
     flash[:notice] = t("layouts.notifications.logout_successful")
-    redirect_to search_path
+    redirect_to landing_page_path
   end
 
   def index
