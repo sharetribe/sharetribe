901c08fd878692af25173ef1b57391a1f7a21299
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-06-23 15:47:16 +0300

    Make all Top bar stories use common props base and Storify

--- client/app/components/sections/Topbar/Topbar.story.js
+++ client/app/components/sections/Topbar/Topbar.story.js
@@ -1,11 +1,12 @@
 import { storiesOf, action } from '@kadira/storybook';
 import r from 'r-dom';
-import withProps, { storify, defaultRailsContext } from '../../Styleguide/withProps';
+import { storify, defaultRailsContext } from '../../Styleguide/withProps';
 
 import Topbar from './Topbar';
 const containerStyle = { style: { minWidth: '600px', background: 'white' } };
 
 const baseProps = {
+  railsContext: defaultRailsContext,
   logo: {
     href: 'http://example.com',
     text: 'Bikerrrs',
@@ -17,6 +18,47 @@ const baseProps = {
     keyword_placeholder: 'Search...',
     location_placeholder: 'Location',
     onSubmit: action('submitting search'),
+  },
+  menu: {
+    links: [
+      {
+        link: 'http://example.com#about',
+        title: 'About',
+      },
+      {
+        link: 'http://example.com#link',
+        title: 'Link',
+      },
+      {
+        link: 'http://example.com#link2',
+        title: 'Link2',
+      },
+      {
+        link: 'http://example.com#longlink',
+        title: 'Lorem ipsum dolor sit amet consectetur adepisci velit',
+      },
+    ],
+  },
+  locales: {
+    current_locale: 'en',
+    current_locale_ident: 'en',
+    available_locales: [
+      {
+        change_locale_uri: 'http://example.com#en',
+        locale_name: 'English',
+        locale_ident: 'en',
+      },
+      {
+        change_locale_uri: 'http://example.com#fi',
+        locale_name: 'Suomi',
+        locale_ident: 'fi',
+      },
+      {
+        change_locale_uri: 'http://example.com#fr',
+        locale_name: 'French',
+        locale_ident: 'fr',
+      },
+    ],
   },
   avatarDropdown: {
     customColor: '#EE4',
@@ -37,109 +79,26 @@ const baseProps = {
   },
 };
 
+const storifyTopbar = (props) => r(storify(r(Topbar, props)), containerStyle);
+
 storiesOf('Top bar')
   .add('Basic state', () => (
-    withProps(Topbar, baseProps)))
+    storifyTopbar(baseProps)))
   .add('Empty state', () => (
-    withProps(Topbar, { logo: baseProps.logo })))
+    storifyTopbar({ logo: baseProps.logo })))
   .add('Text logo', () => (
-    withProps(Topbar, { ...baseProps, logo: {
+    storifyTopbar({ ...baseProps, logo: {
       href: 'http://example.com',
       text: 'My Marketplace',
     } })))
   .add('Long text logo', () => (
-    withProps(Topbar, { ...baseProps, logo: {
+    storifyTopbar({ ...baseProps, logo: {
       href: 'http://example.com',
       text: 'My Marketplace with a looong name',
     } })))
   .add('With keyword search', () => (
-    withProps(Topbar, { ...baseProps, search: { mode: 'keyword' } })))
+    storifyTopbar({ ...baseProps, search: { mode: 'keyword' } })))
   .add('With location search', () => (
-    withProps(Topbar, {
-      logo: {
-        href: 'http://example.com',
-        text: 'Bikerrrs',
-        image: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-        image_highres: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-      },
-      search: {
-        mode: 'location',
-        keyword_placeholder: 'Search...',
-        location_placeholder: 'Location',
-      },
-    })))
+    storifyTopbar({ ...baseProps, search: { mode: 'location' } })))
   .add('With keyword and location search', () => (
-    withProps(Topbar, {
-      logo: {
-        href: 'http://example.com',
-        text: 'Bikerrrs',
-        image: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-        image_highres: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-      },
-      search: {
-        mode: 'keyword-and-location',
-        keyword_placeholder: 'Search...',
-        location_placeholder: 'Location',
-      },
-    })))
-    .add('Menu on About page', () => (
-      r(storify(
-        r(Topbar, Object.assign({}, {
-          logo: {
-            href: 'http://example.com',
-            text: 'Bikerrrs',
-            image: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-            image_highres: 'https://s3.amazonaws.com/sharetribe-manual-assets/styleguide/bikerrrs-logo.png',
-          },
-          search: {
-            mode: 'keyword-and-location',
-            keyword_placeholder: 'Search...',
-            location_placeholder: 'Location',
-          },
-          menu: {
-            links: [
-              {
-                link: 'http://example.com#about',
-                title: 'About',
-              },
-              {
-                link: 'http://example.com#link',
-                title: 'Link',
-              },
-              {
-                link: 'http://example.com#link2',
-                title: 'Link2',
-              },
-              {
-                link: 'http://example.com#longlink',
-                title: 'Lorem ipsum dolor sit amet consectetur adepisci velit',
-              },
-            ],
-          },
-          locales: {
-            current_locale: 'en',
-            current_locale_ident: 'en',
-            available_locales: [
-              {
-                change_locale_uri: 'http://example.com#en',
-                locale_name: 'English',
-                locale_ident: 'en',
-              },
-              {
-                change_locale_uri: 'http://example.com#fi',
-                locale_name: 'Suomi',
-                locale_ident: 'fi',
-              },
-              {
-                change_locale_uri: 'http://example.com#fr',
-                locale_name: 'French',
-                locale_ident: 'fr',
-              },
-            ],
-          },
-        }, {
-          railsContext: defaultRailsContext,
-        })),
-        containerStyle
-      ))
-  ));
+    storifyTopbar({ ...baseProps, search: { mode: 'keyword-and-location' } })));
