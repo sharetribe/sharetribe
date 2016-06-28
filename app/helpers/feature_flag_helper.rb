feb7b7f355dd9f08f14033295dd8e42557221a5f
Boyan Tabakov <boyan@sharetribe.com>
2016-06-28 15:29:35 +0300

    Move fetch_feature_flags method to helper module

--- app/helpers/feature_flag_helper.rb
+++ app/helpers/feature_flag_helper.rb
@@ -12,6 +12,17 @@ module FeatureFlagHelper
     @feature_flags ||= fetch_feature_flags # fetch_feature_flags is defined in ApplicationController
   end
 
+  def fetch_feature_flags
+    flags_from_service = FeatureFlagService::API::Api.features.get(community_id: @current_community.id).maybe[:features].or_else(Set.new)
+
+    is_admin = Maybe(@current_user).is_admin?.or_else(false)
+    temp_flags = ApplicationController.fetch_temp_flags(is_admin, params, session)
+
+    session[:feature_flags] = temp_flags
+
+    flags_from_service.union(temp_flags)
+  end
+
   def search_engine
     use_external_search = Maybe(APP_CONFIG).external_search_in_use.map { |v| v == true || v.to_s.casecmp("true") == 0 }.or_else(false)
     use_external_search ? :zappy : :sphinx
