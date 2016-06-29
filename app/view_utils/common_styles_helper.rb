0467dd28c0ef453ebaf32a101dda6405747cb52b
Kimmo Puputti <kimmo@sharetribe.com>
2016-06-29 21:43:29 +0300

    Fix default color when custom color missing

--- app/view_utils/common_styles_helper.rb
+++ app/view_utils/common_styles_helper.rb
@@ -4,8 +4,8 @@ module CommonStylesHelper
 
   def marketplace_colors(community)
     {
-      marketplace_color1: community ? "##{community.custom_color1}" : "#a64c5d",
-      marketplace_color2: community ? "##{community.custom_color2}" : "#00a26c"
+      marketplace_color1: community&.custom_color1 ? "##{community.custom_color1}" : "#a64c5d",
+      marketplace_color2: community&.custom_color2 ? "##{community.custom_color2}" : "#00a26c"
     }
   end
 end
