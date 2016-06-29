05b9f18a4f8ba3dcf9fb505668346b0f94de6bee
Mikko Koski <mikkokos@gmail.com>
2016-06-29 10:42:55 +0300

    Fix rubocop

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -119,12 +119,12 @@ class LandingPageController < ActionController::Metal
     if cid
       community_backend.set_community!(cid, [locale].map(&:to_sym))
       community_translations = TranslationService::API::Api.translations.get(cid)[:data]
-      TranslationServiceHelper.community_translations_for_i18n_backend(community_translations).each { |locale, data|
+      TranslationServiceHelper.community_translations_for_i18n_backend(community_translations).each { |translation_locale, data|
         # Store community translations to I18n backend.
         #
         # Since the data in data hash is already flatten, we don't want to
         # escape the separators (. dots) in the key
-        community_backend.store_translations(locale, data, escape: false)
+        community_backend.store_translations(translation_locale, data, escape: false)
       }
     end
   end
