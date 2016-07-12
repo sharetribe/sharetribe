c6225b9f6d536ebba2288cef428efcea7311082c
Mikko Koski <mikkokos@gmail.com>
2016-07-12 09:45:53 +0300

    Remove darken

--- app/utils/color_utils.rb
+++ app/utils/color_utils.rb
@@ -19,25 +19,8 @@ module ColorUtils
       .upcase
   end
 
-  # Same as darken(color, percentage) is SASS
-  #
-  # Usage:
-  #
-  # darken("80E619", 20) == "4D8A0F"
-  #
-  def darken(name_or_hex, percentage)
-    hsl = Color::RGB.by_css(name_or_hex).to_hsl
-    hsl.l -= normalize_percentage(percentage)
-    hsl.to_rgb.hex.upcase
-  end
-
   def css_to_rgb_array(css)
     color = Color::RGB.by_css(css)
     [color.red.to_i, color.green.to_i, color.blue.to_i]
   end
-
-  def normalize_percentage(percentage)
-    percentage.to_f / 100.to_f
-  end
-
 end
--- spec/utils/color_utils_spec.rb
+++ spec/utils/color_utils_spec.rb
@@ -7,11 +7,4 @@ describe ColorUtils do
       expect(ColorUtils.brightness("80E619", 80)).to eq("66B814")
     end
   end
-
-  describe "#darken" do
-    it "takes hex color and percentage and returns darkened color" do
-      expect(ColorUtils.darken("80E619", 20)).to eq("4D8A0F")
-    end
-  end
-
 end
