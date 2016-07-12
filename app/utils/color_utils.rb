3866ae7fbdb6968d75c8580aaf25215505f6fd98
Mikko Koski <mikkokos@gmail.com>
2016-07-12 09:41:06 +0300

    Add ColorUtils.brigthness

--- app/utils/color_utils.rb
+++ app/utils/color_utils.rb
@@ -2,6 +2,23 @@ module ColorUtils
 
   module_function
 
+  # Same as in filter: brightness(80%) in CSS
+  #
+  # Usage:
+  #
+  # brightness("80E619", 80) == "66B814"
+  #
+  def brightness(name_or_hex, percentage)
+    # CSS brightness(80%) is the same as adjust_brightness(-20)
+    p = percentage - 100
+
+    Color::RGB
+      .by_css(name_or_hex)
+      .adjust_brightness(p)
+      .hex
+      .upcase
+  end
+
   # Same as darken(color, percentage) is SASS
   #
   # Usage:
--- spec/utils/color_utils_spec.rb
+++ spec/utils/color_utils_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe ColorUtils do
 
+  describe "#brightness" do
+    it "takes hex color and percentage and returns a new color with altered brightness" do
+      expect(ColorUtils.brightness("80E619", 80)).to eq("66B814")
+    end
+  end
+
   describe "#darken" do
     it "takes hex color and percentage and returns darkened color" do
       expect(ColorUtils.darken("80E619", 20)).to eq("4D8A0F")
