72055a124a374694268dd441b29969327f755820
Vesa Luusua <vesa.luusua@gmail.com>
2016-07-28 19:53:03 +0300

    priority links limit: migrations

--- app/models/marketplace_configurations.rb
+++ app/models/marketplace_configurations.rb
@@ -6,6 +6,7 @@
 #  community_id          :integer          not null
 #  main_search           :string(255)      default("keyword"), not null
 #  distance_unit         :string(255)      default("metric"), not null
+#  limit_priority_links  :integer
 #  created_at            :datetime         not null
 #  updated_at            :datetime         not null
 #  limit_search_distance :boolean          default(TRUE), not null
