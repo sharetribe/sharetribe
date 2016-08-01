cd68a8cf5b9a17f7bbe68ce2b389c6f61c99f942
Hannu Lyytikainen <hannu@sharetribe.com>
2016-08-01 10:38:21 +0300

    Add person_id field to feature_flags table

--- app/models/feature_flag.rb
+++ app/models/feature_flag.rb
@@ -4,6 +4,7 @@
 #
 #  id           :integer          not null, primary key
 #  community_id :integer          not null
+#  person_id    :string(255)
 #  feature      :string(255)      not null
 #  enabled      :boolean          default(TRUE), not null
 #  created_at   :datetime         not null
@@ -11,7 +12,7 @@
 #
 # Indexes
 #
-#  index_feature_flags_on_community_id  (community_id)
+#  index_feature_flags_on_community_id_and_person_id  (community_id,person_id)
 #
 
 class FeatureFlag < ActiveRecord::Base
