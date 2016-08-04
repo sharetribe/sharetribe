1f7ecfcbf946ce86a5e53d9137b70c8677be636d
Boyan Tabakov <boyan@sharetribe.com>
2016-08-04 11:31:34 +0300

    Handle HTML GET requests for followed_people
    Avoids a frequent error when browsers seem like mistakenly prefetching
    /:username/followed_people, which is intended only to be called by XHR.

--- app/controllers/followed_people_controller.rb
+++ app/controllers/followed_people_controller.rb
@@ -4,6 +4,7 @@ class FollowedPeopleController < ApplicationController
     target_user = Person.find_by!(username: params[:person_id], community_id: @current_community.id)
 
     respond_to do |format|
+      format.html { render nothing: true,  status: :not_acceptable }
       format.js { render partial: "people/followed_person", collection: target_user.followed_people, as: :person }
     end
   end
