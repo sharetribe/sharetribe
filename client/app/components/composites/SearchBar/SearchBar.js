f08f496d378579edede06f97c1c59b7816dfb9de
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-08-10 12:56:25 +0300

    Don't submit search when location is selected

--- client/app/components/composites/SearchBar/SearchBar.js
+++ client/app/components/composites/SearchBar/SearchBar.js
@@ -44,7 +44,6 @@ class SearchBar extends Component {
       'place_changed',
       () => {
         this.setState({ selectedPlace: autocomplete.getPlace() });
-        this.handleSubmit();
       }
     );
   }
