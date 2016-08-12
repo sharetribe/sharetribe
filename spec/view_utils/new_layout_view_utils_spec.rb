db2cef50555c5075f7be48b5ad999090a0ec75ad
Hannu Lyytikainen <hannu@sharetribe.com>
2016-08-12 14:01:53 +0300

    Improve NewLayoutViewUtils spec

--- spec/view_utils/new_layout_view_utils_spec.rb
+++ spec/view_utils/new_layout_view_utils_spec.rb
@@ -1,73 +1,139 @@
 require 'spec_helper'
 
 describe NewLayoutViewUtils do
-
-  TEST_FEATURES = [
-    { title: "Foo",
-      name: :foo
-    },
-    { title: "Bar",
-      name: :bar
-    },
-    { title: "Wat",
-      name: :wat
-    }
-  ]
-
-  it "#features" do
+  before do
+    TEST_FEATURES = [
+      { title: "Foo",
+        name: :foo
+      },
+      { title: "Bar",
+        name: :bar
+      },
+      { title: "Wat",
+        name: :wat
+      }
+    ]
     stub_const("NewLayoutViewUtils::FEATURES", TEST_FEATURES)
+  end
 
-    allow(FeatureFlagHelper).to receive(:feature_enabled_for_user?)
-      .with(:foo).and_return(true)
-    allow(FeatureFlagHelper) .to receive(:feature_enabled_for_user?)
-      .with(:bar).and_return(true)
-    allow(FeatureFlagHelper) .to receive(:feature_enabled_for_user?)
-      .with(:wat).and_return(false)
-    allow(FeatureFlagHelper).to receive(:feature_enabled_for_community?)
-      .with(:foo).and_return(false)
-    allow(FeatureFlagHelper) .to receive(:feature_enabled_for_community?)
-      .with(:bar).and_return(false)
-    allow(FeatureFlagHelper) .to receive(:feature_enabled_for_community?)
-      .with(:wat).and_return(true)
+  describe "#features" do
+    person_id = "xyz"
+    community_id = 123
 
-    expect(NewLayoutViewUtils.features).to eql([
-        { title: "Foo",
-          name: :foo,
-          enabled_for_user: true,
-          enabled_for_community: false
-        },
-        { title: "Bar",
-          name: :bar,
-          enabled_for_user: true,
-          enabled_for_community: false
-        },
-        { title: "Wat",
-          name: :wat,
-          enabled_for_user: false,
-          enabled_for_community: true
+    context "when features are enabled for person and community" do
+      before do
+        person_features = FeatureFlagService::Store::FeatureFlag::PersonFlag.call(
+          {
+            person_id: person_id,
+            features: [:foo, :bar].to_set
+          }
+        )
+
+        community_features = FeatureFlagService::Store::FeatureFlag::CommunityFlag.call(
+          {
+            community_id: community_id,
+            features: [:wat].to_set
+          }
+        )
+
+        allow(FeatureFlagService::API::Api.features).to receive(:get)
+          .with({person_id: person_id}).and_return(Result::Success.new(person_features))
+        allow(FeatureFlagService::API::Api.features).to receive(:get)
+          .with({community_id: community_id}).and_return(Result::Success.new(community_features))
+      end
+
+      it "should return list of feature flags with corresponding features enabled for person and community" do
+        expect(NewLayoutViewUtils.features(community_id, person_id)).to eql([
+          { title: "Foo",
+            name: :foo,
+            enabled_for_user: true,
+            enabled_for_community: false
+          },
+          { title: "Bar",
+            name: :bar,
+            enabled_for_user: true,
+            enabled_for_community: false
+          },
+          { title: "Wat",
+            name: :wat,
+            enabled_for_user: false,
+            enabled_for_community: true
+          }
+        ])
+      end
+    end
+
+    context "when no features are enabled for person or community" do
+      before do
+        person_features = FeatureFlagService::Store::FeatureFlag::PersonFlag.call(
+          {
+            person_id: person_id,
+            features: Set.new
+          }
+        )
+
+        community_features = FeatureFlagService::Store::FeatureFlag::CommunityFlag.call(
+          {
+            community_id: community_id,
+            features: Set.new
+          }
+        )
+
+        allow(FeatureFlagService::API::Api.features).to receive(:get)
+          .with({person_id: person_id}).and_return(Result::Success.new(person_features))
+        allow(FeatureFlagService::API::Api.features).to receive(:get)
+          .with({community_id: community_id}).and_return(Result::Success.new(community_features))
+      end
+
+      it "should return list of feature flags with no features enabled" do
+        expect(NewLayoutViewUtils.features(community_id, person_id)).to eql([
+          { title: "Foo",
+            name: :foo,
+            enabled_for_user: false,
+            enabled_for_community: false
+          },
+          { title: "Bar",
+            name: :bar,
+            enabled_for_user: false,
+            enabled_for_community: false
+          },
+          { title: "Wat",
+            name: :wat,
+            enabled_for_user: false,
+            enabled_for_community: false
+          }
+        ])
+      end
+    end
+  end
+
+  describe "#enabled_features" do
+    context "when features are enabled" do
+      it "returns a list of those as symbols" do
+        feature_params = {
+          foo: "true",
+          bar: "true",
+          invalid: "value"
         }
-      ])
+        expect(NewLayoutViewUtils.enabled_features(feature_params))
+          .to eql([:foo, :bar])
+      end
+    end
+
+    context "when an empty hash is passed as params" do
+      it "returns an empty list" do
+        expect(NewLayoutViewUtils.enabled_features({}))
+          .to eql([])
+      end
+    end
   end
 
-  it "#enabled_features" do
-    feature_params = {
-      foo: "true",
-      bar: "true",
-      invalid: "value"
-    }
-
-    expect(NewLayoutViewUtils.enabled_features(feature_params))
-      .to eql([:foo, :bar])
-    expect(NewLayoutViewUtils.enabled_features({}))
-      .to eql([])
-  end
-
-  it "#resolve_disabled" do
-    stub_const("NewLayoutViewUtils::FEATURES", TEST_FEATURES)
-
-    expect(NewLayoutViewUtils.resolve_disabled([:foo, :bar]))
-      .to eql([:wat])
-    expect(NewLayoutViewUtils.resolve_disabled([]))
-      .to eql([:foo, :bar, :wat])
+  describe "#resolve_disabled" do
+    it "returns list of features that are not given as parameter" do
+      expect(NewLayoutViewUtils.resolve_disabled([:foo, :bar]))
+        .to eql([:wat])
+      expect(NewLayoutViewUtils.resolve_disabled([]))
+        .to eql([:foo, :bar, :wat])
+    end
   end
 end
