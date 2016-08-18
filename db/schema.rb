6213bfe486239357f7abaf985ebc14d719ff7dae
Kimmo Puputti <kimmo@sharetribe.com>
2016-08-18 14:42:50 +0300

    Change BT preauthorize processes to free

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818110351) do
+ActiveRecord::Schema.define(version: 20160818111724) do
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string   "token",            limit: 255
