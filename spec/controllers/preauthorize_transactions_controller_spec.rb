24a7ec8b03347c55b89d42825aa0382eb4459e55
Mikko Koski <mikkokos@gmail.com>
2016-08-30 11:04:35 +0300

    Tests for validations

--- spec/controllers/preauthorize_transactions_controller_spec.rb
+++ spec/controllers/preauthorize_transactions_controller_spec.rb
@@ -55,3 +55,141 @@ describe PreauthorizeTransactionsController::OrderTotal do
       .to eq(Money.new(129_000, "EUR"))
   end
 end
+
+describe PreauthorizeTransactionsController::Validator do
+
+  let(:validator) { PreauthorizeTransactionsController::Validator }
+
+  describe "#validate_delivery_method" do
+
+    context "valid" do
+      it "passes valid delivery method" do
+        params = {
+          tx_params: {
+            delivery: :shipping
+          },
+          shipping_enabled: true,
+          pickup_enabled: true
+        }
+
+        expect(validator.validate_delivery_method(params).success).to eq(true)
+      end
+    end
+
+    context "invalid" do
+
+      it "fails for invalid delivery method" do
+        params = {
+          tx_params: {
+            delivery: :shipping
+          },
+          shipping_enabled: false,
+          pickup_enabled: false
+        }
+
+        expect(validator.validate_delivery_method(params).data[:code]).to eq(:delivery_method_missing)
+      end
+
+      it "fails if delivery method is missing" do
+
+        params = {
+          tx_params: {
+            delivery: nil
+          },
+          shipping_enabled: true,
+          pickup_enabled: true
+        }
+
+        expect(validator.validate_delivery_method(params).data[:code]).to eq(:delivery_method_missing)
+      end
+    end
+  end
+
+  describe "#validate_booking" do
+    context "valid" do
+      it "passes for valid booking dates" do
+        params = {
+          tx_params: {
+            start_on: 1.day.from_now,
+            end_on: 2.days.from_now
+          },
+          is_booking: true
+        }
+
+        expect(validator.validate_booking(params).success).to eq(true)
+      end
+
+      it "passes if booking is not in use" do
+        params = {
+          tx_params: {},
+          is_booking: false
+        }
+
+        expect(validator.validate_booking(params).success).to eq(true)
+      end
+    end
+
+    context "invalid" do
+      it "fails if start date is after end date" do
+        params = {
+          tx_params: {
+            start_on: 1.day.from_now,
+            end_on: 2.days.ago
+          },
+          is_booking: true
+        }
+
+        expect(validator.validate_booking(params).data[:code]).to eq(:end_cant_be_before_start)
+
+      end
+
+      it "fails if start date or end date is missing" do
+        params = {
+          tx_params: {},
+          is_booking: true
+        }
+
+        expect(validator.validate_booking(params).data[:code]).to eq(:dates_missing)
+      end
+    end
+  end
+
+  describe "#validate_transaction_agreement" do
+    context "valid" do
+      it "passes if agreement is in use and agreed" do
+        params = {
+          tx_params: {
+            contract_agreed: true
+          },
+          transaction_agreement_in_use: true
+        }
+
+        expect(validator.validate_transaction_agreement(params).success).to eq(true)
+      end
+
+      it "passes if agreement is not in use" do
+        params = {
+          tx_params: {},
+          transaction_agreement_in_use: false
+        }
+
+        expect(validator.validate_transaction_agreement(params).success).to eq(true)
+      end
+
+    end
+
+    context "invalid" do
+      it "fails if agreement is in use but not agreed" do
+        params = {
+          tx_params: {
+            contract_agreed: false
+          },
+          transaction_agreement_in_use: true
+        }
+
+        expect(validator.validate_transaction_agreement(params).data[:code]).to eq(:agreement_missing)
+
+      end
+    end
+  end
+end
