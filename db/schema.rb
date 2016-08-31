f14b0ea282793287bddfe864dd759a941214bb33
Mikko Koski <mikkokos@gmail.com>
2016-08-31 13:59:37 +0300

    Run migrations (update schema.rb version)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831054909) do
+ActiveRecord::Schema.define(version: 20160831054910) do
 
   create_table "auth_tokens", force: :cascade do |t|
     t.string   "token",            limit: 255
