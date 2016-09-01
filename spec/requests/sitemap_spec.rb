bab5183686ac7a929c6cc49b00a39435e6f5c225
Mikko Koski <mikkokos@gmail.com>
2016-09-01 11:57:25 +0300

    Make sitemap specs request specs instead of controller specs
    - Controller specs don't work well with Metal controllers

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -1,17 +1,13 @@
 require 'spec_helper'
 
-RSpec.describe SitemapController, type: :controller do
+RSpec.describe SitemapController, type: :request do
 
   let(:valid_session) { {} }
 
   before(:each) do
-    @community = FactoryGirl.create(:community)
-    @current_community = @community
-    @request.host = "#{@community.ident}.lvh.me"
-    @request.env[:current_marketplace] = @community
-    @person = FactoryGirl.create(:person)
+    @domain = "marketplace.example.com"
+    @community = FactoryGirl.create(:community, domain: @domain, use_domain: true)
 
-    FactoryGirl.create(:community_membership, :person => @person, :community => @community)
     @listing = FactoryGirl.create(:listing, community_id: @community.id)
     @community2 = FactoryGirl.create(:community)
     @listing2 = FactoryGirl.create(:listing, community_id: @community2.id)
@@ -19,45 +15,45 @@ RSpec.describe SitemapController, type: :controller do
 
   describe "GET #sitemap" do
     it "creates sitemap" do
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.status).to eq(200)
     end
 
     it "contains root url" do
-      get :sitemap, {}, valid_session
-      expect(response.body).to match("<loc>http[^<]*"+@community.ident+"[^<]*</loc>")
+      get "http://#{@domain}/sitemap.xml"
+      expect(response.body).to match("<loc>http[^<]*"+@community.domain+"[^<]*</loc>")
     end
 
     it "contains listing" do
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.body).to match(Regexp.new("listings/"+@listing.id.to_s))
     end
 
     it "contains listing's nice name" do
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.body).to match(Regexp.new("listings/"+@listing.to_param))
     end
 
     it "doesn't contain other community listing" do
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.body).to_not match(Regexp.new("listings/"+@listing2.id.to_s))
     end
 
     it "if community is private returns forbidden" do
       @community.update(private: true)
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.status).to eq(403)
     end
 
     it "if community is deleted returns not found" do
       @community.update(deleted: true)
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.status).to eq(404)
     end
 
     it "if community is deleted and private returns not found" do
       @community.update(deleted: true, private: true)
-      get :sitemap, {}, valid_session
+      get "http://#{@domain}/sitemap.xml"
       expect(response.status).to eq(404)
     end
   end
