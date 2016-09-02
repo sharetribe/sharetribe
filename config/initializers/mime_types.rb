c99f24173f9451a6c4e59eb5b4980e54dd8c9082
Mikko Koski <mikkokos@gmail.com>
2016-09-02 09:15:38 +0300

    Remove the application/x-gzip mime type
    This was originally added for sitemap.xml.gz. However, we changed it so that we don't serve sitemap.xml.gz, but only sitemap.xml and let the Rack::Deflate middle ware to handle the transfer encoding.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,4 +3,3 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
-Mime::Type.register "application/x-gzip", :xml_gz, [], ["xml.gz"]
