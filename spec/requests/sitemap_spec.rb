49cae96946ca8768ca25854203e7703b5b29bf52
Mikko Koski <mikkokos@gmail.com>
2016-09-05 15:29:40 +0300

    Fix tests

--- spec/requests/sitemap_spec.rb
+++ spec/requests/sitemap_spec.rb
@@ -15,45 +15,49 @@ RSpec.describe SitemapController, type: :request do
 
   describe "GET #sitemap" do
     it "creates sitemap" do
-      get "http://#{@domain}/sitemap.xml"
+      get "http://#{@domain}/sitemap.xml.gz"
       expect(response.status).to eq(200)
     end
 
     it "contains root url" do
-      get "http://#{@domain}/sitemap.xml"
-      expect(response.body).to match("<loc>http[^<]*"+@community.domain+"[^<]*</loc>")
+      get "http://#{@domain}/sitemap.xml.gz"
+      expect(ActiveSupport::Gzip.decompress(response.body))
+        .to match("<loc>http[^<]*"+@community.domain+"[^<]*</loc>")
     end
 
     it "contains listing" do
-      get "http://#{@domain}/sitemap.xml"
-      expect(response.body).to match(Regexp.new("listings/"+@listing.id.to_s))
+      get "http://#{@domain}/sitemap.xml.gz"
+      expect(ActiveSupport::Gzip.decompress(response.body))
+        .to match(Regexp.new("listings/"+@listing.id.to_s))
     end
 
     it "contains listing's nice name" do
-      get "http://#{@domain}/sitemap.xml"
-      expect(response.body).to match(Regexp.new("listings/"+@listing.to_param))
+      get "http://#{@domain}/sitemap.xml.gz"
+      expect(ActiveSupport::Gzip.decompress(response.body))
+        .to match(Regexp.new("listings/"+@listing.to_param))
     end
 
     it "doesn't contain other community listing" do
-      get "http://#{@domain}/sitemap.xml"
-      expect(response.body).to_not match(Regexp.new("listings/"+@listing2.id.to_s))
+      get "http://#{@domain}/sitemap.xml.gz"
+      expect(ActiveSupport::Gzip.decompress(response.body))
+        .to_not match(Regexp.new("listings/"+@listing2.id.to_s))
     end
 
     it "if community is private returns forbidden" do
       @community.update(private: true)
-      get "http://#{@domain}/sitemap.xml"
+      get "http://#{@domain}/sitemap.xml.gz"
       expect(response.status).to eq(403)
     end
 
     it "if community is deleted returns not found" do
       @community.update(deleted: true)
-      get "http://#{@domain}/sitemap.xml"
+      get "http://#{@domain}/sitemap.xml.gz"
       expect(response.status).to eq(404)
     end
 
     it "if community is deleted and private returns not found" do
       @community.update(deleted: true, private: true)
-      get "http://#{@domain}/sitemap.xml"
+      get "http://#{@domain}/sitemap.xml.gz"
       expect(response.status).to eq(404)
     end
   end
