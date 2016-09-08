9ec9bb70407fd8faa5eff59c5fbbf0fc4d48f584
Hannu Lyytikainen <hannu@sharetribe.com>
2016-09-08 13:36:57 +0300

    Create new job for each image

--- db/migrate/20160908062011_process_images_for_listing_cards.rb
+++ db/migrate/20160908062011_process_images_for_listing_cards.rb
@@ -1,8 +1,8 @@
 class ProcessImagesForListingCards < ActiveRecord::Migration
 
   def up
-    select_values("SELECT id FROM listing_images").each_slice(1000) { |ids|
-      Delayed::Job.enqueue(CreateSquareImagesJob.new(ids), priority: 10, queue: "image_reprocess")
+    select_values("SELECT id FROM listing_images").each { |id|
+      Delayed::Job.enqueue(CreateSquareImagesJob.new(id), priority: 10, queue: "image_reprocess")
     }
   end
 
--- db/migrate_jobs/20160908062011/create_square_images_job.rb
+++ db/migrate_jobs/20160908062011/create_square_images_job.rb
@@ -1,28 +1,12 @@
-class CreateSquareImagesJob < Struct.new(:image_ids)
-
-  PAPERCLIP_OPTIONS =
-    if (APP_CONFIG.s3_bucket_name && APP_CONFIG.aws_access_key_id && APP_CONFIG.aws_secret_access_key)
-      {
-        :path => "images/people/:attachment/:id/:style/:filename",
-        :url => ":s3_domain_url"
-      }
-    else
-      {
-        :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-        :url => "/system/:attachment/:id/:style/:filename"
-      }
-    end
+class CreateSquareImagesJob < Struct.new(:image_id)
 
   class ListingImage < ActiveRecord::Base
     self.primary_key = "id"
 
-    has_attached_file(:image,
-                      {
-                        :styles => {
-                          :square => "408x408#",
-                          :square_2x => "816x816#"
-                        }
-                      }.merge(PAPERCLIP_OPTIONS))
+    has_attached_file(:image, styles: {
+      :square => "408x408#",
+      :square_2x => "816x816#"
+    })
 
     validates_attachment_size :image, :less_than => 9.megabytes
     validates_attachment_content_type :image,
@@ -31,8 +15,6 @@ class CreateSquareImagesJob < Struct.new(:image_ids)
   end
 
   def perform
-    ListingImage.where(id: image_ids).each { |i|
-      i.image.reprocess_without_delay!(:square, :square_2x)
-    }
+    ListingImage.find(image_id).image.reprocess_without_delay!(:square, :square_2x)
   end
 end
