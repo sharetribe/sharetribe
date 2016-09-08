48f10e9af8a7a8dcd3920caf99f20646bfafe075
Hannu Lyytikainen <hannu@sharetribe.com>
2016-09-08 14:44:25 +0300

    Write serialized delayed jobs to DB

--- db/migrate/20160908062011_process_images_for_listing_cards.rb
+++ db/migrate/20160908062011_process_images_for_listing_cards.rb
@@ -1,12 +1,25 @@
 class ProcessImagesForListingCards < ActiveRecord::Migration
 
   def up
-    select_values("SELECT id FROM listing_images").each { |id|
-      Delayed::Job.enqueue(CreateSquareImagesJob.new(id), priority: 10, queue: "image_reprocess")
+    select_values("SELECT id FROM listing_images").each_slice(1000) { |ids|
+
+      exec_insert("INSERT INTO delayed_jobs
+                  (priority, handler, last_error, run_at, created_at, updated_at, queue)
+                  VALUES #{values(ids)}", "create_delayed_jobs", [])
     }
   end
 
   def down
     # no op
+  end
+
+  def values(ids)
+    ids.map { |id|
+      "(1, #{handler(id)}, NULL, NOW(), NOW(), NOW(), 'image_reprocess')"
+    }.join(",")
+  end
+
+  def handler(id)
+    "'--- !ruby/struct:CreateSquareImagesJob\nimage_id: #{id}\n'"
   end
 end
