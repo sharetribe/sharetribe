f4096bade9161327956fb9e73e59fe32471b1483
Hannu Lyytikainen <hannu@sharetribe.com>
2016-09-27 15:45:26 +0300

    Apply operation only to open listings
    Only process images from listings that are still valid and open.

--- db/migrate/20160908062011_process_images_for_listing_cards.rb
+++ db/migrate/20160908062011_process_images_for_listing_cards.rb
@@ -1,7 +1,19 @@
 class ProcessImagesForListingCards < ActiveRecord::Migration
 
   def up
-    select_values("SELECT id FROM listing_images").each_slice(1000) { |ids|
+    # Select listing images that are related to listings
+    # that are not closed. For every 1000 of those images
+    # create a delayed job that creates square versions
+    # of the images.
+
+    select_values("
+      SELECT li.id
+      FROM listing_images AS li, listings AS l
+      WHERE 1=1
+        AND li.listing_id = l.id
+        AND l.open = 1
+        AND l.valid_until >= NOW()
+    ").each_slice(1000) { |ids|
 
       exec_insert("INSERT INTO delayed_jobs
                   (priority, handler, last_error, run_at, created_at, updated_at, queue)
