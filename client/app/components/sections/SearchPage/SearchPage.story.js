5584adbd14044dacc3fca4c7595d72da9287a0cd
Vesa Luusua <vesa.luusua@gmail.com>
2016-09-27 14:01:10 +0300

    SearchPage summary story needed some updating.

--- client/app/components/sections/SearchPage/SearchPage.story.js
+++ client/app/components/sections/SearchPage/SearchPage.story.js
@@ -1,17 +1,13 @@
 import r, { div, h1, h2, p } from 'r-dom';
-import { mount } from 'enzyme';
-
-import { storify } from '../../Styleguide/withProps';
-import { formatDistance, formatPrice } from '../../../utils/numbers';
-
+import { toFixedNumber } from '../../../utils/numbers';
 import ListingCard from '../../composites/ListingCard/ListingCard';
+import ListingCardPanel from '../../composites/ListingCardPanel/ListingCardPanel';
 import css from './SearchPage.story.css';
 
-const { storiesOf, specs, expect } = storybookFacade;
-const containerStyle = { style: { background: 'white' } };
+const { storiesOf } = storybookFacade;
+const LISTINGS_COUNT = 7;
 
-
-const ListingCardTemplate = (title, perUnit, price, distance) => (
+const listingCardTemplate = (title, perUnit, price, distance) => (
   r(ListingCard, Object.assign({},
     {
       id: 'iuttei7538746tr',
@@ -21,11 +17,11 @@ const ListingCardTemplate = (title, perUnit, price, distance) => (
       image2xURL: 'https://placehold.it/816x816',
       noImageText: 'No picture',
       avatarURL: 'https://placehold.it/40x40',
-      profileURL: `#profile${Math.random(10)}`,
-      price: price || (Math.random() * 9999).toFixed(2),
+      profileURL: `#profile${Math.random(10)}`,  // eslint-disable-line no-magic-numbers
+      price: price || toFixedNumber(Math.random() * 9999, 2),  // eslint-disable-line no-magic-numbers
       priceUnit: '€',
-      per: perUnit || '/ day',
-      distance: distance || Math.random() * (20000) + 0.01,
+      per: perUnit || '/ day',
+      distance: distance || Math.random() * (20000) + 0.01,  // eslint-disable-line no-magic-numbers
       distanceUnit: 'km',
       color: '#347F9D',
       className: css.listing,
@@ -33,33 +29,30 @@ const ListingCardTemplate = (title, perUnit, price, distance) => (
   ))
 );
 
-const generateListings = () => {
+const generateListings = (arrayLength) => {
   const titleDraft = 'Cisco SF300-48 SRW248G4-K9-NA 10/100 Managed Switch 48 Port';
   const titleDraftLength = titleDraft.length;
-  const listings = new Array(24);
-  for (var i = 0; i < listings.length; i++) {
+  const listings = new Array(arrayLength);
+
+  for (let i = 0; i < arrayLength; i++) {
     const title = titleDraft.substring(0, Math.random() * titleDraftLength);
-    listings[i] = ListingCardTemplate(title);
+    listings[i] = listingCardTemplate(title);
   }
 
   return listings;
-}
+};
 
 
 storiesOf('Search results')
   .add('Summary', () => (
-    div({
-      className: css.previewPage,
-    }, [
+    div(null, [
       h1({ className: css.title }, 'Listings'),
       p({ className: css.description }, 'Search results are shown in grid view using listing cards. They contain square images and the result set is paged.'),
       h2({ className: css.sectionTitle }, 'ListingCard'),
       div({
         className: css.singleListingWrapper,
-      }, ListingCardTemplate('Listing title', '/ hundred centimeters')),
-      h2({ className: css.sectionTitle }, 'ListingPage (not ready yet)'),
-      div({
-        className: css.wrapper,
-      }, generateListings()),
+      }, listingCardTemplate('Listing title', '/ hundred centimeters')),
+      h2({ className: css.sectionTitle }, `ListingCardPanel (${LISTINGS_COUNT} listings)`),
+      r(ListingCardPanel, null, generateListings(LISTINGS_COUNT)),
     ])
   ));
