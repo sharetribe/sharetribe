7edade3451f47ef35c5e5638eb3066ec4759c66d
Vesa Luusua <vesa.luusua@gmail.com>
2016-09-28 17:06:57 +0300

    ListingCard.story updated

--- client/app/components/composites/ListingCard/ListingCard.story.js
+++ client/app/components/composites/ListingCard/ListingCard.story.js
@@ -1,8 +1,10 @@
 import r from 'r-dom';
 import { mount } from 'enzyme';
+import Immutable from 'immutable';
 
 import { storify } from '../../Styleguide/withProps';
 import { formatDistance, formatPrice } from '../../../utils/numbers';
+import ListingModel, { Image, ImageRefs } from '../../../models/ListingModel';
 
 import ListingCard from './ListingCard';
 import css from './ListingCard.story.css';
@@ -13,72 +15,93 @@ const containerStyle = { style: { background: 'white' } };
 const ListingCardBasic =
   r(ListingCard,
     {
-      id: 'lkjg84573874yjdf',
-      title: 'Title',
-      listingURL: 'http://marketplace.com/listing/342iu4',
-      imageURL: 'https://placehold.it/408x408',
-      image2xURL: 'https://placehold.it/816x816',
-      noImageText: 'No picture',
-      avatarURL: 'https://placehold.it/40x40',
-      profileURL: '#profile1',
-      price: 21474836.47,
-      priceUnit: '€',
-      per: '/ hundred centimeters',
-      distance: 12972,
-      distanceUnit: 'mi',
-      color: '#347F9D',
       className: css.listing,
+      color: '#347F9D',
+      listing: new ListingModel({
+        id: 'lkjg84573874yjdf',
+        title: 'Title',
+        images: new Immutable.List([new ImageRefs({
+          square: new Image({
+            url: 'https://placehold.it/408x408',
+          }),
+          square2x: new Image({
+            type: 'square2x',
+            width: 816,
+            height: 816,
+            url: 'https://placehold.it/816x816',
+          }),
+        })]),
+        listingURL: 'https://example.com/listing/342iu4',
+        avatarURL: 'https://placehold.it/40x40',
+        profileURL: `#profile`,
+        price: 21474836.47,  // eslint-disable-line no-magic-numbers
+        priceUnit: '€',
+        per: '/ hundred centimeters',
+        distance: 12972,  // eslint-disable-line no-magic-numbers
+        distanceUnit: 'mi',
+      }),
     },
   );
 
 const ListingCardNoImage =
   r(ListingCard,
     {
-      id: 'lkjg84573874yjdf',
-      title: 'No picture',
-      listingURL: 'http://marketplace.com/listing/342iu4',
-      noImageText: 'No picture',
-      avatarURL: 'https://placehold.it/40x40',
-      profileURL: '#profile1',
-      price: 19,
-      priceUnit: '€',
-      per: '/ day',
-      distance: 0.67,
-      distanceUnit: 'km',
-      color: '#347F9D',
       className: css.listing,
+      color: '#347F9D',
+      listing: new ListingModel({
+        id: 'lkjg84573874yjdf',
+        title: 'No picture',
+        images: new Immutable.List([new ImageRefs()]),
+        listingURL: 'https://example.com/listing/342iu4',
+        avatarURL: 'https://placehold.it/40x40',
+        profileURL: `#profile`,
+        price: 19,  // eslint-disable-line no-magic-numbers
+        priceUnit: '€',
+        per: '/ day',
+        distance: 0.67,  // eslint-disable-line no-magic-numbers
+        distanceUnit: 'km',
+      }),
     },
   );
 
 const ListingCardImageError =
   r(ListingCard,
     {
-      id: 'lkjg84573874yjdf',
-      title: 'Picture load fails',
-      listingURL: 'http://marketplace.com/listing/342iu4',
-      imageURL: 'https://example.com/image.png',
-      image2xURL: 'https://example.com/image@2x.png',
-      noImageText: 'No picture',
-      avatarURL: 'https://placehold.it/40x40',
-      profileURL: '#profile1',
-      price: 199,
-      priceUnit: '€',
-      distance: 9,
-      distanceUnit: 'km',
-      color: '#347F9D',
       className: css.listing,
+      color: '#347F9D',
+      listing: new ListingModel({
+        id: 'lkjg84573874yjdf',
+        title: 'Picture load fails',
+        images: new Immutable.List([new ImageRefs({
+          square: new Image({ url: 'https://example.com/image.png' }),
+          square2x: new Image({
+            type: 'square2x',
+            width: '816',
+            height: '816',
+            url: 'https://example.com/image@2x.png',
+          }),
+        })]),
+        listingURL: 'https://example.com/listing/342iu4',
+        avatarURL: 'https://placehold.it/40x40',
+        profileURL: `#profile`,
+        price: 199,  // eslint-disable-line no-magic-numbers
+        priceUnit: '€',
+        per: '/ day',
+        distance: 9,  // eslint-disable-line no-magic-numbers
+        distanceUnit: 'km',
+      }),
     },
   );
 
 
 const testPrice = function priceTest(card, mountedCard) {
   it('Should display formatted price', () => {
-    expect(mountedCard.text()).to.include(formatPrice(card.props.price, card.props.priceUnit));
+    expect(mountedCard.text()).to.include(formatPrice(card.props.listing.get('price'), card.props.listing.get('priceUnit')));
   });
 };
 const testDistance = function priceTest(card, mountedCard) {
   it('Should display formatted distance', () => {
-    expect(mountedCard.text()).to.include(formatDistance(card.props.distance, card.props.distanceUnit));
+    expect(mountedCard.text()).to.include(formatDistance(card.props.listing.get('distance'), card.props.listing.get('distanceUnit')));
   });
 };
 
@@ -90,7 +113,7 @@ storiesOf('Search results')
 
     specs(() => describe('ListingCard - basic', () => {
       it('Should not display "No picture"', () => {
-        expect(mountedCard.text()).to.not.include(card.props.noImageText);
+        expect(mountedCard.text()).to.not.include('No picture');
         expect(mountedCard.find('.ListingCard_image')).to.have.length(1);
       });
       testPrice(card, mountedCard);
@@ -105,7 +128,7 @@ storiesOf('Search results')
 
     specs(() => describe('ListingCard - no image', () => {
       it('Should display "No picture"', () => {
-        expect(mountedCard.text()).to.include(card.props.noImageText);
+        expect(mountedCard.text()).to.include('No picture');
         expect(mountedCard.find('.ListingCard_image')).to.have.length(0);
       });
       testPrice(card, mountedCard);
@@ -122,7 +145,7 @@ storiesOf('Search results')
       it('Should display "No picture"', () => {
         const mounted = mount(card);
         mounted.setState({ imageStatus: 'failed' });
-        expect(mounted.text()).to.include(card.props.noImageText);
+        expect(mounted.text()).to.include('No picture');
         expect(mounted.find('.ListingCard_image')).to.have.length(0);
       });
       testPrice(card, mountedCard);
