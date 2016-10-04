394f8e442dfd980baaf57079b98c0fe5e8425079
beschloo <brennan.schloo@gmail.com>
2016-10-04 00:19:38 -0700

    reverse post order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
                            :use_ssl  => true)
 
     @posts = wp.getPosts(filter: {number: 1000, post_status: "publish"})
+    @posts = @posts.reverse
 
 
     render json: @posts if request.format.json?
