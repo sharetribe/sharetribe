72a933c68203176e9f9b8d0650046e736e6a5ad6
Sebastian Bean <sebbean@gmail.com>
2016-10-04 21:19:24 -0400

    posts show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
   def index
-    wp = Rubypress::Client.new(:host => "weareallnatives.wordpress.com", 
-                           :username => "weareallnatives", 
-                           :password => "changeme12",
-                           :use_ssl  => true)
-
     @posts = wp.getPosts(filter: {number: 1000, post_status: "publish"})
     @posts = @posts.reverse
 
 
     render json: @posts if request.format.json?
+  end
+
+  def show
+    @post = wp.getPost(post_id: params[:id])
+  end
+
+  private 
+
+  def wp
+    Rubypress::Client.new(host: "weareallnatives.wordpress.com",
+                      username: "weareallnatives", 
+                      password: "changeme12",
+                       use_ssl: true) 
   end
 end
