52f2229c25fe45c28801bc9226af1f3607f83725
Vesa Luusua <vesa.luusua@gmail.com>
2016-10-06 19:53:45 +0300

    Refactor ListingModel

--- client/app/models/ListingModel.js
+++ client/app/models/ListingModel.js
@@ -14,32 +14,30 @@ export const Money = Immutable.Record({
 
 const ListingModel = Immutable.Record({
   id: 'uuid',
+  authorId: 'foo',
   distance: new Distance(),
-  price: new Money(),
-  title: 'Listing',
   images: new Immutable.List([new ImageRefs({
     square: new Image(),
     square2x: new Image(),
   })]),
-  authorId: 'foo',
-  author: new Immutable.Record(),
+  price: new Immutable.Map({
+    ':money': new Money(),
+    ':pricingUnit': new Immutable.Map(),
+  }),
+  title: 'Listing',
 
   // these need to be updated
-  price: 1,
-  priceUnit: '$',
-  per: '/ day',
-  distance: 1,
-  distanceUnit: 'km',
+  author: new Immutable.Record(),
   listingURL: 'https://example.com/listing/1',
 });
 
 export const parse = (l) => new ListingModel({
   id: l.get(':id'),
+  authorId: l.getIn([':relationships', ':author', ':id']),
   distance: l.getIn([':attributes', ':distance']),
+  images: l.getIn([':attributes', ':images']),
   price: l.getIn([':attributes', ':price']),
   title: l.getIn([':attributes', ':title']),
-  images: l.getIn([':attributes', ':images']),
-  authorId: l.getIn([':relationships', ':author', ':id']),
 });
 
 export default ListingModel;
