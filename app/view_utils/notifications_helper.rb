6146f5f98b226f6a93b96a775994b1a5c62feea8
Mikko Porkola <mikko.porkola@sharetribe.com>
2016-10-11 14:01:37 +0300

    fixes

--- app/view_utils/notifications_helper.rb
+++ app/view_utils/notifications_helper.rb
@@ -5,11 +5,10 @@ module NotificationsHelper
     return nil if RequestStore.store.key?(:notifications_displayed)
 
     RequestStore.store[:notifications_displayed] = true
-    [:notice, :warning, :error].reduce(Hash.new) do |acc, level|
+    [:notice, :warning, :error].each_with_object({}) do |level, acc|
       if flash[level]
         acc[level] = flash[level]
       end
-      acc
     end.compact
   end
 end
