4cb66d869c3cc9ae18863d3e1efa3912440baed0
Mikko Koski <mikkokos@gmail.com>
2016-10-12 21:56:57 +0300

    Fix typo: queit -> quiet

--- features/support/feature_tests/worker.rb
+++ features/support/feature_tests/worker.rb
@@ -3,7 +3,7 @@ module FeatureTests
     module_function
 
     def work_until(&block)
-      worker = Delayed::Worker.new(queit: true)
+      worker = Delayed::Worker.new(quiet: true)
 
       default_max_wait_time = Capybara.default_max_wait_time
       Capybara.default_max_wait_time = 0.2
