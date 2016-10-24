59b6783f826d990103c2d32ce2fe8e6ea16f0df9
Antti Virolainen <antti@sharetribe.com>
2016-10-24 16:03:40 +0300

    Make allowed email check case-insensitive

--- app/services/email_service.rb
+++ app/services/email_service.rb
@@ -88,7 +88,7 @@ class EmailService
     allowed_array.each do |allowed_domain_or_address|
       allowed_domain_or_address.strip!
       allowed_domain_or_address.gsub!('.', '\.') #change . to be \. to only match a dot, not any char
-      if address =~ /#{allowed_domain_or_address}$/
+      if address.downcase =~ /#{allowed_domain_or_address.downcase}$/
         allowed = true
         break
       end
