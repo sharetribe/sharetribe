b8cd0ed2455581e6dda46b481dc53d20b410b67c
Mikko Koski <mikkokos@gmail.com>
2016-10-26 14:53:01 +0200

    Revert back to Transit JSON

--- app/services/harmony_client.rb
+++ app/services/harmony_client.rb
@@ -19,7 +19,7 @@ HarmonyClient = ServiceClient::Client.new(
     ServiceClient::Middleware::Timeout.new,
     ServiceClient::Middleware::Logger.new,
     ServiceClient::Middleware::Timing.new,
-    ServiceClient::Middleware::BodyEncoder.new(:transit_msgpack),
+    ServiceClient::Middleware::BodyEncoder.new(:transit_json),
     ServiceClient::Middleware::ParamEncoder.new,
     ServiceClient::Middleware::JwtAuthenticator.new(APP_CONFIG.harmony_api_disable_authentication,
                                                     APP_CONFIG.harmony_api_token_secret),
