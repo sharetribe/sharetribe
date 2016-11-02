28999c7b84c1a80a88693146aabe7aa51b9afe49
Mikko Koski <mikkokos@gmail.com>
2016-11-02 15:46:07 +0200

    Delete unnecessary validations
    TransactionService is responsible for saving booking data. The validations are made there.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -18,12 +18,6 @@ class Booking < ActiveRecord::Base
 
   belongs_to :tx, class_name: "Transaction", foreign_key: "transaction_id"
 
-  validates :start_on, :end_on_exclusive, presence: true
-  validates_with DateValidator,
-                 attribute: :end_on_exclusive,
-                 compare_to: :start_on,
-                 restriction: :on_or_after
-
   def self.columns
     super.reject { |c| c.name == "end_on" }
   end
