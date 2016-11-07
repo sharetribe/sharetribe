823239d4edce9e5687626a41cb4b2d1cbc3bf394
Kimmo Puputti <kimmo@sharetribe.com>
2016-11-07 16:18:02 +0200

    Add min price and min tx fee currencies to payment settings

--- app/models/payment_settings.rb
+++ app/models/payment_settings.rb
@@ -2,17 +2,19 @@
 #
 # Table name: payment_settings
 #
-#  id                            :integer          not null, primary key
-#  active                        :boolean          not null
-#  community_id                  :integer          not null
-#  payment_gateway               :string(64)
-#  payment_process               :string(64)
-#  commission_from_seller        :integer
-#  minimum_price_cents           :integer
-#  confirmation_after_days       :integer          not null
-#  created_at                    :datetime         not null
-#  updated_at                    :datetime         not null
-#  minimum_transaction_fee_cents :integer
+#  id                               :integer          not null, primary key
+#  active                           :boolean          not null
+#  community_id                     :integer          not null
+#  payment_gateway                  :string(64)
+#  payment_process                  :string(64)
+#  commission_from_seller           :integer
+#  minimum_price_cents              :integer
+#  minimum_price_currency           :string(3)
+#  minimum_transaction_fee_cents    :integer
+#  minimum_transaction_fee_currency :string(3)
+#  confirmation_after_days          :integer          not null
+#  created_at                       :datetime         not null
+#  updated_at                       :datetime         not null
 #
 # Indexes
 #
