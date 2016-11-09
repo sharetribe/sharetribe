409523af8be607614afa0507b782bce9e3045606
Mikko Koski <mikkokos@gmail.com>
2016-11-09 15:01:27 +0200

    Nicer indent and shorter line

--- app/services/harmony_client.rb
+++ app/services/harmony_client.rb
@@ -21,16 +21,17 @@ HarmonyClient = ServiceClient::Client.new(
     ServiceClient::Middleware::Timing.new,
     ServiceClient::Middleware::BodyEncoder.new(:transit_msgpack),
     ServiceClient::Middleware::ParamEncoder.new,
-    ServiceClient::Middleware::JwtAuthenticator.new(disable: APP_CONFIG.harmony_api_disable_authentication,
-                                                    secret: APP_CONFIG.harmony_api_token_secret,
-                                                    default_auth_context: ->(){
-                                                      session_ctx = SessionContextStore.get
+    ServiceClient::Middleware::JwtAuthenticator.new(
+      disable: APP_CONFIG.harmony_api_disable_authentication,
+      secret: APP_CONFIG.harmony_api_token_secret,
+      default_auth_context: ->(){
+        session_ctx = SessionContextStore.get
 
-                                                      {
-                                                        marketplace_id: session_ctx[:marketplace_uuid],
-                                                        actor_id: session_ctx[:user_uuid] || UUIDUtils.v0_uuid,
-                                                        actor_role: session_ctx[:user_role]
-                                                      }
-                                                    }),
+        {
+          marketplace_id: session_ctx[:marketplace_uuid],
+          actor_id: session_ctx[:user_uuid] || UUIDUtils.v0_uuid,
+          actor_role: session_ctx[:user_role]
+        }
+      }),
   ]
 )
