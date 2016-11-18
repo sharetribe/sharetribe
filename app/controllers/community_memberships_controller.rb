25ede79543612fa480c2e56fdae67c0fea60bde6
Mikko Koski <mikkokos@gmail.com>
2016-11-18 13:54:17 +0200

    Redirect user to previous page after sign up

--- app/controllers/community_memberships_controller.rb
+++ app/controllers/community_memberships_controller.rb
@@ -74,7 +74,13 @@ class CommunityMembershipsController < ApplicationController
       Analytics.record_event(flash, "GaveConsent")
 
       flash[:notice] = t("layouts.notifications.you_are_now_member")
-      redirect_to search_path
+
+      if session[:return_to]
+        redirect_to session[:return_to]
+        session[:return_to] = nil
+      else
+        redirect_to search_path
+      end
 
     }.on_error { |msg, data|
 
