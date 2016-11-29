357c4f60da9b66be92c5d6048e4f19d0f33ab07e
Boyan Tabakov <boyan@sharetribe.com>
2016-11-29 14:55:06 +0200

    Add support for setting passenger minimum instances

--- script/startup.sh
+++ script/startup.sh
@@ -2,4 +2,4 @@
 
 rm -f tmp/pids/server.pid
 
-exec bundle exec passenger start -p "${PORT-3000}" --max-pool-size "${PASSENGER_MAX_POOL_SIZE-1}"
+exec bundle exec passenger start -p "${PORT-3000}" --min-instances "${PASSENGER_MIN_INSTANCES-1}" --max-pool-size "${PASSENGER_MAX_POOL_SIZE-1}"
