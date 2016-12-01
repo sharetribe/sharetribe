8f16277d38f14148f4d7e5a1dd9f6f754b5ec89f
Fred Browne <fredericosetra@yahoo.com.br>
2016-12-01 00:52:02 -0200

    Image carousel - fix

--- app/models/listing_image_js_adapter.rb
+++ app/models/listing_image_js_adapter.rb
@@ -8,7 +8,7 @@ class ListingImageJSAdapter < JSAdapter
     @errored = listing_image.error.present?
     @images = {
       thumb: listing_image.image.url(:thumb),
-      big: listing_image.image.url(:big)
+      big: listing_image.image.url(:original)
     }
     @urls = {
       remove: listing_image_path(listing_image.id),
