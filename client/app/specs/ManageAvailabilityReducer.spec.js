39877d0f0828237cb852a2e801c4c1431a819f30
Kimmo Puputti <kimmo@sharetribe.com>
2016-12-12 11:35:33 +0200

    Update specs for renames and saving

--- client/app/specs/ManageAvailabilityReducer.spec.js
+++ client/app/specs/ManageAvailabilityReducer.spec.js
@@ -12,25 +12,38 @@ import reducer, { blockedDays, hasChanges } from '../reducers/ManageAvailability
 const CURRENT_MONTH = moment().startOf('month');
 const TODAY = moment();
 const TOMORROW = moment(TODAY).add(1, 'days');
+const DAY_AFTER_TOMORROW = moment(TODAY).add(2, 'days');
+
+const applyActions = (reducerFn, state, actionList) => {
+  if (actionList.size === 0) {
+    return state;
+  }
+  const head = actionList.first();
+  const tail = actionList.shift();
+  return applyActions(reducerFn, reducerFn(state, head), tail);
+};
 
 describe('ManageAvailabilityReducer', () => {
 
+  const stateEmpty = new Map({
+    isOpen: true,
+    visibleMonth: CURRENT_MONTH,
+    bookings: new List(),
+    blocks: new List(),
+    changes: new List(),
+    saveInProgress: false,
+    marketplaceUuid: null,
+    listingUuid: null,
+  });
+
+  const stateTodayBlocked = stateEmpty.set('blocks', new List([TODAY]));
+  const stateTodayBooked = stateEmpty.set('bookings', new List([TODAY]));
+
   describe('changes', () => {
-
-    const stateEmpty = new Map({
-      isOpen: true,
-      visibleMonth: CURRENT_MONTH,
-      reservedDays: new List(),
-      blockedDays: new List(),
-      changes: new List(),
-    });
-
-    const stateTodayBlocked = stateEmpty.set('blockedDays', new List([TODAY]));
-    const stateTodayReserved = stateEmpty.set('reservedDays', new List([TODAY]));
 
     it('has no changes initially', () => {
       const state = stateEmpty;
-      expect(state.get('blockedDays').size).to.equal(0);
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(0);
       expect(hasChanges(state)).to.equal(false);
       expect(blockedDays(state).size).to.equal(0);
@@ -38,7 +51,7 @@ describe('ManageAvailabilityReducer', () => {
 
     it('has an initial block', () => {
       const state = stateTodayBlocked;
-      expect(state.get('blockedDays').size).to.equal(1);
+      expect(state.get('blocks').size).to.equal(1);
       expect(state.get('changes').size).to.equal(0);
       expect(hasChanges(state)).to.equal(false);
       const blocked = blockedDays(state);
@@ -48,7 +61,7 @@ describe('ManageAvailabilityReducer', () => {
 
     it('adds a single block', () => {
       const state = reducer(stateEmpty, actions.blockDay(TODAY));
-      expect(state.get('blockedDays').size).to.equal(0);
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(1);
       expect(hasChanges(state)).to.equal(true);
       const blocked = blockedDays(state);
@@ -58,7 +71,7 @@ describe('ManageAvailabilityReducer', () => {
 
     it('adds a second block', () => {
       const state = reducer(stateTodayBlocked, actions.blockDay(TOMORROW));
-      expect(state.get('blockedDays').size).to.equal(1);
+      expect(state.get('blocks').size).to.equal(1);
       expect(state.get('changes').size).to.equal(1);
       expect(hasChanges(state)).to.equal(true);
       const blocked = blockedDays(state);
@@ -68,28 +81,31 @@ describe('ManageAvailabilityReducer', () => {
     });
 
     it('blocks and allows a day', () => {
-      const blockAction = actions.blockDay(TODAY);
-      const allowAction = actions.allowDay(TODAY);
-      const state = reducer(reducer(stateEmpty, blockAction), allowAction);
-      expect(state.get('blockedDays').size).to.equal(0);
+      const state = applyActions(reducer, stateEmpty, new List([
+        actions.blockDay(TODAY),
+        actions.unblockDay(TODAY),
+      ]));
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(2);
       expect(hasChanges(state)).to.equal(false);
       expect(blockedDays(state).size).to.equal(0);
     });
 
     it('allows an initially blocked day', () => {
-      const state = reducer(stateTodayBlocked, actions.allowDay(TODAY));
-      expect(state.get('blockedDays').size).to.equal(1);
+      const state = reducer(stateTodayBlocked, actions.unblockDay(TODAY));
+      expect(state.get('blocks').size).to.equal(1);
       expect(state.get('changes').size).to.equal(1);
       expect(hasChanges(state)).to.equal(true);
       expect(blockedDays(state).size).to.equal(0);
     });
 
     it('allows and blocks again an initially blocked day', () => {
-      const allowAction = actions.allowDay(TODAY);
-      const blockAction = actions.blockDay(TODAY);
-      const state = reducer(reducer(stateTodayBlocked, allowAction), blockAction);
-      expect(state.get('blockedDays').size).to.equal(1);
+      const state = applyActions(reducer, stateTodayBlocked, new List([
+        actions.unblockDay(TODAY),
+        actions.blockDay(TODAY),
+      ]));
+
+      expect(state.get('blocks').size).to.equal(1);
       expect(state.get('changes').size).to.equal(2);
       expect(hasChanges(state)).to.equal(false);
       const blocked = blockedDays(state);
@@ -98,29 +114,67 @@ describe('ManageAvailabilityReducer', () => {
     });
 
     it('allows an initially allowed day', () => {
-      const state = reducer(stateEmpty, actions.allowDay(TODAY));
-      expect(state.get('blockedDays').size).to.equal(0);
+      const state = reducer(stateEmpty, actions.unblockDay(TODAY));
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(1);
       expect(hasChanges(state)).to.equal(false);
       expect(blockedDays(state).size).to.equal(0);
     });
 
-    it('ignores an allow to a reserved day', () => {
-      const state = reducer(stateTodayReserved, actions.allowDay(TODAY));
-      expect(state.get('reservedDays').size).to.equal(1);
-      expect(state.get('blockedDays').size).to.equal(0);
+    it('ignores an allow to a booked day', () => {
+      const state = reducer(stateTodayBooked, actions.unblockDay(TODAY));
+      expect(state.get('bookings').size).to.equal(1);
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(0);
       expect(hasChanges(state)).to.equal(false);
       expect(blockedDays(state).size).to.equal(0);
     });
 
-    it('ignores a block to a reserved day', () => {
-      const state = reducer(stateTodayReserved, actions.blockDay(TODAY));
-      expect(state.get('reservedDays').size).to.equal(1);
-      expect(state.get('blockedDays').size).to.equal(0);
+    it('ignores a block to a booked day', () => {
+      const state = reducer(stateTodayBooked, actions.blockDay(TODAY));
+      expect(state.get('bookings').size).to.equal(1);
+      expect(state.get('blocks').size).to.equal(0);
       expect(state.get('changes').size).to.equal(0);
       expect(hasChanges(state)).to.equal(false);
       expect(blockedDays(state).size).to.equal(0);
+    });
+
+  });
+
+  describe('saving', () => {
+
+    it('toggles the save in progress flag', () => {
+      let state = stateEmpty;
+      expect(state.get('saveInProgress')).to.equal(false);
+      state = reducer(state, actions.startSaving());
+      expect(state.get('saveInProgress')).to.equal(true);
+      state = reducer(state, actions.changesSaved());
+      expect(state.get('saveInProgress')).to.equal(false);
+    });
+
+    it('ignores allows and blocks while saving', () => {
+      const initial = reducer(stateEmpty, actions.startSaving());
+      const afterStartSaving = reducer(initial, actions.startSaving());
+      const afterBlock = reducer(afterStartSaving, actions.blockDay(TODAY));
+      expect(afterBlock.equals(afterStartSaving)).to.equal(true);
+      const afterAllow = reducer(afterStartSaving, actions.unblockDay(TODAY));
+      expect(afterAllow.equals(afterStartSaving)).to.equal(true);
+    });
+
+    it('applies pending changes when saved', () => {
+      const state = applyActions(reducer, stateTodayBlocked, new List([
+        actions.blockDay(TOMORROW),
+        actions.blockDay(DAY_AFTER_TOMORROW),
+        actions.unblockDay(TOMORROW),
+        actions.startSaving(),
+        actions.changesSaved(),
+      ]));
+      expect(state.get('saveInProgress')).to.equal(false);
+      expect(hasChanges(state)).to.equal(false);
+      const blocked = state.get('blocks');
+      expect(blocked.size).to.equal(2);
+      expect(isSameDay(blocked.first(), TODAY)).to.equal(true);
+      expect(isSameDay(blocked.last(), DAY_AFTER_TOMORROW)).to.equal(true);
     });
 
   });
