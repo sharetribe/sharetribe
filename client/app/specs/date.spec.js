81deeb5aadb511551013aec9efe0386ff5ea9326
Mikko Koski <mikkokos@gmail.com>
2016-12-20 12:09:57 +0200

    Timezone fixes

--- client/app/specs/date.spec.js
+++ client/app/specs/date.spec.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-magic-numbers */
+
 import { expect } from 'chai';
 
 import { fromMidnightUTCDate, toMidnightUTCDate } from '../utils/date';
@@ -6,10 +8,10 @@ describe('Date utils', () => {
   describe('fromMidnightUTCDate', () => {
     it('converts midnight UTC dates to local midnight dates', () => {
       // Wednesday 21th Dec, midnight, UTC
-      var utc21dec = new Date(Date.UTC(2016, 11, 21, 0, 0, 0, 0));
+      const utc21dec = new Date(Date.UTC(2016, 11, 21, 0, 0, 0, 0));
 
       // Wednesday 21th Dec, midnight, local timezone
-      var local21dec = new Date(2016, 11, 21, 0, 0, 0, 0);
+      const local21dec = new Date(2016, 11, 21, 0, 0, 0, 0);
 
       expect(fromMidnightUTCDate(utc21dec).getTime()).to.equal(local21dec.getTime());
     });
@@ -18,10 +20,10 @@ describe('Date utils', () => {
   describe('toMidnightUTCDate', () => {
     it('converts local midnight dates to midnight UTC dates', () => {
       // Wednesday 21th Dec, midnight, UTC
-      var utc21dec = new Date(Date.UTC(2016, 11, 21, 0, 0, 0, 0));
+      const utc21dec = new Date(Date.UTC(2016, 11, 21, 0, 0, 0, 0));
 
       // Wednesday 21th Dec, midnight, local timezone
-      var local21dec = new Date(2016, 11, 21, 0, 0, 0, 0);
+      const local21dec = new Date(2016, 11, 21, 0, 0, 0, 0);
 
       expect(toMidnightUTCDate(local21dec).getTime()).to.equal(utc21dec.getTime());
     });
--- client/app/utils/date.js
+++ client/app/utils/date.js
@@ -2,18 +2,17 @@
    Common utilities for handling JavaScript native Date objects
 */
 
-
 /* Takes a UTC midnight date and converts it to local midnight date */
 export const fromMidnightUTCDate = (d) =>
   new Date(
     d.getUTCFullYear(),
     d.getUTCMonth(),
     d.getUTCDate(),
-    0, // h
-    0, // min
-    0, // s
-    0 // ms
-  )
+    0,
+    0,
+    0,
+    0
+  );
 
 /* Takes a JavaScript Date in local timezone and converts it to UTC
 midnight date */
@@ -23,8 +22,8 @@ export const toMidnightUTCDate = (d) =>
       d.getFullYear(),
       d.getMonth(),
       d.getDate(),
-      0, // h
-      0, // min
-      0, // s
-      0 // ms
-    ))
+      0,
+      0,
+      0,
+      0
+    ));
