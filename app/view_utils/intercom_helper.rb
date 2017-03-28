d08cb93f6d80f88fa17e5fb8e86dd4c80aeb1b3e
Mikko Koski <mikkokos@gmail.com>
2017-03-28 16:37:49 +0300

    Fix lint

--- app/view_utils/intercom_helper.rb
+++ app/view_utils/intercom_helper.rb
@@ -13,7 +13,7 @@ module IntercomHelper
     #
     # Remove the feature flag helper when this is published to everyone
     #
-    APP_CONFIG.admin_intercom_respond_enabled.to_s.downcase == "true" &&
+    APP_CONFIG.admin_intercom_respond_enabled.to_s.casecmp("true") &&
       FeatureFlagHelper.feature_enabled?(:admin_intercom_respond)
   end
 
