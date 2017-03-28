376183afccc8b6112f1e4b902c027245c8ea06f9
Mikko Koski <mikkokos@gmail.com>
2017-03-28 15:06:01 +0300

    Enable secure mode

--- app/view_utils/intercom_helper.rb
+++ app/view_utils/intercom_helper.rb
@@ -2,6 +2,13 @@ module IntercomHelper
 
   module_function
 
+  # Create a user_hash for Secure mode
+  # https://docs.intercom.com/configure-intercom-for-your-product-or-site/staying-secure/enable-secure-mode-on-your-web-product
+  def user_hash(user_id)
+    secret = APP_CONFIG.admin_intercom_secure_mode_secret
+    OpenSSL::HMAC.hexdigest('sha256', secret, user_id) if secret.present?
+  end
+
   def admin_intercom_respond_enabled?
     #
     # Remove the feature flag helper when this is published to everyone
