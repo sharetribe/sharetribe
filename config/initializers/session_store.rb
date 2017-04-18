2c228a969140d99aac10c86b44ae1ec7417e9592
Mikko Koski <mikkokos@gmail.com>
2017-04-18 15:13:37 +0300

    Store improvements

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,8 +2,28 @@
 
 require_relative "../../lib/active_sessions_helper"
 
+# Session store to seamlessly migrate from Database store to Cookie store
+#
+# This session store inherits the Cookie store. It overrides two methods:
+#
+# - load_session
+# - session_exists?
+#
+# After the migration period from Database store to Cookie store is over,
+# this session store can be replaced with the default Cookie store.
+#
 class ActionDispatch::Session::MigrateToCookieStore < ActionDispatch::Session::CookieStore
 
+  # Load session data from the cookie
+  #
+  # If session data can't be found from the cookie, it
+  # tries to the session data from the database store.
+  #
+  # If session is found from the database, a new "active session"
+  # is created.
+  #
+  # Old session from the database is deleted.
+  #
   def load_session(req)
     stale_session_check! do
       data = unpacked_cookie_data(req)
@@ -15,9 +35,14 @@ class ActionDispatch::Session::MigrateToCookieStore < ActionDispatch::Session::C
         db_session = find_db_session(req)
 
         if db_session
+          # Return session id from Database store
           db_id = ActiveSessionsHelper.create_from_migrated.to_s
-          [db_session.session_id, db_session.data.merge(db_id: db_id)]
+          session_data = [db_session.session_id, db_session.data.merge(db_id: db_id)]
+          db_session.destroy
+
+          session_data
         else
+          # Create new session using the normal CookieStore
           data = persistent_session_id!(data)
           [data["session_id"], data]
         end
@@ -26,6 +51,9 @@ class ActionDispatch::Session::MigrateToCookieStore < ActionDispatch::Session::C
     end
   end
 
+  # Checks if session exists.
+  # First if tries to find the session from the cookie. If not found
+  # from the cookie, it tries to find it from the database
   def session_exists?(env)
     super(env) || find_db_session(env).present?
   end
