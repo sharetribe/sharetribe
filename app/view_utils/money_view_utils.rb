7c9c1ace6c040422fa7a940830fd300bb0711720
Sampo Toiva <sampo.toiva@gmail.com>
2017-04-21 16:06:03 +0300

    Add utils for better money localization options

---
+++ app/view_utils/money_view_utils.rb
@@ -0,0 +1,17 @@
+module MoneyViewUtils
+  extend ActionView::Helpers::NumberHelper
+
+  module_function
+
+  # Converts instance of Money to properly formatted and localized string for view purposes
+  # Replaces spaces with narrow non breaking spaces, we do not want monetary amounts to be split on separate lines
+  def to_humanized(m, locale = I18n.locale)
+    separator = I18n.t("number.currency.format.separator", locale: locale)
+    precision = m.currency.exponent.to_i
+    zero_cents = "0" * precision
+    number_to_currency(m.amount, unit: m.symbol, locale: locale, precision: m.currency.exponent.to_i)
+      .tr(" ", "\u202F")
+      .gsub("#{separator}#{zero_cents}", "") # remove cents if they are zero
+      .encode('utf-8')
+  end
+end
