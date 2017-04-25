66bfb6ec91819851bdb320f76e59c9ac1a66b129
Sampo Toiva <sampo.toiva@gmail.com>
2017-04-25 22:02:03 +0300

    Add feature flag for currency formatting
    Javascript formatting is still applied to all places

--- app/view_utils/money_view_utils.rb
+++ app/view_utils/money_view_utils.rb
@@ -1,17 +1,25 @@
 module MoneyViewUtils
   extend ActionView::Helpers::NumberHelper
+  extend MoneyRails::ActionViewExtension # TODO: Remove when feature flag is removed
 
   module_function
 
   # Converts instance of Money to properly formatted and localized string for view purposes
   # Replaces spaces with narrow non breaking spaces, we do not want monetary amounts to be split on separate lines
   def to_humanized(m, locale = I18n.locale)
-    separator = I18n.t("number.currency.format.separator", locale: locale)
-    precision = m.currency.exponent.to_i
-    zero_cents = "0" * precision
-    number_to_currency(m.amount, unit: m.symbol, locale: locale, precision: m.currency.exponent.to_i)
-      .tr(" ", "\u202F")
-      .gsub("#{separator}#{zero_cents}", "") # remove cents if they are zero
-      .encode('utf-8')
+    # TODO: Remove feature flag
+    if FeatureFlagHelper.feature_enabled?(:currency_formatting)
+      separator = I18n.t("number.currency.format.separator", locale: locale)
+      precision = m.currency.exponent.to_i
+      zero_cents = "0" * precision
+      number_to_currency(m.amount, unit: m.symbol, locale: locale, precision: m.currency.exponent.to_i)
+        .tr(" ", "\u202F")
+        .gsub("#{separator}#{zero_cents}", "") # remove cents if they are zero
+        .encode('utf-8')
+    else
+      humanized_money_with_symbol(m).upcase
+    end
+  rescue FeatureFlagHelperNotInitialized
+    humanized_money_with_symbol(m).upcase
   end
 end
