69cf1d4e433068696bc9cde6b53564410e1f8cd6
Nik <devnik@ithouse.lv>
2017-06-14 11:33:31 +0300

    analytics: client side analytics scripts are subscribers for st-analytics:setup and st-analytics:event events

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -6,10 +6,10 @@ window.ST.analytics.setup = function(user_and_community_info) {
   $(document).trigger('st-analytics:setup', user_and_community_info);
 }
 
-window.ST.analytics.logEvent = function(category, action, opt_label) {
-  $(document).trigger('st-analytics:event', {category: category, action: action, opt_label: opt_label});
+window.ST.analytics.logEvent = function(category, action, opt_label, props) {
+  $(document).trigger('st-analytics:event', {category: category, action: action, opt_label: opt_label, props: props});
 }
 
 window.ST.analytics.logout = function() {
-  $(document).trigger('st-analytics:eventout');
+  $(document).trigger('st-analytics:logout');
 }
