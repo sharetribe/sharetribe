940ad5645b77c89131fd554644499c63633dc968
Valdis Pornieks <valdis@ithouse.lv>
2017-07-11 15:51:09 +0300

    Fix: updated  migration for image reordering for rails 5.1

--- db/migrate/20170613153965_set_default_listing_images_position.rb
+++ db/migrate/20170613153965_set_default_listing_images_position.rb
@@ -1,7 +1,9 @@
-class SetDefaultListingImagesPosition < ActiveRecord::Migration
-  def change
+class SetDefaultListingImagesPosition < ActiveRecord::Migration[5.1]
+  def up
     # MySQL InnoDB by default returns rows in order of primary key, but that behavior is not reliable.
     # Adding extra position column with NULL value broke expected implicit sort order
     ListingImage.where("position IS NULL OR position = 0").update_all("position = id")
   end
+
+  def down; end
 end
