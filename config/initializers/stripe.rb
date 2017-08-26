65235f23dfdd4a09a47c0f3f5b939218c1cec24c
Nikolai <devnik@ithouse.lv>
2017-08-26 08:55:39 +0300

    stripe integration: remove cached country_specs - not used anymore

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,3 @@
 unless defined? STRIPE_JS_HOST
   STRIPE_JS_HOST = 'https://js.stripe.com'
 end
-STRIPE_COUNTRY_SPECS = JSON.parse(IO.read(Rails.root.join("config/stripe_country_specs_cache.json")))
-
