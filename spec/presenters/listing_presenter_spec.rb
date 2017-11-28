32b40281b8a7a43eabcd0cdaf34680696b57dcca
Aivils Štoss <aivils@ithouse.lv>
2017-11-28 10:52:00 +0200

    Availability per hour - fixed blocked dates   - added timecop.js for front-end

--- spec/presenters/listing_presenter_spec.rb
+++ spec/presenters/listing_presenter_spec.rb
@@ -38,12 +38,15 @@ RSpec.describe ListingPresenter, type: :presenter do
     let(:listing) { FactoryGirl.create(:listing, community_id: community.id, listing_shape_id: 123) }
     let(:time_slot1) { FactoryGirl.create(:listing_working_time_slot, listing: listing, week_day: :tue, from: '09:00', till: '11:00') }
     let(:time_slot2) { FactoryGirl.create(:listing_working_time_slot, listing: listing, week_day: :tue, from: '14:00', till: '15:00') }
-    let(:transaction1) { FactoryGirl.create(:transaction, community: community, listing: listing) }
-    let(:transaction2) { FactoryGirl.create(:transaction, community: community, listing: listing) }
-    let(:transaction3) { FactoryGirl.create(:transaction, community: community, listing: listing) }
+    let(:transaction1) { FactoryGirl.create(:transaction, community: community, listing: listing, current_state: 'paid') }
+    let(:transaction2) { FactoryGirl.create(:transaction, community: community, listing: listing, current_state: 'paid') }
+    let(:transaction3) { FactoryGirl.create(:transaction, community: community, listing: listing, current_state: 'paid') }
+    let(:transaction4) { FactoryGirl.create(:transaction, community: community, listing: listing, current_state: 'rejected') }
     let(:booking1) { FactoryGirl.create(:booking, tx: transaction1, start_time: '2017-11-14 09:00', end_time: '2017-11-14 10:00', per_hour: true) }
     let(:booking2) { FactoryGirl.create(:booking, tx: transaction2, start_time: '2017-11-14 10:00', end_time: '2017-11-14 11:00', per_hour: true) }
     let(:booking3) { FactoryGirl.create(:booking, tx: transaction2, start_time: '2017-11-14 14:00', end_time: '2017-11-14 15:00', per_hour: true) }
+    let(:booking4) { FactoryGirl.create(:booking, tx: transaction4, start_time: '2017-11-14 09:00', end_time: '2017-11-14 10:00', per_hour: true) }
+
 
     it '#working_hours_blocked_days 2017-11-14 is busy full day.
         2017-11-13, 2017-11-15 no working hours' do
@@ -106,6 +109,25 @@ RSpec.describe ListingPresenter, type: :presenter do
           [
             {:value=>"09:00", :name=>" 9:00 am", :disabled=>true},
             {:value=>"10:00", :name=>"10:00 am", :disabled=>true},
+            {:value=>"11:00", :name=>"11:00 am", :disabled=>true, :slot_end=>true},
+            {:value=>"14:00", :name=>" 2:00 pm"},
+            {:value=>"15:00", :name=>" 3:00 pm", :disabled=>true, :slot_end=>true}
+          ]
+        )
+      end
+    end
+
+    it '#availability_per_hour_options_for_select_grouped_by_day rejected booking does not block.' do
+      Timecop.freeze(Time.local(2017, 11, 13)) do
+        time_slot1
+        time_slot2
+        booking4
+        options = ListingPresenter.new(listing, community, {}, person)
+          .availability_per_hour_options_for_select_grouped_by_day
+        expect(options["2017-11-14"]).to eq(
+          [
+            {:value=>"09:00", :name=>" 9:00 am"},
+            {:value=>"10:00", :name=>"10:00 am"},
             {:value=>"11:00", :name=>"11:00 am", :disabled=>true, :slot_end=>true},
             {:value=>"14:00", :name=>" 2:00 pm"},
             {:value=>"15:00", :name=>" 3:00 pm", :disabled=>true, :slot_end=>true}
