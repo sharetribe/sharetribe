003c62ef3edb6af633b51f0b116a1dc990174cd0
Valdis Pornieks <valdis@ithouse.lv>
2017-12-13 15:43:48 +0200

    fixed: make migrations rollback'able

--- db/migrate/20170814125622_do_not_store_personal_data_in_stripe_accounts.rb
+++ db/migrate/20170814125622_do_not_store_personal_data_in_stripe_accounts.rb
@@ -1,16 +1,16 @@
 class DoNotStorePersonalDataInStripeAccounts < ActiveRecord::Migration[5.1]
   def change
-    remove_column :stripe_accounts, :first_name
-    remove_column :stripe_accounts, :last_name
-    remove_column :stripe_accounts, :address_country
-    remove_column :stripe_accounts, :address_city
-    remove_column :stripe_accounts, :address_line1
-    remove_column :stripe_accounts, :address_postal_code
-    remove_column :stripe_accounts, :address_state
-    remove_column :stripe_accounts, :birth_date
-    remove_column :stripe_accounts, :bank_account_last_4
-    remove_column :stripe_accounts, :stripe_source_info
-    remove_column :stripe_accounts, :tos_date
-    remove_column :stripe_accounts, :tos_ip
+    remove_column :stripe_accounts, :first_name, :string
+    remove_column :stripe_accounts, :last_name, :string
+    remove_column :stripe_accounts, :address_country, :string
+    remove_column :stripe_accounts, :address_city, :string
+    remove_column :stripe_accounts, :address_line1, :string
+    remove_column :stripe_accounts, :address_postal_code, :string
+    remove_column :stripe_accounts, :address_state, :string
+    remove_column :stripe_accounts, :birth_date, :date
+    remove_column :stripe_accounts, :bank_account_last_4, :string
+    remove_column :stripe_accounts, :stripe_source_info, :string
+    remove_column :stripe_accounts, :tos_date, :datetime
+    remove_column :stripe_accounts, :tos_ip, :string
   end
 end
