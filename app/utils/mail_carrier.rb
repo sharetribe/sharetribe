4833b69120229125a51283229c4e1623f17740e8
Wisley Alves <33531578+wisley7l@users.noreply.github.com>
2017-12-28 22:34:35 -0200

    Update mail_carrier.rb
    Integration of Mailjet for sending emails

--- app/utils/mail_carrier.rb
+++ app/utils/mail_carrier.rb
@@ -1,3 +1,15 @@
+# These needed to load the config.yml
+require File.expand_path('/var/rails/sharetribe/config/config_loader', __FILE__)
+# Read the config from the config.yml
+APP_CONFIG = ConfigLoader.load_app_config
+# settings MailJet 
+require '/usr/local/lib/ruby/gems/2.3.0/gems/mailjet-1.5.4/lib/mailjet.rb'
+Mailjet.configure do |config|
+  config.api_key = APP_CONFIG.mailjet_user
+  config.secret_key = APP_CONFIG.mailjet_password
+  config.default_from = APP_CONFIG.mailjet_reply
+end
+
 module MailCarrier
 
   module_function
@@ -9,7 +21,16 @@ module MailCarrier
   end
 
   def deliver_now(message)
-    message.deliver_now
+    # Using MailJet
+    variable = Mailjet::Send.create(
+        from_email: "contato@e-com.club",
+        from_name: "E-Com",
+        subject: message.subject,
+        text_part: "",
+        html_part: message.decoded,
+        recipients: [{ 'Email'=> message.to}])
+    # Default not using
+    #message.deliver_now
   end
 
 end
