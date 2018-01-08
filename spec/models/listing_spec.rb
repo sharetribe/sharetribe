d614ab5ad79fc439a8e5338090b9ff30e4861c38
Aivils Štoss <aivils@ithouse.lv>
2018-01-08 14:11:05 +0200

    [Per hour] old listing schedule

--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -46,6 +46,7 @@
 #  shipping_price_cents            :integer
 #  shipping_price_additional_cents :integer
 #  availability                    :string(32)       default("none")
+#  per_hour_ready                  :boolean          default(FALSE)
 #
 # Indexes
 #
