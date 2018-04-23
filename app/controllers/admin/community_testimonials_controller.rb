aaf520860c9fdf31076fdd084ad36520bb201e0f
Aivils Štoss <aivils@ithouse.lv>
2018-04-23 15:53:11 +0300

    [Reviews] Update one copy text

--- app/controllers/admin/community_testimonials_controller.rb
+++ app/controllers/admin/community_testimonials_controller.rb
@@ -4,6 +4,7 @@ class Admin::CommunityTestimonialsController < Admin::AdminBaseController
     @selected_left_navi_link = "testimonials"
     @transactions = resource_scope.order("#{sort_column} #{sort_direction}")
       .paginate(:page => params[:page], :per_page => 30)
+    @testimonials = testimonials(@transactions)
   end
 
   private
@@ -21,5 +22,18 @@ class Admin::CommunityTestimonialsController < Admin::AdminBaseController
 
   def sort_direction
     params[:direction] || 'desc'
+  end
+
+  def testimonials(transactions)
+    pages = transactions.total_pages
+    tx_from = transactions.offset + 1
+    tx_to = pages > 1 ? (transactions.offset + transactions.length) : transactions.total_entries
+    {
+      all_count: Testimonial.merge(resource_scope).joins(:tx).count,
+      page_count: transactions.map{ |tx| tx.testimonials.size }.sum,
+      tx_total_pages: pages,
+      tx_from: tx_from,
+      tx_to: tx_to,
+    }
   end
 end
