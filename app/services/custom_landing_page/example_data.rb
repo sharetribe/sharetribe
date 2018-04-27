11485fece3fe0e6e471f7c72b43872a31a8f1bec
Luis-RG <luis@sharetribe.com>
2018-04-27 13:25:21 +0300

    changes pinterest for soundcloud

--- app/services/custom_landing_page/example_data.rb
+++ app/services/custom_landing_page/example_data.rb
@@ -276,7 +276,7 @@ module CustomLandingPage
         {"service": "googleplus", "url": "https://plus.google.com/114869130265262677354/"},
         {"service": "linkedin", "url": "https://www.linkedin.com/company/2626583"},
         {"service": "pinterest", "url": "https://www.pinterest.com/"},
-        {"service": "pinterest", "url": "https://soundcloud.com/"}"
+        {"service": "soundcloud", "url": "https://soundcloud.com/"}"
       ],
       "copyright": "Copyright Marketplace Ltd 2016"
     }
