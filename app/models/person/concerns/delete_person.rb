db5816e86030bac3fca8a9a446c7bc8188c9bf48
Boyan Tabakov <boyan@sharetribe.com>
2018-05-16 13:28:56 +0300

    Improve user deletion - clean all personal data

--- app/models/person/concerns/delete_person.rb
+++ app/models/person/concerns/delete_person.rb
@@ -13,9 +13,13 @@ module DeletePerson
           person.update_attributes(
             given_name: nil,
             family_name: nil,
+            display_name: nil,
             phone_number: nil,
             description: nil,
             facebook_id: nil,
+            username: "deleted_#{SecureRandom.hex(5)}",
+            current_sign_in_ip: nil,
+            last_sign_in_ip: nil,
             # To ensure user can not log in anymore we have to:
             #
             # 1. Delete the password (Devise rejects login attempts if the password is empty)
@@ -26,6 +30,9 @@ module DeletePerson
 
           # Delete emails
           person.emails.destroy_all
+
+          # Delete location
+          person.location&.destroy
 
           # Delete avatar
           person.image.destroy
