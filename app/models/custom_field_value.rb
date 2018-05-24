664f14d207f58afadecc2ad6a3c1607cd8952c33
Aivils Štoss <aivils@ithouse.lv>
2018-05-24 13:11:06 +0300

    [User fields] Copy person's phone number to the TextField

--- app/models/custom_field_value.rb
+++ app/models/custom_field_value.rb
@@ -12,10 +12,12 @@
 #  updated_at      :datetime         not null
 #  type            :string(255)
 #  delta           :boolean          default(TRUE), not null
+#  person_id       :string(255)
 #
 # Indexes
 #
 #  index_custom_field_values_on_listing_id  (listing_id)
+#  index_custom_field_values_on_person_id   (person_id)
 #  index_custom_field_values_on_type        (type)
 #
 
@@ -23,6 +25,7 @@ class CustomFieldValue < ApplicationRecord
 
   belongs_to :listing
   belongs_to :question, :class_name => "CustomField", :foreign_key => "custom_field_id"
+  belongs_to :person
 
   delegate :with_type, :to => :question
 
