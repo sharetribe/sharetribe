f24f7f1a812d5776db3ba09ef40051793365845c
Aivils Štoss <aivils@ithouse.lv>
2018-07-23 16:44:47 +0300

    [Pro] Footer single section in admin panel

--- app/services/admin/communities/footer_service.rb
+++ app/services/admin/communities/footer_service.rb
@@ -22,19 +22,27 @@ class Admin::Communities::FooterService
     community.footer_theme == Community::FOOTER_DARK
   end
 
+  def social_links
+    return @social_links if defined?(@social_links)
+    SocialLink.social_provider_list.each do |provider|
+      next if community.social_links.by_provider(provider).any?
+      community.social_links.build(provider: provider)
+    end
+    @social_links = community.social_links
+  end
+
   private
 
   def footer_params
     params.require(:community).permit(
-      :footer_theme, :footer_copyright,
+      :footer_theme, :footer_copyright, :footer_enabled,
       footer_menu_links_attributes: [
         :id, :entity_type, :sort_priority, :_destroy,
         translation_attributes: Hash[community.locales.collect { |item| [item.to_sym, [:title, :url]] } ]
+      ],
+      social_links_attributes: [
+        :id, :provider, :url, :sort_priority, :enabled
       ]
     )
-  end
-
-  def configuration_params
-    params.require(:configuration).permit(:footer_style)
   end
 end
