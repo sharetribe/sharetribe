63bfe5e51e0ea424ef1001def73476ce488d5fd6
Viktor Rak <v.rak@sloboda-studio.com>
2018-09-04 17:58:31 +0300

    Ignore puma.rb config file

---
+++ config/puma.example.rb
@@ -0,0 +1,34 @@
+# Change to match your CPU core count
+workers 2
+
+# Min and Max threads per worker
+threads 1, 6
+
+app_dir = File.expand_path('../..', __FILE__)
+shared_dir = "#{app_dir}/shared"
+
+# Default to production
+rails_env = ENV['RAILS_ENV'] || 'production'
+environment rails_env
+
+# Set up socket location
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+# Set master PID and state locations
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
+
+on_worker_boot do
+  require 'active_record'
+  begin
+    ActiveRecord::Base.connection.disconnect!
+  rescue ActiveRecord::ConnectionNotEstablished
+    # already disconnected
+    nil
+  end
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
