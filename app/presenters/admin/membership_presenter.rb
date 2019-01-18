03d22d4bc0c42880cbf39c32c07e1ffaf94dea45
Aivils Štoss <aivils@ithouse.lv>
2019-01-18 14:21:12 +0200

    [Admin panel] [Users] For feature flag admin_acts_as_user, add shortcuts in Users table

--- app/presenters/admin/membership_presenter.rb
+++ app/presenters/admin/membership_presenter.rb
@@ -1,10 +1,20 @@
 class Admin::MembershipPresenter
   include Collator
 
-  attr_reader :params
+  private
 
-  def initialize(params)
+  attr_reader :service, :params
+
+  public
+
+  attr_reader :admin_acts_as_user
+
+  delegate :memberships, :community, to: :service, prefix: false, allow_nil: false
+
+  def initialize(service:, params:, admin_acts_as_user:)
+    @service = service
     @params = params
+    @admin_acts_as_user = admin_acts_as_user
   end
 
   def selected_statuses_title
@@ -25,5 +35,27 @@ class Admin::MembershipPresenter
 
   def status_checked?(status)
     params[:status].present? && params[:status].include?(status)
+  end
+
+  def person_name(person)
+    if admin_acts_as_user
+      display_name = person.display_name.present? ? " (#{person.display_name})" : ''
+      "#{person.given_name} #{person.family_name}#{display_name}"
+    else
+      "#{person.given_name} #{person.family_name}"
+    end
+  end
+
+  def can_post_listing(membership)
+    ready = !(membership.pending_consent? || membership.pending_email_confirmation?)
+    if require_verification_to_post_listings
+      membership.can_post_listings && ready
+    else
+      ready
+    end
+  end
+
+  def require_verification_to_post_listings
+    community.require_verification_to_post_listings
   end
 end
