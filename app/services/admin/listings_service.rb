146c8d18712938eab24d4498eefcacf778a89455
Aivils Štoss <aivils@ithouse.lv>
2019-02-13 16:40:55 +0200

    [Admin panel][Listings] Admin approve/reject listing

---
+++ app/services/admin/listings_service.rb
@@ -0,0 +1,22 @@
+class Admin::ListingsService
+  attr_reader :community, :params
+
+  def initialize(community:, params:)
+    @params = params
+    @community = community
+  end
+
+  def listing
+    @listing ||= resource_scope.find(params[:id])
+  end
+
+  def update
+    listing.update_column(:approval, params[:listing][:approval]) # rubocop::disable Rails/SkipsModelValidations
+  end
+
+  private
+
+  def resource_scope
+    community.listings
+  end
+end
