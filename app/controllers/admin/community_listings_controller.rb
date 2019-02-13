0c686d0a122fc9625a7bd7d0933057db64cbbdaa
Aivils Štoss <aivils@ithouse.lv>
2019-02-13 16:40:55 +0200

    [Admin panel][Listings] Admin approve/reject listing

--- app/controllers/admin/community_listings_controller.rb
+++ app/controllers/admin/community_listings_controller.rb
@@ -1,6 +1,24 @@
 class Admin::CommunityListingsController < Admin::AdminBaseController
-  def index
-    @selected_left_navi_link = "listings"
+  before_action :set_selected_left_navi_link
+  before_action :set_service
+
+  layout false, only: [:edit, :update]
+  respond_to :html, :js
+
+  def update
+    @service.update
+  end
+
+  private
+
+  def set_selected_left_navi_link
+    @selected_left_navi_link = 'listings'
+  end
+
+  def set_service
+    @service = Admin::ListingsService.new(
+      community: @current_community,
+      params: params)
     @presenter = Listing::ListPresenter.new(@current_community, @current_user, params, true)
   end
 end
