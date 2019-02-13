45c2462f07a2b245f697521b8623e33a432191e9
Aivils Štoss <aivils@ithouse.lv>
2019-02-13 16:40:55 +0200

    [Admin panel][Listings] Admin approve/reject listing

--- config/routes.rb
+++ config/routes.rb
@@ -271,7 +271,7 @@ Rails.application.routes.draw do
           get "getting_started_guide/invitation",             to: redirect("/admin/getting_started_guide/invitation")
 
         end
-        resources :listings, controller: :community_listings, only: [:index]
+        resources :listings, controller: :community_listings, only: [:index, :edit, :update]
         resources :transactions, controller: :community_transactions, only: :index do
           collection do
             get 'export'
