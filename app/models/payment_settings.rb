b253198befda405ce8766d6c97a20a8db307ab76
Aivils Štoss <aivils@ithouse.lv>
2019-04-18 14:04:16 +0300

    [Payment settings- Stripe] Prevent lowering minimum transaction size to less than minimum transaction fee with Stripe

--- app/models/payment_settings.rb
+++ app/models/payment_settings.rb
@@ -32,9 +32,21 @@
 #
 
 class PaymentSettings < ApplicationRecord
+  belongs_to :community
+
   validates_presence_of(:community_id)
 
   scope :preauthorize, -> { where(payment_process: :preauthorize) }
   scope :paypal, -> { preauthorize.where(payment_gateway: :paypal) }
   scope :stripe, -> { preauthorize.where(payment_gateway: :stripe) }
+  scope :active, -> { where(active: true) }
+
+  class << self
+    def max_minimum_transaction_fee(community)
+      active.stripe.or(PaymentSettings.paypal)
+        .where(community: community)
+        .pluck(:minimum_transaction_fee_cents)
+        .max
+    end
+  end
 end
