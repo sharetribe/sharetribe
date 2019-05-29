755462057448d2e81ad7632fef19ec5ed71b009e
georgejeffery <georgejeffery@gmail.com>
2019-05-29 10:45:57 +0100

    Listing image changes

--- app/view_utils/topbar_helper.rb
+++ app/view_utils/topbar_helper.rb
@@ -1,10 +1,8 @@
 module TopbarHelper
-
   module_function
 
   def topbar_props(community:, path_after_locale_change:, user: nil, search_placeholder: nil,
                    locale_param: nil, current_path: nil, landing_page: false, host_with_port:)
-
     links = links(community: community, user: user, locale_param: locale_param, host_with_port: host_with_port)
 
     main_search =
@@ -17,12 +15,12 @@ module TopbarHelper
     search_path_string = PathHelpers.search_url({
       community_id: community.id,
       opts: {
-        only_path: true
-      }
+        only_path: true,
+      },
     })
 
     given_name, family_name = *PersonViewUtils.person_display_names(user, community)
-    avatar_image = user&.image&.present? && !user.image_processing ? { url: user.image.url(:thumb) } : nil
+    avatar_image = user&.image&.present? && !user.image_processing ? {url: user.image.url(:thumb)} : nil
 
     {
       logo: {
@@ -31,28 +29,28 @@ module TopbarHelper
           default_locale: community.default_locale,
           logged_in: user.present?,
           locale_param: locale_param,
-          custom: true
+          custom: true,
         ),
         text: community.name(I18n.locale),
         image: community.wide_logo.present? ? community.stable_image_url(:wide_logo, :header) : nil,
-        image_highres: community.wide_logo.present? ? community.stable_image_url(:wide_logo, :header_highres) : nil
+        image_highres: community.wide_logo.present? ? community.stable_image_url(:wide_logo, :header_highres) : nil,
       },
       search: landing_page ? nil : {
         search_placeholder: search_placeholder,
-        mode: main_search.to_s
+        mode: main_search.to_s,
       },
       search_path: search_path_string,
       menu: {
         links: links,
-        limit_priority_links: community.configuration.limit_priority_links
+        limit_priority_links: community.configuration.limit_priority_links,
       },
       locales: landing_page ? nil : locale_props(community, I18n.locale, path_after_locale_change, user.present?),
       avatarDropdown: {
         avatar: {
           image: avatar_image,
           givenName: given_name,
-          familyName: family_name
-        }
+          familyName: family_name,
+        },
       },
       ##  Removed Listing Button from Header
       #newListingButton: {
@@ -60,33 +58,33 @@ module TopbarHelper
       #},
       i18n: {
         locale: I18n.locale,
-        defaultLocale: I18n.default_locale
+        defaultLocale: I18n.default_locale,
       },
       marketplace: {
         marketplace_color1: CommonStylesHelper.marketplace_colors(community)[:marketplace_color1],
-        location: current_path
+        location: current_path,
       },
       user: {
         loggedInUsername: user&.username,
-        isAdmin: user&.has_admin_rights?(community) || false
+        isAdmin: user&.has_admin_rights?(community) || false,
       },
-      unReadMessagesCount: InboxService.notification_count(user&.id, community.id)
+      unReadMessagesCount: InboxService.notification_count(user&.id, community.id),
     }
   end
 
   def links(community:, user:, locale_param:, host_with_port:)
     user_links = Maybe(community.menu_links)
       .map { |menu_links|
-        menu_links
-          .map { |menu_link|
-            {
-              link: menu_link.url(I18n.locale),
-              title: menu_link.title(I18n.locale),
-              priority: menu_link.sort_priority,
-              external: link_external?(menu_link.url(I18n.locale), host_with_port)
-            }
-          }
-      }.or_else([])
+      menu_links
+        .map { |menu_link|
+        {
+          link: menu_link.url(I18n.locale),
+          title: menu_link.title(I18n.locale),
+          priority: menu_link.sort_priority,
+          external: link_external?(menu_link.url(I18n.locale), host_with_port),
+        }
+      }
+    }.or_else([])
 
     links = [
       {
@@ -95,18 +93,18 @@ module TopbarHelper
           logged_in: user.present?,
           default_locale: community.default_locale,
           locale_param: locale_param,
-          custom: true
+          custom: true,
         ),
         title: I18n.t("header.home"),
-        priority: -1
-      }
+        priority: -1,
+      },
     ]
 
     if community.configuration.display_about_menu
       links << {
         link: paths.about_infos_path(locale: locale_param),
         title: I18n.t("header.about"),
-        priority: 0
+        priority: 0,
       }
     end
 
@@ -114,7 +112,7 @@ module TopbarHelper
       links << {
         link: paths.new_user_feedback_path(locale: locale_param),
         title: I18n.t("header.contact_us"),
-        priority: !user_links.empty? ? user_links.last[:priority] + 1 : 1
+        priority: !user_links.empty? ? user_links.last[:priority] + 1 : 1,
       }
     end
 
@@ -122,7 +120,7 @@ module TopbarHelper
       links << {
         link: paths.new_invitation_path(locale: locale_param),
         title: I18n.t("header.invite"),
-        priority: !user_links.empty? ? user_links.last[:priority] + 2 : 2
+        priority: !user_links.empty? ? user_links.last[:priority] + 2 : 2,
       }
     end
 
@@ -138,13 +136,13 @@ module TopbarHelper
         locale_ident: loc[:ident],
         change_locale_uri: PathHelpers.change_locale_path(is_logged_in: is_logged_in,
                                                           locale: loc[:ident],
-                                                          redirect_uri: path_after_locale_change)
+                                                          redirect_uri: path_after_locale_change),
       }
     }
 
-    { current_locale_ident: I18n.locale,
-      current_locale: Maybe(Sharetribe::AVAILABLE_LOCALES.find { |l| l[:ident] == current_locale.to_s })[:language].or_else(current_locale).to_s,
-      available_locales: community_locales }
+    {current_locale_ident: I18n.locale,
+     current_locale: Maybe(Sharetribe::AVAILABLE_LOCALES.find { |l| l[:ident] == current_locale.to_s })[:language].or_else(current_locale).to_s,
+     available_locales: community_locales}
   end
 
   def missing_profile_image_path
