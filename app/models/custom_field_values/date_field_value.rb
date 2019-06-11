462157ae10d52498474c827ab651376d864700c1
Nikolai <devnik@ithouse.lv>
2019-06-11 11:18:39 +0300

    [bug] fix validation for custom date fields

--- app/models/custom_field_values/date_field_value.rb
+++ app/models/custom_field_values/date_field_value.rb
@@ -23,7 +23,7 @@
 
 class DateFieldValue < CustomFieldValue
 
-  validates_presence_of :date_value
+  validates :date_value, presence: true, if: proc { |date_field_value| date_field_value.question&.required? }
 
   def display_value
     I18n.l(date_value, format: :short_date)
