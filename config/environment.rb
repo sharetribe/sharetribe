aa93ae454d4fcf38a5f4626784fdf73199947849
Aivils Štoss <aivils@ithouse.lv>
2019-08-23 11:42:26 +0300

    [CLP Editor] back-end validation for listings section

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,17 @@ APP_CONFIG = ConfigLoader.load_app_config
 # because I didn't ask for any wrappers to magically appear and screw my form styles. So, disable them.
 # See more: https://coderwall.com/p/s-zwrg
 ActionView::Base.field_error_proc = proc do |html_tag, instance|
-  html_tag.html_safe
+  wrap_field_with_error_classes = [
+    LandingPageVersion::Section::Listings
+  ]
+  object = instance.object
+  method_name = instance.instance_variable_get("@method_name")
+  if wrap_field_with_error_classes.include?(object.class) &&
+    instance.class == ActionView::Helpers::Tags::TextField
+    "#{html_tag}<label class='error'>#{object.errors[method_name].join(', ')}</label>".html_safe
+  else
+    html_tag.html_safe
+  end
 end
 
 # Initialize the rails application
