46156a2ae1794adb01d35664f6d4a14fc9a11b9f
Nikolajs <nik@ithouse.lv>
2019-08-27 16:57:06 +0300

    [emails-v2-layout] sample for email to seller

--- app/services/feature_flag_service/store.rb
+++ app/services/feature_flag_service/store.rb
@@ -25,6 +25,7 @@ module FeatureFlagService::Store
       :stripe_payment_intents,
       :clp_editor,
       :new_tx_page,
+      :email_layout_v2
     ].to_set
 
     def initialize(additional_flags:)
