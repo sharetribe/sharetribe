c5609567913b20e856c2731d5303ac1468c32115
Nikolajs <nik@ithouse.lv>
2019-09-26 16:36:30 +0300

    [emails-v2-layout] updated template for new transaction notification

--- app/mailers/transaction_mailer.rb
+++ app/mailers/transaction_mailer.rb
@@ -189,9 +189,11 @@ class TransactionMailer < ActionMailer::Base
     with_locale(recipient.locale, community.locales.map(&:to_sym), community.id) do
       @community_name = community.full_name(recipient.locale)
       @skip_unsubscribe_footer = true
-      premailer_mail(to: recipient.confirmed_notification_emails_to,
-                     from: community_specific_sender(community),
-                     subject: t("emails.new_transaction.subject"))
+      mail(to: recipient.confirmed_notification_emails_to,
+           from: community_specific_sender(community),
+           subject: t("emails.new_transaction.subject")) do |format|
+             format.html { render v2_template(community.id, 'new_transaction'), layout: v2_layout(community.id) }
+      end
     end
   end
 
