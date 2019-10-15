9ae1518c88eeca190b2cbbd733f13a4f23717386
Nikolajs <nik@ithouse.lv>
2019-10-15 14:04:38 +0300

    [emails-v2-layout] updated template for welcome email

--- app/mailers/person_mailer.rb
+++ app/mailers/person_mailer.rb
@@ -368,10 +368,10 @@ class PersonMailer < ActionMailer::Base
       @show_branding_info = !PlanService::API::Api.plans.get_current(community_id: community.id).data[:features][:whitelabel]
 
       subject = t("emails.welcome_email.welcome_email_subject", :community => community.full_name(recipient.locale), :person => PersonViewUtils.person_display_name_for_type(person, "first_name_only"))
-      premailer_mail(:to => recipient.confirmed_notification_emails_to,
-                     :from => community_specific_sender(community),
-                     :subject => subject) do |format|
-        format.html { render :layout => 'email_blank_layout' }
+      mail(:to => recipient.confirmed_notification_emails_to,
+           :from => community_specific_sender(community),
+           :subject => subject) do |format|
+        format.html { render v2_template(community.id, 'welcome_email'), layout: v2_layout(community.id, 'email_blank_layout') }
       end
     end
   end
