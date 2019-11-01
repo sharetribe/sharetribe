20bb185d7cddd512bdc197eeeb1e0dbaabd6f4db
Aivils Štoss <aivils@ithouse.lv>
2019-11-01 16:14:35 +0200

    [CLP Editor] Release version if changes are made

--- spec/controllers/admin/landing_page_versions_controller_spec.rb
+++ spec/controllers/admin/landing_page_versions_controller_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Admin::LandingPageVersionsController, type: :controller do
   let(:community) { FactoryGirl.create(:community) }
+  let(:landing_page) { FactoryGirl.create(:landing_page, community: community, released_version: '1') }
   let(:landing_page_version1) { FactoryGirl.create(:landing_page_version, community: community, version: '1') }
   let(:landing_page_version2) { FactoryGirl.create(:landing_page_version, community: community, version: '2') }
 
@@ -21,6 +22,29 @@ describe Admin::LandingPageVersionsController, type: :controller do
       presenter = assigns(:presenter)
       landing_page_version = presenter.landing_page_version
       expect(landing_page_version.version).to eq 2
+    end
+  end
+
+  describe('#release') do
+    before(:each) do
+      landing_page
+      landing_page_version1
+      landing_page_version2
+    end
+
+    it 'releases current version' do
+      expect(LandingPage.released_version(community)).to eq 1
+      content = landing_page_version2.parsed_content
+      content['something_somewhere'] = 'changed'
+      landing_page_version2.update_content(content)
+      get :release, params: {id: landing_page_version2.id}
+      expect(LandingPage.released_version(community)).to eq 2
+    end
+
+    it 'does not releases current version if there are no changes' do
+      expect(LandingPage.released_version(community)).to eq 1
+      get :release, params: {id: landing_page_version2.id}
+      expect(LandingPage.released_version(community)).to eq 1
     end
   end
 end
