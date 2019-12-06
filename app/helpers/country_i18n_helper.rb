46e960a86a37f725c6dac2949c6030a4e0f556a8
Nikolajs <nik@ithouse.lv>
2019-12-06 11:21:03 +0200

    [Stripe] Added support for Mexico accounts

--- app/helpers/country_i18n_helper.rb
+++ app/helpers/country_i18n_helper.rb
@@ -109,4 +109,41 @@ module CountryI18nHelper
       ["Western Australia", "WA"]
     ]
   end
+
+  def mx_states
+    [
+      ["Aguascalientes", "AG"],
+      ["Baja California", "BC"],
+      ["Baja California Sur", "BS"],
+      ["Chihuahua", "CH"],
+      ["Colima", "CL"],
+      ["Campeche", "CM"],
+      ["Coahuila", "CO"],
+      ["Chiapas", "CS"],
+      ["Federal District", "DF"],
+      ["Durango", "DG"],
+      ["Guerrero", "GR"],
+      ["Guanajuato", "GT"],
+      ["Hidalgo", "HG"],
+      ["Jalisco", "JA"],
+      ["México State", "ME"],
+      ["Michoacán", "MI"],
+      ["Morelos", "MO"],
+      ["Nayarit", "NA"],
+      ["Nuevo León", "NL"],
+      ["Oaxaca", "OA"],
+      ["Puebla", "PB"],
+      ["Querétaro", "QE"],
+      ["Quintana Roo", "QR"],
+      ["Sinaloa", "SI"],
+      ["San Luis Potosí", "SL"],
+      ["Sonora", "SO"],
+      ["Tabasco", "TB"],
+      ["Tlaxcala", "TL"],
+      ["Tamaulipas", "TM"],
+      ["Veracruz", "VE"],
+      ["Yucatán", "YU"],
+      ["Zacatecas", "ZA"]
+    ]
+  end
 end
