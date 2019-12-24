455a276600f59b104eaa7f06682db7ea70fc7572
charoyan <kinovhod@gmail.com>
2019-12-24 11:22:10 +0200

    try to fix CI tests

--- features/support/listing_helpers.rb
+++ features/support/listing_helpers.rb
@@ -20,7 +20,7 @@ module ListingHelpers
     # all_translations = TranslationService::API::Api.translations.get(community.id)[:data]
     all_translations = CommunityTranslation.where(community_id: community.id)
                                            .pluck(:translation, :translation_key)
-                                           .map{|a| {translation: a[0], translation_key: a[1]} }
+                                           .map{ |a| {translation: a[0], translation_key: a[1]} }
     all_shapes.find { |shape|
       all_translations.any? { |tr|
         tr[:translation_key] == shape[:name_tr_key] && tr[:translation] == name
