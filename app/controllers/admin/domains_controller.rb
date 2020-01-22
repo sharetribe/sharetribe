c9f7ab51cf3d4437a722fc9a240147e52c113f96
Aivils Štoss <aivils@ithouse.lv>
2020-01-22 13:18:10 +0200

    [Admin] Allow admin to update the marketplace "ident"

--- app/controllers/admin/domains_controller.rb
+++ app/controllers/admin/domains_controller.rb
@@ -2,6 +2,20 @@ class Admin::DomainsController < Admin::AdminBaseController
   before_action :set_selected_left_navi_link
   before_action :set_presenter
 
+  def update
+    if @service.update
+      redirect_to "#{APP_CONFIG.always_use_ssl ? 'https' : 'http'}://#{@service.ident}.#{APP_CONFIG.domain}"
+    else
+      redirect_to admin_domain_path
+    end
+  end
+
+  def check_availability
+    respond_to do |format|
+      format.json { render :json => @service.ident_available? }
+    end
+  end
+
   private
 
   def set_selected_left_navi_link
@@ -9,9 +23,13 @@ class Admin::DomainsController < Admin::AdminBaseController
   end
 
   def set_presenter
-    @presenter = Admin::DomainsPresenter.new(
+    @service = Admin::DomainsService.new(
       community: @current_community,
-      plan: @current_plan
+      plan: @current_plan,
+      params: params
+    )
+    @presenter = Admin::DomainsPresenter.new(
+      service: @service
     )
   end
 end
