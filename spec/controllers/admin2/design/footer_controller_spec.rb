46a908995b3efec5582b2050a3023cd7127745e2
charoyan <kinovhod@gmail.com>
2020-02-07 08:18:25 +0200

    add footer to admin v2

--- spec/controllers/admin2/design/footer_controller_spec.rb
+++ spec/controllers/admin2/design/footer_controller_spec.rb
@@ -27,26 +27,24 @@ describe Admin2::Design::FooterController, type: :controller do
         {"footer_theme" => "light",
          "footer_enabled" => 1,
          "footer_menu_links_attributes" =>
-          {"0" =>
+           {"0" =>
             {"id" => "",
              "entity_type" => "for_footer",
              "sort_priority" => "0",
              "_destroy" => "false",
-             "translation_attributes" =>
-              {"en"=>{"title"=>"ccc", "url"=>"http://example.com"}}}},
+             "translations_attributes" =>
+              {"0"=>{"locale" => 'en', "title"=>"ccc", "url"=>"http://example.com"}}}},
          "footer_copyright" => "Sample",
          "social_links_attributes" =>
           {"0" =>
             {"id" => "",
              "sort_priority" => "0",
              "provider" => "youtube",
-             "enabled" => "1",
              "url" => "hoho"},
            "1" =>
             {"id" => "",
              "sort_priority" => "1",
              "provider" => "facebook",
-             "enabled" => "0",
              "url" => ""},
            "2" =>
             {"id" => "",
@@ -58,31 +56,26 @@ describe Admin2::Design::FooterController, type: :controller do
             {"id" => "",
              "sort_priority" => "3",
              "provider" => "instagram",
-             "enabled" => "0",
              "url" => ""},
            "4" =>
             {"id" => "",
              "sort_priority" => "4",
              "provider" => "googleplus",
-             "enabled" => "0",
              "url" => ""},
            "5" =>
             {"id" => "",
              "sort_priority" => "5",
              "provider" => "linkedin",
-             "enabled" => "0",
              "url" => ""},
            "6" =>
             {"id" => "",
              "sort_priority" => "6",
              "provider" => "pinterest",
-             "enabled" => "0",
              "url" => ""},
            "7" =>
             {"id" => "",
              "sort_priority" => "7",
              "provider" => "soundcloud",
-             "enabled" => "0",
              "url" => ""}
           }
         }
@@ -92,7 +85,7 @@ describe Admin2::Design::FooterController, type: :controller do
       expect(community.footer_menu_links.count).to eq 0
       expect(community.social_links.count).to eq 0
       expect(community.footer_enabled).to eq false
-      put :update_footer, params: params
+      patch :update_footer, params: params
       expect(community.footer_enabled).to eq true
       expect(community.reload.footer_menu_links.count).to eq 1
       expect(community.social_links.count).to eq 8
