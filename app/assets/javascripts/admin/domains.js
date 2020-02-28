fe4fbc7ff7938a8d7807974b852d1559478bebb3
Aivils Štoss <aivils@ithouse.lv>
2020-02-28 08:56:31 +0200

    [Admin] UI for setting custom domain

--- app/assets/javascripts/admin/domains.js
+++ app/assets/javascripts/admin/domains.js
@@ -32,8 +32,30 @@ window.ST = window.ST || {};
     });
   };
 
+  var initDomainAvailability = function(options) {
+    $.validator.
+      addMethod( "remove_protocol",
+        function(value, element, param) {
+          var protocolRegex = new RegExp("^(http|https)://");
+          if (value.match(protocolRegex)) {
+            $(element).val(value.replace(protocolRegex, ''));
+          }
+          return true;
+        }
+      );
+    $.validator.
+      addMethod( "valid_domain",
+       function(value, element, param) {
+         v = punycode.toASCII(value);
+         return v.match(new RegExp('(?=.{4,253})^(((?!-)[a-z0-9-]{0,62}[a-z0-9]\\.)+((?![0-9]+$)(?!-)[a-z0-9-]{0,62}[a-z0-9]))$'));
+       }
+     );
+    $('form.create_domain_setup').validate();
+  };
+
+
   module.Domains = {
     init: init,
+    initDomainAvailability: initDomainAvailability,
   };
 })(window.ST);
-
