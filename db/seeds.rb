dc64e6d4524864b19af09d22bb475c7f827033d6
ryankopf <ryan@ryankopf.com>
2020-04-09 16:15:35 -0500

    Adding migration version.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,17 @@ Listing.find(:all).each { |listing| listing.update_attribute :last_modified, lis
 Person.find(:all).each do |person|
   person.settings = Settings.create
 end
+KassiEvent.all.each do |event|
+  event.people.each do |person|
+    if event.realizer_id == person.id
+      role = "provider"
+    elsif event.receiver_id == person.id
+      role = "requester"
+    else
+      role = "none"
+    end
+    KassiEventParticipation.create(:person_id => person.id,
+                                   :kassi_event_id => event.id,
+                                   :role => role)
+  end
+end
