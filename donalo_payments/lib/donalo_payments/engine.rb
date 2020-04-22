3ee7f0193fe57e393e5a022809da871fbe294481
Jorge Morante <jorge@morante.eu>
2020-04-22 11:27:05 +0200

    add missing patches

--- donalo_payments/lib/donalo_payments/engine.rb
+++ donalo_payments/lib/donalo_payments/engine.rb
@@ -9,6 +9,8 @@ module DonaloPayments
       ::StripeHelper
       ::TransactionService::Transaction
       ::StripeService::API::StripeApiWrapper
+      ::StripeService::API::Payments
+      ::StripeService::Report
 
       # StripeHelper.user_active_true? to always return true, so the
       # users don't need to setup their payment setings
@@ -28,8 +30,9 @@ module DonaloPayments
         end
       end
 
-      class ::StripeService::API::StripeHelper
+      class ::StripeService::API::StripeApiWrapper
         class << self
+          # TODO maybe do the same in "charge" method?
           # removing destination fields, and on_behalf fields here, so payment is done to the platform, rather than p2p
           def create_payment_intent(community:, seller_account_id:, payment_method_id:, amount:, currency:, fee:, description:, metadata:)
             with_stripe_payment_config(community) do |payment_settings|
@@ -49,6 +52,34 @@ module DonaloPayments
         end
       end
 
+      class ::StripeService::API::Payments
+        class << self
+          def create_preauth_payment(tx, gateway_fields)
+            seller_account = {}
+
+            if gateway_fields[:stripe_payment_method_id].present?
+              wrap_in_report(tx: tx, start: :create_intent_start, success: :create_intent_success, failed: :create_intent_failed) do
+                do_create_preauth_payment(tx, gateway_fields, seller_account)
+              end
+            elsif gateway_fields[:stripe_token].present?
+              wrap_in_report(tx: tx, start: :create_charge_start, success: :create_charge_success, failed: :create_charge_failed) do
+                do_create_preauth_payment(tx, gateway_fields, seller_account)
+              end
+            else
+              Result::Error.new("No payment method or token present")
+            end
+          end
+        end
+      end
+
+
+      class ::StripeService::Report
+        MockStripeAccount = Struct.new(:stripe_seller_id)
+
+        def stripe_account
+          @stripe_account ||= MockStripeAccount.new
+        end
+      end
       puts "Starting DonaloPayments engine"
     end
   end
