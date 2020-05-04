f433e271c485a9d41d7c2afbf7c33d75aea33d20
Jorge Morante <jorge@morante.eu>
2020-05-04 16:36:13 +0200

    patch do_catpure method to properly capture method without seller_account

--- donalo_payments/lib/donalo_payments/engine.rb
+++ donalo_payments/lib/donalo_payments/engine.rb
@@ -69,6 +69,30 @@ module DonaloPayments
               Result::Error.new("No payment method or token present")
             end
           end
+
+          def do_capture(tx)
+            payment = PaymentStore.get(tx.community_id, tx.id)
+            # TODO maybe we should mock the method that gets seller account instead
+            seller_account = {}
+            payment_data = {status: 'paid'}
+            if payment[:stripe_payment_intent_id].present?
+              intent = stripe_api.capture_payment_intent(community: tx.community,
+                                                         payment_intent_id: payment[:stripe_payment_intent_id])
+              charge = intent['charges']['data'].first
+              payment_data[:stripe_payment_intent_status] = StripePayment::PAYMENT_INTENT_SUCCESS
+            elsif payment[:stripe_charge_id].present?
+              charge = stripe_api.capture_charge(community: tx.community_id, charge_id: payment[:stripe_charge_id], seller_id: seller_account[:stripe_seller_id])
+            else
+              return Result::Error.new("Cannot capture: no intent or charge in payment data")
+            end
+            balance_txn = stripe_api.get_balance_txn(community: tx.community_id, balance_txn_id: charge.balance_transaction, account_id: seller_account[:stripe_seller_id])
+            payment = PaymentStore.update(transaction_id: tx.id, community_id: tx.community_id,
+                                          data: payment_data.merge!({
+                                            real_fee_cents: balance_txn.fee,
+                                            available_on: Time.zone.at(balance_txn.available_on)
+                                          }))
+            Result::Success.new(payment)
+          end
         end
       end
 
