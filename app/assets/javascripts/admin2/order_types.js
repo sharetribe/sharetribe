a22b96e522c5e8f29b6a0737c265efa4682711dd
charoyan <kinovhod@gmail.com>
2020-05-25 09:28:41 +0300

    admin v2 order types

---
+++ app/assets/javascripts/admin2/order_types.js
@@ -0,0 +1,53 @@
+$(function() {
+
+    $('#orderTypesAddModal'). on('show.bs.modal', function() {
+        $('#template_order_type').prop('selectedIndex', 0);
+        $('#category-body').html('');
+        $('.order-type-footer').hide();
+    });
+
+    $(document).on('click', '.confirm-order-type-true', function () {
+        var caption = $(this).data('caption'),
+            url = $(this).data('url'),
+            notice = $(this).data('notice');
+        $('#delete-order-type-body').html(notice);
+        $('#delete-order-form').attr('action', url);
+        $('#orderTypesDeleteModalLabel').html(caption);
+        $('#orderTypesDeleteModal').modal('show');
+    });
+
+    $('#template_order_type').on('change', function(){
+        var url = $(this).data('url'),
+            id = $(this).val();
+        $.get(url, {type_id: id}, null, 'script');
+    });
+
+    var orderList = $('#orderTypeList');
+    if (orderList.length) {
+        var url = orderList.data('url');
+        Sortable.create(orderTypeList, {
+            handle: '.handle-move',
+            animation: 250,
+            onEnd: function (/**Event*/evt) {
+                $('.order_type_position').each(function( index ) {
+                    $(this).find('.shape_sort_priority').val(index);
+                });
+                var ids = [];
+                $('.order_type_position').each(function( index ) {
+                    var input = $(this).find('.shape_sort_priority'),
+                        id = input.data('id'),
+                        position = input.val();
+
+                    ids.push({id: id, position: position});
+                });
+                $.post(url, {ids: ids});
+            },
+        });
+
+        $('#top_bar_div').on('cocoon:after-insert', function(e, insertedItem, originalEvent) {
+            var index = $('.top_bar_link_position').length - 1;
+            insertedItem.find('.sort_priority_class').val(index);
+        });
+    }
+
+});
