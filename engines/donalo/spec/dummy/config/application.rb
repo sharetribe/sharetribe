a68a6ab601e8fb1ef81c62b44a0ec96976debc25
Pau Perez <saulopefa@gmail.com>
2020-06-29 10:12:09 +0200

    Setup RSpec in Donalo engine
    This moves from the default test/ to spec/ and configures RSpec to run
    future engine tests as outlined in
    https://corlewsolutions.com/articles/article-126-how-to-setup-rspec-in-a-rails-engine,
    https://hocnest.com/blog/testing-an-engine-with-rspec/ and
    https://relishapp.com/rspec/rspec-rails/docs/gettingstarted.
    
    Unfortunately, since engine specs need to be executed from within the
    engine directory, there's a bit defensive coding we need to add so that
    not found constants don't break the tests. We could investigate other
    potential solutions though.
    
    We stick to SQLite3 just so we get this out the door ASAP. We're on
    a very tight schedule and there are no specs touching DB yet. I followed
    the make it just work and touch as little as possible, approach.
    
    To run them, you can do:
    
    ```
    $ docker-compose exec web bash
    cd engines/donalo
    bundle exec rake rspec/
    ```

--- engines/donalo/spec/dummy/config/application.rb
+++ engines/donalo/spec/dummy/config/application.rb
@@ -1,6 +1,17 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 require "donalo"
