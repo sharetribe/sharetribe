0815f3ccbadb93654dfb7b536832e91ce55a5ee2
charly3x <kinovhod@gmail.com>
2020-06-30 08:06:37 +0300

    update rails

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: bookings
+#
+#  id             :integer          not null, primary key
+#  transaction_id :integer
+#  start_on       :date
+#  end_on         :date
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  start_time     :datetime
+#  end_time       :datetime
+#  per_hour       :boolean          default(FALSE)
+#
+# Indexes
+#
+#  index_bookings_on_end_time                              (end_time)
+#  index_bookings_on_per_hour                              (per_hour)
+#  index_bookings_on_start_time                            (start_time)
+#  index_bookings_on_transaction_id                        (transaction_id)
+#  index_bookings_on_transaction_start_on_end_on_per_hour  (transaction_id,start_on,end_on,per_hour)
+#
+
 require 'spec_helper'
 
 describe Listing, type: :model do
