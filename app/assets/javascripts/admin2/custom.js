562bb72195e1f94e04c92a57d78e7b97f075657c
Charly3X <kinovhod@gmail.com>
2020-08-14 18:06:07 +0300

    Admin v2 translations (#4176)
    * admin v2 translations

--- app/assets/javascripts/admin2/custom.js
+++ app/assets/javascripts/admin2/custom.js
@@ -104,7 +104,6 @@ $(function(){
         });
     }
 
-
     $('.for-hide-content').on('change', function () {
        var checked = $(this).prop('checked'),
            private_content = $('.hide-content');
@@ -114,7 +113,6 @@ $(function(){
            private_content.addClass('opacity_04');
        }
     });
-
     $('.social-checked').on('change', function () {
        var input_elem = $(this).parents('.social-block').find('.social-data');
        if ($(this).prop('checked')) {
@@ -123,34 +121,9 @@ $(function(){
            input_elem.prop('required', false);
        }
     });
-
     $('.change-file').on('change', function() {
        var place = $(this).parents('.custom-file').find('.choose-filename');
        place.hide();
     });
-
-    if ($('#colorpicker-slogan').length) {
-        $("#colorpicker-slogan, #colorpicker-description").spectrum({
-            color: $(this).val(),
-            showInput: true,
-            preferredFormat: "hex",
-            showPalette: true,
-            showSelectionPalette: false,
-            palette: [["#FFF", "#000", "#FF4E36", "#15778E", "#ff5a5f"]]
-        });
-        $("#colorpicker-slogan, #colorpicker-description").show();
-    }
-
-    if ($('#colorpicker-color').length) {
-        $("#colorpicker-color").spectrum({
-            color: $(this).val(),
-            showInput: true,
-            preferredFormat: "hex",
-            showPalette: true,
-            showSelectionPalette: false,
-            palette: [["#FFF", "#000", "#FF4E36", "#15778E", "#ff5a5f"]]
-        });
-        $("#colorpicker-color").show();
-    }
 
 });
