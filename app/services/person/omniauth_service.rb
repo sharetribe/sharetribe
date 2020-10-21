e20d39177b8e042644d275fff5d95b6166791a53
Boyan Tabakov <boyan@sharetribe.com>
2020-10-21 14:23:14 +0300

    Revert "tmp: test possible breaking change in FB API"
    This reverts commit c2eb537e7074371728a7c1eff1dcc8b94b06beae.

--- app/services/person/omniauth_service.rb
+++ app/services/person/omniauth_service.rb
@@ -148,14 +148,7 @@ class Person::OmniauthService
   def store_picture_from_provider(new_person)
     url = if facebook? && new_person.facebook_id
       resp = RestClient.get(
-        "https://graph.facebook.com/#{FacebookSdkVersion::SERVER}/#{new_person.facebook_id}/picture?type=large&redirect=false&breaking_change=profile_picture")
-      begin
-        logger.info("Image URL from info: #{info.image}")
-        from_info = RestClient.get(info.image)
-        logger.info("Obtained image url: #{JSON.parse(from_info)["data"]["url"]}")
-      rescue StandardError => e
-        logger.error(e.message)
-      end
+        "https://graph.facebook.com/#{FacebookSdkVersion::SERVER}/#{new_person.facebook_id}/picture?type=large&redirect=false")
       JSON.parse(resp)["data"]["url"]
     elsif google_oauth2? && new_person.google_oauth2_id
       info.image
