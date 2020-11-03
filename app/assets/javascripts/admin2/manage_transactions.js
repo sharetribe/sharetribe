22a374aedb78443d43539d9780f6fee631b6f999
charoyan <kinovhod@gmail.com>
2020-11-03 10:52:48 +0200

    [Admin2] Improvements to transaction breakdown

--- app/assets/javascripts/admin2/manage_transactions.js
+++ app/assets/javascripts/admin2/manage_transactions.js
@@ -5,37 +5,22 @@ $(function() {
 
     $('#buyer .breakdown-detailed, #buyer .breakdown-trigger-hide').hide();
 
-    $("#buyer .breakdown-trigger-show").click(function(){
-        $("#buyer .breakdown-detailed, #buyer .breakdown-trigger-hide").show();
-        $("#buyer .breakdown-trigger-show, #buyer .breakdown-overview .breakdown-total-price").hide();
-    });
-
-    $("#buyer .breakdown-trigger-hide").click(function(){
-        $("#buyer .breakdown-detailed, #buyer .breakdown-trigger-hide").hide();
-        $("#buyer .breakdown-trigger-show, #buyer .breakdown-overview .breakdown-total-price").show();
+    $("#buyer .breakdown-overview").click(function(){
+        $('#buyer .breakdown-detailed').slideToggle(200);
+        $("p", this).toggleClass("breakdown-trigger-show breakdown-trigger-hide");
     });
 
     $('#seller .breakdown-detailed, #seller .breakdown-trigger-hide').hide();
 
-    $("#seller .breakdown-trigger-show").click(function(){
-        $("#seller .breakdown-detailed, #seller .breakdown-trigger-hide").show();
-        $("#seller .breakdown-trigger-show, #seller .breakdown-overview .breakdown-total-price").hide();
-    });
-
-    $("#seller .breakdown-trigger-hide").click(function(){
-        $("#seller .breakdown-detailed, #seller .breakdown-trigger-hide").hide();
-        $("#seller .breakdown-trigger-show, #seller .breakdown-overview .breakdown-total-price").show();
+    $("#seller .breakdown-overview").click(function(){
+        $('#seller .breakdown-detailed').slideToggle(200);
+        $("p", this).toggleClass("breakdown-trigger-show breakdown-trigger-hide");
     });
 
     $('#marketplace .breakdown-detailed, #marketplace .breakdown-trigger-hide').hide();
 
-    $("#marketplace .breakdown-trigger-show").click(function(){
-        $("#marketplace .breakdown-detailed, #marketplace .breakdown-trigger-hide").show();
-        $("#marketplace .breakdown-trigger-show, #marketplace .breakdown-overview .breakdown-total-price").hide();
-    });
-
-    $("#marketplace .breakdown-trigger-hide").click(function(){
-        $("#marketplace .breakdown-detailed, #marketplace .breakdown-trigger-hide").hide();
-        $("#marketplace .breakdown-trigger-show, #marketplace .breakdown-overview .breakdown-total-price").show();
+    $("#marketplace .breakdown-overview").click(function(){
+        $('#marketplace .breakdown-detailed').slideToggle(200);
+        $("p", this).toggleClass("breakdown-trigger-show breakdown-trigger-hide");
     });
 });
