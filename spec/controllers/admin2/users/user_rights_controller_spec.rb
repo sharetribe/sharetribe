af517c97b5516fa4ba6497d7ef67d34f3e909990
charly <kinovhod@gmail.com>
2020-12-04 12:44:51 +0200

    [Admin2] Move "Invite only" setting to "Signup and Login"

--- spec/controllers/admin2/users/user_rights_controller_spec.rb
+++ spec/controllers/admin2/users/user_rights_controller_spec.rb
@@ -20,13 +20,11 @@ describe Admin2::Users::UserRightsController, type: :controller do
   describe "#update_user_rights" do
     it "works" do
       params = {
-        join_with_invite_only: true,
         users_can_invite_new_users: true,
         require_verification_to_post_listings: true,
         allow_free_conversations: true
       }
 
-      expect(community.join_with_invite_only).to eq false
       expect(community.users_can_invite_new_users).to eq false
       expect(community.require_verification_to_post_listings).to eq false
       expect(community.allow_free_conversations).to eq false
@@ -34,7 +32,6 @@ describe Admin2::Users::UserRightsController, type: :controller do
       put :update_user_rights, params: { community: params }
       community.reload
 
-      expect(community.join_with_invite_only).to eq true
       expect(community.users_can_invite_new_users).to eq true
       expect(community.require_verification_to_post_listings).to eq true
       expect(community.allow_free_conversations).to eq true
