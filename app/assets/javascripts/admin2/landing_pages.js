05aba56b0007ccaebf66718b3e1302d1b29f1dd2
charly <kinovhod@gmail.com>
2021-02-01 16:45:05 +0200

    [Admin2] Custom Landing Page Editor

--- app/assets/javascripts/admin2/landing_pages.js
+++ app/assets/javascripts/admin2/landing_pages.js
@@ -3,6 +3,11 @@ $(function() {
         Sortable.create(landingSection, {
             handle: '.handle-move',
             animation: 250,
+            onMove: function (/**Event*/evt, originalEvent) {
+                if ($(evt.related).hasClass('allow_edit_false')) {
+                    return false;
+                }
+            },
             onEnd: function (/**Event*/evt) {
                 $('.top_bar_link_position').each(function (index) {
                     $(this).find('.sort_priority_class').val(index);
