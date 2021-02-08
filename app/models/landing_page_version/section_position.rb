babfdcd6501e74d7c3d4e5ddba82bb4efe7b8949
charoyan <kinovhod@gmail.com>
2021-02-08 15:02:42 +0200

    [Admin2] Custom Landing Page Editor

--- app/models/landing_page_version/section_position.rb
+++ app/models/landing_page_version/section_position.rb
@@ -5,11 +5,24 @@ class LandingPageVersion::SectionPosition
     :id,
     :position,
     :kind,
-    :variation
+    :variation,
+    :columns
   )
 
   def removable?
     kind != LandingPageVersion::Section::HERO && kind != LandingPageVersion::Section::FOOTER
+  end
+
+  def kind_info
+    if kind == LandingPageVersion::Section::INFO
+      if variation == 'single_column'
+        I18n.t('admin2.landing_page.sections.type.column_1')
+      else
+        I18n.t("admin2.landing_page.sections.type.column_#{columns}")
+      end
+    else
+      I18n.t("admin2.landing_page.sections.type.#{kind}")
+    end
   end
 
   def sortable?
