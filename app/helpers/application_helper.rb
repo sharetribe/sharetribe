5c15c5a3c8e96b95e3391e3ccbbba49beee55763
Thomas Malbaux <thomas.malbaux@gmail.com>
2024-03-28 14:31:15 +0200

    Reverting to the earlier syntax

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -337,7 +337,7 @@ module ApplicationHelper
         :topic => :general,
         :text => t("admin.left_hand_navigation.help_center"),
         :icon_class => icon_class("help"),
-        :path => #{APP_CONFIG.knowledge_base_url}.to_s,
+        :path => "#{APP_CONFIG.knowledge_base_url}/",
         :name => "help_center",
         :target => "_blank"
       },
