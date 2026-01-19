26e3fff7712a7985cde1034e4ead02e80c4f8e29
Boyan Tabakov <boyan@sharetribe.com>
2026-01-19 17:03:13 +0200

    Use mariadb client libraries

--- script/ci-install-deps.sh
+++ script/ci-install-deps.sh
@@ -5,9 +5,8 @@ set -eo pipefail
 # Google APT key signatures have changed and we need to update the key for apt
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 
-sudo ./script/setup-mysql-apt-repo.sh
-
-sudo apt-get install libmysqlclient-dev mysql-community-client sphinxsearch
+sudo apt-get update
+sudo apt-get install -y default-libmysqlclient-dev default-mysql-client sphinxsearch
 
 if [[ -f ~/.nvm/nvm.sh ]] ; then
     echo "NVM already installed"
